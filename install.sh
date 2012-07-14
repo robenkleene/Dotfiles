bf2c086572fb05eee9b5ce511f94ad5e64632061
Roben Kleene <robenk@gmail.com>
2012-07-14 16:37:44 -0400

    Added oh-my-zsh as submodule and modified install and update process

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ ln -s ~/Dotfiles/profile ~/.profile
 ln -s ~/Dotfiles/inputrc ~/.inputrc
 ln -s ~/Dotfiles/vim ~/.vim
 ln -s ~/Dotfiles/vimrc ~/.vimrc
+ln -s ~/Dotfiles/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/Dotfiles/zshrc ~/.zshrc
+git submodule init
