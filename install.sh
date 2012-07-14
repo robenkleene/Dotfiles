c6c2a8742edb12de570cb0dbdd389e7176fb6666
Roben Kleene <robenk@gmail.com>
2012-07-14 19:59:19 -0400

    Replace manually updated install script with one that works automatically

--- install.sh
+++ install.sh
@@ -1,13 +1,32 @@
 #!/bin/sh
 
-# Files
-ln -s ~/Dotfiles/profile ~/.profile
-ln -s ~/Dotfiles/inputrc ~/.inputrc
-ln -s ~/Dotfiles/vimrc ~/.vimrc
-ln -s ~/Dotfiles/zshrc ~/.zshrc
-
-# Directories
-ln -s ~/Dotfiles/vim ~/.vim
-ln -s ~/Dotfiles/oh-my-zsh ~/.oh-my-zsh
-
+# First initialize submodules
 git submodule init
+
+INSTALL_DIRECTORY_NAME="Dotfiles" # The name of the directory to install from.
+
+# Stop if this is being run from any directory besides the install directory
+DIRECTORY=${PWD}
+DIRECTORY_NAME=${PWD##*/}
+if [ ! $DIRECTORY_NAME == $INSTALL_DIRECTORY_NAME ]; then
+	echo "ERROR: This directory \"$DIRECTORY_NAME\" doesn't match $INSTALL_DIRECTORY_NAME."
+	echo "This script only runs from the $INSTALL_DIRECTORY_NAME directory."
+	exit 1
+fi
+
+function MakeSymlink {
+ 	DESTINATION=~/.$thisFILE
+	if [ -f $DESTINATION ] || [ -d $DESTINATION ]; then
+		echo "$DESTINATION skipped because it already exists."
+	else
+#  		echo "$DIRECTORY/$thisFILE $DESTINATION"
+		ln -s $DIRECTORY/$thisFILE $DESTINATION
+	fi	
+}
+
+for thisFILE in *; do 
+	# Exclude shell scripts
+ 	if [[ ! $thisFILE =~ ".sh" ]]; then
+		MakeSymlink $thisFILE
+ 	fi
+done
