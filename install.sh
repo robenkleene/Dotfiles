24d4845be1702ded5b2535d602d7093ef1e683d7
Roben Kleene <robenk@gmail.com>
2012-07-14 16:40:30 -0400

    Organizing install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+# Files
 ln -s ~/Dotfiles/profile ~/.profile
 ln -s ~/Dotfiles/inputrc ~/.inputrc
-ln -s ~/Dotfiles/vim ~/.vim
 ln -s ~/Dotfiles/vimrc ~/.vimrc
-ln -s ~/Dotfiles/oh-my-zsh ~/.oh-my-zsh
 ln -s ~/Dotfiles/zshrc ~/.zshrc
+
+# Directories
+ln -s ~/Dotfiles/vim ~/.vim
+ln -s ~/Dotfiles/oh-my-zsh ~/.oh-my-zsh
+
 git submodule init
