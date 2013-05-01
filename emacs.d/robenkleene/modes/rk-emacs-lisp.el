8cbe2c76da38ba8ee0dff3505b99237bd76ec2fb
Roben Kleene <robenk@gmail.com>
2013-04-30 21:56:57 -0400

    geiser

--- emacs.d/robenkleene/modes/rk-emacs-lisp.el
+++ emacs.d/robenkleene/modes/rk-emacs-lisp.el
@@ -3,13 +3,13 @@
 
 (eval-after-load "lisp-mode"
   '(progn
-     (define-key emacs-lisp-mode-map (kbd "s-r") 'rk-eval-lisp)
-     (define-key lisp-interaction-mode-map (kbd "s-r") 'rk-eval-lisp)
+     (define-key emacs-lisp-mode-map (kbd "s-r") 'rk-eval-emacs-lisp)
+     (define-key lisp-interaction-mode-map (kbd "s-r") 'rk-eval-emacs-lisp)
      (define-key emacs-lisp-mode-map (kbd "C-E") 'eval-last-sexp)
      (define-key lisp-interaction-mode-map (kbd "C-E") 'eval-last-sexp)
      ))
 
-(defun rk-eval-lisp ()
+(defun rk-eval-emacs-lisp ()
   (interactive)
   (if (use-region-p)
       (call-interactively 'eval-region)
--- emacs.d/robenkleene/modes/rk-geiser.el
+++ emacs.d/robenkleene/modes/rk-geiser.el
@@ -7,3 +7,16 @@
      (setq geiser-active-implementations '(racket))
      (setq geiser-racket-binary "/usr/local/bin/racket")
      ))
+
+(eval-after-load "geiser-mode"
+  '(progn
+     (define-key geiser-mode-map (kbd "s-r") 'geiser-eval-region)
+     (define-key geiser-mode-map (kbd "C-E") 'geiser-eval-last-sexp)
+     ))
+
+
+;; (defun rk-eval-lisp ()
+;;   (interactive)
+;;   (if (use-region-p)
+;;       (call-interactively 'eval-region)
+;;       (eval-buffer)))
