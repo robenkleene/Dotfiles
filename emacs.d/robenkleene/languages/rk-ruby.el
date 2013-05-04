bc11bef56f6e4c4c2ae6170eae5b6b478125ac1b
Roben Kleene <robenk@gmail.com>
2013-05-04 18:34:16 -0400

    ruby emacs

--- emacs.d/robenkleene/languages/rk-ruby.el
+++ emacs.d/robenkleene/languages/rk-ruby.el
@@ -6,6 +6,12 @@
      (define-key ruby-mode-map (kbd "<C-s-268632082>") 'rk-ruby-toggle-irb)
      ))
 
+(eval-after-load 'inf-ruby
+  '(progn
+     (define-key inf-ruby-mode-map (kbd "C-p") 'comint-previous-input)
+     (define-key inf-ruby-mode-map (kbd "C-n") 'comint-next-input)
+     ))
+
 (defun rk-ruby-toggle-irb ()
   (interactive)
   (if (null (get-buffer "*ruby*"))
