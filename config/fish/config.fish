b0a64d218c1d8a8fea535820e4c277c9ef3272e3
Roben Kleene <robenk@gmail.com>
2013-05-17 20:49:34 -0400

    Setup fish

---
+++ config/fish/config.fish
@@ -0,0 +1,25 @@
+# in .config/fish/config.fish:
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+
+# Status Chars
+# set __fish_git_prompt_char_dirtystate '⚡'
+# set __fish_git_prompt_char_stagedstate '→'
+# set __fish_git_prompt_char_stashstate '↩'
+# set __fish_git_prompt_char_upstream_ahead '↑'
+# set __fish_git_prompt_char_upstream_behind '↓'
+
+
+function fish_prompt
+	set_color $fish_color_cwd
+	echo -n (prompt_pwd)
+	set_color normal
+	echo -n ' > '
+end
+
+function fish_right_prompt
+	echo -n (__fish_git_prompt)
+end
