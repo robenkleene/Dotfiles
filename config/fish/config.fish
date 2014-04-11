ee9b7775259a8b1786b0f20866caf0438c8fd283
Roben Kleene <robenk@gmail.com>
2014-04-11 11:00:08 -0400

    BBFind

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -42,6 +42,11 @@ function gc
 	emacsclient -c -n --eval "(rk-magit-status-startup)"
 end
 
+# BBFind
+function bbf
+  bbfind "$argv[1]" --gui .
+end
+
 function cleanopenwith
 	/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
 end
