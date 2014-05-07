e96fcc1d31dae92c45427c086583cf6735177d88
Roben Kleene <robenk@gmail.com>
2014-05-07 10:47:14 -0400

    Git

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,22 @@
+# Environment Variables
 set PATH /usr/local/bin ~/Development/Scripts/bin $PATH
 
+# Editor
+set EDITOR mate
+
+# rbenv
+set PATH $HOME/.rbenv/bin $PATH
+set PATH $HOME/.rbenv/shims $PATH
+rbenv rehash >/dev/null ^&1
+
+# Atom
+set -x ATOM_PATH $HOME/Applications/Atom.app
+
+# Node
+set -x NODE_PATH /usr/local/share/npm/lib/node_modules /usr/local/lib/node_modules
+set PATH /usr/local/share/npm/bin $PATH
+
+# Prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showupstream 'yes'
@@ -29,64 +46,79 @@ function egitn
 	end
 end
 
-# Editor
-set EDITOR mate
-
 # Emacs Client
 function ec
 	emacsclient -c -n
 end
 
-# Emacs Magit
-function gc
-	emacsclient -c -n --eval "(rk-magit-status-startup)"
-end
+# Emacs Client Magit
+# function gc
+# 	emacsclient -c -n --eval "(rk-magit-status-startup)"
+# end
 
+# Emacs Magit
 function magit
 	emacs --eval "(rk-magit-status-startup)"
 end
 
-# # BBFind
-# function bbf
-#   bbfind "$argv[1]" --gui .
-# end
-# 
-# # wcsearch
+# wcsearch
 # function sea
 #   wcsearch $argv
 # end
 
 # BBFind
-function sea
+function bbf
   bbfind --grep --gui --case-sensitive "$argv[1]" .
 end
 
 # BBFind in files
-function seaf
+function bbff
   bbfind --grep --gui --case-sensitive --name-pattern "$argv[1]" "$argv[2]" .
 end
 
-
 # Files
-function fnd
+function ackf
   ack -ig "$argv[1]"
 end
 
-
-
-
+# Misc
 function cleanopenwith
 	/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
 end
 
-# rbenv
-set PATH $HOME/.rbenv/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
-rbenv rehash >/dev/null ^&1
+# tig
+function ts
+	tig status
+end
 
-# Atom
-set -x ATOM_PATH $HOME/Applications/Atom.app
+# Git
+function gs
+	git status
+end
+function gd
+	git diff $argv
+end
 
-# Node
-set -x NODE_PATH /usr/local/share/npm/lib/node_modules /usr/local/lib/node_modules
-set PATH /usr/local/share/npm/bin $PATH
+function ga
+	git add :/
+end
+
+function gc
+	git commit
+end
+
+function gl
+	git log
+end
+
+function gca
+	git commit --amend
+end
+
+function gcm
+	git commit -m $argv[1]
+end
+
+function gcam
+	git commit --amend -m $argv[1]
+end
