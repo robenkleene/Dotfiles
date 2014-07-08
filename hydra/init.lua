5e4678c7b16e80b6dfc82c8ccda2c52074174999
Roben Kleene <robenk@gmail.com>
2014-07-08 13:07:01 -0400

    Hydra

--- hydra/init.lua
+++ hydra/init.lua
@@ -20,7 +20,7 @@ function movewindow_righthalf()
   local win = window.focusedwindow()
   local newframe = win:screen():frame_without_dock_or_menu()
   newframe.w = newframe.w / 2
-  newframe.x = newframe.w -- comment this line to push it to left half of screen
+  newframe.x = newframe.x + newframe.w -- comment this line to push it to left half of screen
   win:setframe(newframe)
 end
 
@@ -34,21 +34,25 @@ end
 hotkey.bind({"cmd", "ctrl", "alt"}, "R", repl.open)
 
 local mash = {"alt"}
-local mashshift = {"ctrl", "alt"}
+local mash2 = {"ctrl", "alt"}
+local mash3 = {"ctrl", "alt", "shift"}
 
 hotkey.bind(mash, 'h', function() window.focusedwindow():focuswindow_west() end)
 hotkey.bind(mash, 'l', function() window.focusedwindow():focuswindow_east() end)
 hotkey.bind(mash, 'k', function() window.focusedwindow():focuswindow_north() end)
 hotkey.bind(mash, 'j', function() window.focusedwindow():focuswindow_south() end)
 
--- hotkey.new(mashshift, "h", movewindow_lefthalf):enable()
--- hotkey.new(mashshift, "l", movewindow_righthalf):enable()
+hotkey.new(mash2, "h", movewindow_lefthalf):enable()
+hotkey.new(mash2, "l", movewindow_righthalf):enable()
 
-hotkey.bind(mashshift, 'J', ext.grid.pushwindow_down)
-hotkey.bind(mashshift, 'K', ext.grid.pushwindow_up)
-hotkey.bind(mashshift, 'H', ext.grid.pushwindow_left)
-hotkey.bind(mashshift, 'L', ext.grid.pushwindow_right)
+hotkey.bind(mash3, 'l', ext.grid.pushwindow_nextscreen)
+hotkey.bind(mash3, 'h', ext.grid.pushwindow_prevscreen)
 
-hotkey.bind(mashshift, 'U', ext.grid.resizewindow_taller)
-hotkey.bind(mashshift, 'O', ext.grid.resizewindow_wider)
-hotkey.bind(mashshift, 'I', ext.grid.resizewindow_thinner)
+-- hotkey.bind(mash2, 'J', ext.grid.pushwindow_down)
+-- hotkey.bind(mash2, 'K', ext.grid.pushwindow_up)
+-- hotkey.bind(mash2, 'H', ext.grid.pushwindow_left)
+-- hotkey.bind(mash2, 'L', ext.grid.pushwindow_right)
+--
+-- hotkey.bind(mash2, 'U', ext.grid.resizewindow_taller)
+-- hotkey.bind(mash2, 'O', ext.grid.resizewindow_wider)
+-- hotkey.bind(mash2, 'I', ext.grid.resizewindow_thinner)
