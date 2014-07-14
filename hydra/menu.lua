18575a91150b5bd70e3f332e034da889501db55e
Roben Kleene <robenk@gmail.com>
2014-07-13 21:08:45 -0400

    Update hydra

--- hydra/menu.lua
+++ hydra/menu.lua
@@ -61,32 +61,25 @@ local function showupdate()
   os.execute('open https://github.com/sdegutis/Hydra/releases')
 end
 
--- what to do when an update is checked
-function updates.available(available)
-  if available then
-    notify.show("Hydra update available", "", "Click here to see the changelog and maybe even install it", "showupdate")
-  else
-    notify.show("No update available", "", "", "")
-  end
+-- save the time when updates are checked
+function checkforupdates()
+  updates.check(function(available)
+      -- what to do when an update is checked
+      if available then
+        notify.show("Hydra update available", "", "Click here to see the changelog and maybe even install it", "showupdate")
+      else
+        notify.show("No update available", "", "", "")
+      end
+  end)
+  settings.set('lastcheckedupdates', os.time())
 end
 
 -- check for updates every week
 timer.new(timer.weeks(1), checkforupdates):start()
 notify.register("showupdate", showupdate)
 
--- if this is your first time running Hydra, you're launching it more than a week later, check now
+-- if this is your first time running Hydra, or you're launching it more than a week later, check now
 local lastcheckedupdates = settings.get('lastcheckedupdates')
 if lastcheckedupdates == nil or lastcheckedupdates <= os.time() - timer.days(7) then
   checkforupdates()
-end
-
-local function donate()
-  -- Donate via PayPal (default)
-  os.execute("open 'https://www.paypal.com/cgi-bin/webscr?business=sbdegutis@gmail.com&cmd=_donations&item_name=Hydra.app%20donation'")
-
-  -- Donate via Gittip (inherently recurring)
-  -- os.execute("open https://www.gittip.com/sdegutis/")
-
-  -- Donate via Bitcoin: 18LEhURYNgkC9PPdtdXShDoyaHXGaLENe7
-  -- hydra.alert("Bitcoin address: 18LEhURYNgkC9PPdtdXShDoyaHXGaLENe7")
 end
