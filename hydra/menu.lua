57b832de11fac3fe4c25d32be3836487920b8e49
Roben Kleene <robenk@gmail.com>
2014-07-25 10:01:03 -0400

    Hydra

--- hydra/menu.lua
+++ hydra/menu.lua
@@ -1,25 +1,19 @@
--- save the time when updates are checked
-function checkforupdates()
-  updates.check()
-  settings.set('lastcheckedupdates', os.time())
-end
-
 function close_hydra_windows()
   local hydra_app = fnutils.find(application.applicationsforbundleid("org.degutis.Hydra"), function(app) return app:title() == "Hydra" end)
   fnutils.each(hydra_app:allwindows(), function(window) window:close() end)
 end
 
 function open_hydra_windows()
-  repl.open()
+  -- repl.open()
   logger.show()
 end
 
 function toggle_repl()
   if hydra.indock() then
     close_hydra_windows()
-    hydra.putindock(false)
+	hydra.dockicon.hide()
   else
-    hydra.putindock(true)
+    hydra.dockicon.show()
     open_hydra_windows()
   end
 end
@@ -32,54 +26,35 @@ function snap_window_to_grid()
   ext.grid.snap(window.focusedwindow())
 end
 
-function show_menu()
-  menu.show(function()
-      local updatetitles = {[true] = "Install Update", [false] = "Check for Update..."}
-      local updatefns = {[true] = updates.install, [false] = checkforupdates}
-      local hasupdate = (updates.newversion ~= nil)
 
-      return {
-        {title = "About", fn = hydra.showabout},
-        {title = updatetitles[hasupdate], fn = updatefns[hasupdate]},
-        {title = "-"},
-        {title = "Snap to Grid", fn = snap_to_grid},
-        {title = "Snap Window to Grid", fn = snap_window_to_grid},
-        {title = "-"},
-        {title = "Reload Config", fn = hydra.reload},
-        {title = "Toggle Dev Tools", fn = toggle_repl},
-        {title = "-"},
-        {title = "Quit Hydra", fn = os.exit},
-      }
-  end)
-end
+hydra.menu.show(function()
+    local t = {
+		{title = "About", fn = hydra.showabout},
+		{title = "Check for Updates...", fn = function() hydra.updates.check(nil, true) end},
+		{title = "-"},
+		{title = "Snap to Grid", fn = snap_to_grid},
+		{title = "Snap Window to Grid", fn = snap_window_to_grid},
+		{title = "-"},
+		{title = "Reload Config", fn = hydra.reload},
+		{title = "Toggle Dev Tools", fn = toggle_dev_tools},
+		{title = "-"},
+		{title = "Quit Hydra", fn = os.exit},
+    }
+
+    if not hydra.license.haslicense() then
+      table.insert(t, 1, {title = "Buy or Enter License...", fn = hydra.license.enter})
+      table.insert(t, 2, {title = "-"})
+    end
+
+    return t
+end)
 
 
 -- From Default
 
--- show available updates
-local function showupdate()
-  os.execute('open https://github.com/sdegutis/Hydra/releases')
-end
+-- when the "update is available" notification is clicked, open the website
+notify.register("showupdate", function() os.execute('open https://github.com/sdegutis/Hydra/releases') end)
 
--- save the time when updates are checked
-function checkforupdates()
-  updates.check(function(available)
-      -- what to do when an update is checked
-      if available then
-        notify.show("Hydra update available", "", "Click here to see the changelog and maybe even install it", "showupdate")
-      else
-        notify.show("No update available", "", "", "")
-      end
-  end)
-  settings.set('lastcheckedupdates', os.time())
-end
-
--- check for updates every week
-timer.new(timer.weeks(1), checkforupdates):start()
-notify.register("showupdate", showupdate)
-
--- if this is your first time running Hydra, or you're launching it more than a week later, check now
-local lastcheckedupdates = settings.get('lastcheckedupdates')
-if lastcheckedupdates == nil or lastcheckedupdates <= os.time() - timer.days(7) then
-  checkforupdates()
-end
+-- check for updates every week, and also right now (when first launching)
+timer.new(timer.weeks(1), hydra.updates.check):start()
+hydra.updates.check()
