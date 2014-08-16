fee82c3a704ecbb55c22f686d555aa302fdd6299
Roben Kleene <robenk@gmail.com>
2014-08-16 19:28:59 -0400

    More git macros

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -136,10 +136,13 @@ set fish_user_abbreviations $fish_user_abbreviations 'gcam=git commit --am -m'
 set fish_user_abbreviations $fish_user_abbreviations 'gsi=git submodule init'
 set fish_user_abbreviations $fish_user_abbreviations 'gsu=git submodule update'
 set fish_user_abbreviations $fish_user_abbreviations 'gur=git pull --rebase'
+set fish_user_abbreviations $fish_user_abbreviations 'gpbo=git push --set-upstream origin (git rev-parse --abbrev-ref HEAD)'
 # Tig
 set fish_user_abbreviations $fish_user_abbreviations 'ts=tig status'
 # Hub
 set fish_user_abbreviations $fish_user_abbreviations 'hb=hub browse'
+# Git Tower
+set fish_user_abbreviations $fish_user_abbreviations 'gt=gittower .'
 
 # # tig
 # function ts
