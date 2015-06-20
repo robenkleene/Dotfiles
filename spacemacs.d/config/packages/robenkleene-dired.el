f4bf27860da755c36c39e37860c195e1dfd78f28
Roben Kleene <robenk@gmail.com>
2015-06-20 10:21:52 -0400

    Dired hide directories

--- spacemacs.d/config/packages/robenkleene-dired.el
+++ spacemacs.d/config/packages/robenkleene-dired.el
@@ -10,4 +10,6 @@
 (define-key dired-mode-map (kbd "?") 'evil-search-backward)
 (define-key dired-mode-map (kbd "-") 'dired-up-directory) 
 
+(add-hook 'dired-mode-hook 'dired-hide-details-mode) 
+
 (provide 'robenkleene-dired)
--- spacemacs.d/config/robenkleene-theme.el
+++ spacemacs.d/config/robenkleene-theme.el
@@ -1,9 +1,11 @@
-
-;; Theme
+;; Colors
 (defconst robenkleene/highlight-background-color "#000000")
 (defconst robenkleene/selection-background-color "#444444")
+;; TODO: Make helm headers use these
 ;; (defconst robenkleene/header-background-color "#AFD7FE")
 ;; (defconst robenkleene/header-foreground-color "#000000")
+
+;; Transparent background
 (defun rk-transparent-background-in-terminal ()
   (unless (display-graphic-p (selected-frame))
     (set-face-background 'default "unspecified-bg" (selected-frame))
@@ -11,10 +13,12 @@
   )
 (add-hook 'window-setup-hook 'rk-transparent-background-in-terminal)
 (add-hook 'server-visit-hook 'rk-transparent-background-in-terminal)
+
 ;; Line Numbers interfere with git gutter
 ;; (global-linum-mode t)
 ;; Cursor
 ;; (set-face-attribute 'highlight nil :foreground 'unspecified :underline nil :background "#000000")
+
 ;; Highlight Line
 (set-face-foreground 'hl-line nil)
 (set-face-underline 'hl-line nil)
@@ -32,7 +36,13 @@
  '(diff-removed ((t (:foreground "#DA4D31" :background nil :inherit nil))))
  )
 
-
+;; Evil
 (set-face-foreground 'evil-search-highlight-persist-highlight-face "#000000")
 (set-face-background 'evil-search-highlight-persist-highlight-face "#5FD6FF")
+
+
+;; Matches
+;; TODO: Why isn't this working?
+;; (set-face-background 'show-paren-match "#000000")
+
 (provide 'robenkleene-theme)
