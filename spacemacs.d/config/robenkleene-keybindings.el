14e3dd25bbd7249d23ccaa5f2cf2376c024f7ba3
Roben Kleene <robenk@gmail.com>
2015-06-23 21:43:05 -0400

    Make working isearch delete line

--- spacemacs.d/config/robenkleene-keybindings.el
+++ spacemacs.d/config/robenkleene-keybindings.el
@@ -5,11 +5,11 @@
 (defmacro robenkleene-rsify (mode map)
   "Hurt your hands in MODE and MAP."
   `(eval-after-load ,mode
-    '(progn
-       (define-key ,map (kbd "C-w") 'backward-kill-word)
-       (define-key ,map (kbd "C-u") '(lambda () (interactive) (kill-line 0)))
-       )
-    )
+     '(progn
+        (define-key ,map (kbd "C-w") 'backward-kill-word)
+        (define-key ,map (kbd "C-u") '(lambda () (interactive) (kill-line 0)))
+        )
+     )
   )
 (defvar helm-swoop-map)
 (defvar helm-map)
@@ -18,18 +18,45 @@
 (robenkleene-rsify 'helm helm-map)
 (robenkleene-rsify 'helm-grep helm-grep-map)
 
-
+;; Define C-u & C-w in isearch
 ;; (defun robenkleene/isearch-delete-line ()
+;;   "Delete line."
 ;;   (interactive "p")
 ;;   (if (= 0 (length isearch-string))
 ;;       (ding)
 ;;     (setq isearch-string "")
 ;;           isearch-message (mapconcat 'isearch-text-char-description
-;;                                      isearch-string "")))
-;; (if isearch-other-end (goto-char isearch-other-end))
-;; (isearch-search)
-;; (isearch-push-state)
-;; (isearch-update)))
+;;                                      isearch-string ""))
+;;   (if isearch-other-end (goto-char isearch-other-end))
+;;   (isearch-search)
+;;   (isearch-push-state)
+;;   (isearch-update))
+(defun robenkleene/isearch-delete-line (&optional arg)
+  "Delete character from end of search string and search again.
+Unlike `isearch-delete-char', it only deletes the last character,
+but doesn't cancel the effect of other isearch command.
+If search string is empty, just beep."
+  (interactive "p")
+  (if (= 0 (length isearch-string))
+      (ding)
+    (setq isearch-string ""
+          ;; (substring isearch-string 0
+				  ;;   (- (min (or arg 1)
+					;;     (length isearch-string))))
+          isearch-message (mapconcat 'isearch-text-char-description
+                                     isearch-string "")))
+  ;; Use the isearch-other-end as new starting point to be able
+  ;; to find the remaining part of the search string again.
+  ;; This is like what `isearch-search-and-update' does,
+  ;; but currently it doesn't support deletion of characters
+  ;; for the case where unsuccessful search may become successful
+  ;; by deletion of characters.
+  (if isearch-other-end (goto-char isearch-other-end))
+  (isearch-search)
+  (isearch-push-state)
+  (isearch-update))
+
+
 
 
 (require 's)
@@ -45,8 +72,8 @@
     (setq isearch-string nil
           isearch-message nil)
     (isearch-yank-string string)))
+(define-key isearch-mode-map (kbd "C-u") 'robenkleene/isearch-delete-line)
 (define-key isearch-mode-map (kbd "C-w") 'robenkleene/isearch-delete-word)
-;; (define-key isearch-mode-map (kbd "C-u") 'robenkleene/isearch-delete-line)
 
 (defvar robenkleene-rsi-minor-mode-map (make-keymap))
 (define-key robenkleene-rsi-minor-mode-map (kbd "C-w") 'backward-kill-word)
