11339fe534250603336315ee2180301699c63eaf
Roben Kleene <robenk@gmail.com>
2015-07-03 22:00:46 -0400

    Delete spacemacs

--- spacemacs.d/config/robenkleene-ui.el
+++
@@ -1,9 +0,0 @@
-;;; robenkleene-ui.el --- robenkleene-ui
-;;; Commentary:
-;;; Code:
-
-;; Offset the number by two spaces to work around some weird fringe glitch
-;; (setq linum-format "  %d ")
-;; (setq linum-format "%d ")
-(provide 'robenkleene-ui)
-;;; robenkleene-ui.el ends here
