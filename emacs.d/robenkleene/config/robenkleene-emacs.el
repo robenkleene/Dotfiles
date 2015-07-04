f2d19bccff1b81185aa64325ba56cfac555ce3d7
Roben Kleene <robenk@gmail.com>
2015-07-04 00:04:33 -0400

    Move auto-save files to temp directory

---
+++ emacs.d/robenkleene/config/robenkleene-emacs.el
@@ -0,0 +1,9 @@
+;;; robenkleene-emacs.el --- robenkleene-emacs
+;;; Commentary:
+;;; Code:
+
+(setq auto-save-file-name-transforms
+                `((".*" ,temporary-file-directory t)))
+
+(provide 'robenkleene-emacs)
+;;; robenkleene-emacs.el ends here
--- emacs.d/robenkleene/robenkleene.el
+++ emacs.d/robenkleene/robenkleene.el
@@ -3,6 +3,9 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/robenkleene/themes/")
 (load-theme 'robenkleene-dark t)
 
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/robenkleene/config"))
+(require 'robenkleene-emacs)
+
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/robenkleene/packages"))
 (require 'robenkleene-helm)
 (require 'robenkleene-helm-swoop)
