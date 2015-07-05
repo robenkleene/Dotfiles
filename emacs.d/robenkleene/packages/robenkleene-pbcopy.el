cf313dec6001485ffe9aa844b14c44c2453facfc
Roben Kleene <robenk@gmail.com>
2015-07-04 20:47:39 -0400

    pbcopy

--- emacs.d/robenkleene/packages/robenkleene-pbcopy.el
+++ emacs.d/robenkleene/packages/robenkleene-pbcopy.el
@@ -7,10 +7,8 @@
 (require 'use-package)
 (defvar pbcopy)
 (use-package pbcopy
-  :config
-  (if (and (robenkleene/system-is-mac) (not (display-graphic-p)))
-      (turn-on-pbcopy)
-    )
+  :if (and (robenkleene/system-is-mac) (not (display-graphic-p)))
+  :init (turn-on-pbcopy)
   )
 
 (provide 'robenkleene-pbcopy)
