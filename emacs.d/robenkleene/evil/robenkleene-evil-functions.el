20116062ceebd2c883884ad0d95f17bb579c8394
Roben Kleene <robenk@gmail.com>
2015-07-11 22:32:31 -0400

    Add vim-eunuch style rename command

--- emacs.d/robenkleene/evil/robenkleene-evil-functions.el
+++ emacs.d/robenkleene/evil/robenkleene-evil-functions.el
@@ -2,6 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'evil-commands)
 
 (defmacro robenkleene/advice-after-evil-initial-state (func)
   "Takes a FUNC, set the buffer to evil initial state after."
@@ -9,6 +10,27 @@
      (evil-change-to-initial-state)
      )
   )
+
+(evil-define-command robenkleene/evil-rename (new-filename &optional bang)
+  :repeat nil
+  :move-point nil
+  (interactive "<f><!>")
+  (let ((name (buffer-name))
+        (old-filename (buffer-file-name)))
+    (if (not old-filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+        (progn
+          (rename-file name new-filename bang)
+          (kill-buffer new-filename)
+          (rename-buffer new-filename)
+          (set-visited-file-name new-filename)
+          (set-buffer-modified-p nil)
+          )
+        )
+      )
+    )
+
+(evil-ex-define-cmd "Rename" 'robenkleene/evil-rename)
 
 (provide 'robenkleene-evil-functions)
 ;;; robenkleene-evil-functions.el ends here
