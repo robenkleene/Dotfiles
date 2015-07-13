0c1389dafa7a799f3b9cf8d3a8cc854ccbea1113
Roben Kleene <robenk@gmail.com>
2015-07-12 21:31:28 -0400

    Complete company mode like fish shell

--- emacs.d/robenkleene/packages/robenkleene-company.el
+++ emacs.d/robenkleene/packages/robenkleene-company.el
@@ -7,10 +7,11 @@
   :ensure t
   :init
   (global-company-mode)
+  ;; `company-capf': built-in autocomplete
   (setq company-backends '((company-keywords company-semantic company-files company-gtags company-etags company-dabbrev-code) company-capf company-ispell company-dabbrev))
+  (define-key company-active-map (kbd "C-f") 'company-complete-selection)
   )
 
-;; `company-capf': built-in autocomplete
 
 (provide 'robenkleene-company)
 ;;; robenkleene-company.el ends here
