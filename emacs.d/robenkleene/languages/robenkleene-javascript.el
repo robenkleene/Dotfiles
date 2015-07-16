b2ca68d8a93df5a7501311ec55f87155121b61b3
Roben Kleene <robenk@gmail.com>
2015-07-15 22:08:30 -0400

    Work setting up JavaScript

--- emacs.d/robenkleene/languages/robenkleene-javascript.el
+++ emacs.d/robenkleene/languages/robenkleene-javascript.el
@@ -9,12 +9,48 @@
   :mode "\\.js\\'"
   :config
   (progn
+    ;; Repl Map
+    (defvar robenkleene/javascript-repl-leader-map (make-keymap))
+    (define-key robenkleene/javascript-repl-leader-map (kbd "o") 'nodejs-repl)
+
+    (defvar robenkleene/javascript-leader-map (make-keymap))
+    (define-key robenkleene/javascript-leader-map (kbd "l") robenkleene/javascript-repl-leader-map)
+    (define-key robenkleene/javascript-leader-map (kbd "s") 'helm-semantic-or-imenu)
+    
+    ;; Add Map to mode
+    (declare-function evil-declare-key "evil")
+    (evil-declare-key 'motion js2-mode-map (kbd ",") robenkleene/javascript-leader-map)
+
+    ;; Node REPL
     (use-package nodejs-repl
       :ensure t
+      :commands nodejs-repl
       :config
       (progn
         (defvar nodejs-repl-command)
         (setq nodejs-repl-command "/Users/robenkleene/.nvm/versions/node/v0.12.2/bin/node")
+
+        ;; Define function to evaluate region
+        (defun robenkleene/nodejs-repl-eval-region (start end)
+          "Evaluate the region specified by `START' and `END'."
+          (let ((proc (get-process nodejs-repl-process-name)))
+            (comint-simple-send proc (buffer-substring-no-properties start end))))
+        
+        (defun robenkleene/nodejs-repl-eval-buffer (&optional buffer)
+          "Evaluate the current buffer or the one given as `BUFFER'.  `BUFFER' should be a string or buffer."
+          (interactive)
+          (let ((buffer (or buffer (current-buffer))))
+            (with-current-buffer buffer
+              (robenkleene/nodejs-repl-eval-region (point-min) (point-max)))))
+
+        ;; Map function to evaluate region
+        (define-key robenkleene/javascript-repl-leader-map (kbd "r") (lambda (begin end)
+                                                                       (interactive "r")
+                                                                       (robenkleene/nodejs-repl-eval-region begin end)
+                                                                       (deactivate-mark)
+                                                                       ))
+        (define-key robenkleene/javascript-repl-leader-map (kbd "b") 'robenkleene/nodejs-repl-eval-buffer)
+        
         )
       )
     )
