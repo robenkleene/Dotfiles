2fcf64439ccfd273fadc250058f6cf22068c2a39
Roben Kleene <robenk@gmail.com>
2016-01-04 21:46:09 -0500

    Setup SLIME

--- emacs.d/robenkleene/languages/robenkleene-common-lisp.el
+++ emacs.d/robenkleene/languages/robenkleene-common-lisp.el
@@ -7,10 +7,33 @@
 (use-package slime
   :ensure t
   :defer t
-  ;; :config
-  ;; (progn
-  ;;   (setq inferior-lisp-program "/usr/bin/sbcl")
-  ;;   (slime-setup))
+  :config
+  (progn
+    (setq inferior-lisp-program "/usr/local/bin/sbcl")
+    
+    (defvar robenkleene/lisp-eval-leader-map (make-keymap))
+    (define-key robenkleene/lisp-eval-leader-map (kbd "b") (lambda ()
+                                                             (interactive)
+                                                             (slime-compile-and-load-file)
+                                                             (message "Compiled & loaded buffer")
+                                                             )
+      )
+    (define-key robenkleene/lisp-eval-leader-map (kbd "r") (lambda (begin end)
+                                                             (interactive "r")
+                                                             (slime-eval-region begin end)
+                                                             (message "Evaluated region")
+                                                             (deactivate-mark)
+                                                             )
+      )
+
+    ;; Maps
+    (defvar robenkleene/emacs-lisp-leader-map (make-keymap))
+    (define-key robenkleene/emacs-lisp-leader-map (kbd "e") robenkleene/lisp-eval-leader-map)
+
+    ;; Add maps to mode
+    (declare-function evil-declare-key "evil")
+    (evil-declare-key 'motion lisp-mode-map (kbd "\\") robenkleene/emacs-lisp-leader-map)
+    )
   )
 
 (provide 'robenkleene-common-lisp)
--- emacs.d/robenkleene/languages/robenkleene-emacs-lisp.el
+++ emacs.d/robenkleene/languages/robenkleene-emacs-lisp.el
@@ -29,14 +29,14 @@
 
      ;;; Main Map
 
-     ;; Eval Map
+     ;; Maps
      (defvar robenkleene/emacs-lisp-leader-map (make-keymap))
      (define-key robenkleene/emacs-lisp-leader-map (kbd "e") robenkleene/emacs-lisp-eval-leader-map)
      (define-key robenkleene/emacs-lisp-leader-map (kbd "s") 'helm-semantic-or-imenu)
      (define-key robenkleene/emacs-lisp-leader-map (kbd "l") robenkleene/emacs-lisp-linter-leader-map)
      (define-key robenkleene/emacs-lisp-leader-map (kbd "p") 'robenkleene/print-variable)
 
-     ;; Add map to mode
+     ;; Add maps to mode
      (declare-function evil-declare-key "evil")
      (evil-declare-key 'motion emacs-lisp-mode-map (kbd "\\") robenkleene/emacs-lisp-leader-map)
      (evil-declare-key 'motion lisp-interaction-mode-map (kbd "\\") robenkleene/emacs-lisp-leader-map)
