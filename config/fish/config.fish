1f19d4681d29c60a43c034242566fc6bf308506f
Roben Kleene <robenk@gmail.com>
2016-03-11 18:03:26 -0500

    Add fasd comment

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,7 @@
 # Fisherman
 # To setup, install fisherman and plugins:
 # bass
+# fasd
 set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
 source $fisher_home/config.fish
