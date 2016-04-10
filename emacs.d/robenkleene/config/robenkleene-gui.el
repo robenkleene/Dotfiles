3327583eb3abc2e15aad09bf78fa20e996fa7ee4
Roben Kleene <robenk@gmail.com>
2016-04-09 23:38:51 -0400

    Add some gui keys

--- emacs.d/robenkleene/config/robenkleene-gui.el
+++ emacs.d/robenkleene/config/robenkleene-gui.el
@@ -6,8 +6,10 @@
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/robenkleene/themes/")
 (load-theme 'kleene-dark t)
-
 (set-face-background 'default "#232323")
+
+(setq mac-command-modifier 'control)
+(setq mac-option-modifier 'meta)
 
 (provide 'robenkleene-gui)
 ;;; robenkleene-gui.el ends here
