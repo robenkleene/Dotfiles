26571348ef1c0ba8c937ef577a1e1650f0dfade4
Roben Kleene <robenk@gmail.com>
2016-12-04 14:06:59 -0500

    Archive clean-open-with

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -339,10 +339,6 @@ function cd-copy-path
   pwd | tr -d '\n' | pbcopy
 end
 
-function clean-open-with
-  /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
-end
-
 # BBEdit
 function bbfind-gui-grep-case
   bbfind --grep --gui --case-sensitive $argv .
