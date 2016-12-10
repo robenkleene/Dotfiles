e60e305269fbaf80b2996fc9867473616aae3624
Roben Kleene <robenk@gmail.com>
2016-12-09 21:12:29 -0500

    Add commented out theme selection

--- emacs.d/robenkleene/config/robenkleene-terminal.el
+++ emacs.d/robenkleene/config/robenkleene-terminal.el
@@ -4,6 +4,7 @@
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/robenkleene/themes/")
 (load-theme 'kleene-dark t)
+; (load-theme 'kleene-dark2 t)
 
 (provide 'robenkleene-terminal)
 ;;; robenkleene-terminal.el ends here
