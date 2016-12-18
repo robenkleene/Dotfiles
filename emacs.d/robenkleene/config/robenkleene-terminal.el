1ba9d5b9562478caf7fb69c3406e34ebbf6472e4
Roben Kleene <robenk@gmail.com>
2016-12-18 12:14:42 -0500

    Switch comment

--- emacs.d/robenkleene/config/robenkleene-terminal.el
+++ emacs.d/robenkleene/config/robenkleene-terminal.el
@@ -3,7 +3,7 @@
 ;;; Code:
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/robenkleene/themes/")
-; (load-theme 'kleene-dark t)
+;; (load-theme 'kleene-dark t)
 (load-theme 'kleene-dark2 t)
 
 (provide 'robenkleene-terminal)
