14d9883018aeaaa4dea0a6c34ab885cba5b76c41
Roben Kleene <robenk@gmail.com>
2016-12-28 13:10:08 -0500

    Remove old theme versions

--- emacs.d/robenkleene/config/robenkleene-gui.el
+++ emacs.d/robenkleene/config/robenkleene-gui.el
@@ -2,10 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (load-theme 'wombat)
-
 (add-to-list 'custom-theme-load-path "~/.emacs.d/robenkleene/themes/")
-(load-theme 'kleene-dark t)
+(load-theme 'kleene-dark3 t)
 (set-face-background 'default "#232323")
 
 (add-to-list 'default-frame-alist '(cursor-color . "#4BC840"))
--- emacs.d/robenkleene/config/robenkleene-terminal.el
+++ emacs.d/robenkleene/config/robenkleene-terminal.el
@@ -3,8 +3,6 @@
 ;;; Code:
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/robenkleene/themes/")
-;; (load-theme 'kleene-dark t)
-;; (load-theme 'kleene-dark2 t)
 (load-theme 'kleene-dark3 t)
 
 (provide 'robenkleene-terminal)
