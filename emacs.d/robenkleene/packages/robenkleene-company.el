df3395273395ad12babdd4551a8ab5839681cfbe
Roben Kleene <robenk@gmail.com>
2016-12-29 11:50:28 -0500

    Disable company for some modes

--- emacs.d/robenkleene/packages/robenkleene-company.el
+++ emacs.d/robenkleene/packages/robenkleene-company.el
@@ -7,6 +7,7 @@
   :ensure t
   :init
   (global-company-mode)
+  (setq company-global-modes '(not org-mode markdown-mode))
   (setq company-idle-delay .1)
   ;; `company-capf': built-in autocomplete
   (setq company-backends '((company-keywords company-semantic company-files company-gtags company-etags company-dabbrev-code) company-capf company-ispell company-dabbrev))
