b06cf9e23b29fcdde58de3b1250bbe2697c6c84e
Roben Kleene <robenk@gmail.com>
2016-12-30 13:16:14 -0500

    Disable spell check in code blocks

--- emacs.d/robenkleene/config/robenkleene-config.el
+++ emacs.d/robenkleene/config/robenkleene-config.el
@@ -86,6 +86,22 @@
 ;; Org
 (setq org-replace-disputed-keys t)
 (setq org-todo-keywords '("TODO" "STARTED" "DONE"))
+;; Disable spell check in code blocks
+(defadvice org-mode-flyspell-verify (after org-mode-flyspell-verify-hack activate)
+  (let ((rlt ad-return-value)
+        (begin-regexp "^[ \t]*#\\+begin_\\(src\\|html\\|latex\\)")
+        (end-regexp "^[ \t]*#\\+end_\\(src\\|html\\|latex\\)")
+        old-flag
+        b e)
+    (when ad-return-value
+      (save-excursion
+        (setq old-flag case-fold-search)
+        (setq case-fold-search t)
+        (setq b (re-search-backward begin-regexp nil t))
+        (if b (setq e (re-search-forward end-regexp nil t)))
+        (setq case-fold-search old-flag))
+      (if (and b e (< (point) e)) (setq rlt nil)))
+    (setq ad-return-value rlt)))
 
 (provide 'robenkleene-config)
 ;;; robenkleene-config.el ends here
