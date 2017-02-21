be9313954acfb1e03f0e5ad3981ac74ee3a6d552
Roben Kleene <robenk@gmail.com>
2017-02-21 10:48:36 -0500

    Replace custom with built-in

--- config/fish/ui.fish
+++ config/fish/ui.fish
@@ -7,15 +7,10 @@ set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch cyan
 function fish_prompt
-  # Note this function will be built-in in fish `2.5`
-  # After upgrading remove it and replace with `prompt_hostname`
-  if not set -q __prompt_hostname
-    set -g __prompt_hostname (hostname|cut -d . -f 1)
-  end
   set_color $fish_color_comment
   echo -n (date "+%I:%M %p")
   echo -n ' '
-  echo -n $USER@$__prompt_hostname
+  echo -n $USER@(prompt_hostname)
   echo -n ' '
   set_color $fish_color_cwd
   echo -n (prompt_pwd)
