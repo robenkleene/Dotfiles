1b5bf1e6dcd41390ebcef2eeb32aafdbe3f38d38
Roben Kleene <robenk@gmail.com>
2017-02-20 19:47:51 -0500

    Setup grep alias and other tweaks

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -1,12 +1,16 @@
 # Enable Completion
-autoload -Uz compinit promptinit
+autoload -Uz compinit
 compinit
+# By default, ^S freezes terminal output and ^Q resumes it. Disable that so
+# that those keys can be used for other things.
+unsetopt flowcontrol
 
 # Color in `ls`
 export CLICOLOR=1
 
 # Required for abbreviations
 setopt extendedglob
+
 
 # Highlight tab match
 zstyle ':completion:*' menu select
