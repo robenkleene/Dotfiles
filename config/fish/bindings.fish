6bfa024856a577e575eef4e03cf919c3e2622dba
Roben Kleene <robenk@gmail.com>
2017-02-21 12:56:32 -0500

    Move history delete function

--- config/fish/bindings.fish
+++ config/fish/bindings.fish
@@ -1,3 +1,11 @@
+function delete-current-history-search
+  if commandline --search-mode
+    echo -e "\n"
+    history --delete (commandline)
+    commandline -f history-search-backward
+  end
+end
+
 function fish_user_key_bindings
   # Better binding but I can't get this to work
   # bind \cx\ce 'edit_cmd'
