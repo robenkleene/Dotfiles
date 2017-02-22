d1344118f7ac4803dac45532bd7f8acb9112b969
Roben Kleene <robenk@gmail.com>
2017-02-21 22:43:57 -0500

    Change edit binding

--- config/fish/bindings.fish
+++ config/fish/bindings.fish
@@ -6,9 +6,22 @@ function delete-current-history-search
   end
 end
 
+function edit_cmd --description 'Input command in external editor'
+  set tempfile (mktemp /tmp/fish.cmd.XXXXXXXX)
+  and test -n "$tempfile"
+  and set cursorposition (commandline -C)
+  and commandline -b > $tempfile
+  and vim-edit -c 'set ft=fish' $tempfile
+  and set edited (cat $tempfile)
+  and test -n "$edited"
+  and commandline -r $edited
+  and commandline -C $cursorposition
+  and command rm $tempfile
+end
+
 function fish_user_key_bindings
   # Better binding but I can't get this to work
-  # bind \cx\ce 'edit_cmd'
-  bind \ee 'edit_cmd'
+  bind \cx\ce 'edit_cmd'
+  # bind \ee 'edit_cmd'
   bind \eD delete-current-history-search
 end
--- config/fish/functions.fish
+++ config/fish/functions.fish
@@ -337,16 +337,3 @@ end
 #   wcsearch $argv
 # end
 
-function edit_cmd --description 'Input command in external editor'
-  set tempfile (mktemp /tmp/fish.cmd.XXXXXXXX)
-  and test -n "$tempfile"
-  and set cursorposition (commandline -C)
-  and commandline -b > $tempfile
-  and vim-edit -c 'set ft=fish' $tempfile
-  and set edited (cat $tempfile)
-  and test -n "$edited"
-  and commandline -r $edited
-  and commandline -C $cursorposition
-  and command rm $tempfile
-end
-
