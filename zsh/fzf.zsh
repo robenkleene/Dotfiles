eb0ffd48250d129fd753ffcedc1778cdd76e3746
Roben Kleene <robenk@gmail.com>
2017-02-23 22:16:00 -0500

    Set up fzf.zsh

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,2 +1,14 @@
 local resolvedir="${TMPDIR:-/tmp}"
 local tmpdir=$resolvedir[1]
+
+function ack-lines() {
+  rg --no-heading $@
+}
+function ack-lines-color() {
+  ack-lines --color=always $@
+}
+function ack-lines-no-color() {
+  ack-lines --color=never $@
+}
+
+export FZF_DEFAULT_COMMAND='rg --files -g ""'
