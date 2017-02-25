6033fd9b78786d732515541d065456ebb628fdbc
Roben Kleene <robenk@gmail.com>
2017-02-25 15:32:11 -0500

    Setup fzf bindings

--- config/fish/bindings.fish
+++ config/fish/bindings.fish
@@ -19,9 +19,17 @@ function edit_cmd --description 'Input command in external editor'
   and command rm $tempfile
 end
 
+# Make `fzf` bindings available, since this is being manually imported here
+# `fzf` should be installed at `~/.fzf/`, using the command 
+# `~/.fzf/install --bin`, which avoids installing the completions automatically
+source ~/.fzf/shell/key-bindings.fish
+
 function fish_user_key_bindings
   # Better binding but I can't get this to work
   bind \cx\ce 'edit_cmd'
   # bind \ee 'edit_cmd'
   bind \eD delete-current-history-search
+  bind \ct fzf-file-widget
+  bind \cr fzf-history-widget
+  bind \ec fzf-cd-widget
 end
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,3 +2,10 @@ bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 bindkey -M emacs '^P' history-substring-search-up
 bindkey -M emacs '^N' history-substring-search-down
+
+# fzf
+# Make `fzf` bindings available, since this is being manually imported here
+# `fzf` should be installed at `~/.fzf/`, using the command 
+# `~/.fzf/install --bin`, which avoids installing the completions automatically
+source ~/.fzf/shell/completion.zsh
+source ~/.fzf/shell/key-bindings.zsh
