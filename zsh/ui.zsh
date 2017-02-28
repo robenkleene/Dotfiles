ea44208ad731e26bd60ae5643b6904695d10dce0
Roben Kleene <robenk@gmail.com>
2017-02-28 15:01:48 -0500

    Add SSH and exit status to prompt

--- zsh/ui.zsh
+++ zsh/ui.zsh
@@ -51,5 +51,5 @@ chpwd() {
   update_prompt
 }
 
-PS1='%F{$comment}%D{%I:%M %p} %n@%m %F{cyan}${prompt_path} %f%(1j. %j& .)%# '
+PS1='%F{$comment}%D{%I:%M %p} %n@%m %F{cyan}${prompt_path} %f%(1j.%j& .)%(?..%?? )%#${SSH_CONNECTION:+%#} '
 RPROMPT='${vcs_info_msg_0_}'
