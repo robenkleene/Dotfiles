081a1062fc5c44b26df60bc6ddf1c9660e2d183e
Roben Kleene <robenk@gmail.com>
2017-03-06 14:57:54 -0500

    Fix autosuggestion issue

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -3,6 +3,9 @@ bindkey '^[[B' history-substring-search-down
 bindkey -M emacs '^P' history-substring-search-up
 bindkey -M emacs '^N' history-substring-search-down
 
+# Make my custom abbreviations expanstion clear autosuggestions
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=magic-abbrev-expand-and-accept
+
 # fzf
 # Make `fzf` bindings available, since this is being manually imported here
 # `fzf` should be installed at `~/.fzf/`, using the command 
