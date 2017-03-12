a00da85df344c4eb3888b7e41cbc566337cbf174
Roben Kleene <robenk@gmail.com>
2017-03-12 15:28:58 -0400

    Use backslash for tmux leader

--- zsh/startup.zsh
+++ zsh/startup.zsh
@@ -7,6 +7,10 @@ if ps -u $USER | grep 'emacs --daemon' | grep --silent -v grep; then
 fi
 
 # tmux
+# Fix for `tmux` overwriting this binding in `tmux.conf` with `send-prefix`
+if [ ! -z "$TMUX" ]; then
+  tmux bind-key 'C-\' select-pane -t :.+
+fi
 # Echo running sessions if there are any
 if tmux ls >/dev/null 2>/dev/null; then
   # Only echo if we aren't already in a tmux session
