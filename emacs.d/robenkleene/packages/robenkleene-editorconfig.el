00a7c640bb4f5ad11409f3bb38f0405907a5986b
Roben Kleene <robenk@gmail.com>
2017-03-12 17:01:07 -0400

    Improve editorconfig markdown support

--- emacs.d/robenkleene/packages/robenkleene-editorconfig.el
+++ emacs.d/robenkleene/packages/robenkleene-editorconfig.el
@@ -2,9 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
+
 (require 'use-package)
 (use-package editorconfig
   :ensure t
+  :config
+  (add-to-list 'editorconfig-indentation-alist '(markdown-mode markdown-list-indent-width))
+  (editorconfig-mode 1)
   )
 
 (provide 'robenkleene-editorconfig)
