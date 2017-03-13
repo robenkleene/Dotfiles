95c1e3ccc0825c18f5aaabf52242c3c78c7983bd
Roben Kleene <robenk@gmail.com>
2017-03-12 20:31:52 -0400

    Go back to q for tmux prefix

--- zsh/startup.zsh
+++ zsh/startup.zsh
@@ -8,9 +8,10 @@ fi
 
 # tmux
 # Fix for `tmux` overwriting this binding in `tmux.conf` with `send-prefix`
-if [ ! -z "$TMUX" ]; then
-  tmux bind-key 'C-\' select-pane -t :.+
-fi
+# This is only necessary if using `C-\` as prefix
+# if [ ! -z "$TMUX" ]; then
+#   tmux bind-key 'C-\' select-pane -t :.+
+# fi
 # Echo running sessions if there are any
 if tmux ls >/dev/null 2>/dev/null; then
   # Only echo if we aren't already in a tmux session
