bdb797215f00b9779e57b9a11541616247d9bad5
Roben Kleene <robenk@gmail.com>
2017-03-22 21:15:34 -0400

    Improve help

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -9,6 +9,19 @@ setopt nocaseglob
 # `cd` just by typing name
 setopt autocd
 
+# Help
+autoload -Uz run-help
+unalias run-help
+alias help=run-help
+autoload -Uz run-help-git
+# autoload -Uz run-help-ip
+# autoload -Uz run-help-openssl
+# autoload -Uz run-help-p4
+# autoload -Uz run-help-sudo
+# autoload -Uz run-help-svk
+# autoload -Uz run-help-svn
+
+
 # History
 # Share history between sessions
 setopt share_history
