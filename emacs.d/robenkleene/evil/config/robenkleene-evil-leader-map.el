612c6995cdbabc4c3c0cba9a05f022571aac81f4
Roben Kleene <robenk@gmail.com>
2017-07-05 22:06:26 -0400

    Add switch buffer command

--- emacs.d/robenkleene/evil/config/robenkleene-evil-leader-map.el
+++ emacs.d/robenkleene/evil/config/robenkleene-evil-leader-map.el
@@ -13,6 +13,7 @@
 (define-key robenkleene/leader-map (kbd "r") 'robenkleene/reveal-in-finder)
 (define-key robenkleene/leader-map (kbd "e") 'robenkleene/external-editor)
 (define-key robenkleene/leader-map (kbd "o") robenkleene/open-map)
+(define-key robenkleene/leader-map (kbd "b") 'ido-switch-buffer)
 (define-key robenkleene/leader-map (kbd "f") '(lambda () (interactive)
                                                 (projectile-find-file-in-directory default-directory)))
 (define-key robenkleene/leader-map (kbd "a") 'helm-do-ag)
