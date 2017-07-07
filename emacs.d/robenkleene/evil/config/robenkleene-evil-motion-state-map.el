e9aaed1a18917f96e00f8d6cfeafce24482afe86
Roben Kleene <robenk@gmail.com>
2017-07-06 22:10:51 -0400

    Tweak some bindings

--- emacs.d/robenkleene/evil/config/robenkleene-evil-motion-state-map.el
+++ emacs.d/robenkleene/evil/config/robenkleene-evil-motion-state-map.el
@@ -7,12 +7,14 @@
 (define-key evil-motion-state-map (kbd "M-k") 'evil-window-up)
 (define-key evil-motion-state-map (kbd "M-j") 'evil-window-down)
 (define-key evil-motion-state-map (kbd "M-l") 'evil-window-right)
-(define-key evil-motion-state-map (kbd "M-e") '(lambda () (interactive)
-                                                 (projectile-find-file-in-directory default-directory)))
+(define-key evil-motion-state-map (kbd "M-e") 'projectile-find-file-in-directory)
+;; (define-key evil-motion-state-map (kbd "M-e") '(lambda () (interactive)
+;;                                                  (projectile-find-file-in-directory default-directory)))
 (define-key evil-motion-state-map (kbd "M-c") 'projectile-find-dir)
 (define-key evil-normal-state-map (kbd "C-w t") 'make-frame-command)
 (define-key evil-motion-state-map (kbd "M-n") 'other-frame)
 (define-key evil-motion-state-map (kbd "M-p") 'previous-multiframe-window)
+(define-key evil-motion-state-map (kbd "C-w q") 'delete-frame)
 
 ;; Dired
 (define-key evil-motion-state-map "-" 'dired-jump)
