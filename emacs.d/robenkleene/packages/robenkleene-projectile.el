523107ba0fd3943460308f53a8a31b4288b08db5
Roben Kleene <robenk@gmail.com>
2017-07-09 16:10:52 -0400

    Add comment

--- emacs.d/robenkleene/packages/robenkleene-projectile.el
+++ emacs.d/robenkleene/packages/robenkleene-projectile.el
@@ -20,9 +20,12 @@
               )
   :config
   (defun robenkleene/projectile-find-file-default-directory ()
+    "Run `projectile-find-file' with `default-directory'."
     (interactive)
     (projectile-find-file-in-directory default-directory)
     )
+
+
   
   ;; `helm-projectile-ag' should do project level `rg' by default, but it isn't
   ;; compatible with `rg', so instead this is currently done with a function
