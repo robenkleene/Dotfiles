bbb7f1e6d753e2df38a71c21eb5ccd9eb7b5bf57
Roben Kleene <robenk@gmail.com>
2017-07-27 22:26:27 -0400

    Use a m-tab key

--- emacs.d/robenkleene/config/robenkleene-terminal.el
+++ emacs.d/robenkleene/config/robenkleene-terminal.el
@@ -5,5 +5,8 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/robenkleene/themes/")
 (load-theme 'kleene-dark t)
 
+;; A proper `<M-tab>' key is more important than `C-M-Tab`
+(define-key key-translation-map  (kbd "C-M-i") (kbd "<M-tab>"))
+
 (provide 'robenkleene-terminal)
 ;;; robenkleene-terminal.el ends here
