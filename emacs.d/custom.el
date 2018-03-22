957eda35d91600255e90cea5d3669c7f6c6c4b8a
Roben Kleene <robenk@gmail.com>
2018-03-22 09:52:47 -0400

    Update packages

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -7,7 +7,7 @@
    ["#000000" "#D32D1E" "#62D63F" "#E5E64B" "#005FFF" "#af00df" "#4AA4B0" "#ffffff"])
  '(package-selected-packages
    (quote
-    (page-break-lines fish-mode markdown-mode swift-mode robe nodejs-repl js2-mode slime fill-column-indicator helm-swoop helm macrostep fasd rainbow-mode rainbow-delimiters projectile yasnippet ido-completing-read+ flycheck magit-filenotify evil-terminal-cursor-changer bind-map evil-visualstar evil-commentary wgrep buffer-move smex pbcopy jade-mode enh-ruby-mode editorconfig company better-defaults aggressive-indent))))
+    (page-break-lines fish-mode markdown-mode swift-mode robe nodejs-repl js2-mode slime fill-column-indicator helm-swoop helm fasd rainbow-delimiters projectile yasnippet ido-completing-read+ flycheck macrostep rainbow-mode magit-filenotify evil-terminal-cursor-changer bind-map evil-visualstar evil-commentary wgrep buffer-move smex pbcopy jade-mode enh-ruby-mode editorconfig company better-defaults aggressive-indent))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
