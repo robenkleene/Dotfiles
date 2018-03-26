b10589f5ff1c1044bcd0d26725dea3b09fe1fbd0
Roben Kleene <robenk@gmail.com>
2018-03-25 22:02:37 -0400

    Touched custom.el

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -7,7 +7,28 @@
    ["#000000" "#D32D1E" "#62D63F" "#E5E64B" "#005FFF" "#af00df" "#4AA4B0" "#ffffff"])
  '(package-selected-packages
    (quote
-    (page-break-lines fish-mode markdown-mode swift-mode robe nodejs-repl js2-mode slime fill-column-indicator helm-swoop helm fasd rainbow-delimiters projectile yasnippet ido-completing-read+ flycheck macrostep rainbow-mode magit-filenotify evil-terminal-cursor-changer bind-map evil-visualstar evil-commentary wgrep buffer-move smex pbcopy jade-mode enh-ruby-mode editorconfig company better-defaults aggressive-indent))))
+    (smartparens page-break-lines fish-mode markdown-mode swift-mode robe nodejs-repl js2-mode slime fill-column-indicator helm-swoop helm fasd rainbow-delimiters projectile yasnippet ido-completing-read+ flycheck macrostep rainbow-mode magit-filenotify evil-terminal-cursor-changer bind-map evil-visualstar evil-commentary wgrep buffer-move smex pbcopy jade-mode enh-ruby-mode editorconfig company better-defaults aggressive-indent)))
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
