e3a6b5b465a71e6a15719801c056c2ed2116e1bf
Roben Kleene <robenk@gmail.com>
2018-03-27 20:51:30 -0400

    Setup highlight line

--- emacs.d/robenkleene/packages/robenkleene-god-mode.el
+++ emacs.d/robenkleene/packages/robenkleene-god-mode.el
@@ -5,6 +5,12 @@
 (use-package god-mode
   :bind
   ("C-z" . god-mode-all)
+  :init
+  (global-hl-line-mode)
+  (make-variable-buffer-local 'global-hl-line-mode)
+  (add-hook 'god-mode-enabled-hook (lambda () (setq global-hl-line-mode t)))
+  (add-hook 'god-mode-disabled-hook (lambda () (setq global-hl-line-mode nil)))
+  (god-mode-all)
   :config
   (define-key god-local-mode-map (kbd "i") 'god-local-mode)
   (global-set-key (kbd "C-x C-o") 'other-window)
@@ -12,7 +18,6 @@
   (global-set-key (kbd "C-x C-2") 'split-window-below)
   (global-set-key (kbd "C-x C-3") 'split-window-right)
   (global-set-key (kbd "C-x C-0") 'delete-window)
-
   )
 
 (provide 'robenkleene-god-mode)
