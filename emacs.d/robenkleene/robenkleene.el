ec928d08369e3aa6a33e9c82785ae53a9f0b54ea
Roben Kleene <robenk@gmail.com>
2018-04-02 15:35:44 -0400

    Fix bug by moving better-defaults higher

--- emacs.d/robenkleene/robenkleene.el
+++ emacs.d/robenkleene/robenkleene.el
@@ -22,9 +22,9 @@
 ;; Packages
 ;; `git-gutter' moves the frame in an annoying way
 ;; `show-trailing-whitespace' isn't compatible with `fill-column-indicator'
+(require 'robenkleene-better-defaults)
 (require 'robenkleene-god-mode)
 (require 'robenkleene-aggressive-indent)
-(require 'robenkleene-better-defaults)
 (require 'robenkleene-editorconfig)
 (require 'robenkleene-flycheck)
 (require 'robenkleene-ido-completing-read)
