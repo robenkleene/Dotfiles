3eb08e5b8624db9f6ec71638686f12d1c2310a5b
Roben Kleene <robenk@gmail.com>
2018-04-02 12:59:47 -0400

    Re-enabled god-mode

--- emacs.d/robenkleene/robenkleene.el
+++ emacs.d/robenkleene/robenkleene.el
@@ -22,7 +22,7 @@
 ;; Packages
 ;; `git-gutter' moves the frame in an annoying way
 ;; `show-trailing-whitespace' isn't compatible with `fill-column-indicator'
-;; (require 'robenkleene-god-mode)
+(require 'robenkleene-god-mode)
 (require 'robenkleene-aggressive-indent)
 (require 'robenkleene-better-defaults)
 (require 'robenkleene-editorconfig)
