c633bf2e875ffe791a8f8b36b4e5949a22bcbb72
Roben Kleene <contact@robenkleene.com>
2018-08-05 14:16:38 -0400

    Change ledger file type

--- emacs.d/robenkleene/languages/robenkleene-ledger.el
+++ emacs.d/robenkleene/languages/robenkleene-ledger.el
@@ -5,7 +5,7 @@
 (eval-when-compile (require 'use-package))
 (use-package ledger-mode
   :ensure t
-  :mode "\\.dat\\'")
+  :mode "\\.ledger\\'")
 
 (provide 'robenkleene-ledger)
 ;;; robenkleene-ledger.el ends here
