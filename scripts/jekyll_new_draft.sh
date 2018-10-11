5aa45eea6d3d4703e6fc8cd3869f84865f1034bb
Roben Kleene <contact@robenkleene.com>
2018-10-10 20:26:23 -0400

    Improve formatting

--- scripts/jekyll_new_draft.sh
+++ scripts/jekyll_new_draft.sh
@@ -3,13 +3,16 @@
 set -e
 
 if [ "$#" -ne 1 ]; then
-    echo "Wrong number of arguments" >&2
-    exit 1
+  echo "Wrong number of arguments" >&2
+  exit 1
 fi
 
 # Make all lowercase and replace hyphens with spaces
 title=$1
-slug=$(echo "$1" | tr -dc '[:alnum:]\r\n.\-/ ' | tr -s ' ' | tr '[A-Z]' '[a-z]' | tr ' ' '-')
+slug=$(echo "$1" |
+  tr -dc '[:alnum:]\r\n.\-/ ' |
+  tr -s ' ' | tr '[A-Z]' '[a-z]' |
+  tr ' ' '-')
 
 drafts_directory=~/Development/Projects/Web/robenkleene.github.io/_drafts
 if [[ ! -d "$drafts_directory" ]]; then
@@ -27,8 +30,8 @@ categories:
 ---
 "
 
-echo "$content" > "$post_path"
-cat >> "$post_path"
+echo "$content" >"$post_path"
+cat >>"$post_path"
 
 destination_post_path="$drafts_directory/$today-$slug.md"
 mv -n "$post_path" "$destination_post_path"
