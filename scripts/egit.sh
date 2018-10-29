c5901d977eabd09975ec4add597ae87f5cfe07b8
Roben Kleene <contact@robenkleene.com>
2018-10-29 14:57:54 -0400

    Add Xcode UserData

--- scripts/egit.sh
+++ scripts/egit.sh
@@ -3,7 +3,7 @@
 EGITREPOS=~/Development/Dotfiles/:~/Development/Snippets/:~/Development/Archive/:~/Development/Settings/:~/Documentation/design-references/:~/Documentation/development-references/:~/Documentation/music-production-references/:~/Documentation/software-references/:~/Development/Scratch/:~/Documentation/notes/:~/Development/Planning/personal-projects:~/Development/Planning/thepotionlab-development:~/Development/Planning/thepotionlab-projects
 
 if [ "$(uname)" == "Darwin" ]; then
-  EGITREPOS=$EGITREPOS:~/Library/Services/:~/Library/Application\ Support/TextMate/Bundles/Roben\ Kleene.tmbundle/:~/Library/Scripts/:~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+  EGITREPOS=$EGITREPOS:~/Library/Services/:~/Library/Application\ Support/TextMate/Bundles/Roben\ Kleene.tmbundle/:~/Library/Scripts/:~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/:~/Library/Developer/Xcode/UserData/
 fi
 
 IFS=':' read -ra repos <<< "$EGITREPOS"
