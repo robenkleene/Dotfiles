1b39650dc9df5137ed0dcf2b4949373aa692ee77
Roben Kleene <contact@robenkleene.com>
2018-11-16 15:46:54 -0500

    Update egit

--- scripts/egit.sh
+++ scripts/egit.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-EGITREPOS=~/Development/Dotfiles/:~/Development/Snippets/:~/Development/Archive/:~/Development/Settings/:~/Documentation/design-references/:~/Documentation/development-references/:~/Documentation/music-production-references/:~/Documentation/software-references/:~/Development/Scratch/:~/Documentation/notes/:~/Documentation/Planning/personal-projects:~/Documentation/Planning/thepotionlab-development:~/Documentation/Planning/thepotionlab-projects
-
+# System Repos
+EGITREPOS=~/Development/Dotfiles/
 if [ "$(uname)" == "Darwin" ]; then
-  EGITREPOS=$EGITREPOS:~/Library/Services/:~/Library/Application\ Support/TextMate/Bundles/Roben\ Kleene.tmbundle/:~/Library/Scripts/:~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/:~/Library/Developer/Xcode/UserData/
+  EGITREPOS+=:~/Library/Services/:~/Library/Application\ Support/TextMate/Bundles/Roben\ Kleene.tmbundle/:~/Library/Scripts/:~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/:~/Library/Developer/Xcode/UserData/
 fi
+
+# General Text Repos
+EGITREPOS+=:~/Development/Snippets/:~/Development/Archive/:~/Development/Scratch/:~/Development/Settings/
+# Specific Text Repos
+EGITREPOS+=:~/Documentation/notes/:~/Documentation/design-references/:~/Documentation/development-references/:~/Documentation/music-production-references/:~/Documentation/software-references/:~/Documentation/Planning/personal-projects:~/Documentation/Planning/thepotionlab-development:~/Documentation/Planning/thepotionlab-projects
 
 IFS=':' read -ra repos <<< "$EGITREPOS"
 
