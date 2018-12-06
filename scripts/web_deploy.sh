b4c4184a3120ccdaf01b4ed327a08cb6bfd12b06
Roben Kleene <contact@robenkleene.com>
2018-12-05 19:51:06 -0500

    Work on web deploy script

--- scripts/web_deploy.sh
+++ scripts/web_deploy.sh
@@ -10,11 +10,18 @@ if [[ -f ".web_deploy" ]]; then
 fi
 
 force=false
+deploy_local=false
 set_args() {
-  while getopts ":s:l:fh" option; do
+  while getopts ":s:d:p:lfh" option; do
     case "$option" in
+      d)
+        deploy_paths+=("$OPTARG")
+        ;;
+      p)
+        local_paths+=("$OPTARG")
+        ;;
       l)
-        local_path="$OPTARG"
+        deploy_local=true
         ;;
       f)
         force=true
@@ -41,6 +48,26 @@ if [[ -z "$servers" && ! $local_sync ]]; then
   exit 1
 fi
 
+# TODO: The count of the `deploy_paths` has to match the count of the
+# `local_paths`
+
+${#array[@]}
+
+if [[ -z "$deploy_paths" ]]; then
+  echo "Missing deploy paths with -d option" >&2
+  exit 1
+fi
+
+if [[ -z "$local_paths" ]]; then
+  echo "Missing local paths with -l option" >&2
+  exit 1
+fi
+
+if [[ ${#local_paths[@]} != ${#deploy_paths[@]} ]]; then
+  echo "Number of local paths ${#local_paths[@]} doesn't match deploy paths ${#deploy_paths[@]}" >&2
+  exit 1
+fi
+
 dry_run="--dry-run"
 if $force; then
   dry_run=""
@@ -48,16 +75,12 @@ else
   echo "Dry Run\n"
 fi
 
-script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-project_dir="$script_dir/../"
-destination_path="/var/www/html/robenkleene.com/"
-
-if $local_sync; then
-  
+if $deploy_local; then
+  echo "Got here"
 fi
 
 for server in "${servers[@]}"; do
-  server_path="$server:"
+  server_path="$server:$deploy_path"
   # Sync base website
   echo "Syncing robenkleene.com"
   rsync --rsh=ssh --verbose --archive $dry_run --delete \
