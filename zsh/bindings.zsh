7b6785cb01fbf83a69462c3ecb563386732a11a6
Roben Kleene <contact@robenkleene.com>
2018-12-06 12:10:23 -0500

    Clean up function names

--- zsh/bindings.zsh
+++ zsh/bindings.zsh
@@ -8,18 +8,18 @@ bindkey -e
 
 # Use bash-style `backwards-kill-word`
 autoload -Uz select-word-style
-bash_backward_kill_word() {
+_bash_backward_kill_word() {
   select-word-style bash
   WORDCHARS='*?[]~\!#$%^(){}<>|`@#$%^*()+:?' zle backward-kill-word
   select-word-style normal
 }
-zle -N bash_backward_kill_word
-bindkey "^[^?" bash_backward_kill_word
-bindkey "^[^H" bash_backward_kill_word
+zle -N _bash_backward_kill_word
+bindkey "^[^?" _bash_backward_kill_word
+bindkey "^[^H" _bash_backward_kill_word
+
 # Edit in editor
 autoload -z edit-command-line
 zle -N edit-command-line
-
 _custom_edit_command_line() {
   EDITOR="$VIM_COMMAND -c 'DisableBackgrounding'" zle edit-command-line
 }
@@ -37,22 +37,22 @@ zmodload zsh/complist
 bindkey -M menuselect '^[[Z' reverse-menu-complete
 
 # Clipboard
-system-copy-region-as-kill() {
+_system-copy-region-as-kill() {
   zle copy-region-as-kill
   echo "$CUTBUFFER" | safecopy
 }
-zle -N system-copy-region-as-kill
-system-backward-kill-word() {
+zle -N _system-copy-region-as-kill
+_system-backward-kill-word() {
   zle backward-kill-word
   # print -rn "$CUTBUFFER" | safecopy
   echo "$CUTBUFFER" | safecopy
 }
-zle -N system-backward-kill-word
-system-yank() {
+zle -N _system-backward-kill-word
+_system-yank() {
   CUTBUFFER=$(safepaste)
   zle yank
 }
-zle -N system-yank
-bindkey -e '\ew' system-copy-region-as-kill
-bindkey -e '^W' system-backward-kill-word
-bindkey -e '^Y' system-yank
+zle -N _system-yank
+bindkey -e '\ew' _system-copy-region-as-kill
+bindkey -e '^W' _system-backward-kill-word
+bindkey -e '^Y' _system-yank
