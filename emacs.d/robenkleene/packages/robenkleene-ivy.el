ec281c9f98dd399e79103fc656c3b837d6fddc65
Roben Kleene <contact@robenkleene.com>
2019-01-14 07:16:59 -0500

    Use better rg function

--- emacs.d/robenkleene/packages/robenkleene-ivy.el
+++ emacs.d/robenkleene/packages/robenkleene-ivy.el
@@ -7,9 +7,22 @@
   :bind
   (:map robenkleene/leader-map
         ("l" . swiper)
-        ("a" . counsel-rg)
+        ("a" . robenkleene/counsel-rg)
         ("r" . ivy-resume)
         )
+  :config
+  (defun robenkleene/counsel-rg (dir)
+    "Find file recursively in DIR."
+    (interactive
+     (list
+      (if current-prefix-arg
+          (read-directory-name "Base directory: ")
+        (expand-file-name default-directory)
+        )
+      )
+     )
+    (counsel-rg nil dir)
+    )
   )
 
 (provide 'robenkleene-ivy)
