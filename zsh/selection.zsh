c10bbdbdadfb7c459ac03e20c63eae5d101f2b24
Roben Kleene <contact@robenkleene.com>
2019-01-17 11:44:26 -0600

    Improve selection

--- zsh/selection.zsh
+++ zsh/selection.zsh
@@ -9,6 +9,7 @@ __selection_delete() {
 }
 
 __selection_deselect() {
+  zle set-mark-command
   ((REGION_ACTIVE = 0))
   local widget_name=$1
   shift
@@ -26,45 +27,45 @@ for seq mode widget (
 
 # All are ordered left, right, up, down
 # Arrow
-$'\EOD' deselect backward-char
-$'\EOC' deselect forward-char
-$'\EOA' deselect up-line-or-history
-$'\EOB' deselect down-line-or-history
+$'\EOD' 'deselect' 'backward-char'
+$'\EOC' 'deselect' 'forward-char'
+$'\EOA' 'deselect' 'up-line-or-history'
+$'\EOB' 'deselect' 'down-line-or-history'
 
 # Arrow & Meta
 # Meta left/right are mapped to `C-b`/`C-f`
 
 # Arrow & Shift
-$'\E[1;2D' select backward-char
-$'\E[1;2C' select forward-char
-$'\E[1;2A' select up-line-or-history
-$'\E[1;2B' select down-line-or-history
+$'\E[1;2D' 'select' 'backward-char'
+$'\E[1;2C' 'select' 'forward-char'
+$'\E[1;2A' 'select' 'up-line-or-history'
+$'\E[1;2B' 'select' 'down-line-or-history'
 
 # Arrow, Meta & Shift
-$'\E[1;6D' select backward-word
-$'\E[1;6C' select forward-word
+$'\E[1;6D' 'select' 'backward-word'
+$'\E[1;6C' 'select' 'forward-word'
 
 # Movement
 
 # Character
-$'^B' deselect backward-char
-$'^F' deselect forward-char
+$'^B' 'deselect' 'backward-char'
+$'^F' 'deselect' 'forward-char'
 
 # Character Select
-$'\EB' select backward-char
-$'\EF' select forward-char
+$'\EB' 'select' 'backward-char'
+$'\EF' 'select' 'forward-char'
 
 # Word
-$'\Eb' deselect backward-word
-$'\Ef' deselect forward-word
+$'\Eb' 'deselect' 'backward-word'
+$'\Ef' 'deselect' 'forward-word'
 
 # Word Select
-$'\EB' select backward-word
-$'\EB' select forward-word
+$'\EB' 'select' 'backward-word'
+$'\EB' 'select' 'forward-word'
 
 # Line
-$'^E' deselect end-of-line
-$'^A' deselect beginning-of-line
+$'^E' 'deselect' 'end-of-line'
+$'^A' 'deselect' 'beginning-of-line'
 
 # Line Select
 # These aren't setup in my terminal
@@ -72,17 +73,17 @@ $'^A' deselect beginning-of-line
 # Delete
 
 # Character
-$'^D' delete delete-char-or-list
-$'^H' delete backward-delete-char
+$'^D' 'delete' 'delete-char-or-list'
+$'^H' 'delete' 'backward-delete-char'
 
 # Word
-$'\Ed' delete kill-word
-$'^W' delete _system_kill_region_or_backward_kill_word
-$'\E^?' delete _bash_backward_kill_word
+$'\Ed' 'delete' 'kill-word'
+$'^W' 'delete' '_system_kill_region_or_backward_kill_word'
+$'\E^?' 'delete' '_bash_backward_kill_word'
 
 # Line
-$'^U' delete backward-kill-line
-$'^K' delete kill-line
+$'^U' 'delete' 'backward-kill-line'
+$'^K' 'delete' 'kill-line'
 ) {
 
   local function_name=__override_${mode}_${widget}
