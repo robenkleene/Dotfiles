a3c6419eb124ddf5cf69dc30ec4a5a18c3a1346b
Roben Kleene <contact@robenkleene.com>
2019-01-17 10:27:19 -0600

    Clean up selection

--- zsh/selection.zsh
+++ zsh/selection.zsh
@@ -23,37 +23,37 @@ __selection_select() {
   zle $widget_name -- $@
 }
 
-for key     kcap   seq        mode   widget (
-    sleft   kLFT   $'\e[1;2D' select   backward-char
-    sright  kRIT   $'\e[1;2C' select   forward-char
-    sup     kri    $'\e[1;2A' select   up-line-or-history
-    sdown   kind   $'\e[1;2B' select   down-line-or-history
+for kcap   seq        mode     widget (
+    kLFT   $'\e[1;2D' select   backward-char
+    kRIT   $'\e[1;2C' select   forward-char
+    kri    $'\e[1;2A' select   up-line-or-history
+    kind   $'\e[1;2B' select   down-line-or-history
 
-    send    kEND   $'\E[1;2F' select   end-of-line
-    send2   x      $'\E[4;2~' select   end-of-line
+    kEND   $'\E[1;2F' select   end-of-line
+    x      $'\E[4;2~' select   end-of-line
 
-    shome   kHOM   $'\E[1;2H' select   beginning-of-line
-    shome2  x      $'\E[1;2~' select   beginning-of-line
+    kHOM   $'\E[1;2H' select   beginning-of-line
+    x      $'\E[1;2~' select   beginning-of-line
 
-    left    kcub1  $'\EOD'    deselect backward-char
-    right   kcuf1  $'\EOC'    deselect forward-char
+    kcub1  $'\EOD'    deselect backward-char
+    kcuf1  $'\EOC'    deselect forward-char
 
-    end     kend   $'\EOF'    deselect end-of-line
-    end2    x      $'\E4~'    deselect end-of-line
+    kend   $'\EOF'    deselect end-of-line
+    x      $'\E4~'    deselect end-of-line
 
-    home    khome  $'\EOH'    deselect beginning-of-line
-    home2   x      $'\E1~'    deselect beginning-of-line
+    khome  $'\EOH'    deselect beginning-of-line
+    x      $'\E1~'    deselect beginning-of-line
 
-    csleft  x      $'\E[1;6D' select   backward-word
-    csright x      $'\E[1;6C' select   forward-word
-    csend   x      $'\E[1;6F' select   end-of-line
-    cshome  x      $'\E[1;6H' select   beginning-of-line
+    x      $'\E[1;6D' select   backward-word
+    x      $'\E[1;6C' select   forward-word
+    x      $'\E[1;6F' select   end-of-line
+    x      $'\E[1;6H' select   beginning-of-line
 
-    cleft   x      $'\E[1;5D' deselect backward-word
-    cright  x      $'\E[1;5C' deselect forward-word
+    x      $'\E[1;5D' deselect backward-word
+    x      $'\E[1;5C' deselect forward-word
 
-    del     kdch1   $'\E[3~'  delete delete-char
-    bs      x       $'^?'     delete backward-delete-char
+    kdch1   $'\E[3~'  delete delete-char
+    x       $'^?'     delete backward-delete-char
 
   ) {
 
