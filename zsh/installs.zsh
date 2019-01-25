6342572c18479f26aca8921b5b88158c835bb890
Roben Kleene <contact@robenkleene.com>
2019-01-25 08:08:28 -0500

    Autochange ruby

--- zsh/installs.zsh
+++ zsh/installs.zsh
@@ -7,11 +7,13 @@ fi
 if [[ "$(uname)" = "Darwin" ]]; then
   if [[ -f /usr/local/share/chruby/chruby.sh ]]; then
     source /usr/local/share/chruby/chruby.sh
+    source /usr/local/share/chruby/auto.sh
     chruby ruby-2.5.3
   fi
 elif [[ "$(uname)" = "Linux" ]]; then
   if [[ -f /home/linuxbrew/.linuxbrew/opt/chruby/share/chruby/chruby.sh ]]; then
     source /home/linuxbrew/.linuxbrew/opt/chruby/share/chruby/chruby.sh
+    source /usr/local/share/chruby/auto.sh
     chruby ruby-2.5.3
   fi
 fi
