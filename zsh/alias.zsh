7c162cbfea716cdadce9dcb637d9648fe2f67fa8
Roben Kleene <contact@robenkleene.com>
2019-02-04 13:09:12 -0500

    Make rg page automatically

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -37,10 +37,6 @@ alias strs='ssh_tmux_restore_start'
 # Override
 
 # rg
-# Adding `--line-number` interferes with piping to `rg`, but without it piping
-# to `vim` will also not have line numbers, so it needs to be turned on here.
-# To work around this, when piping to it use the `A` global alias.
-alias rg="rg --with-filename --smart-case --line-number --colors 'match:fg:black' --colors 'match:bg:cyan' --colors 'path:fg:cyan' --colors 'line:fg:white'"
 # mitmproxy
 # Supposedly these can be set in a `~/.mitmproxy/config.yaml`, it wasn't
 # working with:
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -12,6 +12,18 @@ _robenkleene_ack_lines_no_color() {
 
 # Commands
 
+# Adding `--line-number` interferes with piping to `rg`, but without it piping
+# to `vim` will also not have line numbers, so it needs to be turned on here.
+# To work around this, when piping to it use the `A` global alias.
+# alias rg="rg --with-filename --smart-case --line-number --colors 'match:fg:black' --colors 'match:bg:cyan' --colors 'path:fg:cyan' --colors 'line:fg:white'"
+rg() {
+    if [ -t 1 ]; then
+        command rg --with-filename --smart-case --line-number --colors 'match:fg:black' --colors 'match:bg:cyan' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@" | less -RFX
+    else
+        command rg "$@"
+    fi
+}
+
 egitn() {
   local gitnext=$(egit -n)
   if [[ -n "$gitnext" ]]; then
