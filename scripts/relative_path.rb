39c712896cc4631775ec094fc1886cd29e7b5975
Roben Kleene <contact@robenkleene.com>
2019-02-05 06:40:29 -0500

    Update relative path

--- scripts/relative_path.rb
+++ scripts/relative_path.rb
@@ -2,10 +2,10 @@
 
 require 'pathname'
 
-abort("Wrong number of arguments") unless ARGV.count != 2
+abort("Wrong number of arguments") unless ARGV.count == 2
 file_one = ARGV[0]
 file_two = ARGV[1]
 abort("#{file_one} does not exist") unless File.exists?(file_one)
 abort("#{file_two} does not exist") unless File.exists?(file_two)
 
-puts Pathname.new(file_one).relative_path_from(Pathname.new(file_two)).to_s
+puts Pathname.new(file_two).relative_path_from(Pathname.new(file_one)).to_s
