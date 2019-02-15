d0fc1ef035287bdadec8f5cf294db43c84b7a0a3
Roben Kleene <contact@robenkleene.com>
2019-02-15 07:25:20 -0500

    Update journal location

--- scripts/journal_newest.sh
+++ scripts/journal_newest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-journal_directory=~/Documents/Text/Journal
+journal_directory=~/Dropbox/Collection/Text/Journal
 filename=$(ls "$journal_directory" | tail -n 1 | tr -d '\n')
 echo "$journal_directory/$filename"
