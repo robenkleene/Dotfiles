7225a882fddb82cbc93eea4bfd23c62ad6106ab3
Roben Kleene <contact@robenkleene.com>
2019-03-10 16:18:03 -0400

    Add dump tags

---
+++ scripts/dump_tags.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+while [[ $PWD != / ]]; do
+  if [[ -f tags ]]; then
+    break
+  fi
+  cd ..
+done
+
+emacs="false"
+if ls | grep "^TAGS$"; then
+  emacs="true"
+fi
+
+if "$emacs" == "true"; then
+  echo "Emacs"
+else
+  ~/Development/Dotfiles/vim/plugged/fzf.vim/bin/tags.pl tags |\
+    cut -f 1 | cut -f 1 -d$ | cut -f 1 -d' '
+fi
+
+if [[ $PWD == / ]]; then
+  echo "No tags file found" >&2
+fi
