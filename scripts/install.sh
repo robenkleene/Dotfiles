77892fb109136cf234a0b87c03a38fb22b742601
Roben Kleene <contact@robenkleene.com>
2019-03-10 15:44:53 -0400

    Clean up

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,32 +6,33 @@ INSTALL_DIRECTORY_NAME="scripts" # The name of the directory to install from.
 DIRECTORY=${PWD}
 DIRECTORY_NAME=${PWD##*/}
 if [ ! $DIRECTORY_NAME == $INSTALL_DIRECTORY_NAME ]; then
-	echo "ERROR: This directory \"$DIRECTORY_NAME\" doesn't match $INSTALL_DIRECTORY_NAME."
-	echo "This script only runs from the $INSTALL_DIRECTORY_NAME directory."
-	exit 1
+  echo "ERROR: This directory \"$DIRECTORY_NAME\" doesn't match $INSTALL_DIRECTORY_NAME."
+  echo "This script only runs from the $INSTALL_DIRECTORY_NAME directory."
+  exit 1
 fi
 
-function MakeSymlink {
-	FILENAME=$1
-	EXECUTABLE_NAME="${FILENAME%.*}"
+function MakeSymlink() {
+  FILENAME=$1
+  EXECUTABLE_NAME="${FILENAME%.*}"
   DESTINATION_DIRECTORY="$HOME/.bin"
-	[ -d "$DESTINATION_DIRECTORY" ] || mkdir "$DESTINATION_DIRECTORY"
+  [ -d "$DESTINATION_DIRECTORY" ] || mkdir "$DESTINATION_DIRECTORY"
   DESTINATION=$DESTINATION_DIRECTORY/$EXECUTABLE_NAME
-	if [ -f $DESTINATION ] || [ -d $DESTINATION ]; then
-		echo "$DESTINATION skipped because it already exists."
-	else
-		ln -s $DIRECTORY/$thisFILE $DESTINATION
-	fi
+  if [ -f $DESTINATION ] || [ -d $DESTINATION ]; then
+    echo "$DESTINATION skipped because it already exists."
+  else
+    ln -s $DIRECTORY/$thisFILE $DESTINATION
+  fi
 }
 
 shopt -s nullglob
-for thisFILE in *; do 
-	# Exclude this install script and directories
- 	if [[ ! $thisFILE =~ "install.sh" ]] && 
-		[[ ! $thisFILE == "tags" ]] &&
-		[ ! -d $thisFILE ]; then
-		MakeSymlink $thisFILE
- 	fi
+for thisFILE in *; do
+  # Exclude this install script and directories
+  if [[ ! $thisFILE =~ "install.sh" ]] &&
+    [[ ! $thisFILE == "TAGS" ]] &&
+    [[ ! $thisFILE == "tags" ]] &&
+    [ ! -d $thisFILE ]; then
+    MakeSymlink $thisFILE
+  fi
 done
 
 # Symlink the no bin directory so scripts can reference them
