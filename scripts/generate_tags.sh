88b86a8cd27f66b337c8c1856d2d94f26b6f9865
Roben Kleene <contact@robenkleene.com>
2019-03-10 16:03:03 -0400

    Make script handle filenames

--- scripts/generate_tags.sh
+++ scripts/generate_tags.sh
@@ -2,6 +2,9 @@
 
 # set -e
 
+FILENAME="tags"
+EMACS_FILENAME="TAGS"
+
 emacs=false
 while getopts ":eh" option; do
   case "$option" in
@@ -23,19 +26,37 @@ while getopts ":eh" option; do
   esac
 done
 
+correct_filenames() {
+  src="$1"
+  dst="$2"
+  # Can't test filenames case-sensitive this way
+  # if [[ ! -f $dst ]]; then
+  if [[ -f $src ]]; then
+    mv $src $dst
+  else
+    echo "Error tags file missing" >&2
+    exit 1
+  fi
+  # fi
+}
+
 process_dir() {
   if [[ -f "Gemfile" ]]; then
     if [[ "$emacs" == "true" ]]; then
       ripper-tags --emacs -R
+      correct_filenames $FILENAME $EMACS_FILENAME
     else
       ripper-tags -R
+      correct_filenames $EMACS_FILENAME $FILENAME
     fi
     return 0
   elif [[ -d ".git" ]]; then
     if [[ "$emacs" == "true" ]]; then
       ctags -e
+      correct_filenames $FILENAME $EMACS_FILENAME
     else
       ctags
+      correct_filenames $EMACS_FILENAME $FILENAME
     fi
     return 0
   fi
@@ -43,13 +64,13 @@ process_dir() {
   return 1
 }
 
-while [[ $PWD != / ]] ; do
-    process_dir
-    status=$?
-    if [[ $status -eq 0 ]]; then
-      break
-    fi
-    cd ..
+while [[ $PWD != / ]]; do
+  process_dir
+  status=$?
+  if [[ $status -eq 0 ]]; then
+    break
+  fi
+  cd ..
 done
 
 if [[ $PWD == / ]]; then
