db89eebce6f36056bfbf6a5d4a50f9dd8cee247a
Roben Kleene <contact@robenkleene.com>
2019-03-10 20:13:16 -0400

    Fix dump tags

--- scripts/dump_tags.sh
+++ scripts/dump_tags.sh
@@ -15,7 +15,7 @@ if ls | grep "^TAGS$" 2>&1 >/dev/null; then
 fi
 
 if "$emacs" == "true"; then
-  cat TAGS | grep '' | cut -f 1 -d $'' | awk '{$1=$1};1'
+  cat TAGS | grep '' | cut -f 2 -d $'' | cut -f1 -d $''
 else
   ~/Development/Dotfiles/vim/plugged/fzf.vim/bin/tags.pl tags |\
     cut -f 1 | cut -f 1 -d$ | cut -f 1 -d' '
