d354b1261760b87f3f41a540684b752e96ab1de5
Roben Kleene <contact@robenkleene.com>
2019-03-13 20:02:46 -0400

    Re-org dired

--- emacs.d/robenkleene/config/robenkleene-dired.el
+++ emacs.d/robenkleene/config/robenkleene-dired.el
@@ -5,36 +5,31 @@
 ;; Load `C-x C-j' command for `dired-jump'
 (require 'dired-x)
 
-;; Hide details (show them with "\("
-(add-hook 'dired-mode-hook 'dired-hide-details-mode)
-;; Auto-refresh on file system change
-(add-hook 'dired-mode-hook 'auto-revert-mode)
+(with-eval-after-load 'dired
+  (defun robenkleene/dired-toggle-hidden ()
+    "Show/hide hidden files except . and .."
+    (interactive)
+    (setq dired-omit-mode (not dired-omit-mode))
+    (revert-buffer)
+    )
+  (define-key dired-mode-map (kbd "C-c g h") 'robenkleene/dired-toggle-hidden)
+  (define-key dired-mode-map (kbd "<mouse-2>") 'dired-find-file)
+  ;; Suppress error message
+  (defvar dired-use-ls-dired)
+  (setq dired-use-ls-dired nil)
 
-;; Hack to allow `SPC' to work as leader in Dired buffers
-(define-key dired-mode-map (kbd "SPC") nil)
-
-;; Omit hidden files
-(require 'dired-x)
-(setq-default dired-omit-mode t)
-(setq-default dired-omit-files
-              (concat dired-omit-files "\\|^\\..+$"))
-
-;; Suppress error message
-(defvar dired-use-ls-dired)
-(setq dired-use-ls-dired nil)
-
+  ;; Omit hidden files
+  (setq-default dired-omit-mode t)
+  (setq-default dired-omit-files
+                (concat dired-omit-files "\\|^\\..+$"))
+  )
 (add-hook 'dired-mode-hook
           (lambda ()
-            (define-key dired-mode-map (kbd "C-c g h") 'robenkleene/dired-toggle-hidden)
-            (define-key dired-mode-map (kbd "<mouse-2>") 'dired-find-file)
+            ;; Hide details (showthem with "\("
+            (dired-hide-details-mode)
+            ;; Auto-refresh on file system change
+            (auto-revert-mode)
             ))
-
-(defun robenkleene/dired-toggle-hidden ()
-  "Show/hide hidden files except . and .."
-  (interactive)
-  (setq dired-omit-mode (not dired-omit-mode))
-  (revert-buffer)
-  )
 
 (provide 'robenkleene-dired)
 ;;; robenkleene-dired.el ends here
