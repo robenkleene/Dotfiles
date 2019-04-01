0a0638067dbc9f4920662918c588903730886da0
Roben Kleene <contact@robenkleene.com>
2019-04-01 12:31:10 -0400

    Fix open latest

--- scripts/journal_newest.sh
+++ scripts/journal_newest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 journal_directory="$1"
-filename=$(find "$journal_directory" -name '[[:digit:]]*' | tail -n 1 | tr -d '\n')
+filename=$(find -s "$journal_directory" -name '[[:digit:]]*' | tail -n 1 | tr -d '\n')
 echo "$filename"
