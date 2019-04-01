83821923f2939cac86fe90a0889d0e5b948f152e
Roben Kleene <contact@robenkleene.com>
2019-04-01 10:59:39 -0400

    Update Journal Script

--- scripts/journal_newest.sh
+++ scripts/journal_newest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 journal_directory="$1"
-filename=$(ls "$journal_directory" | tail -n 1 | tr -d '\n')
-echo "$journal_directory/$filename"
+filename=$(find "$journal_directory" -name '[[:digit:]]*' | tail -n 1 | tr -d '\n')
+echo "$filename"
