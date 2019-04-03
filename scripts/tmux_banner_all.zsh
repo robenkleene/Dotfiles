e0fd91f106bda683a2c1bd19714f47fdacf6a7bb
Roben Kleene <contact@robenkleene.com>
2019-04-03 13:21:54 -0400

    Fix banner

--- scripts/tmux_banner_all.zsh
+++ scripts/tmux_banner_all.zsh
@@ -3,5 +3,5 @@
 # Setting this as `zsh` allows it to be called from AppleScript for some reason
 for i in $(tmux list-sessions -F "#{?session_attached,#S,}" | grep .); do
   # tmux send -t $i tmux-banner ENTER
-  tmux run-shell -t "$i" "tmux-banner" >/dev/null
+  tmux run-shell -t "$i" "~/.bin/tmux_banner" >/dev/null
 done
