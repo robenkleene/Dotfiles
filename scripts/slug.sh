17850ad4b1a15fa4d880e9abc525924317aef553
Roben Kleene <contact@robenkleene.com>
2019-04-09 06:24:03 -0400

    Replace periods

--- scripts/slug.sh
+++ scripts/slug.sh
@@ -6,7 +6,8 @@ set -e
 # `tr -s ' '`: Consolidate spaces to one space
 # `tr '[A-Z]' '[a-z]'`: Lowercase
 # `tr ' ' '-'`: Replace spaces with hyphens
+# `tr '.' '-'`: Replace period with hyphens
 
 tr -dc '[:alnum:]\r\n.\-/ ' |
   tr -s ' ' | tr '[:upper:]' '[:lower:]' |
-  tr ' ' '-'
+  tr ' ' '-' | tr '.' '-'
