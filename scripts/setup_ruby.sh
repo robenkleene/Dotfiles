c00f0a490e006b9a3d37717bd2295e8c6ebd4b3a
Roben Kleene <contact@robenkleene.com>
2019-05-08 12:47:36 -0400

    Update setup Ruby

--- scripts/setup_ruby.sh
+++ scripts/setup_ruby.sh
@@ -87,10 +87,10 @@ ci: lint
 ac: autocorrect
 
 lint:
-	git ls-files '*.rb' -z | xargs -0 rubocop
+	git ls-files "*.rb" "*Rakefile" "*Gemfile" -z | xargs -0 rubocop
 
 autocorrect:
-	git ls-files '*.rb' -z | xargs -0 rubocop -a
+	git ls-files "*.rb" "*Rakefile" "*Gemfile" -z | xargs -0 rubocop -a
 "
   overwrite "Makefile" "$contents"
 }
