165018a59273f60f3e4ecc682c71215c6cf0026e
Roben Kleene <contact@robenkleene.com>
2019-08-16 14:07:16 -0400

    Don't open packages

--- scripts/open_custom.sh
+++ scripts/open_custom.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
+directory_extension() {
+  path="$1"
+  directory=${path##*/}
+  extension="${directory##*.}"
+  [[ $directory != "$extension" ]] && echo -n "$extension"
+}
+
 if [[ $# -eq 0 ]]; then
   # ~/.bin/finder_new
-  open .
+  extension=$(directory_extension "${PWD}")
+  if [[ -z "$extension" ]]; then
+    open .
+  else
+    open -R .
+  fi
 else
   # This has been removed because it isn't compatiable with passing flags to
   # open
