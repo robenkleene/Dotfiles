91be025b70038a1e6437084f89487c3395543992
Roben Kleene <contact@robenkleene.com>
2019-09-04 11:36:19 -0400

    Add full screen command

--- emacs.d/robenkleene/config/robenkleene-mac.el
+++ emacs.d/robenkleene/config/robenkleene-mac.el
@@ -50,6 +50,10 @@
 ;; (define-key robenkleene/mac-bindings-minor-mode-map
 ;;   (kbd "s-c")
 ;;   'kill-ring-save)
+;; (define-key robenkleene/mac-bindings-minor-mode-map (kbd "C-s-f") 'toggle-frame-fullscreen)
+(define-key robenkleene/mac-bindings-minor-mode-map
+  (kbd "<C-s-268632070>")
+  'toggle-frame-fullscreen)
 
 ;; (define-key robenkleene/mac-bindings-minor-mode-map
 ;;   (kbd "<s-up>")
