7635bd03fe4ea14df1e750af3b57a9da27c3092f
Roben Kleene <contact@robenkleene.com>
2019-10-24 14:17:47 -0400

    Add new shell script

---
+++ scripts/tmux_shell_new.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+# if we're already in a shell session, switch back to the previous session
+if [[ -n "$TMUX" ]]; then
+  if tmux display-message -p '#S' | grep "^\d\+$"; then
+    tmux switch-client -l
+    exit 0
+  fi
+fi
+
+unattached_shell=$(tmux list-sessions -F "#{session_name} #{session_attached}" \
+  | grep "^\d\+ 0$" | head -1 | tr -d '\n')
+if [[ -z "$unattached_shell" ]]; then
+  if [[ -n "$TMUX" ]]; then
+    session=$(tmux new-session -d -P | tr -d '\n')
+    tmux switch-client -t "$session" >/dev/null
+  else
+    tmux new-session
+  fi
+else
+  unattached_shell=${unattached_shell::${#unattached_shell}-2}
+  if [[ -n "$TMUX" ]]; then
+    tmux switch-client -t "$unattached_shell"
+  else
+    tmux attach-session -t "$unattached_shell"
+  fi
+fi
