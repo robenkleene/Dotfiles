5fdd38453190b20ab733e21b0eaba150f813d036
Roben Kleene <contact@robenkleene.com>
2019-12-03 13:05:16 -0500

    Add Max

--- scripts/git_pull_all.sh
+++ scripts/git_pull_all.sh
@@ -10,6 +10,8 @@ cd "$HOME/Development/Settings/Source Control/Directories/Text" && \
 if [[ "$(uname)" = "Darwin" ]]; then
   cd "$HOME/Development/Settings/Source Control/Directories/Projects" && \
     ./compare.sh mac.txt
+  cd "$HOME/Development/Settings/Source Control/Directories/Max/" && \
+    ./compare.sh
 elif [[ "$(uname)" = "Linux" ]]; then
   cd "$HOME/Development/Settings/Source Control/Directories/Projects" && \
     ./compare.sh linux.txt
@@ -17,3 +19,6 @@ fi
 
 ~/.bin/sgitt -l
 ~/.bin/sgitp -l
+if [[ "$(uname)" = "Darwin" ]]; then
+  ~/.bin/sgitm -l
+fi
---
+++ scripts/sgitm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "/Users/Shared/Max 8/Packages" && "$HOME/.bin/sgit" "$@"
