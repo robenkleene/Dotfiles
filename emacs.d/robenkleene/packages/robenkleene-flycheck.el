041ee82bf717a388a3b23ce3199af1b4dc40fcbc
Roben Kleene <contact@robenkleene.com>
2020-01-14 18:05:39 -0500

    Whitelist checkers

--- emacs.d/robenkleene/packages/robenkleene-flycheck.el
+++ emacs.d/robenkleene/packages/robenkleene-flycheck.el
@@ -7,6 +7,31 @@
   :init
   (global-flycheck-mode)
   (setq flycheck-display-errors-delay 0.1)
+  (setq flycheck-checkers
+        '(
+          c/c++-clang
+          css-stylelint
+          cuda-nvcc
+          cwl
+          d-dmd
+          dockerfile-hadolint
+          emacs-lisp
+          emacs-lisp-checkdoc
+          erlang-rebar3
+          html-tidy
+          javascript-eslint
+          proselint
+          ruby-rubocop
+          ruby
+          scss-stylelint
+          sh-bash
+          sh-posix-dash
+          sh-posix-bash
+          sh-zsh
+          sh-shellcheck
+          slim
+          xml-xmllint
+          ))
   )
 
 (provide 'robenkleene-flycheck)
