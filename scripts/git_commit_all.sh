55f443b32c2584480578445b8e16b156f465fdc3
Roben Kleene <contact@robenkleene.com>
2020-01-16 16:01:35 -0500

    Add git commit all

---
+++ scripts/git_commit_all.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+source ~/.bin/nobin/_giterator.sh
+
+message="$1"
+if [[ -z "$message" ]]; then
+  message="Update"
+fi
+
+nothing_to_commit=$(commit_status)
+
+if [ -n "$message" ] && [ "$nothing_to_commit" = "false" ]; then
+  git add -A :/ && git commit -m "$message"
+  nothing_to_commit=$(commit_status)
+fi
+
+if [ "$nothing_to_commit" = "true" ]; then
+  if ! git diff --exit-code origin/master..master >/dev/null; then
+    git push
+  fi
+fi
+
