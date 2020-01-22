e15b1d345d96000f748e4823b37f74860a5ca5bb
Roben Kleene <contact@robenkleene.com>
2020-01-21 19:41:11 -0500

    Fix backup line

--- scripts/backup_text.sh
+++ scripts/backup_text.sh
@@ -27,6 +27,9 @@ backup_directory=$backup_root_directory$today
 mkdir -p "$backup_directory"
 archive_file=$(mktemp "$backup_directory/$today-ArchivedText.XXXX")
 cat > "$archive_file"
+# Add a new line to the end of the file if it's missing, without this `wc` will
+# say `0` lines for `1` line.
+sed -i '' -e '$a\' "$archive_file"
 destination_archive_file=$archive_file.txt
 mv -n "$archive_file" "$destination_archive_file"
 if [[ -f "$archive_file" ]]; then
