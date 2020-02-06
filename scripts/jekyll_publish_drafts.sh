550ed40a21f1cbb59af638f41f5d8cc6c2c1df87
Roben Kleene <contact@robenkleene.com>
2020-02-06 12:49:53 -0500

    Make publishing drafts change date to today

--- scripts/jekyll_publish_drafts.sh
+++ scripts/jekyll_publish_drafts.sh
@@ -10,15 +10,28 @@ if [[ ! -d "$drafts_directory" ]]; then
   exit 1
 fi
 
+
 if [[ ! -d "$posts_directory" ]]; then
   echo "$posts_directory is not a directory" >&2
   exit 1
 fi
 
 shopt -s nullglob
-for markdown_file in "$drafts_directory"/*.md; do
-  mv -n "$markdown_file" "$posts_directory"
-  if [[ -f "$markdown_file" ]]; then
-    echo "Moving $markdown_file failed" >&2
+for path in "$drafts_directory"/*.md; do
+  filename=$(basename "$path")
+  if [[ ! $filename =~ ^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])-[^[:space:]]*\.md$ ]]; then
+    echo "$filename is an invalid filename" >&2
+    exit 1
+  fi
+  today=$(date +%Y-%m-%d)
+  new_filename=${today}-${filename:11}
+  if [[ ! $new_filename =~ ^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])-[^[:space:]]*\.md$ ]]; then
+    echo "New $new_filename is an invalid filename" >&2
+    exit 1
+  fi
+  destination="${posts_directory}/${new_filename}"
+  mv -n "$path" "$destination"
+  if [[ -f "$path" ]]; then
+    echo "Moving $path failed" >&2
   fi
 done
