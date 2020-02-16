208822679a954563fe59b91973930f8ec116a82c
Roben Kleene <contact@robenkleene.com>
2020-02-16 20:42:02 +0000

    Make pgrep more portable

--- zsh/startup.zsh
+++ zsh/startup.zsh
@@ -10,11 +10,11 @@
 if tmux ls >/dev/null 2>/dev/null; then
   # Only echo if we aren't already in a tmux session
   if [ -z "$TMUX" ]; then
-    if pgrep -f -q '[Ee]macs.*--bg-daemon'; then
+    if pgrep -f '[Ee]macs.*--bg-daemon' >/dev/null; then
       echo "Emacs server is running"
     fi
 
-    if pgrep -q mysqld; then
+    if pgrep mysqld >/dev/null; then
       echo "MySQL is running"
     fi
 
