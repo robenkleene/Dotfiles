f2c05a7a022ad65f03fd0c6303b1dbcf9a7f83f2
Roben Kleene <contact@robenkleene.com>
2020-02-22 17:14:24 -0500

    Add commented out Brave

--- scripts/nobin/_urls_open.sh
+++ scripts/nobin/_urls_open.sh
@@ -13,6 +13,17 @@ if [[ "$THEURLS" == "{}" ]]; then
   exit 1
 fi
 
+# osascript <<-APPLESCRIPT
+# set URLs to $THEURLS
+# tell application "Brave Browser"
+# 	activate
+# 	open location {URL:item 1 of URLs}
+#   repeat with theURL in rest of the URLs
+#     open location theURL
+#   end repeat
+# end tell
+# APPLESCRIPT
+
 osascript <<-APPLESCRIPT
 set URLs to $THEURLS
 tell application "Safari"
