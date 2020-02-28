a25c450f2c226323eebd6b6b1161f58dbe87c05e
Roben Kleene <contact@robenkleene.com>
2020-02-28 11:10:54 -0500

    Add browser support

--- scripts/url_open.sh
+++ scripts/url_open.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-firefox="false"
-technology="false"
-while getopts ":fth" option; do
+while getopts ":b:h" option; do
   case "$option" in
-    f)
-      firefox="true"
-      ;;
-    t)
-      technology="true"
+    b)
+      browser_flag="$OPTARG"
       ;;
     h)
       echo "Usage: command [-hb] [-a <file>]"
@@ -25,25 +20,16 @@ while getopts ":fth" option; do
   esac
 done
 
-if [[ "$firefox" == "false" && "$technology" == "false" ]]; then
-  if [[ "$BROWSER_FLAG" = "-t" ]]; then
-    technology="true"
-  elif [[ "$BROWSER_FLAG" = "-f" ]]; then
-    firefox="true"
+if [[ -z "$browser_flag" ]]; then
+  if [[ -n "$BROWSER" ]]; then
+    browser_arg="-a$BROWSER"
   fi
-fi
-
-if [[ "$firefox" == "true" ]]; then
-  browser_arg="-a Firefox"
-elif [[ "$technology" == "true" ]]; then
-  # Couldn't figure out how to encode this into a variable with spaces
-  # browser_arg="-a \"Safari Technology Preview\""
-  open -a "Safari" "$(~/.bin/urls -b)"
-  exit 0
-fi
-
-if [[ -n "$1" ]]; then
-  open "$browser_arg" "$(~/.bin/urls -b)"
 else
+  browser_arg="-a$browser_flag"
+fi
+
+if [[ -z "$browser_arg" ]]; then
   open "$(~/.bin/urls -b)"
+else
+  open "$browser_arg" "$(~/.bin/urls -b)"
 fi
