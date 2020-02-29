87363229249f11b51323a93eb2dc67ee4a4abe4f
Roben Kleene <contact@robenkleene.com>
2020-02-29 15:10:07 -0500

    Improve pull request processing script

--- scripts/git_process_pull_request.sh
+++ scripts/git_process_pull_request.sh
@@ -2,7 +2,25 @@
 
 set -e
 
-git checkout master
+branch="$1"
+if [[ -z "$1" ]]; then
+  branch="master"
+else
+  branch="$1"
+fi
+
+if ! git show-ref --verify --quiet refs/heads/"${branch}"; then
+  echo "Branch $branch does not exist" >&2
+  exit 1
+fi
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$branch" = "$current_branch" ]]; then
+  echo "You're already on $branch" >&2
+  exit 1
+fi
+
+git checkout "$branch"
 git pull -r
 git remote prune origin
 git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -D
