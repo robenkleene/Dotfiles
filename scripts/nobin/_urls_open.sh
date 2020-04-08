51ab81080b33f6e568aee46738616e21dd47b50c
Roben Kleene <contact@robenkleene.com>
2020-04-08 13:39:41 -0400

    Use generic URL open

--- scripts/nobin/_urls_open.sh
+++ scripts/nobin/_urls_open.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 
-THEURLS="{"
+# The default browser method
 while read LINE; do
-  THEURLS=$THEURLS"\""${LINE}"\", "
+  open $LINE
 done
-THEURLS=${THEURLS%??}
-THEURLS=$THEURLS"}"
 
-if [[ "$THEURLS" == "{}" ]]; then
-  echo "No urls"
-  exit 1
-fi
+# The Safari method
+# THEURLS="{"
+# while read LINE; do
+#   THEURLS=$THEURLS"\""${LINE}"\", "
+# done
+# THEURLS=${THEURLS%??}
+# THEURLS=$THEURLS"}"
 
-osascript <<-APPLESCRIPT
-set URLs to $THEURLS
-tell application "Safari"
-	activate
-	make new document with properties {URL:item 1 of URLs}
-	tell window 1
-		repeat with theURL in rest of the URLs
-			make new tab with properties {URL:theURL}
-		end repeat
-	end tell
-end tell
-APPLESCRIPT
+# if [[ "$THEURLS" == "{}" ]]; then
+#   echo "No urls"
+#   exit 1
+# fi
+
+# osascript <<-APPLESCRIPT
+# set URLs to $THEURLS
+# tell application "Safari"
+# 	activate
+# 	make new document with properties {URL:item 1 of URLs}
+# 	tell window 1
+# 		repeat with theURL in rest of the URLs
+# 			make new tab with properties {URL:theURL}
+# 		end repeat
+# 	end tell
+# end tell
+# APPLESCRIPT
