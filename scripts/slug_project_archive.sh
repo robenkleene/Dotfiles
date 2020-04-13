7bdc7add11036325aaaa3368188e0f99cac6b6e9
Roben Kleene <contact@robenkleene.com>
2020-04-13 18:11:28 -0400

    Start writing slug project archive

---
+++ scripts/slug_project_archive.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+project_dir="$1"
+if [[ ! -d "$project_dir" ]]; then
+  project_dir=$(dirname "$1")
+fi
+
+# Remove leading and trailing slash and leading period
+project_dir=${project_dir#\.}
+project_dir=${project_dir#/}
+project_dir=${project_dir%/}
+
+destination_dir="$project_dir/../../archive/projects"
+
+
+echo "project_dir = $project_dir"
+echo "destination_dir = $destination_dir"
+
+if [[ ! -d "$destination_dir" ]]; then
+  echo "$destination_dir does not exist" >&2
+  exit 1
+fi
+
