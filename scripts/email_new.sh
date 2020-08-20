d5572ccc50cc9567aa282e2d6467e54997bd677a
Roben Kleene <contact@robenkleene.com>
2020-08-20 15:51:12 -0400

    Add new email

---
+++ scripts/email_new.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Wrong number of arguments" >&2
+    exit 1
+fi
+
+title="$1"
+if [[ -z "$title" ]]; then
+    echo "Invalid title" >&2
+    exit 1
+fi
+~/.bin/markdown_new_file "$title" ~/Documents/Text/Writing/Email/
