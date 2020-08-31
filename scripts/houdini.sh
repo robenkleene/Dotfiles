708ff9755002eb75731ecf5439eb8518939d1777
Roben Kleene <contact@robenkleene.com>
2020-08-31 17:12:58 -0400

    Use apprentice

--- scripts/houdini.sh
+++ scripts/houdini.sh
@@ -3,6 +3,6 @@
 set -e
 
 cd /Applications/Houdini/Houdini18.0.416/Frameworks/Houdini.framework/Versions/Current/Resources/
-source ./houdini_setup
-cd -
-/Applications/Houdini/Houdini18.0.416/Frameworks/Houdini.framework/Versions/Current/Resources/bin/houdini "$@"
+source ./houdini_setup >/dev/null
+cd - >/dev/null
+/Applications/Houdini/Houdini18.0.416/Frameworks/Houdini.framework/Versions/Current/Resources/bin/houdini -apprentice "$@"
