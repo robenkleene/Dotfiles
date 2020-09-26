0c122c494ac0fbba96e0e026264441602db8bf3c
Roben Kleene <contact@robenkleene.com>
2020-09-26 10:35:55 -0400

    Update install script

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-INSTALL_DIRECTORY_NAME="Dotfiles" # The name of the directory to install from.
+install_directory_name="Dotfiles" # The name of the directory to install from.
 
 # Stop if this is being run from any directory besides the install directory
-DIRECTORY=${PWD}
-DIRECTORY_NAME=${PWD##*/}
-if [ ! $DIRECTORY_NAME == $INSTALL_DIRECTORY_NAME ]; then
-  echo "ERROR: This directory \"$DIRECTORY_NAME\" doesn't match $INSTALL_DIRECTORY_NAME."
-  echo "This script only runs from the $INSTALL_DIRECTORY_NAME directory."
+directory=${PWD}
+directory_name=${PWD##*/}
+if [ ! "$directory_name" == $install_directory_name ]; then
+  echo "ERROR: This directory \"$directory_name\" doesn't match $install_directory_name."
+  echo "This script only runs from the $install_directory_name directory."
   exit 1
 fi
 
-function MakeSymlink {
-  DESTINATION=~/.$thisFILE
-  if [ -f $DESTINATION ] || [ -d $DESTINATION ]; then
-    if [ ! -L $DESTINATION ]; then
-      echo "ERROR: $DESTINATION is a file and it's not a symlink!"
+function make_symlink {
+  destination=~/.$1
+  if [ -f "$destination" ] || [ -d "$destination" ]; then
+    if [ ! -L "$destination" ]; then
+      echo "ERROR: $destination is a file and it's not a symlink!"
     fi
   else
-    ln -s $DIRECTORY/$thisFILE $DESTINATION
+    ln -s "$directory/$1" "$destination"
   fi
 }
 
@@ -27,7 +27,10 @@ for thisFILE in *; do
     # Exclude shell scripts
     if [[ ! $thisFILE =~ "scripts" ]]; then
       # Exclude the scripts directory
-      MakeSymlink $thisFILE
+      make_symlink "$thisFILE"
     fi
   fi
 done
+
+cd scripts || exit
+./install.sh
