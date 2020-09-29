0c91d254c882b9258244f366b2fdca9787796c7b
Roben Kleene <contact@robenkleene.com>
2020-09-29 13:23:53 -0400

    Work on cd widget

--- config/fish/fzf.fish
+++ config/fish/fzf.fish
@@ -6,4 +6,13 @@ fzf_key_bindings
 set -xg FZF_DEFAULT_COMMAND 'fd --type f --hidden --exclude .git --exclude .DS_Store'
 set -xg FZF_ALL_COMMAND 'fd --hidden --exclude .git --exclude .DS_Store'
 
-bind \ec fzf-cd-widget
+function robenkleene-fzf-cd-widget
+  set -l commandline (__fzf_parse_commandline)
+  if test "$PWD/" != "$HOME/**/*"
+    echo "Only use in a subdirectory of home" >&2
+    commandline -f repaint
+    return 1
+  end
+  fzf-cd-widget
+end
+bind \ec robenkleene-fzf-cd-widget
