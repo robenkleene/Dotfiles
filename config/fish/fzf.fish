4f09f701cdff801885b6227f197bf53f3423c450
Roben Kleene <contact@robenkleene.com>
2020-09-29 14:56:24 -0400

    Fix home match and add editor widget

--- config/fish/fzf.fish
+++ config/fish/fzf.fish
@@ -8,7 +8,7 @@ set -xg FZF_ALL_COMMAND 'fd --hidden --exclude .git --exclude .DS_Store'
 
 function robenkleene-fzf-cd-widget
   set -l commandline (__fzf_parse_commandline)
-  if not string match --regex --quiet "^$HOME\/.*\/" $PWD
+  if not string match --regex --quiet "^$HOME\/.+" $PWD
     echo "Only use in a subdirectory of home" >&2
     commandline -f repaint
     return 1
@@ -37,3 +37,29 @@ function robenkleene-fzf-z-widget
   commandline -f repaint
 end
 bind \ez robenkleene-fzf-z-widget
+
+function robenkleene-fzf-edit-widget
+  set -l commandline (__fzf_parse_commandline)
+  if not string match --regex --quiet "^$HOME\/.+" $PWD
+    echo "Only use in a subdirectory of home" >&2
+    commandline -f repaint
+    return 1
+  end
+  set -l dir $commandline[1]
+  set -l fzf_query $commandline[2]
+
+  set -l cmd $FZF_CTRL_T_COMMAND
+
+  test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 40%
+  begin
+    set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS"
+    eval "$cmd | "(__fzfcmd)' +m --query "'$fzf_query'"' | read -l result
+
+    if [ -n "$result" ]
+      eval $EDITOR \"$result\"
+      commandline -t ""
+    end
+  end
+  commandline -f repaint
+end
+bind \ee robenkleene-fzf-edit-widget
