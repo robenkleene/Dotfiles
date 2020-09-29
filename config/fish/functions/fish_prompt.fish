441ebacb7508c2425cf7e8a63bd4bc83d6fe3d1f
Roben Kleene <contact@robenkleene.com>
2020-09-29 11:15:25 -0400

    Tweak prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,22 +1,33 @@
 set fish_color_comment 555555
 set -xg fish_color_search_match black --background=$fish_color_comment
 
-# Prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch cyan
+
 function fish_prompt
   set_color $fish_color_comment
   echo -n (date "+%I:%M %p")
-  echo -n ' '
-  echo -n $USER@(prompt_hostname)
+  if test $SSH_CONNECTION
+    echo -n ' '
+    echo -n $USER@(prompt_hostname)
+  end
   echo -n ' '
   set_color $fish_color_cwd
-  echo -n (prompt_pwd)
+  if test -e .git
+    echo -n '.'
+  else
+    echo -n (prompt_pwd)
+  end
   set_color normal
-  echo -n '> '
+  if test $SSH_CONNECTION
+    echo -n ' >> '
+  else
+    echo -n ' > '
+  end
 end
+
 function fish_right_prompt
   echo -n (__fish_git_prompt)
 end
