b04338575d5e097219dcda5a3703008376d86f92
Roben Kleene <contact@robenkleene.com>
2020-09-29 13:07:42 -0400

    Add first fzf binding

---
+++ config/fish/fzf.fish
@@ -0,0 +1,9 @@
+if test -f ~/.fzf/shell/key-bindings.fish
+  source ~/.fzf/shell/key-bindings.fish
+end
+fzf_key_bindings
+
+set -xg FZF_DEFAULT_COMMAND 'fd --type f --hidden --exclude .git --exclude .DS_Store'
+set -xg FZF_ALL_COMMAND 'fd --hidden --exclude .git --exclude .DS_Store'
+
+bind \ec fzf-cd-widget
