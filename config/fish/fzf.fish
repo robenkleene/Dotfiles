1e5508b9afddffca447213740a4e67c27de2c563
Roben Kleene <contact@robenkleene.com>
2020-09-29 15:49:49 -0400

    Make edit widget work as arg

--- config/fish/fzf.fish
+++ config/fish/fzf.fish
@@ -7,7 +7,6 @@ set -xg FZF_DEFAULT_COMMAND 'fd --type f --hidden --exclude .git --exclude .DS_S
 set -xg FZF_ALL_COMMAND 'fd --hidden --exclude .git --exclude .DS_Store'
 
 function _robenkleene-fzf-cd-widget
-  set -l commandline (__fzf_parse_commandline)
   if not string match --regex --quiet "^$HOME\/.+" $PWD
     echo "Only use in a subdirectory of home" >&2
     commandline -f repaint
@@ -39,12 +38,13 @@ end
 bind \ez _robenkleene-fzf-z-widget
 
 function _robenkleene-fzf-edit-widget
-  set -l commandline (__fzf_parse_commandline)
   if not string match --regex --quiet "^$HOME\/.+" $PWD
     echo "Only use in a subdirectory of home" >&2
     commandline -f repaint
     return 1
   end
+  set -l current_cmd (commandline)
+  set -l commandline (__fzf_parse_commandline)
   set -l dir $commandline[1]
   set -l fzf_query $commandline[2]
 
@@ -55,11 +55,46 @@ function _robenkleene-fzf-edit-widget
     set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS"
     eval "$cmd | "(__fzfcmd)' +m --query "'$fzf_query'"' | read -l result
 
-    if [ -n "$result" ]
-      eval $EDITOR \"$result\"
-      commandline -t ""
+    if test -z $current_cmd
+      if [ -n "$result" ]
+        eval $EDITOR \"$result\"
+      end
+    else
+      commandline -i "$result"
     end
   end
   commandline -f repaint
 end
 bind \ee _robenkleene-fzf-edit-widget
+
+# function _robenkleene-fzf-commands-widget
+#   set -l commandline (__fzf_parse_commandline)
+#   set -l dir $commandline[1]
+#   set -l fzf_query $commandline[2]
+
+#   set -l cmd string split ' ' '(zsh -c \"echo \${(k)commands}\"); string split ' ' (functions -n); builtin --names'
+
+#   test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 40%
+#   begin
+#     set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS"
+#     eval "$cmd | "(__fzfcmd)' +m --query "'$fzf_query'"' | read -l result
+
+#     if [ -n "$result" ]
+#       eval $EDITOR \"$result\"
+#       commandline -t ""
+#     end
+#   end
+#   commandline -f repaint
+# end
+# bind \ee _robenkleene-fzf-edit-widget
+
+
+
+# builtin --names
+# functions -n
+# zsh -c "echo \${(k)commands}"
+# string split ' ' (zsh -c "echo \${(k)commands}")
+
+# string split ' ' (zsh -c "echo \${(k)commands}") && string split ' ' (functions -n) && builtin --names
+
+# string split ' ' (zsh -c "echo \${(k)commands}"); string split ' ' (functions -n); builtin --names
