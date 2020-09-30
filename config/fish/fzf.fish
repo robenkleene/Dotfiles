21ef93026c1f5f706a2777a327f44e20cfa8e3eb
Roben Kleene <contact@robenkleene.com>
2020-09-29 20:01:37 -0400

    Format

--- config/fish/fzf.fish
+++ config/fish/fzf.fish
@@ -1,5 +1,5 @@
 if test -f ~/.fzf/shell/key-bindings.fish
-  source ~/.fzf/shell/key-bindings.fish
+    source ~/.fzf/shell/key-bindings.fish
 end
 fzf_key_bindings
 
@@ -9,104 +9,104 @@ set -xg FZF_TMUX_HEIGHT '40%'
 set -xg FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS"
 
 function _robenkleene-fzf-cd-widget
-  if not string match --regex --quiet "^$HOME\/.+" $PWD
-    echo "Only use in a subdirectory of home" >&2
-    commandline -f repaint
-    return 1
-  end
-
-  set -l cmd $FZF_ALT_C_COMMAND
-  set -l commandline (commandline)
-  begin
-    eval "$cmd | "(__fzfcmd) | read -l result
-    if test -n "$result"
-      if test -z $commandline
-        cd $result
-      else
-        commandline -i "$result"
-      end
+    if not string match --regex --quiet "^$HOME\/.+" $PWD
+        echo "Only use in a subdirectory of home" >&2
+        commandline -f repaint
+        return 1
     end
-  end
 
-  commandline -f repaint
+    set -l cmd $FZF_ALT_C_COMMAND
+    set -l commandline (commandline)
+    begin
+        eval "$cmd | "(__fzfcmd) | read -l result
+        if test -n "$result"
+            if test -z $commandline
+                cd $result
+            else
+                commandline -i "$result"
+            end
+        end
+    end
+
+    commandline -f repaint
 end
 bind \ec _robenkleene-fzf-cd-widget
 
 function _robenkleene-fzf-z-widget
-  set -l cmd "fasd -Rdl"
-  set -l commandline (commandline)
-  begin
-    eval "$cmd | "(__fzfcmd) | read -l result
-    if test -n "$result"
-      if test -z $commandline
-        cd $result
-      else
-        commandline -i "$result"
-      end
+    set -l cmd "fasd -Rdl"
+    set -l commandline (commandline)
+    begin
+        eval "$cmd | "(__fzfcmd) | read -l result
+        if test -n "$result"
+            if test -z $commandline
+                cd $result
+            else
+                commandline -i "$result"
+            end
+        end
     end
-  end
 
-  commandline -f repaint
+    commandline -f repaint
 end
 bind \ez _robenkleene-fzf-z-widget
 
 function _robenkleene-fzf-edit-widget
-  if not string match --regex --quiet "^$HOME\/.+" $PWD
-    echo "Only use in a subdirectory of home" >&2
-    commandline -f repaint
-    return 1
-  end
-
-  set -l cmd $FZF_CTRL_T_COMMAND
-  set -l commandline (commandline)
-  begin
-    eval "$cmd | "(__fzfcmd) | read -l result
-    if test -n "$result"
-      if test -z $commandline
-        eval $EDITOR \"$result\"
-      else
-        commandline -i "$result"
-      end
+    if not string match --regex --quiet "^$HOME\/.+" $PWD
+        echo "Only use in a subdirectory of home" >&2
+        commandline -f repaint
+        return 1
     end
-  end
 
-  commandline -f repaint
+    set -l cmd $FZF_CTRL_T_COMMAND
+    set -l commandline (commandline)
+    begin
+        eval "$cmd | "(__fzfcmd) | read -l result
+        if test -n "$result"
+            if test -z $commandline
+                eval $EDITOR \"$result\"
+            else
+                commandline -i "$result"
+            end
+        end
+    end
+
+    commandline -f repaint
 end
 bind \ee _robenkleene-fzf-edit-widget
 
 function _robenkleene_fish_commands
-  string split ' ' (zsh -c "echo \${(k)commands}")
-  string split ' ' (functions -n);
-  builtin --names
+    string split ' ' (zsh -c "echo \${(k)commands}")
+    string split ' ' (functions -n)
+    builtin --names
 end
 
 function _robenkleene-fzf-commands-widget
-  set -l cmd "_robenkleene_fish_commands"
-  begin
-    eval "$cmd | "(__fzfcmd) | read -l result
-    if test -n "$result"
-      commandline -i "$result"
+    set -l cmd "_robenkleene_fish_commands"
+    begin
+        eval "$cmd | "(__fzfcmd) | read -l result
+        if test -n "$result"
+            commandline -i "$result"
+        end
     end
-  end
 
-  commandline -f repaint
+    commandline -f repaint
 end
 bind \ex _robenkleene-fzf-commands-widget
 
 function _robenkleene-fzf-developer-widget
-  set -l cmd "fd --type d --exclude .git . ~/Developer"
-  set -l commandline (commandline)
-  begin
-    eval "$cmd | "(__fzfcmd) | read -l result
-    if test -n "$result"
-      if test -z $commandline
-        cd $result
-      else
-        commandline -i "$result"
-      end
+    set -l cmd "fd --type d --exclude .git . ~/Developer"
+    set -l commandline (commandline)
+    begin
+        eval "$cmd | "(__fzfcmd) | read -l result
+        if test -n "$result"
+            if test -z $commandline
+                cd $result
+            else
+                commandline -i "$result"
+            end
+        end
     end
-  end
 
-  commandline -f repaint
+    commandline -f repaint
 end
 bind \eg _robenkleene-fzf-developer-widget
