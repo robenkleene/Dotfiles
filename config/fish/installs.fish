fb6c6246dae8a1fe8bf36b18a344e7fba3f7f40e
Roben Kleene <contact@robenkleene.com>
2020-09-30 13:45:31 -0400

    Add LLVM

--- config/fish/installs.fish
+++ config/fish/installs.fish
@@ -1,11 +1,22 @@
 if command -sq fasd
-  function __fasd_run -e fish_preexec -d "fasd takes record of the directories changed into"
-      if test $argv[1] != "exit"
-          command fasd --proc (command fasd --sanitize "$argv") > "/dev/null" 2>&1 &
-      end
-  end
+    function __fasd_run -e fish_preexec -d "fasd takes record of the directories changed into"
+        if test $argv[1] != "exit"
+            command fasd --proc (command fasd --sanitize "$argv") >"/dev/null" 2>&1 &
+        end
+    end
 end
 
+# Ruby
 source ~/.config/fish/vendor/chruby/chruby.fish
 source ~/.config/fish/vendor/chruby/auto.fish
 
+# Python
+
+# Node
+
+# llvm
+switch (uname)
+    case Darwin
+        set -U fish_user_paths /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin $fish_user_paths
+        set -gx SDKROOT (xcrun --sdk macosx --show-sdk-path)
+end
