7a1296b78d4b10ea567d8042afa06b3454f15fc3
Roben Kleene <contact@robenkleene.com>
2020-10-01 14:45:21 -0400

    Add insiders

--- scripts/egit.sh
+++ scripts/egit.sh
@@ -3,7 +3,7 @@
 # System Repos
 EGITREPOS=~/Developer/Dotfiles/
 if [[ "$(uname)" == "Darwin" ]]; then
-  EGITREPOS+=:~/Library/Services/:~/Library/Application\ Support/TextMate/Bundles/Roben\ Kleene.tmbundle/:~/Developer/AppleScripts/:~/Library/Developer/Xcode/UserData/:~/Music/Audio\ Music\ Apps/:~/Library/Application\ Support/Code/User
+  EGITREPOS+=:~/Library/Services/:~/Library/Application\ Support/TextMate/Bundles/Roben\ Kleene.tmbundle/:~/Developer/AppleScripts/:~/Library/Developer/Xcode/UserData/:~/Music/Audio\ Music\ Apps/:~/Library/Application\ Support/Code/User:~/Library/Application\ Support/Code\ -\ Insiders/User
 elif [[ "$(uname)" = "Linux" ]]; then
   EGITREPOS+=:~/.config/Code/User
   EGITREPOS+=:~/.config/Code\ -\ Insiders/User
