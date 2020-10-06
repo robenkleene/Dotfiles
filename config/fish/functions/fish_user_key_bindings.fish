9ec7139c20008617363e3f4246fb0174ebf45c87
Roben Kleene <contact@robenkleene.com>
2020-10-06 12:35:28 -0400

    Merge history

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,2 +1,6 @@
 # bind \t accept-autosuggestion
 bind \cq push_line
+# Merge history first when doing a search
+# bind -k up 'history --merge ; up-or-search'
+# For some reason the above doesn't get picked up, but below does
+bind \e\[A 'history --merge ; up-or-search'
