c8dfe9630a14c18781a04b623d2ce3318f41b5e2
Roben Kleene <contact@robenkleene.com>
2020-10-12 16:27:07 -0400

    Disable shared history

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -3,4 +3,6 @@ bind \cq push_line
 # Merge history first when doing a search
 # bind -k up 'history --merge ; up-or-search'
 # For some reason the above doesn't get picked up, but below does
-bind \e\[A 'history --merge ; up-or-search'
+# This isn't so great, because if you have two sessions you're working in, you
+# want "up-or-search" to return the previous command.
+# bind \e\[A 'history --merge ; up-or-search'
