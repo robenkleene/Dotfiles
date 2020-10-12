5c812fb3d10091ec40d1565d771f91c1e8c01a6a
Roben Kleene <contact@robenkleene.com>
2020-10-12 16:20:11 -0400

    Add options

--- scripts/format_kindle_highlight.sh
+++ scripts/format_kindle_highlight.sh
@@ -2,25 +2,57 @@
 
 set -euo pipefail
 
-stored=""
-while read -r line; do
-  if [[ -z $line ]]; then
-    continue
-  fi
-  if [[ $line =~ ^LOCATION:[[:space:]]([0-9]+) ]]; then
-    printf '`'
-    echo -n "${BASH_REMATCH[1]}"
-    printf '`:'
-    echo
-    echo
-    echo "> ${stored}"
-    echo
-    stored=""
-  elif [[ -n $stored ]]; then
-    echo "# *${stored}* by ${line}"
-    echo
-    stored=""
-  else
-    stored+=$line
-  fi
-done <"${1:-/dev/stdin}"
+write="false"
+while getopts ":wh" option; do
+  case "$option" in
+    w)
+      write="true"
+      ;;
+    h)
+      echo "Usage: command [-hf] [-p <file_path>]"
+      exit 0
+      ;;
+    :)
+      echo "Option -OPTARG requires an argument" >&2
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+process() {
+  local file="${1:-/dev/stdin}"
+  local stored=""
+  while read -r line; do
+    if [[ -z $line ]]; then
+      continue
+    fi
+    if [[ $line =~ ^LOCATION:[[:space:]]([0-9]+) ]]; then
+      printf '`'
+      echo -n "${BASH_REMATCH[1]}"
+      printf '`:'
+      echo
+      echo
+      echo "> ${stored}"
+      echo
+      stored=""
+    elif [[ -n $stored ]]; then
+      echo "# *${stored}* by ${line}"
+      echo
+      stored=""
+    else
+      stored+=$line
+    fi
+  done <"${file}"
+}
+
+if [ $# -gt 0 ]; then
+  for file in "$@"; do
+    process $file
+  done
+else
+  process
+fi
