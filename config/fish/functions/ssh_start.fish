3056ba20307e4301d32e5de8b6bc9a84f13721b4
Roben Kleene <contact@robenkleene.com>
2020-10-22 11:22:41 +0000

    Fix ssh

--- config/fish/functions/ssh_start.fish
+++ config/fish/functions/ssh_start.fish
@@ -1,8 +1,9 @@
 function ssh_start
     switch (uname)
         case Linux
-           if test -z $SSH_AGENT_PID 
-               ssh_start
+           if test -z $SSH_AGENT_PID
+                eval (ssh-agent -c)
+                ssh-add
            end
     end
 end
