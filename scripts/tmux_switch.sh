312804178805e2afaee5f49b28614191b84b6d9b
Roben Kleene <contact@robenkleene.com>
2020-10-23 16:01:09 +0000

    Make script exit cleanly

--- scripts/tmux_switch.sh
+++ scripts/tmux_switch.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-set -e
+set -uo pipefail
 
-tmux list-sessions -F "#S" | fzf-tmux | tr "\n" "\0" | xargs -0 tmux switch-client -t
+result=$(tmux list-sessions -F "#S" | fzf-tmux)
+if [[ -z "$result" ]]; then
+  exit 0
+fi
+echo "$result" | tr "\n" "\0" | xargs -0 tmux switch-client -t
