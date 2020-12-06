0a17e377bddf6ec700226563b046c6932a92d426
Roben Kleene <contact@robenkleene.com>
2020-12-06 13:00:46 -0500

    Fix paths

--- config/fish/functions/fish_set_user_paths.fish
+++ config/fish/functions/fish_set_user_paths.fish
@@ -7,9 +7,9 @@ function fish_set_user_paths
     # Python
     switch (uname)
         case Linux
-            set -U fish_user_paths ~/Library/Python/3.9/bin $fish_user_paths
-        case Darwin
             set -U fish_user_paths ~/.local/bin $fish_user_paths
+        case Darwin
+            set -U fish_user_paths ~/Library/Python/3.9/bin $fish_user_paths
     end
 
     # Node
