ccd03873f7a1258e7e3a2795d1d56f70d9f9b26c
Roben Kleene <contact@robenkleene.com>
2020-12-07 23:12:16 +0000

    Add python shims

---
+++ scripts/pip.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Some scripts expect this to exist
+
+pip3 "$@"
---
+++ scripts/python.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Some scripts except this to exist
+
+python3 "$@"
