74113f8dd45b57519a36abda72a5f544d2beee3b
Roben Kleene <contact@robenkleene.com>
2021-01-22 13:15:55 -0500

    Make name directory save

--- scripts/tmux_name_directory.sh
+++ scripts/tmux_name_directory.sh
@@ -3,3 +3,8 @@
 set -e
 
 tmux rename-window "$(basename "$PWD")"
+
+# Save if possible
+if [[ -x ~/.tmux/plugins/tmux-resurrect/scripts/save.sh ]]; then
+  ~/.tmux/plugins/tmux-resurrect/scripts/save.sh
+fi
