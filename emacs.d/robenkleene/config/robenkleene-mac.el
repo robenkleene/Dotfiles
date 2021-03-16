14eb4eb1afda059ef1d1b6196ec4d62e83eddebb
Roben Kleene <contact@robenkleene.com>
2021-03-16 15:38:17 -0400

    Add second quick open

--- emacs.d/robenkleene/config/robenkleene-mac.el
+++ emacs.d/robenkleene/config/robenkleene-mac.el
@@ -56,6 +56,10 @@
 ;;   'robenkleene/make-frame-with-current-frame)
 
 (define-key robenkleene/mac-bindings-minor-mode-map
+  (kbd "s-O")
+  'robenkleene/ido-quick-open-file)
+
+(define-key robenkleene/mac-bindings-minor-mode-map
   (kbd "s-z")
   'undo-tree-undo)
 (define-key robenkleene/mac-bindings-minor-mode-map
