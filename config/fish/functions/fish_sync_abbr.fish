495780341cb57d26264193b26ba9094cdf1597e5
Roben Kleene <contact@robenkleene.com>
2021-04-19 06:52:38 -0400

    Setup z

--- config/fish/functions/fish_sync_abbr.fish
+++ config/fish/functions/fish_sync_abbr.fish
@@ -7,7 +7,6 @@ function fish_sync_abbr
 
     abbr -a -- - 'cd -'
     abbr -a v "$VIM_COMMAND"
-    abbr -a z "zsh"
     abbr -a vd "$VIM_COMMAND -d"
     abbr -a or 'open -R'
     abbr -a orm 'open README.md'
--- config/fish/installs.fish
+++ config/fish/installs.fish
@@ -1,9 +1,20 @@
+# fasd
 if command -sq fasd
     function __fasd_run -e fish_preexec -d "fasd takes record of the directories changed into"
         if test $argv[1] != "exit"
             command fasd --proc (command fasd --sanitize "$argv") >"/dev/null" 2>&1 &
         end
     end
+    function fasd_cd -d "fasd builtin cd"
+      if test (count $argv) -le 1
+        command fasd "$argv"
+      else
+        fasd -e 'printf %s' $argv | read -l ret
+        test -z "$ret"; and return
+        test -d "$ret"; and cd "$ret"; or printf "%s\n" $ret
+      end
+    end
+    alias z="fasd_cd -d"
 end
 
 # Ruby
