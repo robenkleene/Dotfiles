e3d55001b57705d91a7585973fb78e506dd934d2
Roben Kleene <contact@robenkleene.com>
2021-04-23 18:52:23 +0000

    Finish swap

--- scripts/git_swap_format.sh
+++ scripts/git_swap_format.sh
@@ -24,8 +24,7 @@ while getopts ":Hsh" option; do
 done
 
 if [[ "$use_https" == "true" ]]; then
-  sed 's/git@/https\:\/\//g' | perl -pe 's|(https\://.*?)/|\1:.git|'
-  # | perl -pe 's|(git@.*?)/|\1:|'
+  sed 's/git@/https\:\/\//g' | perl -pe 's|(https\://.*?):|\1/|'
 else
   sed 's/^https\:\/\//git@/' | perl -pe 's|(git@.*?)/|\1:|'
 fi
