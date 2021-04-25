5123e4a1a1795fbd092c3bd770c9bb97b1111142
Roben Kleene <contact@robenkleene.com>
2021-04-25 20:45:49 +0000

    Exclude on save

--- scripts/sgitt.sh
+++ scripts/sgitt.sh
@@ -3,6 +3,11 @@
 set -euo pipefail
 
 cd ~/Documentation/ && "$HOME/.bin/sgit" "$@"
+
+if [[ -n "$PUBLIC_EGIT" ]]; then
+  exit 0
+fi
+
 cd ~/Text/ && "$HOME/.bin/sgit" "$@"
 
 source ~/.bin/nobin/_giterator.sh
