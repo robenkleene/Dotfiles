b685d3a2d00a52abc73278da47fe8e6de21934c5
Roben Kleene <contact@robenkleene.com>
2021-04-25 14:08:22 -0400

    Add tmux check

--- scripts/tmux_session_auto_restore.sh
+++ scripts/tmux_session_auto_restore.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if tmux info &> /dev/null; then
+  echo "tmux is running, exit tmux first"
+  exit 1
+fi
+
 tmux new-session 'tmux run-shell ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh'
