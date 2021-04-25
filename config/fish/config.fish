29256989796bd589a38bfadac2b6258d8a5cda32
Roben Kleene <contact@robenkleene.com>
2021-04-25 20:39:48 +0000

    Handle private

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,6 +5,10 @@ if test -f ~/.bin/print_shell_status
   ~/.bin/print_shell_status
 end
 
+if test -f ~/.config_local.fish
+    source ~/.config_local.fish
+end
+
 source ~/.config/fish/settings.fish
 source ~/.config/fish/variables.fish
 source ~/.config/fish/installs.fish
--- scripts/egit.sh
+++ scripts/egit.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eo pipefail
 
 # System Repos
 repos=~/Developer/Dotfiles/
 if [[ "$(uname)" == "Darwin" ]]; then
   repos+=:~/Library/Services/:~/Library/Application\ Support/TextMate/Bundles/Roben\ Kleene.tmbundle/:~/Developer/AppleScripts/:~/Library/Developer/Xcode/UserData/:~/Music/Audio\ Music\ Apps/:~/Library/Application\ Support/Code/User:~/Library/Application\ Support/Code\ -\ Insiders/User
+  if [[ -z "$PUBLIC_EGIT" ]]; then
+    repos+=:~/Music/Audio\ Music\ Apps/
+  fi
 elif [[ "$(uname)" = "Linux" ]]; then
   repos+=:~/.config/Code/User
   repos+=:~/.config/Code\ -\ Insiders/User
 fi
 
-# General Text Repos
-repos+=:~/Developer/Snippets/:~/Developer/Settings/
-# Last Repos
-repos+=:~/Developer/Backup/:~/Developer/Archive/
+if [[ -z "$PUBLIC_EGIT" ]]; then
+  # General Text Repos
+  repos+=:~/Developer/Snippets/:~/Developer/Settings/
+  # Last Repos
+  repos+=:~/Developer/Backup/:~/Developer/Archive/
+fi
 
 IFS=':' read -ra repos <<<"$repos"
 
