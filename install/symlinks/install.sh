ec689e6dd733a334f379694a00c2160f222d1fe3
Roben Kleene <contact@robenkleene.com>
2021-05-03 21:29:31 -0400

    Improve symlink

--- install/symlinks/install.sh
+++ install/symlinks/install.sh
@@ -17,18 +17,24 @@ if [[ "$(uname)" = "Darwin" ]]; then
 fi
 
 # Give a consistent path for brew installs
-brew_prefix=$(brew --prefix)
+brew_bin_dir="$HOME/.brew"
+brew_bin="$brew_bin_dir/bin"
+if [[ -e "$brew_bin" ]]; then
+  exit 0
+fi
+
+if [[ -e "/usr/local/bin/brew" ]]; then
+  brew_prefix=$(/usr/local/bin/brew --prefix)
+elif [[ -e "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
+  brew_prefix=$(/home/linuxbrew/.linuxbrew/bin/brew --prefix)
+fi
 brew_prefix_bin="$brew_prefix/bin"
 if [[ ! -e "$brew_prefix_bin" ]]; then
   echo "Error: $brew_prefix_bin does not exist" >&2
   exit 1
 fi
 
-brew_bin_dir="$HOME/.brew"
-brew_bin="$brew_bin_dir/bin"
-if [[ ! -e "$brew_bin" ]]; then
-  if [[ ! -e "$brew_bin_dir" ]]; then
-    mkdir -p "$brew_bin_dir"
-  fi
-  ln -s "$brew_prefix_bin" "$brew_bin"
+if [[ ! -e "$brew_bin_dir" ]]; then
+  mkdir -p "$brew_bin_dir"
 fi
+ln -s "$brew_prefix_bin" "$brew_bin"
