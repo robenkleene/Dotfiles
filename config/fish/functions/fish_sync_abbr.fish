cbec94c933a3dfb9cf21be8026abc40af2f7db85
Roben Kleene <contact@robenkleene.com>
2021-05-19 19:08:32 +0000

    Add quick server abbreviation

--- config/fish/functions/fish_sync_abbr.fish
+++ config/fish/functions/fish_sync_abbr.fish
@@ -31,6 +31,7 @@ function fish_sync_abbr
     abbr -a tmls 'tmux ls'
     abbr -a tmsn 'tmux new'
     abbr -a tmd 'tmux detach'
+    abbr -a mdv 'mosh aresdev -- tmux attach'
     abbr -a m 'make'
     abbr -a hpr 'hub pull-request'
     abbr -a ghpr 'gh pr create'
