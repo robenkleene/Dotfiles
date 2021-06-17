17389beafccaf5260e74fcc234a6416a997a510b
Roben Kleene <contact@robenkleene.com>
2021-06-17 21:26:47 +0000

    Update dirs

--- install/files/dirs.sh
+++ install/files/dirs.sh
@@ -9,3 +9,18 @@ elif [[ ! -d "$developer_path" ]]; then
   echo "Error: A file exists at $developer_path and it's not a directory" >&2
   exit 1
 fi
+
+
+emacs_path="${HOME}/Developer/Dotfiles/emacs.d"
+untitled_emacs_path="${HOME}/Developer/Dotfiles/emacs.d"
+if [[ ! -L "$emacs_path" ]]; then
+  if [[ ! -e "$untitled_emacs_path" ]]; then
+    mkdir -p "$untitled_emacs_path"
+  elif [[ ! -d "$untitled_emacs_path" ]]; then
+    echo "Error: A file exists at $untitled_emacs_path and it's not a directory" >&2
+    exit 1
+  fi
+else
+    echo "Error: A file exists at $emacs_path is not a symlink" >&2
+    exit 1
+fi
