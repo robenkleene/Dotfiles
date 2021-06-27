d8fe1d1c3a3e5adef0f49b06714a85e38460289e
Roben Kleene <contact@robenkleene.com>
2021-06-27 18:17:35 +0000

    Tweak meta delay

--- config/fish/settings.fish
+++ config/fish/settings.fish
@@ -13,3 +13,6 @@ end
 
 # Temporary work around for slow autocomplete on macOS
 function __fish_describe_command; end
+
+# This delay allows `<ESC>` to be used as a replacement for the meta key
+set fish_escape_delay_ms 100
