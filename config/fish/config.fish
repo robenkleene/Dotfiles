3e3ba3cc53070ddc8ff9a59a8cd35d5ca4327cd1
Roben Kleene <contact@robenkleene.com>
2021-07-08 01:49:33 +0000

    Change order

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,12 +5,12 @@ if test -f ~/.bin/print_shell_status
     ~/.bin/print_shell_status
 end
 
-if test -f ~/.local.fish
-    source ~/.local.fish
-end
-
 source ~/.config/fish/settings.fish
 source ~/.config/fish/variables.fish
 source ~/.config/fish/installs.fish
 source ~/.config/fish/fzf.fish
 source ~/.config/fish/alias.fish
+
+if test -f ~/.local.fish
+    source ~/.local.fish
+end
