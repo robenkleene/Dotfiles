40c76bcb629de884e9ac37692ac1befeb080d5e1
Roben Kleene <contact@robenkleene.com>
2021-07-25 18:45:50 +0000

    Update time log

--- scripts/time_log.sh
+++ scripts/time_log.sh
@@ -2,4 +2,25 @@
 
 set -euo pipefail
 
-echo "$(date)", "$1" > "${2:-$HOME/Text/journal/time.csv}"
+log="$HOME/Text/journal/time.csv"
+while getopts ":p:h" option; do
+  case "$option" in
+    p)
+      log="$OPTARG"
+      ;;
+    h)
+      echo "Usage: command [-hf] [-p <file_path>]"
+      exit 0
+      ;;
+    :)
+      echo "Option -OPTARG requires an argument" >&2
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+echo "$(date)", "$1" > "$log"
