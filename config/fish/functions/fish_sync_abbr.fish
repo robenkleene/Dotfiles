9ef873eb35f051bd254c878fa35138548610c32e
Roben Kleene <contact@robenkleene.com>
2021-07-27 14:55:28 +0000

    Update abbr

--- config/fish/functions/fish_sync_abbr.fish
+++ config/fish/functions/fish_sync_abbr.fish
@@ -113,7 +113,7 @@ function fish_sync_abbr
     # Mercurial
     abbr -a hgdb 'hg diff -r "bottom^" --root .'
     abbr -a hgdbs 'hg diff -r "bottom^" --stat --root .'
-    abbr -a hgdl 'hg diff -pr .^1'
+    abbr -a hgdc 'hg diff -pr .^1'
     abbr -a hgd 'hg diff --root .'
     abbr -a hgcm 'hg commit -m'
     abbr -a hgrc 'hg rebase --continue'
