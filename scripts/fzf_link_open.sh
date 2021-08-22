77b87510c632362cab7de2695d8997be75b53281
Roben Kleene <contact@robenkleene.com>
2021-08-22 15:30:03 -0400

    Add link

---
+++ scripts/fzf_link_open.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+__fzfcmd() {
+  [ -n "$TMUX_PANE" ] && { [ "${FZF_TMUX:-0}" != 0 ] || [ -n "$FZF_TMUX_OPTS" ]; } &&
+    echo "fzf-tmux ${FZF_TMUX_OPTS:--d${FZF_TMUX_HEIGHT:-40%}} -- " || echo "fzf"
+}
+
+cd ~/Developer/Snippets/ || return 1
+cmd="rg --no-filename --only-matching \"\[([^\[\]]*)\]\((http.*?)\)\" ~/Text ~/Documentation"
+fzfcmd="$(__fzfcmd)"
+
+eval "$cmd" | $fzfcmd | ~/.bin/urls_open
