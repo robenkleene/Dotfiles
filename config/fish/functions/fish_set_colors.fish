08e1668eecb7f3b52a421d626857f0c00a0e1b0f
Roben Kleene <contact@robenkleene.com>
2021-09-02 19:31:55 +0000

    Tweak colors

--- config/fish/functions/fish_set_colors.fish
+++ config/fish/functions/fish_set_colors.fish
@@ -4,9 +4,9 @@ function fish_set_colors
     set -U fish_color_quote C2D94C #C2D94C
     set -U fish_color_redirection FFEE99 #FFEE99
     set -U fish_color_end F29668 #F29668
-    set -U fish_color_error FF3333 red
+    set -U fish_color_error red red
     set -U fish_color_param 95E6CB #95E6CB
-    set -U fish_color_match F07178 #F07178
+    set -U fish_color_match FFEE99 #FFEE99
     set -U fish_color_selection --background=E6B450 #E6B450
     set -U fish_color_history_current --bold
     set -U fish_color_operator E6B450 #E6B450
