d10286506fc691379b26ddb2bfa79fb668a068f2
Roben Kleene <contact@robenkleene.com>
2021-09-03 06:57:57 -0400

    Tweak colors

--- config/fish/functions/fish_set_colors.fish
+++ config/fish/functions/fish_set_colors.fish
@@ -1,20 +1,20 @@
 function fish_set_colors
     set -U fish_color_normal B3B1AD #B3B1AD
-    set -U fish_color_command 95E6CB #95E6CB
-    set -U fish_color_quote C2D94C #C2D94C
-    set -U fish_color_redirection FFEE99 #FFEE99
-    set -U fish_color_end FFEE99 #FFEE99
+    set -U fish_color_command cyan
+    set -U fish_color_quote green
+    set -U fish_color_redirection yellow
+    set -U fish_color_end yellow
     set -U fish_color_error red red
-    set -U fish_color_param 39BAE6 #39BAE6
-    set -U fish_color_match FFEE99 #FFEE99
+    set -U fish_color_param blue
+    set -U fish_color_match yellow
     set -U fish_color_selection --background=E6B450 #E6B450
     set -U fish_color_history_current --bold
-    set -U fish_color_operator FFEE99 #FFEE99
-    set -U fish_color_escape 39BAE6 #39BAE6
-    set -U fish_color_cwd 39BAE6 #39BAE6
+    set -U fish_color_operator yellow
+    set -U fish_color_escape blue
+    set -U fish_color_cwd blue
     set -U fish_color_cwd_root red
     set -U fish_color_valid_path --underline
-    set -U fish_color_autosuggestion 4D5566 #4D5566
+    set -U fish_color_autosuggestion 808080 #808080
     set -U fish_color_user brgreen
     set -U fish_color_host normal
     set -U fish_color_cancel -r
@@ -22,6 +22,6 @@ function fish_set_colors
     set -U fish_pager_color_description B3A06D yellow #B3A06D
     set -U fish_pager_color_prefix normal --bold --underline
     set -U fish_pager_color_progress brwhite --background=cyan
-    set -U fish_color_comment 555555 #555555
+    set -U fish_color_comment 808080 #808080
     set -U fish_color_search_match black --background=$fish_color_comment
 end
