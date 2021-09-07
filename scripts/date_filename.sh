5fe18b5d0c772b2bc42f2d68110dafb678e6a52a
Roben Kleene <contact@robenkleene.com>
2021-09-07 22:54:12 +0000

    Add date filename

---
+++ scripts/date_filename.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+today=$(date +%F)
+filename="$1"
+if [[ -z "${1-}" ]]; then
+  echo "Error: No file" >&2
+  exit 0
+fi
+if [[ ! -e "$filename" ]]; then
+  echo "Error: $filename doesn't exist" >&2
+  exit 0
+fi
+newfilename="$today-$filename"
+if [[ -e "$newfilename" ]]; then
+  echo "Error: A file already exists at $newfilename" >&2
+  exit 0
+fi
+mv "$filename" "$newfilename"
