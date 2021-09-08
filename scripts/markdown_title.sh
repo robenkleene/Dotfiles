3fa00c339d9437b18023bf6f99684b7dce28170a
Roben Kleene <contact@robenkleene.com>
2021-09-08 19:57:37 +0000

    Handle underscores

--- scripts/markdown_title.sh
+++ scripts/markdown_title.sh
@@ -3,10 +3,10 @@
 filename=$(basename "$1")
 filename_no_ext=${filename%.*}
 script_dir=$(dirname $0)
-if [[ $filename_no_ext =~ ^[a-z0-9-]*$ ]]; then
-    # If the filename contains only lowercase letters and hyphens then assume
+if [[ $filename_no_ext =~ ^[a-z0-9_-]*$ ]]; then
+    # If the filename contains only lowercase letters, hyphens, and underscores then assume
     # convert it to title case with spaces.
-  title=$(echo $filename_no_ext | tr "-" " " | $script_dir/title_case)
+  title=$(echo $filename_no_ext | tr "-" " " | tr "_" " " | $script_dir/title_case)
 else
   title=$(echo $filename_no_ext | $script_dir/title_case)
 fi
