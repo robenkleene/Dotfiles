d99b8e2165f447b885cc73768c4535c339689a4f
Roben Kleene <contact@robenkleene.com>
2021-09-08 00:15:58 +0000

    Update scripts

--- scripts/date_filename.sh
+++ scripts/date_filename.sh
@@ -6,15 +6,15 @@ today=$(date +%F)
 filename="$1"
 if [[ -z "${1-}" ]]; then
   echo "Error: No file" >&2
-  exit 0
+  exit 1
 fi
 if [[ ! -e "$filename" ]]; then
   echo "Error: $filename doesn't exist" >&2
-  exit 0
+  exit 1
 fi
 newfilename="$today-$filename"
 if [[ -e "$newfilename" ]]; then
   echo "Error: A file already exists at $newfilename" >&2
-  exit 0
+  exit 1
 fi
 mv "$filename" "$newfilename"
--- scripts/format_json.sh
+++ scripts/format_json.sh
@@ -1,9 +1,39 @@
 #!/usr/bin/env bash
 
+inplace="false"
+while getopts ":p:ih" option; do
+  case "$option" in
+    p)
+      file_path="$OPTARG"
+      ;;
+    i)
+      inplace="true"
+      ;;
+    h)
+      echo "Usage: command [-hf] [-p <file_path>]"
+      exit 0
+      ;;
+    :)
+      echo "Option -OPTARG requires an argument" >&2
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
 if [[ $# -eq 0 ]] ; then
   python -m json.tool
 else
-  for file in "$@"; do
-    python -m json.tool "$file"
-  done
+  if [[ -z "${file_path-}" ]]; then
+    echo "Error: No file" >&2
+    exit 1
+  fi
+  if [[ "$inplace" == "true" ]]; then
+    echo "$(python -m json.tool "$file_path")" > "$file_path"
+  else
+    python -m json.tool "$file_path"
+  fi
 fi
