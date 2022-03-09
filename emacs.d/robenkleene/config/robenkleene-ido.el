5e613817557e3ec1300562c858c1ea6f5f51be14
Roben Kleene <contact@robenkleene.com>
2022-03-09 12:33:57 +0000

    Limit results

--- emacs.d/robenkleene/config/robenkleene-ido.el
+++ emacs.d/robenkleene/config/robenkleene-ido.el
@@ -268,7 +268,7 @@
           (split-string
            (shell-command-to-string
             (concat "fd "
-                    " --type f --follow --hidden --exclude .git --exclude .hg --exclude .DS_Store . "
+                    "--max-results=1000 --type f --follow --hidden --exclude .git --exclude .hg --exclude .DS_Store . "
                     dir
                     )) "\n"))
     (setq key-to-path (make-hash-table :test 'equal))
@@ -292,7 +292,7 @@
           (split-string
            (shell-command-to-string
             (concat "fd "
-                    " --type d --follow --hidden --exclude .git --exclude .hg . "
+                    "--max-results=1000 --type d --follow --hidden --exclude .git --exclude .hg . "
                     dir
                     )) "\n"))
     (setq key-to-path (make-hash-table :test 'equal))
@@ -316,7 +316,7 @@
           (split-string
            (shell-command-to-string
             (concat "fd "
-                    "--hidden --follow --exclude .git --exclude .hg . "
+                    "--max-results=1000 --hidden --follow --exclude .git --exclude .hg . "
                     dir
                     )) "\n"))
     (setq key-to-path (make-hash-table :test 'equal))
