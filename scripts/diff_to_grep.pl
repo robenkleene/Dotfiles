8ffa2d4635a03c84dee6e7e80be7437d661eaceb
Roben Kleene <contact@robenkleene.com>
2022-03-15 18:46:00 +0000

    Support removals and adds

--- scripts/diff_to_grep.pl
+++ scripts/diff_to_grep.pl
@@ -14,11 +14,18 @@ while (<STDIN>) {
       $printed = 0;
       $filename = $1;
       $filename =~ s/\s+$//;
+   } elsif (m(^\-\-\- a/(.*)$)) {
    } elsif (m(^@@ -\d+(?:,\d+)? \+(\d+))) {
       $line = $1;
       $offset = 0;
       $printed = 0;
    } elsif (m(^\+(.*)$)) {
+      my $data = $1 || '-';
+      print "$filename:" . ($offset + $line) . ":$data\n"
+         unless $printed;
+      $offset++;
+      $printed = 1;
+   } elsif (m(^\-(.*)$)) {
       my $data = $1 || '-';
       print "$filename:" . ($offset + $line) . ":$data\n"
          unless $printed;
