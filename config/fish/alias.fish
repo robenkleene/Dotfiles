e0aaa7715b1882e0a596189fa3fb86ac180050c7
Roben Kleene <contact@robenkleene.com>
2022-03-25 12:16:18 +0000

    Add clip

--- config/fish/alias.fish
+++ config/fish/alias.fish
@@ -39,6 +39,7 @@ alias u='ssh_start && egit -u && ~/Developer/Dotfiles/update.sh && fish_update'
 alias ut='ssh_git_pull_all -t'
 alias ua='ssh_git_pull_all'
 alias pull='ssh_start && egit -u && ~/Developer/Dotfiles/update.sh && git_pull_all -t'
+alias clip='safepaste | vim -'
 if test -f "$HOME/.personal"
     alias s='ssh_start && begin; egit -p || egitn; end && echo "Auto" && sgitt -cp'
 else
