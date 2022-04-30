f169997c780316947a32f953ffd538bf3088c0bb
Roben Kleene <contact@robenkleene.com>
2022-04-30 12:35:58 -0400

    Remove awk

--- scripts/clipboard_history_save.sh
+++ scripts/clipboard_history_save.sh
@@ -7,5 +7,7 @@ max_size=10000
 
 head --bytes=10000 | tr '\n' '\0' >> "$clipboard_history"
 temp_file=$(mktemp "${TMPDIR:-/tmp}/clipboard_history.XXXX")
-tail -n "$max_size" < "$clipboard_history" | awk '!x[$0]++' > "$temp_file"; mv "$temp_file" "$clipboard_history"
+# awk on macOS doesn't strips everything after a null byte
+# tail -n "$max_size" < "$clipboard_history" | awk '!x[$0]++' > "$temp_file"; mv "$temp_file" "$clipboard_history"
+tail -n "$max_size" < "$clipboard_history" > "$temp_file"; mv "$temp_file" "$clipboard_history"
 echo >> "$clipboard_history"
