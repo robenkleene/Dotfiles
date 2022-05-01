46dd47c908ecebd6bb10478cbc4a644d784ba01e
Roben Kleene <contact@robenkleene.com>
2022-04-30 21:31:34 -0400

    Fix copying

--- scripts/safecopy.sh
+++ scripts/safecopy.sh
@@ -23,17 +23,18 @@ while getopts ":sh" option; do
   esac
 done
 
-if [ -n "${TMUX:-}" ]; then
-  if [[ "$skip_history" == "true" ]]; then
-    tr '\0' '\n' | sed s'/⏎$//' | tmux loadb -
-  else
-    sed s'/⏎$//' | tee >(~/.bin/clipboard_history_save) | tmux loadb -
-  fi
-elif [[ "$(uname)" == "Darwin" ]]; then
+# macOS has to go first to be able to copy from tmux to macOS
+if [[ "$(uname)" == "Darwin" ]]; then
   if [[ "$skip_history" == "true" ]]; then
     tr '\0' '\n' | pbcopy
   else
     tee >(~/.bin/clipboard_history_save) | pbcopy
+  fi
+elif [ -n "${TMUX:-}" ]; then
+  if [[ "$skip_history" == "true" ]]; then
+    tr '\0' '\n' | sed s'/⏎$//' | tmux loadb -
+  else
+    sed s'/⏎$//' | tee >(~/.bin/clipboard_history_save) | tmux loadb -
   fi
 else
   cat >/dev/null
