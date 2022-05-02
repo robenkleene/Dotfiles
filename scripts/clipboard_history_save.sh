28d8e3c3037f322400593321c5cbb765745c29a9
Roben Kleene <contact@robenkleene.com>
2022-05-02 22:17:11 +0000

    Fix clipboard history save

--- scripts/clipboard_history_save.sh
+++ scripts/clipboard_history_save.sh
@@ -7,7 +7,7 @@ max_line_bytes=10000
 max_file_bytes=10000000
 resize_file_bytes=1000000
 
-{ echo; head --bytes=$max_line_bytes; } | tr '\n' '\0' | grep -a . >> "$clipboard_history"
+{ echo; head --bytes=$max_line_bytes | tr '\n' '\0'; } | grep -a . >> "$clipboard_history"
 temp_file=$(mktemp "${TMPDIR:-/tmp}/clipboard_history.XXXX")
 # awk on macOS doesn't strips everything after a null byte
 # tail -n "$max_size" < "$clipboard_history" | awk '!x[$0]++' > "$temp_file"; mv "$temp_file" "$clipboard_history"
