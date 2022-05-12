60d0dceab8535d2df86a07ecabb91cc2f32d8d52
Roben Kleene <contact@robenkleene.com>
2022-05-12 04:43:37 -0400

    Add example custom helm code

--- emacs.d/robenkleene/packages/robenkleene-helm.el
+++ emacs.d/robenkleene/packages/robenkleene-helm.el
@@ -145,6 +145,99 @@ directory."
     (robenkleene/helm-recursive-find-file-or-dir "~/Documentation/")
     )
 
+  ;; (defun robenkleene/helm-fd-transformer (candidates _source)
+  ;;   "A helm transformer to process fd result CANDIDATES."
+  ;;   (let (non-essential
+  ;;         (default-directory (helm-default-directory)))
+  ;;     (cl-loop for i in candidates
+  ;;              for abs = (helm-aif (file-remote-p default-directory)
+  ;;                                  (concat it (file-relative-name i))
+  ;;                                  (file-relative-name i))
+  ;;              for type = (car (file-attributes abs))
+  ;;              for disp = (if (and helm-ff-transformer-show-only-basename
+  ;;                                  (not (string-match "[.]\\{1,2\\}$" i)))
+  ;;                             (helm-basename abs) abs)
+  ;;              collect (cond ((eq t type)
+  ;;                             (cons (propertize disp 'face 'helm-ff-directory)
+  ;;                                   abs))
+  ;;                            ((stringp type)
+  ;;                             (cons (propertize disp 'face 'helm-ff-symlink)
+  ;;                                   abs))
+  ;;                            (t (cons (propertize disp 'face 'helm-ff-file)
+  ;;                                     abs))))))
+  
+  ;; (defun robenkleene/helm-fd--build-cmd-line ()
+  ;;   "Build the fd command line from current `helm-pattern'."
+  ;;   (let* ((default-directory (or (file-remote-p default-directory 'localname)
+  ;;                                 default-directory))
+  ;;          (patterns+options (split-string helm-pattern "\\(\\`\\| +\\)\\* +"))
+  ;;          (patterns (split-string (car patterns+options)))
+  ;;          (additional-options (and (cdr patterns+options)
+  ;;                                   (list (concat (cadr
+  ;;                                                  patterns+options) " ")))))
+  ;;     (concat "fd"
+  ;;             " --type f --follow --hidden --exclude .git --exclude .hg --exclude .DS_Store . "
+  ;;             (shell-quote-argument (expand-file-name default-directory)) " "
+  ;;             (string-join additional-options " "))))
+  
+  ;; (defun robenkleene/helm-fd-shell-command-fn ()
+  ;;   "Run and supervise the fd subprocess."
+  ;;   (let* (process-connection-type
+  ;;          non-essential
+  ;;          (cmd (robenkleene/helm-fd--build-cmd-line))
+  ;;          (proc (start-file-process-shell-command "hfd" helm-buffer cmd)))
+  ;;     (helm-log "fd command:\n%s" cmd)
+  ;;     (prog1 proc
+  ;;       (set-process-sentinel
+  ;;        proc
+  ;;        (lambda (process event)
+  ;;          (helm-process-deferred-sentinel-hook
+  ;;           process event (helm-default-directory))
+  ;;          (if (string= event "finished\n")
+  ;;              (with-helm-window
+  ;;               (setq mode-line-format
+  ;;                     '(" " mode-line-buffer-identification " "
+  ;;                       (:eval (format "L%s" (helm-candidate-number-at-point)))
+  ;;                       " "
+  ;;                       (:eval (propertize
+  ;;                               (format "[fd process finished - (%s results)]"
+  ;;                                       (max (1- (count-lines
+  ;;                                                 (point-min) (point-max)))
+  ;;                                            0))
+  ;;                               'face 'helm-locate-finish))))
+  ;;               (force-mode-line-update))
+  ;;            (helm-log "Error: fd %s"
+  ;;                      (replace-regexp-in-string "\n" "" event))))))))
+  
+  ;; ;; (defvar robenkleene/helm-fd-map
+  ;; ;;   (let ((map (make-sparse-keymap)))
+  ;; ;;     (set-keymap-parent map helm-generic-files-map)
+  ;; ;;     (define-key map (kbd "DEL") 'helm-delete-backward-no-update)
+  ;; ;;     map))
+  
+  ;; (defvar robenkleene/helm-fd-source
+  ;;   (helm-build-async-source "fd"
+  ;;                            :header-name (lambda (name)
+  ;;                                           (concat name " in [" (helm-default-directory) "]"))
+  ;;                            :candidates-process 'robenkleene/helm-fd-shell-command-fn
+  ;;                            :filtered-candidate-transformer 'robenkleene/helm-fd-transformer
+  ;;                            :action-transformer 'helm-transform-file-load-el
+  ;;                            :persistent-action 'helm-ff-kill-or-find-buffer-fname
+  ;;                            :action 'helm-type-file-actions
+  ;;                            :help-message 'helm-generic-file-help-message
+  ;;                            ;; :keymap robenkleene/helm-fd-map
+  ;;                            :candidate-number-limit 9999
+  ;;                            :requires-pattern 3))
+  
+  ;; (defun robenkleene/helm-fd (dir)
+  ;;   "Run `fd' in DIR."
+  ;;   (let ((default-directory (file-name-as-directory dir)))
+  ;;     (helm :sources 'robenkleene/helm-fd-source
+  ;;           :buffer "*helm fd*"
+  ;;           :ff-transformer-show-only-basename nil
+  ;;           :case-fold-search helm-file-name-case-fold-search))
+  ;;   )
+  
   ;; (defun robenkleene/helm-clipboard-history-copy ()
   ;;   "Copy from clipboard history."
   ;;   (interactive)
