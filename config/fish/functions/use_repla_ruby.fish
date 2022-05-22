ad8664eac30eca77248b73c424343cb82ddd213a
Roben Kleene <contact@robenkleene.com>
2022-05-22 05:12:50 -0400

    Work on use repla ruby

--- config/fish/functions/use_repla_ruby.fish
+++ config/fish/functions/use_repla_ruby.fish
@@ -1,4 +1,7 @@
 function use_repla_ruby
     set PATH ~/Temp/rake_test/ $PATH
     set PATH /Applications/Repla.app/Contents/Resources/bin $PATH
+    set --query GEM_PATH && set --erase GEM_PATH
+    set --query GEM_HOME && set --erase GEM_HOME
+    set --query RUBY_ROOT && set --erase RUBY_ROOT
 end
