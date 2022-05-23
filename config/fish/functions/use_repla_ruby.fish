61126d816a3b48061dd26d0485e1f9bc2369d2de
Roben Kleene <contact@robenkleene.com>
2022-05-23 04:57:48 -0400

    Fix bad status code

--- config/fish/functions/use_repla_ruby.fish
+++ config/fish/functions/use_repla_ruby.fish
@@ -1,7 +1,7 @@
 function use_repla_ruby
     set PATH ~/Temp/rake_test/ $PATH
     set PATH /Applications/Repla.app/Contents/Resources/bin $PATH
-    set --query GEM_PATH && set --erase GEM_PATH
-    set --query GEM_HOME && set --erase GEM_HOME
-    set --query RUBY_ROOT && set --erase RUBY_ROOT
+    set --query GEM_PATH && set --erase GEM_PATH || true
+    set --query GEM_HOME && set --erase GEM_HOME || true
+    set --query RUBY_ROOT && set --erase RUBY_ROOT || true
 end
