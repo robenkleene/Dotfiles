5d0d7313db3ba16fa8619746fb8a3b9ceed1bc87
Roben Kleene <contact@robenkleene.com>
2022-05-25 09:12:58 -0400

    Use purple match color

--- scripts/rg_reload.sh
+++ scripts/rg_reload.sh
@@ -3,5 +3,5 @@
 set -e
 
 if [[ ${#1} -gt 1 ]] || [[ $# -gt 1 ]]; then
-  exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:black' --colors 'match:bg:cyan' --colors 'path:fg:cyan' --colors 'line:fg:white' --pretty -- "$@"
+  exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' --pretty -- "$@"
 fi
