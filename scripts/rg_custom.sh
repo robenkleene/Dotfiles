da5450690a59b05e6fc9e0aeb4413bc81c59e65c
Roben Kleene <contact@robenkleene.com>
2022-05-25 09:11:05 -0400

    Use match color

--- scripts/rg_custom.sh
+++ scripts/rg_custom.sh
@@ -5,9 +5,9 @@ set -e
 # Only use colors in an interactive shell
 if [ -t 1 ]; then
   if [ "$TERM" = "dumb" ]; then
-    exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:black' --colors 'match:bg:cyan' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@"
+    exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@"
   else
-    exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:black' --colors 'match:bg:cyan' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@" #| less -RFX
+    exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@" #| less -RFX
   fi
 else
     exec rg --with-filename --smart-case --line-number "$@"
