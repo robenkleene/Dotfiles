7d8cf8cf63236edb34d1ae315031f55ba2fe2b21
Roben Kleene <contact@robenkleene.com>
2022-06-12 21:14:39 -0400

    Work on Spotlight block list

--- install/files/spotlight.sh
+++ install/files/spotlight.sh
@@ -2,10 +2,48 @@
 
 set -euo pipefail
 
-# 1. Ignore all /Applications folders that don't have a `.app` in them
-
-# Also list all the excluded paths that don't exist
-
+blacklist="
+/Applications/Adobe After Effects 2022/Plug-ins/
+/Applications/Adobe After Effects 2022/Presets/
+/Applications/Adobe After Effects 2022/Scripts/
+/Applications/Adobe Illustrator 2022/Cool Extras.localized/
+/Applications/Adobe Illustrator 2022/Plug-ins.localized/
+/Applications/Adobe Illustrator 2022/Presets.localized/
+/Applications/Adobe Illustrator 2022/Scripting.localized/
+/Applications/Adobe Illustrator 2022/Support Files/
+/Applications/Adobe Photoshop 2022/Configuration/
+/Applications/Adobe Photoshop 2022/Locales/
+/Applications/Adobe Photoshop 2022/Plug-ins/
+/Applications/Adobe Photoshop 2022/Presets/
+/Applications/Houdini/Houdini19.0.589/Administrative Tools/
+/Applications/Houdini/Houdini19.0.589/Frameworks/
+/Applications/Maxon Cinema 4D R25/corelibs/
+/Applications/Maxon Cinema 4D R25/Exchange Plugins/
+/Applications/Maxon Cinema 4D R25/help/
+/Applications/Maxon Cinema 4D R25/library/
+/Applications/Maxon Cinema 4D R25/resource/
+/Applications/Maxon Cinema 4D R26/corelibs/
+/Applications/Maxon Cinema 4D R26/Exchange Plugins/
+/Applications/Maxon Cinema 4D R26/help/
+/Applications/Maxon Cinema 4D R26/library/
+/Applications/Maxon Cinema 4D R26/plugins/
+/Applications/Maxon Cinema 4D R26/Redshift/
+/Applications/Maxon Cinema 4D R26/resource/
+/Applications/Native Instruments/Reaktor 6/Library/
+/Applications/Native Instruments/Reaktor 6/Documentation/
+/Applications/Native Instruments/Maschine 2/Documentation/
+/Applications/Native Instruments/Komplete Kontrol/Documentation/
+/Applications/Native Instruments/Controller Editor/Template Support Files/
+/Applications/Native Instruments/Controller Editor/Documentation/
+/Applications/redshift/
+/Applications/Utilities/Adobe Application Manager/
+/Applications/Utilities/Adobe Creative Cloud/
+/Applications/Utilities/Adobe Creative Cloud Experience/
+/Applications/Utilities/Adobe Installers/
+/Applications/Utilities/Adobe Sync/
+/Users/robenkleene/Developer/
+/Users/Shared/
+"
 raw=$(sudo /usr/libexec/PlistBuddy -c "Print :Exclusions" /System/Volumes/Data/.Spotlight-V100/VolumeConfiguration.plist | sed 's/^[ \t]*//;s/[ \t]*$//' || echo "Error: This terminal probably doesn't have full disk access" >&2)
 
 current=""
