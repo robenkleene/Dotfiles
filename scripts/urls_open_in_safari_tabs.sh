30c59a2818b56fa0a98d26b04b780801e768a819
Roben Kleene <contact@robenkleene.com>
2022-07-07 14:16:07 +0000

    Add open in Safari tabs

---
+++ scripts/urls_open_in_safari_tabs.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+urls="{"
+while read -r line; do
+  urls=$urls\"${line}\", 
+done <<< "$(~/.bin/urls)"
+
+urls=${urls%??}
+urls=$urls\"}
+
+if [[ "$urls" == "{}" ]]; then
+  echo "No urls"
+  exit 1
+fi
+
+osascript <<-APPLESCRIPT
+set URLs to $urls
+tell application "Safari"
+	activate
+	make new document with properties {URL:item 1 of URLs}
+	tell window 1
+		repeat with theURL in rest of the URLs
+			make new tab with properties {URL:theURL}
+		end repeat
+	end tell
+end tell
+APPLESCRIPT
