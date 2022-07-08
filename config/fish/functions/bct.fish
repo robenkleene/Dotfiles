345b61b7300c10f35d33866dc803b68bc52ed38e
Roben Kleene <contact@robenkleene.com>
2022-07-08 00:11:50 +0000

    Update bct

--- config/fish/functions/bct.fish
+++ config/fish/functions/bct.fish
@@ -2,12 +2,13 @@ function bct --argument-names 'term'
 
     cd ~/Documents/Text/Notes/Bucket
 
-    set -l cmd
-    if test -z "$term"
-        set cmd "fd --type f ."
-    else
-        set cmd "rg \"$term\""
-    end
+    # set -l cmd
+    # if test -z "$term"
+    #     set cmd "fd --type f ."
+    # else
+    #     set cmd "rg \"$term\""
+    # end
+    set -l cmd "rg \"^\#\w+\""
     set -l commandline (commandline)
     eval "$cmd | "(__fzfcmd) | read -l result
     if test -e "$result"
