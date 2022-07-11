0645590c6b03dee212fa2742c699e6278bdb6280
Roben Kleene <contact@robenkleene.com>
2022-07-11 18:05:38 +0000

    Update result path

--- config/fish/functions/bct.fish
+++ config/fish/functions/bct.fish
@@ -11,8 +11,9 @@ function bct --argument-names 'term'
     set -l cmd "rg \"^\#\w+\""
     set -l commandline (commandline)
     eval "$cmd | "(__fzfcmd) | read -l result
-    if test -e "$result"
-        cat $result | ~/.bin/url_open
+    set result_path (echo "$result" | cut -d: -f1)
+    if test -e "$result_path"
+        cat $result_path | ~/.bin/url_open
     end
 end
 
