e9d48debe1f52621fe146442348f5aa95d6997bf
Roben Kleene <contact@robenkleene.com>
2022-07-16 20:30:27 +0000

    Make vim cd take arguments

--- config/fish/functions/vim_cd.fish
+++ config/fish/functions/vim_cd.fish
@@ -1,6 +1,6 @@
 function vim_cd
   set -l tempfile '/tmp/vim.robenkleene/chdir/chdir'
-  eval $VIM_COMMAND ./
+  eval $VIM_COMMAND $argv
   if test -f $tempfile
     if test (cat $tempfile) != (pwd)
       cd (cat $tempfile)
