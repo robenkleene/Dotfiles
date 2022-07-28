1cf19c6eca2d72d88ef48e2d131606d45f4023b8
Roben Kleene <contact@robenkleene.com>
2022-07-28 17:43:37 +0000

    Re-enable pager

--- scripts/rg_custom.sh
+++ scripts/rg_custom.sh
@@ -7,7 +7,7 @@ if [ -t 1 ]; then
   if [ "$TERM" = "dumb" ]; then
     exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@"
   else
-    exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@" #| less -RFX
+    exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@" | less -RFX
   fi
 else
     exec rg --with-filename --smart-case --line-number "$@"
