33cbc47f438c139776d2ee9afc153dc74cd9cd0c
Roben Kleene <contact@robenkleene.com>
2022-09-23 01:20:41 +0000

    Create file if missing

--- config/fish/functions/emacs_cd.fish
+++ config/fish/functions/emacs_cd.fish
@@ -1,5 +1,9 @@
 function emacs_cd
   set -l tempfile '/tmp/vim.robenkleene/chdir/chdir'
+  if ! test -f $tempfile
+    mkdir -p /tmp/vim.robenkleene/chdir/
+    touch $tempfile
+  end
   eval $EMACS_COMMAND $argv
   if test -f $tempfile
     if test (cat $tempfile) != (pwd)
