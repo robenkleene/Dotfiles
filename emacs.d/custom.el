fe93865c2c91719bd477541c4de5d90b292ecd9f
Roben Kleene <contact@robenkleene.com>
2022-09-24 13:51:17 -0400

    Touch file

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -5,6 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#000000" "#D32D1E" "#62D63F" "#E5E64B" "#005FFF" "#af00df" "#4AA4B0" "#ffffff"])
+ '(custom-safe-themes
+   '("ad114fd8820edf7d947f356a237468ae2c402565a60b813ee36f6cd3f1d325ae" "c6fb2c37975e0d8fcc91ae7501f06bc9c58f3fb55e658be0fce30cd58bd66f3e" default))
+ '(helm-fd-switches
+   '("--follow" "--hidden" "--exclude" ".git" "--exclude" ".hg" "--exclude" ".DS_Store" "--color" "never"))
  '(package-selected-packages
    '(crux evil-numbers evil-commentary helm-swoop helm evil-goggles evil-terminal-cursor-changer evil-surround evil-visualstar evil expand-region diff-hl bind-key use-package undo-tree super-save flimenu move-text lsp-ui lsp-mode imenu-list literate-calc-mode nodejs-repl csv-mode ledger-mode fish-mode markdown-mode swift-mode inf-ruby enh-ruby-mode slime company page-break-lines smartparens rainbow-mode rainbow-delimiters magit yasnippet ido-completing-read+ flycheck editorconfig anzu iedit js2-mode fasd macrostep magit-filenotify bind-map wgrep smex jade-mode aggressive-indent))
  '(vc-annotate-color-map
