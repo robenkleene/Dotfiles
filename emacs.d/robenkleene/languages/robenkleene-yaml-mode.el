003ac346ef8a31c6f0547b6560b5fb923ee116dc
Roben Kleene <contact@robenkleene.com>
2022-09-24 14:30:42 -0400

    ADd YAML mode

---
+++ emacs.d/robenkleene/languages/robenkleene-yaml-mode.el
@@ -0,0 +1,14 @@
+;;; robenkleene-yaml-mode.el --- robenkleene-yaml-mode
+;;; Commentary:
+;;; Code:
+
+(use-package yaml-mode :ensure t)
+
+(provide 'robenkleene-yaml-mode)
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;;; robenkleene-yaml-mode.el ends here
