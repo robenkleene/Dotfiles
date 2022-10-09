82ba2713d4c62867090e8556183771bdf50898fb
Roben Kleene <contact@robenkleene.com>
2022-10-08 22:26:46 -0400

    Setup crates install

--- install/rust/install.sh
+++ install/rust/install.sh
@@ -5,3 +5,4 @@ set -euo pipefail
 # `-y`: Disable confirmation
 # `--no-modify-path`: Don't customize path
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
+./crates/install.sh
