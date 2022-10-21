62532e3124d7426ee2deb04c7ce5363046f2ecb7
Roben Kleene <contact@robenkleene.com>
2022-10-21 03:02:44 +0000

    Break up more config files

---
+++ config/nvim/plugin/bindings.lua
@@ -0,0 +1,6 @@
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
