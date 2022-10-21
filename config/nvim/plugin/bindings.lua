6ec84eadf285f38d3f61af9972acf9b67cc4faa2
Roben Kleene <contact@robenkleene.com>
2022-10-21 09:41:03 -0400

    Set leader first

--- config/nvim/plugin/bindings.lua
+++ config/nvim/plugin/bindings.lua
@@ -1,11 +1,11 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
