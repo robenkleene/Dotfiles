c7f0c15ec3947fc7747c67a7a7c5ed6ea624b605
Roben Kleene <contact@robenkleene.com>
2022-10-22 13:44:18 +0000

    Work on snippets

--- config/nvim/plugin/plugins/cmp-luasnip.lua
+++ config/nvim/plugin/plugins/cmp-luasnip.lua
@@ -1,6 +1,7 @@
 -- nvim-cmp setup
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
+-- require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/Code/User/snippets" } })
 
 cmp.setup {
   snippet = {
