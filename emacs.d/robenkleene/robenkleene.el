e4ac97fed186a64b717476152a224fc576fd438f
Roben Kleene <contact@robenkleene.com>
2022-10-24 14:19:27 -0400

    Add back page break lines

--- emacs.d/robenkleene/robenkleene.el
+++ emacs.d/robenkleene/robenkleene.el
@@ -44,6 +44,7 @@
 (require 'robenkleene-helm)
 (require 'robenkleene-eglot)
 (require 'robenkleene-autothemer)
+(require 'robenkleene-page-break-lines)
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/robenkleene/themes/")
 ;; (load-theme 'kleene-dark t)
