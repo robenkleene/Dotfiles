49b0314ed9c556010b3baf5682efb2f98d596600
Roben Kleene <contact@robenkleene.com>
2022-10-25 15:06:48 +0000

    Add blame

--- config/fish/functions/fish_sync_abbr.fish
+++ config/fish/functions/fish_sync_abbr.fish
@@ -106,6 +106,7 @@ function fish_sync_abbr
     abbr -a gdns 'git diff --name-status'
     abbr -a gsh 'git show'
     abbr -a gg 'git grep'
+    abbr -a gbl 'git blame'
     abbr -a gcleanup 'git checkout -- . && git clean -dff'
     abbr -a chu 'carthage update'
     abbr -a chunub 'carthage update --no-use-binaries'
