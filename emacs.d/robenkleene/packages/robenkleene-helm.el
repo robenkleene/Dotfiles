eacc7f3acf75e1a5e313bf7933008f824f609dd3
Roben Kleene <contact@robenkleene.com>
2022-10-28 15:39:05 +0000

    Disable helm bindings

--- emacs.d/robenkleene/packages/robenkleene-helm.el
+++ emacs.d/robenkleene/packages/robenkleene-helm.el
@@ -18,9 +18,9 @@
   ;; ("M-A" . helm-do-rg)
   ;; ("M-a" . robenkleene/helm-ag-in-directory)
   ;; ("C-x b" . helm-buffers-list)
-  ("M-o" . robenkleene/helm-recursive-find-file)
-  ("M-c" . robenkleene/helm-recursive-find-dir)
-  ("M-z" . robenkleene/helm-z)
+  ;; ("M-o" . robenkleene/helm-recursive-find-file)
+  ;; ("M-c" . robenkleene/helm-recursive-find-dir)
+  ;; ("M-z" . robenkleene/helm-z)
   (:map robenkleene/leader-map
         ;; ("h" . helm-resume)
         ("'" . helm-resume)
@@ -175,21 +175,21 @@ directory."
 
   (defvar robenkleene/helm-clipboard-history-copy-source
     (helm-build-async-source
-     "helm clipboard history"
-     :candidates-process
-     'robenkleene/helm-clipboard-history-candidates-process
-     :action (lambda (candidate)
-               (kill-new (string-replace "\0" "\n" candidate)))
-     ))
+        "helm clipboard history"
+      :candidates-process
+      'robenkleene/helm-clipboard-history-candidates-process
+      :action (lambda (candidate)
+                (kill-new (string-replace "\0" "\n" candidate)))
+      ))
 
   (defvar robenkleene/helm-clipboard-history-insert-source
     (helm-build-async-source
-     "helm clipboard history"
-     :candidates-process
-     'robenkleene/helm-clipboard-history-candidates-process
-     :action (lambda (candidate)
-               (insert (string-replace "\0" "\n" candidate)))
-     ))
+        "helm clipboard history"
+      :candidates-process
+      'robenkleene/helm-clipboard-history-candidates-process
+      :action (lambda (candidate)
+                (insert (string-replace "\0" "\n" candidate)))
+      ))
 
   (defun robenkleene/helm-clipboard-history-copy ()
     (interactive)
