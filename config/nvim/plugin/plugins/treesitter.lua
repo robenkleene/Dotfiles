b5e821a0fdd24446920cfee60dbd62a0fdc215c2
Roben Kleene <contact@robenkleene.com>
2022-11-01 21:05:40 +0000

    Add some languages

--- config/nvim/plugin/plugins/treesitter.lua
+++ config/nvim/plugin/plugins/treesitter.lua
@@ -1,6 +1,19 @@
 require('nvim-treesitter.configs').setup {
   -- `query` for treesitter playground
-  ensure_installed = { 'c', 'cpp', 'lua', 'markdown', 'markdown_inline', 'python', 'query', 'rust', 'typescript' },
+  ensure_installed = {
+    'bash',
+    'c',
+    'cpp',
+    'lua',
+    'html',
+    'markdown',
+    'markdown_inline',
+    'python',
+    'query',
+    'rust',
+    'toml',
+    'typescript'
+  },
   highlight = { enable = true },
   indent = { enable = true },
   -- Trying to customize Markdown colors, this doesn't work for some reason
