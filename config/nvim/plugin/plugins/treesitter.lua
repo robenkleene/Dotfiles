5db6a00cc3c4847a6be492faa2a40c87b9033f9c
Roben Kleene <contact@robenkleene.com>
2022-11-01 16:45:47 +0000

    Add color attempt

--- config/nvim/plugin/plugins/treesitter.lua
+++ config/nvim/plugin/plugins/treesitter.lua
@@ -3,4 +3,9 @@ require('nvim-treesitter.configs').setup {
   ensure_installed = { 'c', 'cpp', 'lua', 'markdown', 'markdown_inline', 'python', 'query', 'rust', 'typescript' },
   highlight = { enable = true },
   indent = { enable = true },
+  -- Trying to customize Markdown colors, this doesn't work for some reason
+  custom_captures = {
+    ["text.uri"] = "Comment",
+    ["punctuation.delimiter"] = "Comment",
+  }
 }
