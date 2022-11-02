11dcec7bcea065487e9222d2ed76b3cc3edb6577
Roben Kleene <contact@robenkleene.com>
2022-11-02 00:56:49 +0000

    Cleanup

--- config/nvim/plugin/plugins/telescope.lua
+++ config/nvim/plugin/plugins/telescope.lua
@@ -1,5 +1,3 @@
--- [[ Configure Telescope ]]
--- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
     mappings = {
@@ -11,21 +9,9 @@ require('telescope').setup {
   },
 }
 
--- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
--- See `:help telescope.builtin`
--- vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers)
-vim.keymap.set('n', '<leader>l', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end)
+vim.keymap.set('n', '<leader>l', require('telescope.builtin').current_buffer_fuzzy_find)
 vim.keymap.set('n', '<leader>F', require('telescope.builtin').find_files)
 vim.keymap.set('n', '<leader>/', require('telescope.builtin').live_grep)
--- vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags)
--- vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string)
--- vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics)
