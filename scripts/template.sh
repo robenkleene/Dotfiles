5c1a6462d89285ab2075aae3275b0f859a314fa1
Roben Kleene <contact@robenkleene.com>
2022-11-05 22:18:04 +0000

    Improve template

--- scripts/template.sh
+++ scripts/template.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 path="$1"
 extension="${path##*.}"
 
-template=$(find ~/Developer/Snippets/Templates -type f -name "*.${extension}" | tail -1)
+template=$(find ~/Developer/Snippets/Templates -type f -name "Template.${extension}" | tail -1)
 if [[ -z $template ]]; then
   echo "Error: No template found for $extension" >&2
   exit 1
