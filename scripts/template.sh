30bd744f59874ae6040009dcf1a9e603e00821c2
Roben Kleene <contact@robenkleene.com>
2022-11-05 22:12:12 +0000

    Work on template

--- scripts/template.sh
+++ scripts/template.sh
@@ -2,24 +2,7 @@
 
 set -euo pipefail
 
-while getopts ":p:h" option; do
-  case "$option" in
-    p)
-      file_path="$OPTARG"
-      ;;
-    h)
-      echo "Usage: command [-hf] [-p <file_path>]"
-      exit 0
-      ;;
-    :)
-      echo "Option -OPTARG requires an argument" >&2
-      exit 1
-      ;;
-    \?)
-      echo "Invalid option: -OPTARG" >&2
-      exit 1
-      ;;
-  esac
-done
+path="$1"
+extension="${path##*.}"
 
-
+cat $(find ~/Developer/Snippets/Templates -type f -name "*.${extension}")
