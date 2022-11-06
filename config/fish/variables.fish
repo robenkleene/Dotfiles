594dacdf410d1a3b470e4f8ce96e5af138126fb3
Roben Kleene <contact@robenkleene.com>
2022-11-06 03:28:07 +0000

    Enable Helix

--- config/fish/variables.fish
+++ config/fish/variables.fish
@@ -13,10 +13,10 @@ set -gx VIM_COMMAND nvim
 # set -gx VIM_COMMAND "nvim"
 set -gx CAT_COMMAND "bat"
 set -gx MD_CAT_COMMAND "bat --style plain"
-set -gx VISUAL "$VIM_COMMAND"
+set -gx VISUAL "hx"
+# set -gx VISUAL "$VIM_COMMAND"
 # set -gx VISUAL "$EMACS_COMMAND"
 # `hx` doesn't support jumping to a line number from `tig` for example
-# set -gx VISUAL "hx"
 # set -gx CD_COMMAND "emacs_cd"
 set -gx CD_COMMAND "vim_cd"
 
