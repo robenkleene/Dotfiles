8fc0e37baa57a5e15d3212ca24b9c2d969d10319
Roben Kleene <contact@robenkleene.com>
2022-11-08 18:14:16 +0000

    Try again to prevent rg custom while piping

--- scripts/rg_custom.sh
+++ scripts/rg_custom.sh
@@ -3,7 +3,10 @@
 set -e
 
 # Only use colors in an interactive shell
-if [[ -t 1 ]]; then
+if [[ ! -t 0 ]]; then
+  # Don't show any decorations when piping to `rg`
+  exec rg --smart-case "$@"
+elif [[ -t 1 ]]; then
   if [[ "$TERM" = "dumb" ]]; then
     exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@"
   else
