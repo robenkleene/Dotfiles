26dd6462150008ebc10b9380681dfa4c2d31bdc4
Roben Kleene <contact@robenkleene.com>
2022-11-08 20:43:15 +0000

    Clean up rg

--- scripts/rg_custom.sh
+++ scripts/rg_custom.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-# Only use colors in an interactive shell
 if [[ ! -t 0 ]]; then
   # Don't show any decorations when piping to `rg`
   exec rg --smart-case "$@"
 elif [[ -t 1 ]]; then
   if [[ "$TERM" = "dumb" ]]; then
-    exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@"
+    exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@"
   else
     # With pager
-    # exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@" | less -RFX
+    # exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@" | less -RFX
     # Without pager works nicer if a search is taking a long time and we need to cancel
-    exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@"
+    exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@"
   fi
 else
-  exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@"
+  # Remove colors when piping from rg
+  exec rg --no-heading --with-filename --smart-case --line-number "$@"
 fi
