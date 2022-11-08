42082d7de70afd8adb559cc7b2fcdf9981bd184a
Roben Kleene <contact@robenkleene.com>
2022-11-08 16:08:06 +0000

    Don't show decorations when piping to rg

--- scripts/rg_custom.sh
+++ scripts/rg_custom.sh
@@ -13,5 +13,6 @@ if [[ -t 1 ]]; then
     exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' -p "$@"
   fi
 else
+  # Don't show any decorations when piping to `rg`
   exec rg --smart-case "$@"
 fi
