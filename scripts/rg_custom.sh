506cc40f22b689e905aa96889926f93846c05153
Roben Kleene <contact@robenkleene.com>
2022-11-09 14:40:32 +0000

    Tweak rg

--- scripts/rg_custom.sh
+++ scripts/rg_custom.sh
@@ -3,18 +3,17 @@
 set -e
 
 if [[ ! -t 0 ]]; then
-  # Don't show any decorations when piping to `rg`
+  # Don't show any decorations when piping *to* `rg`
   exec rg --smart-case "$@"
 elif [[ -t 1 ]]; then
   if [[ "$TERM" = "dumb" ]]; then
+    # Emacs
     exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@"
   else
-    # With pager
-    # exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@" | less -RFX
-    # Without pager works nicer if a search is taking a long time and we need to cancel
-    exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@"
+    # Default
+    exec rg --smart-case --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@"
   fi
 else
-  # Remove colors when piping from rg
+  # Use grep format and no colors when piping *from* rg
   exec rg --no-heading --with-filename --smart-case --line-number "$@"
 fi
