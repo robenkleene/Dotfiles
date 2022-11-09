8679268e1f8fb5523267f3a3c2546a3f01c8a511
Roben Kleene <contact@robenkleene.com>
2022-11-09 09:57:50 -0500

    Remove clipboard history

--- scripts/clipboard_history_search.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-if [[ -n "${1:-}" ]]; then
-  tac ~/.clipboard_history | rg --text "$1"
-else
-  tac ~/.clipboard_history
-fi
--- scripts/safecopy.sh
+++ scripts/safecopy.sh
@@ -2,12 +2,8 @@
 
 set -euo pipefail
 
-skip_history="false"
 while getopts ":sh" option; do
   case "$option" in
-    s)
-      skip_history="true"
-      ;;
     h)
       echo "Usage: command [-hf] [-p <file_path>]"
       exit 0
@@ -25,17 +21,9 @@ done
 
 # macOS has to go first to be able to copy from tmux to macOS
 if [[ "$(uname)" == "Darwin" ]]; then
-  if [[ "$skip_history" == "true" ]]; then
-    tr '\0' '\n' | pbcopy
-  else
-    tee >(~/.bin/clipboard_history_save) | pbcopy
-  fi
+  exec tr '\0' '\n' | pbcopy
 elif [ -n "${TMUX:-}" ]; then
-  if [[ "$skip_history" == "true" ]]; then
-    tr '\0' '\n' | sed s'/⏎$//' | tmux loadb -
-  else
-    sed s'/⏎$//' | tee >(~/.bin/clipboard_history_save) | tmux loadb -
-  fi
+  tr '\0' '\n' | sed s'/⏎$//' | tmux loadb -
 else
   cat >/dev/null
 fi
