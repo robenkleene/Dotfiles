2db8bb156a2a31b708f7d5a591a207d7da8daab9
Roben Kleene <contact@robenkleene.com>
2022-11-10 03:26:42 +0000

    Improve title script

--- scripts/markdown_title.sh
+++ scripts/markdown_title.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
-filename=$(basename "$1")
-filename_no_ext=${filename%.*}
-script_dir=$(dirname $0)
-if [[ $filename_no_ext =~ ^[a-z0-9_-]*$ ]]; then
-    # If the filename contains only lowercase letters, hyphens, and underscores then assume
-    # convert it to title case with spaces.
-  title=$(echo $filename_no_ext | tr "-" " " | tr "_" " " | $script_dir/title_case)
+if [[ -e $1 ]]; then
+  filename=$(basename "$1")
+  filename_no_ext=${filename%.*}
+  script_dir=$(dirname "$0")
+  if [[ $filename_no_ext =~ ^[a-z0-9_-]*$ ]]; then
+      # If the filename contains only lowercase letters, hyphens, and underscores then assume
+      # convert it to title case with spaces.
+    title=$(echo "$filename_no_ext" | tr "-" " " | tr "_" " " | "$script_dir"/title_case)
+  else
+    title=$(echo "$filename_no_ext" | "$script_dir"/title_case)
+  fi
 else
-  title=$(echo $filename_no_ext | $script_dir/title_case)
+  # Allow just passing a title as a parameter
+  title="$1"
 fi
+
 echo "# $title"
 echo
