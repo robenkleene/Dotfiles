fd661f46a5375ba58bcd0b5f321ae5bfe01e3a42
Roben Kleene <contact@robenkleene.com>
2022-11-12 22:39:45 +0000

    Remove aliases

--- config/fish/alias.fish
+++ config/fish/alias.fish
@@ -41,8 +41,6 @@ alias u='ssh_start && egit -u && ~/Developer/Dotfiles/update.sh && fish_update'
 alias ut='ssh_git_pull_all -t'
 alias ua='ssh_git_pull_all'
 alias pull='ssh_start && egit -u && ~/Developer/Dotfiles/update.sh && git_pull_all -t'
-alias clip="safepaste | $VIM_COMMAND -"
-alias ch='clipboard_history'
 if test -f "$HOME/.personal"
     alias s='ssh_start && begin; egit -p || egitn; end && echo "Auto" && sgitt -cp'
 else
