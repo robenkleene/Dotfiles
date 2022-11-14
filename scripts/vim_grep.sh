b601ccbe185f7ed3585c91d98996a19fde74da45
Roben Kleene <contact@robenkleene.com>
2022-11-14 06:15:04 -0500

    Add default vim

--- scripts/vim_grep.sh
+++ scripts/vim_grep.sh
@@ -2,8 +2,4 @@
 
 set -e
 
-if [[ -n "$1" ]]; then
-  exec $ACK_COMMAND "$@" | $VIM_COMMAND -c "GrepBuffer" -
-else
-  exec $VIM_COMMAND -c "GrepBuffer" -
-fi
+exec ${VIM_COMMAND:=vim} -c "GrepBuffer" -
