d02b899346b76a36bee725d46b711cfcff3ea6cf
Roben Kleene <contact@robenkleene.com>
2022-11-17 03:06:57 +0000

    Remove plugins

--- zsh/plugins.zsh
+++
@@ -1,7 +0,0 @@
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
-bindkey -M emacs '^P' history-substring-search-up
-bindkey -M emacs '^N' history-substring-search-down
-
-# Make my custom abbreviations expanstion clear autosuggestions
-ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=magic-abbrev-expand-and-accept
