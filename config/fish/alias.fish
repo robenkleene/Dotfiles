af898ae98eab67c6d02a809f0ce2c28b82b7dc0c
Roben Kleene <contact@robenkleene.com>
2022-11-18 14:23:17 +0000

    Organize aliases

--- config/fish/alias.fish
+++ config/fish/alias.fish
@@ -1,77 +1,37 @@
-# "alias --save" creates an autoloaded function for an alias, but there's no
-# easy way to clean thes up later. E.g., "function --erase" still leaves the
-# file
-
-# Use wrapper file instead because piping from an alias to vim doesn't allow
-# backgrounding
-# alias p='safepaste'
+# Bash Script
 alias pv="safepaste | $VIM_COMMAND -"
 alias pq="safepaste | vim_grep"
 alias e='emacsclient_wrapper'
+alias esk='emacs_kill_server'
+alias esr='emacs_reset_server'
+alias eg='emacs_grep'
+alias vsr='vim_session_restore_auto'
+alias gpbo='git_push_branch_origin'
+alias scwp='source_control_open_site -p'
+alias scwr='BROWSER= gh pr view --web'
+alias gemacs="open -a emacs"
+alias ox='fzf_project_xcode'
+alias gua='git_pull_all'
+
+# Tags
+alias tags="generate_tags"
+
+# Function
+alias cdsc='sc_cd_root'
+alias gu='ssh_start && git pull'
+alias gp='ssh_start && git push'
+alias gur='ssh_start && git pull -r'
+alias d='$CD_COMMAND .'
 alias rg='rg_custom'
-
-# fd
-# Display hidden files by default
-# This doesn't work now because the `fzf` commands also add
-# hidden, we should be able to add the `--hidden` flag twice in a
-# future version of `fd` and we can uncomment this then.
-# alias fd='fd --hidden'
-
-# Fish
 alias foi='fish_edit_config'
 alias fsa='fish_sync_abbr'
 alias fu='fish_update'
-
 alias u='ssh_start && egit -u && ~/Developer/Dotfiles/update.sh && fish_update'
 alias ut='ssh_git_pull_all -t'
 alias ua='ssh_git_pull_all'
-alias pull='ssh_start && egit -u && ~/Developer/Dotfiles/update.sh && git_pull_all -t'
 if test -f "$HOME/.personal"
     alias s='ssh_start && begin; egit -p || egitn; end && echo "Auto" && sgitt -cp'
 else
     # Don't automatically commit on non-personal machines
     alias s='ssh_start && begin; egit -p || egitn; end && echo "Auto"; sgitt -p'
 end
-
-# `make`
-alias mt='make test'
-alias ml='make lint'
-alias mac='make autocorrect'
-
-# `node`
-alias nt='npm test'
-alias nrt='npm run test'
-alias nrl='npm run lint'
-alias nrf='npm run format'
-alias nrac='npm run autocorrect'
-
-# `emacs`
-alias esk='emacs_kill_server'
-alias esr='emacs_reset_server'
-alias eg='emacs_grep'
-
-# TUI change dir
-alias d='$CD_COMMAND .'
-
-# `vim`
-alias vsr='vim_session_restore_auto'
-
-# Xcode
-if test -n MACOS
-    alias gemacs="open -a emacs"
-    alias ox='fzf_project_xcode'
-end
-
-# `git`
-alias gu='ssh_start && git pull'
-alias gp='ssh_start && git push'
-alias gur='ssh_start && git pull -r'
-alias gpbo='git_push_branch_origin'
-alias scwp='source_control_open_site -p'
-alias scwr='BROWSER= gh pr view --web'
-
-# Tags
-alias tags="generate_tags"
-
-# Special
-alias cdsc='sc_cd_root'
--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,7 +1,23 @@
-# Basic
+# Abbr
+alias a='rg_custom'
+alias v='nvim'
+
+# Bash Script
+alias pv="safepaste | $VIM_COMMAND -"
+alias esk='emacs_kill_server'
+alias esr='emacs_reset_server'
+if [[ "$(uname)" = "Darwin" ]]; then
+  alias ox='fzf_project_xcode'
+fi
+alias gpbo='git_push_branch_origin'
+alias scwp='source_control_open_site -p'
+alias scwr='BROWSER= gh pr view --web'
+alias gua='git_pull_all'
+alias tags="generate_tags"
+
+# Alias
 alias -- -='cd -'
 alias ..='cd ..'
-
 if [[ "$(uname)" = "Linux" ]]; then
   alias ls='ls --color=auto'
 else
@@ -9,51 +25,18 @@ else
     alias ls='gls --color=auto'
   fi
 fi
-
-alias p='safepaste'
-alias y='safecopy'
-alias pv="safepaste | $VIM_COMMAND -"
-alias gemacs='emacs_app'
-
 alias zoi='zsh_edit_config'
-
+alias gemacs='emacs_app'
 alias rg='rg_custom'
-alias a='rg_custom'
-alias v='nvim'
-
-# `git`
 alias s='ssh_start && { egit -p || egitn } && echo "Auto" && sgitt -cp'
 alias u='ssh_start && egit -u && ~/Developer/Dotfiles/update.sh'
 alias ut='ssh_git_pull_all -t'
 alias ua='ssh_git_pull_all'
-
-# Emacs
-alias esk='emacs_kill_server'
-alias esr='emacs_reset_server'
-
-# `vim_cd`
 alias d='vim_cd'
-
-# Xcode
-if [[ "$(uname)" = "Darwin" ]]; then
-  alias ox='fzf_project_xcode'
-fi
-
-# `git`
 alias gu='ssh_start && git pull'
 alias gp='ssh_start && git push'
 alias gur='ssh_start && git pull -r'
 alias gpt='ssh_start && git push --tags'
-
 alias cdg='git_cd_root'
-alias gpbo='git_push_branch_origin'
-alias scwp='source_control_open_site -p'
-alias scwr='BROWSER= gh pr view --web'
-alias gcu='git_commit_update'
-alias gua='git_pull_all'
-
-# Tags
-alias tags="generate_tags"
-
-# Mercurial
 alias cdhg='cd (hg root)'
+alias gcu='git_commit_update'
