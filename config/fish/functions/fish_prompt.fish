d34b65aaa672b5fd8bcc393924e92fc57c78eeae
Roben Kleene <contact@robenkleene.com>
2022-11-19 16:56:00 +0000

    Cleanup prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,46 +1,37 @@
 function fish_prompt
-    set -l last_pipestatus $pipestatus
     set -l normal (set_color normal)
 
-    # If we're running via SSH, change the host color.
-    set -l color_host $fish_color_host
-    if set -q SSH_TTY
-        set color_host $fish_color_host_remote
-    end
-
-    # Write pipestatus
-    set -l prompt_status (__fish_print_pipestatus " " "?" "|" (set_color $fish_color_status) (set_color $fish_color_status) $last_pipestatus)
-    # Exclude zoxide background process
-    # set -l bg_jobs_count (count (jobs --command | grep -v '^zoxide$' || true))
-    set -l bg_jobs_count (count (jobs --command || true))
-
     set_color $fish_color_comment
-    echo -n (date "+%I:%M %p")
+    printf '%s' (date "+%I:%M %p")
     if test $SSH_CONNECTION
-        echo -n ' '
         # echo -n -s "$USER" $normal @ (set_color $color_host) (prompt_hostname) $normal
-        echo -n -s (set_color $color_host) (prompt_hostname) $normal
+        printf ' %s%s%s' (set_color $fish_color_host_remote) (prompt_hostname) $normal
     end
-    echo -n ' '
 
     set_color $fish_color_cwd
+    # Alternative for `git` integration
     # if test -e .git
     #     echo -n '.'
     # else
     #     echo -n (prompt_pwd)
     # end
-    echo -n (prompt_pwd)
+    printf ' %s' (prompt_pwd)
 
+    # Exclude zoxide background process
+    # set -l bg_jobs_count (count (jobs --command | grep -v '^zoxide$' || true))
+    set -l bg_jobs_count (count (jobs --command || true))
     if test $bg_jobs_count -gt 0
         set_color yellow
-        echo -n " &$bg_jobs_count"
+        printf ' &%s' $bg_jobs_count
     end
-    set_color normal
-    echo -n $prompt_status
 
-    echo -n ' > '
+    # Write pipestatus
+    set -l last_pipestatus $pipestatus
+    set -l prompt_status (__fish_print_pipestatus " " "?" "|" (set_color $fish_color_status) (set_color $fish_color_status) $last_pipestatus)
+    printf '%s%s > ' $prompt_status $normal
 end
 
+# `git` right prompt
 # function fish_right_prompt
 #     set_color cyan
 #     if test -e .git
