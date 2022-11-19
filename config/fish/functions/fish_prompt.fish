d125b6db0fef429c3d06d61faaaafe7e252913fa
Roben Kleene <contact@robenkleene.com>
2022-11-19 17:18:25 +0000

    Fix prompt status

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,4 +1,6 @@
 function fish_prompt
+    # Do this first to get the last pipe status before it changes
+    set -l last_pipestatus $pipestatus
     set -l normal (set_color normal)
 
     set_color $fish_color_comment
@@ -26,7 +28,6 @@ function fish_prompt
     end
 
     # Write pipestatus
-    set -l last_pipestatus $pipestatus
     set -l prompt_status (__fish_print_pipestatus " " "?" "|" (set_color $fish_color_status) (set_color $fish_color_status) $last_pipestatus)
     printf '%s%s > ' $prompt_status $normal
 end
