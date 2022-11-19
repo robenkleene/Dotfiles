3283eb21ee0f5302611fae5ec249ae864fb85d08
Roben Kleene <contact@robenkleene.com>
2022-11-19 10:43:50 +0000

    Work on lazy load

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -73,7 +73,24 @@ require('packer').startup(function(use)
       },
       { 'nvim-lua/plenary.nvim' },
     },
-    module = "telescope"
+    keys = {
+      '<leader>b',
+      '<leader>l',
+      '<leader>F',
+      '<leader>/',
+      '<leader>s',
+      '<leader>S',
+    },
+    config = function()
+      require('telescope').setup()
+      pcall(require('telescope').load_extension, 'fzf')
+      vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers)
+      vim.keymap.set('n', '<leader>l', require('telescope.builtin').current_buffer_fuzzy_find)
+      vim.keymap.set('n', '<leader>F', require('telescope.builtin').find_files)
+      vim.keymap.set('n', '<leader>/', require('telescope.builtin').live_grep)
+      vim.keymap.set('n', '<leader>s', require('telescope.builtin').lsp_document_symbols)
+      vim.keymap.set('n', '<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+    end
   }
   -- Languages
   use { 'dag/vim-fish', ft = { 'fish' }, }
