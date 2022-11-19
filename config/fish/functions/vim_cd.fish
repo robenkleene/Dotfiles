d4013a8fed817a1e207df7f230aa85a9e15fc297
Roben Kleene <contact@robenkleene.com>
2022-11-19 16:09:07 +0000

    Fix weird string issue

--- config/fish/functions/vim_cd.fish
+++ config/fish/functions/vim_cd.fish
@@ -1,13 +1,14 @@
 function vim_cd
-  set -l tempfile '/tmp/vim.robenkleene/chdir/chdir'
-  truncate -s 0 $tempfile
-  eval $VIM_COMMAND $argv
-  if test -f $tempfile
-    set -l result (cat $tempfile)
-    if test -n $result
-      if test "$result" != (pwd)
-        cd $result
-      end
+    set -l tempfile '/tmp/vim.robenkleene/chdir/chdir'
+    truncate -s 0 $tempfile
+    eval $VIM_COMMAND $argv
+    if test -f $tempfile
+        set -l result (cat $tempfile)
+        if test -n $result && \
+            test "$result" != (pwd) && \
+            string length $result && \
+            test -d $result
+            cd $result
+        end
     end
-  end
 end
