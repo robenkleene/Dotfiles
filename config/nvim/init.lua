f056c9ce87546e9a3408705513d3d013f83aa769
Roben Kleene <contact@robenkleene.com>
2022-11-20 20:36:47 +0000

    Tweak statusline

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -73,6 +73,24 @@ require('packer').startup(function(use)
       require('lualine').setup {
         options = {
           theme = 'catppuccin',
+          component_separators = '|',
+          section_separators = '',
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {'filename'},
+          lualine_x = {'location'},
+          lualine_y = {},
+          lualine_z = {}
         }
       }
     end
