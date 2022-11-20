18bbb31defd60e593d26d4669cd096dbcf290554
Roben Kleene <contact@robenkleene.com>
2022-11-20 21:09:13 +0000

    Switch to fidget

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -67,33 +67,10 @@ require('packer').startup(function(use)
       require("catppuccin").setup()
     end
   }
-  use 'arkav/lualine-lsp-progress'
   use {
-    'nvim-lualine/lualine.nvim',
+    'j-hui/fidget.nvim',
     config = function()
-      require('lualine').setup {
-        options = {
-          theme = 'catppuccin',
-          component_separators = '|',
-          section_separators = '',
-        },
-        sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'diagnostics'},
-          lualine_c = {'filename', 'lsp_progress'},
-          lualine_x = {'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'location'},
-          lualine_y = {},
-          lualine_z = {}
-        }
-      }
+      require"fidget".setup({})
     end
   }
   use 'nvim-treesitter/nvim-treesitter'
