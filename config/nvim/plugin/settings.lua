cbd796aa9e73a2d210c2180324879f84a10af1e5
Roben Kleene <contact@robenkleene.com>
2022-11-21 20:31:14 +0000

    Disable highlight

--- config/nvim/plugin/settings.lua
+++ config/nvim/plugin/settings.lua
@@ -26,14 +26,14 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
+-- local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+--   callback = function()
+--     vim.highlight.on_yank()
+--   end,
+--   group = highlight_group,
+--   pattern = '*',
+-- })
 
 -- Use system clipboard
 vim.o.clipboard = vim.o.clipboard .. "unnamedplus"
