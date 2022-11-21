8c9bd7af76fe789ed7b41ed243b3d3b0505cc0ee
Roben Kleene <contact@robenkleene.com>
2022-11-21 05:49:38 -0500

    Remove Alacritty hacks

--- scripts/safecopy.sh
+++ scripts/safecopy.sh
@@ -5,13 +5,14 @@ set -eo pipefail
 # macOS has to go first to be able to copy from tmux to macOS
 # It seems like Alacritty has it's own clipboard integration that conflicts
 # with this, so just disable everything if Alacritty
-if [[ -n $1 ]]; then
-  # Unset Alacritty if we're being piped to from the `y` command
-  unset ALACRITTY
-fi
+# if [[ -n $1 ]]; then
+#   # Unset Alacritty if we're being piped to from the `y` command
+#   unset ALACRITTY
+# fi
 # Chomp the last new line break which makes it safer to paste a single line
 # into the terminal.
-if [[ "$(uname)" == "Darwin" && -z $ALACRITTY ]]; then
+if [[ "$(uname)" == "Darwin" ]]; then
+# if [[ "$(uname)" == "Darwin" && -z $ALACRITTY ]]; then
   exec perl -pe 'chomp if eof' | sed s'/⏎$//' | pbcopy
 fi
 
