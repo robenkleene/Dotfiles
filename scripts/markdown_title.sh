ffc718ba3cd39d46898445c3b61b9301110c081b
Roben Kleene <contact@robenkleene.com>
2022-11-21 05:27:10 -0500

    Don't require a file

--- scripts/markdown_title.sh
+++ scripts/markdown_title.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
-if [[ -e $1 ]]; then
-  filename=$(basename "$1")
-  filename_no_ext=${filename%.*}
-  script_dir=$(dirname "$0")
-  if [[ $filename_no_ext =~ ^[a-z0-9_-]*$ ]]; then
-      # If the filename contains only lowercase letters, hyphens, and underscores then assume
-      # convert it to title case with spaces.
-    title=$(echo "$filename_no_ext" | tr "-" " " | tr "_" " " | "$script_dir"/title_case)
-  else
-    title=$(echo "$filename_no_ext" | "$script_dir"/title_case)
-  fi
+filename=$(basename "$1")
+filename_no_ext=${filename%.*}
+script_dir=$(dirname "$0")
+if [[ $filename_no_ext =~ ^[a-z0-9_-]*$ ]]; then
+# If the filename contains only lowercase letters, hyphens, and underscores then assume
+# convert it to title case with spaces.
+title=$(echo "$filename_no_ext" | tr "-" " " | tr "_" " " | "$script_dir"/title_case)
 else
-  # Allow just passing a title as a parameter
-  title="$1"
+title=$(echo "$filename_no_ext" | "$script_dir"/title_case)
 fi
 
 echo "# $title"
