55cb380d6634cc2f63b46851867a544bbb9f99e0
Roben Kleene <contact@robenkleene.com>
2022-11-26 12:21:28 +0000

    Disable bang

--- config/fish/settings.fish
+++ config/fish/settings.fish
@@ -1,7 +1,8 @@
 set fish_escape_delay_ms 200
 
 # Implement `!!`
-function bangbang --on-event fish_postexec
-    alias !!="$argv[1]"
-end
+# Don't think we need this?
+# function bangbang --on-event fish_postexec
+#     alias !!="$argv[1]"
+# end
 
