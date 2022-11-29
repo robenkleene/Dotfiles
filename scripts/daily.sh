851ed36a257063d3501f9fa050970b569c6357eb
Roben Kleene <contact@robenkleene.com>
2022-11-29 16:59:51 +0000

    Update daily

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -2,4 +2,30 @@
 
 set -euo pipefail
 
-exec $VIM_COMMAND $(~/.bin/daily_file)
+path="false"
+while getopts ":ph" option; do
+  case "$option" in
+    p)
+      path="true"
+      ;;
+    h)
+      echo "Usage: command [-hf] [-p <file_path>]"
+      exit 0
+      ;;
+    :)
+      echo "Option -OPTARG requires an argument" >&2
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [[ "$path" == "true" ]]; then
+  ~/.bin/daily_file
+else
+  exec $VIM_COMMAND "$(~/.bin/daily_file)"
+fi
+
