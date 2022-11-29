42a775380dfaa6c706476709653a0ee5fd4344df
Roben Kleene <contact@robenkleene.com>
2022-11-29 16:59:45 +0000

    Tweak lsp config

--- config/nvim/plugin/plugins/lspconfig.lua
+++ config/nvim/plugin/plugins/lspconfig.lua
@@ -8,6 +8,27 @@ vim.diagnostic.config({
   virtual_text = false,
 })
 
+-- Echo diagnostics, but this forces hitting enter for longer messages
+-- function PrintDiagnostics(opts, bufnr, line_nr, client_id)
+--   bufnr = bufnr or 0
+--   line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
+--   opts = opts or {['lnum'] = line_nr}
+--
+--   local line_diagnostics = vim.diagnostic.get(bufnr, opts)
+--   if vim.tbl_isempty(line_diagnostics) then return end
+--
+--   local diagnostic_message = ""
+--   for i, diagnostic in ipairs(line_diagnostics) do
+--     diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
+--     print(diagnostic_message)
+--     if i ~= #line_diagnostics then
+--       diagnostic_message = diagnostic_message .. "\n"
+--     end
+--   end
+--   vim.api.nvim_echo({{diagnostic_message, "Normal"}}, false, {})
+-- end
+-- vim.cmd [[ autocmd! CursorHold * lua PrintDiagnostics() ]]
+
 local on_attach = function(_, bufnr)
   vim.api.nvim_create_autocmd("CursorHold", {
     buffer = bufnr,
@@ -20,7 +41,8 @@ local on_attach = function(_, bufnr)
         prefix = ' ',
         scope = 'cursor',
       }
-      vim.diagnostic.open_float(nil, options)
+      -- Show in a floating window, but this conflicts with other popups
+      -- vim.diagnostic.open_float(nil, opts)
     end
   })
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -29,6 +51,7 @@ local on_attach = function(_, bufnr)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, bufopts)
   vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
