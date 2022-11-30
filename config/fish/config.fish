652a8e1c134a0138c370043662a9527b42a78de0
Roben Kleene <contact@robenkleene.com>
2022-11-30 16:15:42 +0000

    Disable startup message

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,7 @@
-if test -z $TMUX
-    ~/.bin/print_shell_status
-end
+# Too dangerous to run on startup in case `tmux` or `emacs` is hanging
+# if test -z $TMUX
+#     ~/.bin/print_shell_status
+# end
 
 source ~/.config/fish/settings.fish
 source ~/.config/fish/installs.fish
--- zsh/startup.zsh
+++ zsh/startup.zsh
@@ -1,6 +1,7 @@
 # Startup
 
-if [[ -z $TMUX ]]; then
-  [[ -e ~/.bin/print_shell_status ]] && ~/.bin/print_shell_status
-fi
+# Too dangerous to run on startup in case `tmux` or `emacs` is hanging
+# if [[ -z $TMUX ]]; then
+#   [[ -e ~/.bin/print_shell_status ]] && ~/.bin/print_shell_status
+# fi
 
