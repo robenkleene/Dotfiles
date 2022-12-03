babae2144394cbcdb42877ed8054f5feea2e489f
Roben Kleene <contact@robenkleene.com>
2022-12-03 02:50:48 +0000

    Fix reg

--- scripts/reg.sh
+++ scripts/reg.sh
@@ -2,8 +2,14 @@
 
 set -euo pipefail
 
+if [[ -z "$TMUX" ]]; then
+  echo "Error: tmux isn't running" >&2
+  exit 1
+fi
+
 if [[ ! "$1" =~ [a-z] ]]; then
   echo "Error: $1 is not a register" >&2
+  exit 1
 fi
 
 if [[ "${2-}" = "-" ]]; then
