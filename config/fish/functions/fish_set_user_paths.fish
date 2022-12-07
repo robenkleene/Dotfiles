fb40f00ace71b3a171edb1a2a36634b44a30e1fd
Roben Kleene <contact@robenkleene.com>
2022-12-07 17:15:26 +0000

    Switch path order

--- config/fish/functions/fish_set_user_paths.fish
+++ config/fish/functions/fish_set_user_paths.fish
@@ -1,8 +1,5 @@
 function fish_set_user_paths
     set -U fish_user_paths
-    set -U fish_user_paths ~/.brew/bin $fish_user_paths
-    set -U fish_user_paths ~/.fzf/bin $fish_user_paths
-    set -U fish_user_paths ~/.bin $fish_user_paths
 
     # Python
     switch (uname)
@@ -12,6 +9,10 @@ function fish_set_user_paths
             set -U fish_user_paths /usr/local/bin $fish_user_paths
             set -U fish_user_paths ~/Library/Python/3.9/bin $fish_user_paths
     end
+
+    set -U fish_user_paths ~/.brew/bin $fish_user_paths
+    set -U fish_user_paths ~/.fzf/bin $fish_user_paths
+    set -U fish_user_paths ~/.bin $fish_user_paths
 
     # llvm
     switch (uname)
