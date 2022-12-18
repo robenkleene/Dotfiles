53d69427b4ae0401e1deb26c479a53a4f05fc05f
Roben Kleene <contact@robenkleene.com>
2022-12-18 18:48:50 +0000

    Touch custom

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -37,6 +37,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(bold ((t (:bold t))))
+ '(evil-goggles-default-face ((t (:inherit region))))
  '(helm-candidate-number ((t (:inherit rk-prompt))))
  '(helm-grep-file ((t (:inherit link :underline nil))))
  '(helm-grep-finish ((t (:inherit rk-prompt))))
