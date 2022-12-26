08faab9c9d19e9aaf5b69e2c2a6901c251053283
Roben Kleene <contact@robenkleene.com>
2022-12-26 21:44:04 +0000

    Use y binding

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 # bind \t accept-autosuggestion
 # This conflicts with using the kill ring, use `\cv` instead
-# bind \cy fish_safepaste
+bind \cy fish_safepaste
+# bind \cY yank
 bind \cv fish_safepaste
 bind \cq push_line
 bind \cw backward-kill-bigword
