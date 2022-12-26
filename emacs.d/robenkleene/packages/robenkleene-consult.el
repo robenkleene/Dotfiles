27c837a60ec041e46f08153ccdfeabf2245c0109
Roben Kleene <contact@robenkleene.com>
2022-12-26 17:52:45 +0000

    Add commented out consult fd

--- emacs.d/robenkleene/packages/robenkleene-consult.el
+++ emacs.d/robenkleene/packages/robenkleene-consult.el
@@ -98,6 +98,30 @@
       :state (consult--file-preview)
       :history 'file-name-history)))
 
+  ;; (defvar consult--fd-command nil)
+  ;; (defun consult--fd-builder (input)
+  ;;   (unless consult--fd-command
+  ;;     (setq consult--fd-command
+  ;;           (if (eq 0 (call-process-shell-command "fdfind"))
+  ;;               "fdfind"
+  ;;             "fd")))
+  ;;   (pcase-let* ((`(,arg . ,opts) (consult--command-split input))
+  ;;                (`(,re . ,hl) (funcall consult--regexp-compiler
+  ;;                                       arg 'extended t)))
+  ;;     (when re
+  ;;       (list :command (append
+  ;;                       (list consult--fd-command
+  ;;                             "--color=never" "--full-path"
+  ;;                             (consult--join-regexps re 'extended))
+  ;;                       opts)
+  ;;             :highlight hl))))
+
+  ;; (defun consult-fd (&optional dir initial)
+  ;;   (interactive "P")
+  ;;   (let* ((prompt-dir (consult--directory-prompt "Fd" dir))
+  ;;          (default-directory (cdr prompt-dir)))
+  ;;     (find-file (consult--find (car prompt-dir) #'consult--fd-builder initial))))
+
   (defun robenkleene/consult-fd (&optional dir initial)
     "`consult-find' with `fd'."
     (interactive "P")
