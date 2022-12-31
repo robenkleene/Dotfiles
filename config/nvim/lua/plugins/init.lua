35ee25e2c7159b1c77fccf6e7283e3f9847097e8
Roben Kleene <contact@robenkleene.com>
2022-12-31 20:18:59 +0000

    Add path

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -44,7 +44,12 @@ return {
         sections = {
           lualine_a = {'mode'},
           lualine_b = {'diagnostics'},
-          lualine_c = {'filename'},
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            }
+          },
           lualine_x = {'filetype'},
           lualine_y = {'progress'},
           lualine_z = {'location'}
