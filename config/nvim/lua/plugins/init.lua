a797a7086d1f1cb1f3f0470ef0945252d53d83a2
Roben Kleene <contact@robenkleene.com>
2022-12-31 06:03:30 -0500

    Add lualine

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -32,6 +32,34 @@ return {
         {expr = true, silent = true})
     end
   },
+  {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'catppuccin',
+          component_separators = '|',
+          section_separators = '',
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {'filename'},
+          lualine_x = {'location'},
+          lualine_y = {},
+          lualine_z = {}
+        }
+      }
+    end
+  },
   { 'github/copilot.vim', event = "VeryLazy", },
   { "NvChad/nvim-colorizer.lua", config = true, event = "VeryLazy" },
   { "kylechui/nvim-surround", config = true, event = "VeryLazy" },
--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,10 +3,8 @@ return {
   event = "VeryLazy",
   config = function()
     local opts = { noremap=true, silent=true }
-    -- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-    -- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
     vim.diagnostic.config({
       virtual_text = false,
