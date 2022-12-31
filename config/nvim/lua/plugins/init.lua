b55da608dd7dfa212c202a5062f612f0d439e196
Roben Kleene <contact@robenkleene.com>
2022-12-31 22:18:37 +0000

    Remove lauline and fidget

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -32,40 +32,6 @@ return {
         {expr = true, silent = true})
     end
   },
-  {
-    'nvim-lualine/lualine.nvim',
-    config = function()
-      require('lualine').setup {
-        options = {
-          theme = 'catppuccin',
-          component_separators = '|',
-          section_separators = '',
-        },
-        sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'diagnostics'},
-          lualine_c = {
-            {
-              'filename',
-              path = 1,
-              shorting_target = 20,
-            }
-          },
-          lualine_x = {'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'location'},
-          lualine_y = {},
-          lualine_z = {}
-        }
-      }
-    end
-  },
   { 'github/copilot.vim', event = "VeryLazy", },
   { "NvChad/nvim-colorizer.lua", config = true, event = "VeryLazy" },
   { "kylechui/nvim-surround", config = true, event = "VeryLazy" },
@@ -83,7 +49,6 @@ return {
       vim.cmd("colorscheme catppuccin")
     end
   },
-  { 'j-hui/fidget.nvim', event = "VeryLazy" },
   {
       'nvim-treesitter/playground',
       cmd = 'TSPlaygroundToggle'
