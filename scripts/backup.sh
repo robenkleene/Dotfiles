51eadc77cca5b8e6c06443df98cda77208a4047e
Roben Kleene <contact@robenkleene.com>
2023-01-08 01:20:35 +0000

    Add backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,8 +2,4 @@
 
 set -euo pipefail
 
-if [[ -n "${1-}" ]]; then
-  exec ~/.bin/backup_file "$@"
-else
-  exec ~/.bin/backup_text
-fi
+exec ~/.bin/backup_text
--- scripts/backup_text.sh
+++ scripts/backup_text.sh
@@ -2,13 +2,17 @@
 
 message="false"
 silent="false"
-while getopts ":smh" option; do
+echo="false"
+while getopts ":smeh" option; do
   case "$option" in
     m)
       message="true"
       ;;
     s)
       silent="true"
+      ;;
+    e)
+      echo="true"
       ;;
     h)
       echo "Usage: command [-hm]"
@@ -30,7 +34,11 @@ today=$(date +%Y-%B-%d)
 backup_directory=$backup_root_directory$today
 mkdir -p "$backup_directory"
 archive_file=$(mktemp "$backup_directory/$today-ArchivedText.XXXX")
-cat > "$archive_file"
+if [[ "$echo" == "true" ]]; then
+  cat > "$archive_file"
+else
+  tee "$archive_file"
+fi
 # Add a new line to the end of the file if it's missing, without this `wc` will
 # say `0` lines for `1` line.
 sed -i '' -e '$a\' "$archive_file"
