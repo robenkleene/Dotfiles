71ea72fcfef0f5d8d9c29872414259f7d265f608
Roben Kleene <contact@robenkleene.com>
2023-01-29 05:38:45 -0500

    Fix script

--- scripts/gw.sh
+++ scripts/gw.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 # This was moved from an alias because it's useful in one liners
 # E.g., fd --hidden "release.yml" --exec gprpr -C {//}
-exec ~/.bin/source_control_open_site "$@"
+exec ~/.bin/git_web_open "$@"
