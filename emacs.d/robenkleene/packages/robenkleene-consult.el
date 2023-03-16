9b337d8ecbfc7997990273f5cd4a0aee160d5964
Roben Kleene <contact@robenkleene.com>
2023-03-16 19:18:12 +0000

    Improve Evil ex completion

--- emacs.d/robenkleene/packages/robenkleene-consult.el
+++ emacs.d/robenkleene/packages/robenkleene-consult.el
@@ -71,6 +71,9 @@
   ;; Both < and C-+ work reasonably well.
   (setq consult-narrow-key "<") ;; (kbd "C-+")
 
+  ;; Use `consult' for Emacs `ex' (command line) completions
+  (setq completion-in-region-function 'consult-completion-in-region)
+
   ;; Optionally make narrowing help available in the minibuffer.
   ;; You may want to use `embark-prefix-help-command' or which-key instead.
   ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
