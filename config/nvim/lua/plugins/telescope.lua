9cd06d862f07b7c19ae8f39a7a5059b10754a753
Roben Kleene <contact@robenkleene.com>
2023-03-17 05:23:48 -0400

    Switch telescope theme

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -22,6 +22,16 @@ return {
   },
   config = function()
     pcall(require('telescope').load_extension, 'fzf')
+    require('telescope').setup{
+      pickers = {
+        find_files = { theme = "dropdown" },
+        buffers = { theme = "dropdown" },
+        current_buffer_fuzzy_find = { theme = "dropdown" },
+        live_grep = { theme = "dropdown" },
+        lsp_document_symbols = { theme = "dropdown" },
+        lsp_dynamic_workspace_symbols = { theme = "dropdown" }
+      }
+    }
     vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers)
     vim.keymap.set('n', '<leader>l', require('telescope.builtin').current_buffer_fuzzy_find)
     vim.keymap.set('n', '<leader>F', require('telescope.builtin').find_files)
