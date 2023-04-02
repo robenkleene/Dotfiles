9261287e82ecf3d42fc2febff273c0b39f25dd9c
Roben Kleene <contact@robenkleene.com>
2023-04-02 06:18:56 -0400

    Add Vim style completion

--- emacs.d/robenkleene/packages/robenkleene-corfu.el
+++ emacs.d/robenkleene/packages/robenkleene-corfu.el
@@ -5,10 +5,11 @@
 ;; Enable Corfu completion UI
 ;; See the Corfu README for more configuration tips.
 (use-package corfu
-  ;; :bind (:map corfu-map
-  ;;             ;; Disable return so that `ls<ret>' on Terminal enters the command
-  ;;             ("RET" . nil)
-  ;;             )
+  :bind (:map corfu-map
+              ;; Disable return so that `ls<ret>' on Terminal enters the command
+              ;; ("RET" . nil)
+              ("C-y" . corfu-complete)
+              )
   :init
   (use-package corfu-terminal)
   (use-package cape
