7fec1116cede0f87e5c31b775d02aca282481126
Roben Kleene <contact@robenkleene.com>
2023-04-06 13:24:52 +0000

    Update hg_cd

--- config/fish/functions/hg_cd.fish
+++ config/fish/functions/hg_cd.fish
@@ -1,6 +1,9 @@
 function hg_cd
     cd (string escape (hg root))
     if count $argv > /dev/null
-        $argv
+        # We can probably automatically `cd` now that `hg_root` can be used to
+        # execute a command from root?
+        # $argv
+        cd $argv
     end
 end
