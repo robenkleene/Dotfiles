1054671d662d4687e0b81c528554e7f77c613f7a
Roben Kleene <contact@robenkleene.com>
2023-04-08 07:02:49 -0400

    Finish clipboard to PNG

--- scripts/clipboard_to_png.sh
+++ scripts/clipboard_to_png.sh
@@ -2,4 +2,12 @@
 
 set -euo pipefail
 
-osascript -e "get the clipboard as «class PNGf»" | sed "s/«data PNGf//; s/»//" | xxd -r -p > x.png
+tmp=$(mktemp "clipboard_to_png-XXXXXX")
+osascript -e "get the clipboard as «class PNGf»" | sed "s/«data PNGf//; s/»//" | xxd -r -p > "$tmp"
+dst="${tmp}.png"
+mv -n "$tmp" "$dst"
+if [[ -f "$tmp" ]]; then
+  echo -n "$tmp"
+else
+  echo -n "$dst"
+fi
