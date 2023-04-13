74ef6d31b8877e01260a924e27d13488c2c3e290
Roben Kleene <contact@robenkleene.com>
2023-04-13 15:25:46 +0000

    Update download

--- scripts/aresdev_download_settings.sh
+++ scripts/aresdev_download_settings.sh
@@ -2,5 +2,6 @@
 
 set -euo pipefail
 
-# ssh aresdev 'cd ~/Developer/Settings/ && ~/.bin/git_archive'
+ssh aresdev 'cd ~/Developer/Settings/ && ~/.bin/git_archive'
 sftp aresdev:Developer/Settings/Settings.tar.gz .
+ssh aresdev 'rm ~/Developer/Settings/Settings.tar.gz'
