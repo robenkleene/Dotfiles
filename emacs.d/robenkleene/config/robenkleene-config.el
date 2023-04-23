5e06fc9bc5c3c0e5387e1e962553978911d73db1
Roben Kleene <contact@robenkleene.com>
2023-04-23 12:57:50 -0400

    Improve scrolling

--- emacs.d/robenkleene/config/robenkleene-config.el
+++ emacs.d/robenkleene/config/robenkleene-config.el
@@ -354,6 +354,14 @@
             (modify-syntax-entry ?- "w")
             ))
 
+;; Improve scrolling
+;; Both `scroll-conservatively' and `scroll-margin' together help prevent emacs
+;; from getting "stuck" (bouncing back) when trying to scroll the cursor off
+;; screen
+(setq scroll-conservatively 101)
+;; Size of buffer from bottom of the screen when scrolling
+(setq scroll-margin 1)
+
 (provide 'robenkleene-config)
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
