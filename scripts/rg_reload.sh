a8313effb33d2a5f15694b93affcb1989090069a
Roben Kleene <contact@robenkleene.com>
2023-04-27 05:41:22 -0400

    Remove superfluous script

--- scripts/rg_reload.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-if [[ ${#1} -gt 1 ]] || [[ $# -gt 1 ]]; then
-  exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:91' --colors 'path:fg:cyan' --colors 'line:fg:white' --pretty "$@"
-fi
