fc78d9dc490bdff72705fc17ceb39ec68983187d
Roben Kleene <contact@robenkleene.com>
2023-04-29 14:54:00 +0000

    Touch file

--- config/fish/functions/d.fish
+++ config/fish/functions/d.fish
@@ -1,3 +1,3 @@
-function d --wraps='$CD_COMMAND' --wraps='$CD_COMMAND .' --description 'alias d=$CD_COMMAND .'
+function d --wraps='$CD_COMMAND .' --description 'alias d=$CD_COMMAND .'
   $CD_COMMAND . $argv; 
 end
