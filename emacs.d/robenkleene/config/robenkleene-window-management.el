9b65961dc3e53623530ec5b3a1878c5f9f203ac3
Roben Kleene <contact@robenkleene.com>
2023-05-04 23:34:54 +0000

    Tweak window management

--- emacs.d/robenkleene/config/robenkleene-window-management.el
+++ emacs.d/robenkleene/config/robenkleene-window-management.el
@@ -14,29 +14,45 @@
 ;; Just automatically always select new buffers
 ;; This works to target automatically focus all popups, but it breaks magit
 ;; commit
-(defun rk/switch-to-buffer (buffer alist)
-  (let* (
-         (window (display-buffer-reuse-window
-                  buffer alist))
-         ;; With two frames side-by-side, this will open the buffer in both
-         ;; frames!?
-         ;; (window (or window (display-buffer-use-least-recent-window
-         ;;                     buffer alist)))
-         (window (or window (display-buffer-pop-up-window
-                             buffer alist)))
-         ;; (window (or window (get-buffer-window buffer)))
-         )
-    (if (window-live-p window)
-        (select-window window)
-      )
+;; (defun rk/switch-to-buffer (buffer alist)
+;;   (let* (
+;;          (window (display-buffer-reuse-window
+;;                   buffer alist))
+;;          ;; With two frames side-by-side, this will open the buffer in both
+;;          ;; frames!?
+;;          ;; (window (or window (display-buffer-use-least-recent-window
+;;          ;;                     buffer alist)))
+;;          (window (or window (display-buffer-pop-up-window
+;;                              buffer alist)))
+;;          ;; (window (or window (get-buffer-window buffer)))
+;;          )
+;;     (if (window-live-p window)
+;;         (select-window window)
+;;       )
+;;     )
+;;   )
+
+
+;; (setq display-buffer-alist
+;;       '(
+;;         ("[ ]?[*][^*]+[*]" (rk/switch-to-buffer))
+;;         )
+;;       )
+
+;; Theoretically this is better than the above, because at least sometimes this
+;; should re-use buffers?
+(defadvice display-buffer (after rk/display-buffer-select (buffer &optional action frame) activate)
+  "Wrapper function description."
+  (if buffer
+      (let (
+            (window (get-buffer-window buffer))
+            )
+        (if (window-live-p window)
+            (select-window window)
+          )
+        )
     )
   )
-
-(setq display-buffer-alist
-      '(
-        ("[ ]?[*][^*]+[*]" (rk/switch-to-buffer))
-        )
-      )
 
 (provide 'robenkleene-window-management)
 ;; Local Variables:
