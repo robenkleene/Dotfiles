476358bc5e4f784074627792190ba9518c92beb7
Roben Kleene <contact@robenkleene.com>
2023-05-12 19:00:45 +0000

    Add Company

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -8,7 +8,7 @@
  '(custom-safe-themes
    '("f82e68d489e6c21c9552c4e8e35a03d126d9eba632a8e7b4f9329d1374b4a19c" "ad114fd8820edf7d947f356a237468ae2c402565a60b813ee36f6cd3f1d325ae" "c6fb2c37975e0d8fcc91ae7501f06bc9c58f3fb55e658be0fce30cd58bd66f3e" default))
  '(package-selected-packages
-   '(bind-map pbcopy rainbow-delimiters fill-column-indicator evil-smartparens lua-mode typescript-mode magit eshell-syntax-highlighting monky better-defaults yaml-mode embark-consult kotlin-mode consult ef-themes rust-mode eglot evil-goggles use-package nodejs-repl csv-mode ledger-mode fish-mode markdown-mode swift-mode inf-ruby enh-ruby-mode slime smartparens rainbow-mode yasnippet editorconfig magit-filenotify wgrep jade-mode aggressive-indent page-break-lines expand-region adaptive-wrap vundo vertico orderless marginalia evil-visualstar evil-surround evil-numbers evil-commentary corfu corfu-terminal diminish cape evil-terminal-cursor-changer popon org-modern modus-themes))
+   '(bind-map pbcopy rainbow-delimiters fill-column-indicator evil-smartparens lua-mode typescript-mode magit eshell-syntax-highlighting monky better-defaults yaml-mode embark-consult kotlin-mode consult ef-themes rust-mode eglot evil-goggles use-package nodejs-repl csv-mode ledger-mode fish-mode markdown-mode swift-mode inf-ruby enh-ruby-mode slime smartparens rainbow-mode yasnippet editorconfig magit-filenotify wgrep jade-mode aggressive-indent page-break-lines expand-region adaptive-wrap vundo vertico orderless marginalia evil-visualstar evil-surround evil-numbers evil-commentary corfu corfu-terminal diminish cape evil-terminal-cursor-changer popon org-modern modus-themes company))
  '(vc-annotate-color-map
    '((20 . "#F92672")
      (40 . "#CF4F1F")
