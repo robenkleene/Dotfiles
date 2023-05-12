0163334178e47fdda77665cfbf8c6b79e6be0e44
Roben Kleene <contact@robenkleene.com>
2023-05-12 14:44:12 +0000

    Disable window management

--- emacs.d/robenkleene/config/robenkleene-window-management.el
+++ emacs.d/robenkleene/config/robenkleene-window-management.el
@@ -14,28 +14,30 @@
 ;; Just automatically always select new buffers
 ;; This works better than below `defadvice display-buffer' method with `magit',
 ;; otherwise seems identical
-(defun rk/switch-to-buffer (buffer alist)
-  (let* (
-         (window (display-buffer-reuse-window
-                  buffer alist))
-         ;; With two frames side-by-side, this will open the buffer in both
-         ;; frames!?
-         ;; (window (or window (display-buffer-use-least-recent-window
-         ;;                     buffer alist)))
-         (window (or window (display-buffer-pop-up-window
-                             buffer alist)))
-         ;; (window (or window (get-buffer-window buffer)))
-         )
-    (if (window-live-p window)
-        (select-window window)
-      )
-    )
-  )
-(setq display-buffer-alist
-      '(
-        ("[ ]?[*][^*]+[*]" (rk/switch-to-buffer))
-        )
-      )
+;; This makes `next-error' to cycle through grep matches always open in a new
+;; window
+;; (defun rk/switch-to-buffer (buffer alist)
+;;   (let* (
+;;          (window (display-buffer-reuse-window
+;;                   buffer alist))
+;;          ;; With two frames side-by-side, this will open the buffer in both
+;;          ;; frames!?
+;;          ;; (window (or window (display-buffer-use-least-recent-window
+;;          ;;                     buffer alist)))
+;;          (window (or window (display-buffer-pop-up-window
+;;                              buffer alist)))
+;;          ;; (window (or window (get-buffer-window buffer)))
+;;          )
+;;     (if (window-live-p window)
+;;         (select-window window)
+;;       )
+;;     )
+;;   )
+;; (setq display-buffer-alist
+;;       '(
+;;         ("[ ]?[*][^*]+[*]" (rk/switch-to-buffer))
+;;         )
+;;       )
 
 ;; Theoretically this is better than the above, because at least sometimes this
 ;; should re-use buffers?
