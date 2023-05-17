de7207d841d9f9e530e632d22ee9d97faa2906b5
Roben Kleene <contact@robenkleene.com>
2023-05-17 15:14:33 +0000

    Show tabs again

--- emacs.d/robenkleene/config/robenkleene-config.el
+++ emacs.d/robenkleene/config/robenkleene-config.el
@@ -282,7 +282,8 @@
 ;; Don't show tab bar for one tab
 ;; This causes the echo area to disappear when a new frame is added
 (if window-system
-    (setq tab-bar-show nil)
+    ;; (setq tab-bar-show nil)
+    (setq tab-bar-show t)
   (setq tab-bar-show 1)
   )
 ;; (setq tab-bar-show nil)
