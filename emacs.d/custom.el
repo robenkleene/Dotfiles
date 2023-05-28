0be9e1eea83ed36298acb79a48bbac8a2d45c3ad
Roben Kleene <contact@robenkleene.com>
2023-05-28 13:33:09 -0400

    Touch file

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -29,3 +29,9 @@
      (340 . "#2790C3")
      (360 . "#66D9EF")))
  '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
