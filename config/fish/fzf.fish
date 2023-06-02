d9457500a81b2d251e6093c6c8c45d19a899d18e
Roben Kleene <contact@robenkleene.com>
2023-06-02 05:40:04 -0400

    Add history setup

--- config/fish/fzf.fish
+++ config/fish/fzf.fish
@@ -15,6 +15,28 @@ else
             echo "fzf"
         end
     end
+    function fzf-history-widget -d "Show command history"
+        test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 40%
+        begin
+            set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT $FZF_DEFAULT_OPTS --tiebreak=index --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS +m"
+
+            set -l FISH_MAJOR (echo $version | cut -f1 -d.)
+            set -l FISH_MINOR (echo $version | cut -f2 -d.)
+
+            # history's -z flag is needed for multi-line support.
+            # history's -z flag was added in fish 2.4.0, so don't use it for versions
+            # before 2.4.0.
+            if [ "$FISH_MAJOR" -gt 2 -o \( "$FISH_MAJOR" -eq 2 -a "$FISH_MINOR" -ge 4 \) ];
+                history -z | eval (__fzfcmd) --read0 --print0 -q '(commandline)' | read -lz result
+                and commandline -- $result
+            else
+                history | eval (__fzfcmd) -q '(commandline)' | read -l result
+                and commandline -- $result
+            end
+        end
+        commandline -f repaint
+    end
+    bind \er fzf-history-widget
 end
 
 set -xg FZF_DEFAULT_OPTS "--height 40% --reverse"
