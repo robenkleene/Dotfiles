6d10f35c2eb498cd8f2b11398f846d31765db34e
Roben Kleene <contact@robenkleene.com>
2023-06-04 13:06:00 -0400

    Pair tab bar with window management

--- emacs.d/robenkleene/config/robenkleene-window-management.el
+++ emacs.d/robenkleene/config/robenkleene-window-management.el
@@ -54,6 +54,50 @@
 ;;     )
 ;;   )
 
+;; Don't show tab bar for one tab
+;; This causes the echo area to disappear when a new frame is added
+;; (if window-system
+;;     (setq tab-bar-show nil)
+;;   (setq tab-bar-show 1)
+;;   )
+;; Show for more than 1 tab
+;; (setq tab-bar-show 1)
+;; Always show
+(setq tab-bar-show t)
+;; Never show
+;; (setq tab-bar-show nil)
+;; Don't show close button
+(setq tab-bar-close-button-show nil)
+;; Hide plus button and other extras
+;; (setq tab-bar-format '(tab-bar-format-history tab-bar-format-tabs tab-bar-separator))
+(setq tab-bar-format '(tab-bar-format-tabs))
+;; Hide ugly bar separator in terminal emacs
+(setq tab-bar-separator " ")
+;; Turn on tab bar (must be after setting options)
+(tab-bar-mode)
+;; Show numbers
+(setq tab-bar-tab-hints t)
+;; Allow selecting tabs with `M-#'
+;; Not sure why this doesn't work
+;; (setq tab-bar-select-tab-modifiers '(meta))
+;; Add space to sides of `tab-bar'
+(defun rk/tab-bar-tab-name-format (tab i)
+  (let ((current-p (eq (car tab) 'current-tab)))
+    (propertize
+     (concat
+      " "
+      (if tab-bar-tab-hints (format "%d " i) "")
+      (alist-get 'name tab)
+      (or (and tab-bar-close-button-show
+               (not (eq tab-bar-close-button-show
+                        (if current-p 'non-selected 'selected)))
+               tab-bar-close-button)
+          "")
+      " "
+      )
+     'face (funcall tab-bar-tab-face-function tab))))
+(setq tab-bar-tab-name-format-function 'rk/tab-bar-tab-name-format)
+
 (provide 'robenkleene-window-management)
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
