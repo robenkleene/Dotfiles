2a35ec5b90995a4bbc77b6250cf1ef98ab032817
Roben Kleene <contact@robenkleene.com>
2023-06-18 13:34:02 -0400

    Fix preview

--- scripts/git_fzf_commit.sh
+++ scripts/git_fzf_commit.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 git log --graph --color=always --format="%C(auto)%h %s%d " | \
   fzf --no-sort --ansi --tiebreak=index \
-  --preview 'git show --color=always (echo {} | grep -o "[a-f0-9]\{7,\}") | head -1' \
+  --preview 'git show --color=always (echo {} | grep -o "[a-f0-9]\{7,\}")' \
   --preview-window=top:15
