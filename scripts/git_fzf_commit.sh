849445d6ee71753c29c8342cbf6718259bf4e271
Roben Kleene <contact@robenkleene.com>
2023-06-18 17:42:09 +0000

    Use command

--- scripts/git_fzf_commit.sh
+++ scripts/git_fzf_commit.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 git log --graph --color=always --format="%C(auto)%h %s%d " | \
   fzf --no-sort --ansi --tiebreak=index \
-  --preview 'git show --color=always (echo {} | grep -o "[a-f0-9]\{7,\}")' \
+  --preview 'command git show --color=always (echo {} | command grep -o \"[a-f0-9]\{7,\}\")' \
   --preview-window=top:15 | grep -o "[a-f0-9]\{7,\}"
