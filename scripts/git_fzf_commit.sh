3f65f1672b260a49b328300eb5725d577bb147fb
Roben Kleene <contact@robenkleene.com>
2023-06-18 17:46:33 +0000

    Work on script

--- scripts/git_fzf_commit.sh
+++ scripts/git_fzf_commit.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 git log --graph --color=always --format="%C(auto)%h %s%d " | \
   fzf --no-sort --ansi --tiebreak=index \
-  --preview 'command git show --color=always (echo {} | command grep -o \"[a-f0-9]\{7,\}\")' \
-  --preview-window=top:15 | grep -o "[a-f0-9]\{7,\}"
+  --preview 'command git show --color=always $(echo {} | head -1 | command grep --only-matching \"[a-f0-9]\{7\}\")' \
+  --preview-window=top:15 | grep --only-matching "[a-f0-9]{7}"
