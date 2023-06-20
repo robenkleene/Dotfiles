5a162ac4be55ab38ccb371ff8965d84c346f4071
Roben Kleene <contact@robenkleene.com>
2023-06-20 14:01:09 +0000

    Touch file

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -6,14 +6,18 @@
  '(ansi-color-names-vector
    ["#000000" "#D32D1E" "#62D63F" "#E5E64B" "#005FFF" "#af00df" "#4AA4B0" "#ffffff"])
  '(connection-local-criteria-alist
-   '(((:application tramp :machine "localhost")
+   '(((:application eshell)
+      eshell-connection-default-profile)
+     ((:application tramp :machine "localhost")
       tramp-connection-local-darwin-ps-profile)
      ((:application tramp :machine "robenkleene-mac")
       tramp-connection-local-darwin-ps-profile)
      ((:application tramp)
       tramp-connection-local-default-system-profile tramp-connection-local-default-shell-profile)))
  '(connection-local-profile-alist
-   '((tramp-connection-local-darwin-ps-profile
+   '((eshell-connection-default-profile
+      (eshell-path-env-list))
+     (tramp-connection-local-darwin-ps-profile
       (tramp-process-attributes-ps-args "-acxww" "-o" "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state=abcde" "-o" "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
       (tramp-process-attributes-ps-format
        (pid . number)
@@ -88,7 +92,7 @@
  '(custom-safe-themes
    '("ca9ce9f0c5bb46610c20f4ebcaa3d1c8ed6b8f9c5a956620cba250686fe645cf" "f82e68d489e6c21c9552c4e8e35a03d126d9eba632a8e7b4f9329d1374b4a19c" "ad114fd8820edf7d947f356a237468ae2c402565a60b813ee36f6cd3f1d325ae" "c6fb2c37975e0d8fcc91ae7501f06bc9c58f3fb55e658be0fce30cd58bd66f3e" default))
  '(package-selected-packages
-   '(embark evil bind-map pbcopy rainbow-delimiters fill-column-indicator evil-smartparens lua-mode typescript-mode magit eshell-syntax-highlighting monky better-defaults yaml-mode kotlin-mode ef-themes rust-mode eglot evil-goggles use-package nodejs-repl csv-mode ledger-mode fish-mode markdown-mode swift-mode inf-ruby enh-ruby-mode slime smartparens rainbow-mode yasnippet editorconfig magit-filenotify wgrep jade-mode aggressive-indent page-break-lines expand-region adaptive-wrap vundo vertico orderless marginalia evil-visualstar evil-surround evil-numbers evil-commentary corfu corfu-terminal diminish cape evil-terminal-cursor-changer popon org-modern modus-themes company))
+   '(auto-dim-other-buffers embark evil bind-map pbcopy rainbow-delimiters fill-column-indicator evil-smartparens lua-mode typescript-mode magit eshell-syntax-highlighting monky better-defaults yaml-mode kotlin-mode ef-themes rust-mode eglot evil-goggles use-package nodejs-repl csv-mode ledger-mode fish-mode markdown-mode swift-mode inf-ruby enh-ruby-mode slime smartparens rainbow-mode yasnippet editorconfig magit-filenotify wgrep jade-mode aggressive-indent page-break-lines expand-region adaptive-wrap vundo vertico orderless marginalia evil-visualstar evil-surround evil-numbers evil-commentary corfu corfu-terminal diminish cape evil-terminal-cursor-changer popon org-modern modus-themes company))
  '(vc-annotate-color-map
    '((20 . "#F92672")
      (40 . "#CF4F1F")
