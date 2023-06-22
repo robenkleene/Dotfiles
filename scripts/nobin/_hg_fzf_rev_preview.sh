05d5c4d314e81feb263e977ecf662cc1d4b7a1cd
Roben Kleene <contact@robenkleene.com>
2023-06-22 21:54:48 +0000

    Check history of individual file

--- scripts/nobin/_hg_fzf_rev_preview.sh
+++ scripts/nobin/_hg_fzf_rev_preview.sh
@@ -3,6 +3,10 @@
 set -euo pipefail
 hash=$(cat | head -1 | grep --only-matching "[a-f0-9]\{7\}")
 # No idea why this only works as a separate step
-hash=$(echo $hash | cut -d ' ' -f1)
-hg show --color=always "$hash"
+hash=$(echo "$hash" | cut -d ' ' -f1)
+if [[ "$#" -eq 1 ]]; then
+  hg log --color=always --patch --rev "$hash" "$1"
+else
+  hg show --color=always "$hash"
+fi
 
