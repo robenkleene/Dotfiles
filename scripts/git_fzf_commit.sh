5d073368ba5bbd7cf096fb15d6ff4f54721eb62c
Roben Kleene <contact@robenkleene.com>
2023-06-22 05:07:10 -0400

    Update git fzf

--- scripts/git_fzf_commit.sh
+++ scripts/git_fzf_commit.sh
@@ -5,4 +5,6 @@ set -euo pipefail
 git log --graph --color=always --format="%C(auto)%h %s%d " | \
   fzf --no-sort --ansi --tiebreak=index \
   --preview 'echo {} | ~/.bin/nobin/_git_fzf_commit_preview.sh' \
-  --preview-window=top:15 | grep --only-matching "[a-f0-9]\{7\}"
+  --bind 'ctrl-f:preview-page-down,ctrl-b:preview-page-up' \
+  --height=100% \
+  --preview-window=top,50% | grep --only-matching "[a-f0-9]\{7\}"
