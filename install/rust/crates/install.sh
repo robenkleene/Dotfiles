da292f77d1531d64bc7d6dec94ceca2d62636f31
Roben Kleene <contact@robenkleene.com>
2023-06-25 19:31:54 +0000

    Update Rust installs

--- install/rust/crates/install.sh
+++ install/rust/crates/install.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-cargo install cargo-generate
+# Helper for generating cargo files
+# If this is failing just comment it out
+# cargo install cargo-generate
+# Language server for toml files
 cargo install taplo-cli --locked --features lsp
 cargo install reap-grep
 
