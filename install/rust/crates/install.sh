1cda61de9ddefb42d7f10c6b63a9c7cd142d4467
Roben Kleene <contact@robenkleene.com>
2023-06-25 19:39:27 +0000

    Re-order dependencies

--- install/rust/crates/install.sh
+++ install/rust/crates/install.sh
@@ -2,10 +2,10 @@
 
 set -e
 
+cargo install reap-grep
 # Helper for generating cargo files
 # If this is failing just comment it out
 # cargo install cargo-generate
 # Language server for toml files
 cargo install taplo-cli --locked --features lsp
-cargo install reap-grep
 
