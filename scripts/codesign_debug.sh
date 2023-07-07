20c078cc3d71bc0a8b22dafbb2f34e047e1d69ab
Roben Kleene <contact@robenkleene.com>
2023-07-07 09:44:02 -0400

    Add codesign script

---
+++ scripts/codesign_debug.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+set -euo pipefail
+
+if [[ "$#" -ne 1 ]]; then
+  echo "Error: Missing path to binary" >&2
+  exit 1
+fi
+
+codesign -s - -v -f --entitlements=(echo -n '<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "https://www.apple.com/DTDs/PropertyList-1.0.dtd"\>
+<plist version="1.0">
+    <dict>
+        <key>com.apple.security.get-task-allow</key>
+        <true/>
+    </dict>
+</plist>') "$1"
