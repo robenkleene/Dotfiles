3d720a4ba142686b769473fa4bc52bdce3bdf055
Roben Kleene <contact@robenkleene.com>
2023-07-09 11:08:55 +0000

    Fix up down bindings

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -11,34 +11,48 @@ return {
           luasnip.lsp_expand(args.body)
         end,
       },
-      -- Disable these, just use native `C-y`, `C-n`, `C-p` bindings
-      -- mapping = cmp.mapping.preset.insert {
-      --   ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-      --   ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      --   ['<C-Space>'] = cmp.mapping.complete(),
-      --   ['<CR>'] = cmp.mapping.confirm {
-      --     behavior = cmp.ConfirmBehavior.Replace,
-      --     select = true,
-      --   },
-      --   ['<Tab>'] = cmp.mapping(function(fallback)
-      --     if luasnip.expand_or_jumpable() then
-      --       luasnip.expand_or_jump()
-      --     elseif cmp.visible() then
-      --       cmp.select_next_item()
-      --     else
-      --       fallback()
-      --     end
-      --   end, { 'i', 's' }),
-      --   ['<S-Tab>'] = cmp.mapping(function(fallback)
-      --     if luasnip.jumpable(-1) then
-      --       luasnip.jump(-1)
-      --     elseif cmp.visible() then
-      --       cmp.select_prev_item()
-      --     else
-      --       fallback()
-      --     end
-      --   end, { 'i', 's' }),
-      -- },
+      mapping = cmp.mapping.preset.insert {
+        -- Disable these, just use native `C-y`, `C-n`, `C-p` bindings
+        -- ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        -- ['<C-Space>'] = cmp.mapping.complete(),
+        -- ['<CR>'] = cmp.mapping.confirm {
+        --   behavior = cmp.ConfirmBehavior.Replace,
+        --   select = true,
+        -- },
+        -- ['<Tab>'] = cmp.mapping(function(fallback)
+        --   if luasnip.expand_or_jumpable() then
+        --     luasnip.expand_or_jump()
+        --   elseif cmp.visible() then
+        --     cmp.select_next_item()
+        --   else
+        --     fallback()
+        --   end
+        -- end, { 'i', 's' }),
+        -- ['<S-Tab>'] = cmp.mapping(function(fallback)
+        --   if luasnip.jumpable(-1) then
+        --     luasnip.jump(-1)
+        --   elseif cmp.visible() then
+        --     cmp.select_prev_item()
+        --   else
+        --     fallback()
+        --   end
+        -- end, { 'i', 's' }),
+        ['<C-n>'] = cmp.mapping(function(fallback)
+          if require('cmp').visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<C-p>'] = cmp.mapping(function(fallback)
+          if require('cmp').visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      },
       sources = {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
