956d77c2fb487eb6061ffc9d06ef24fc558bbecb
Roben Kleene <contact@robenkleene.com>
2023-07-10 05:20:02 -0400

    Fix luasnip in cmp

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -1,6 +1,7 @@
 return {
   'hrsh7th/nvim-cmp', dependencies = {
-    'hrsh7th/cmp-nvim-lsp'
+    'hrsh7th/cmp-nvim-lsp',
+    'saadparwaiz1/cmp_luasnip'
   },
   config = function()
     local cmp = require 'cmp'
