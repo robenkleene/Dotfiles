d42fa5f81203354f96431652ccdec032b7cf0d7b
Roben Kleene <contact@robenkleene.com>
2023-07-11 06:03:11 -0400

    Fix entitlements

--- scripts/codesign_debug.sh
+++ scripts/codesign_debug.sh
@@ -7,11 +7,12 @@ if [[ "$#" -ne 1 ]]; then
   exit 1
 fi
 
-codesign -s - -v -f --entitlements=(echo -n '<?xml version="1.0" encoding="UTF-8"?>
+entitlements='<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "https://www.apple.com/DTDs/PropertyList-1.0.dtd"\>
 <plist version="1.0">
     <dict>
         <key>com.apple.security.get-task-allow</key>
         <true/>
     </dict>
-</plist>') "$1"
+</plist>'
+codesign -s - -v -f --entitlements =(echo -n "$entitlements") "$1"
