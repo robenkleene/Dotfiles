0c86ba2cccabf06df21c62700a4d135b129e3caa
Roben Kleene <contact@robenkleene.com>
2023-07-15 18:33:30 +0000

    Add back sorting

--- scripts/fzf_file.sh
+++ scripts/fzf_file.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-fzf --ansi --no-sort --multi --preview "$HOME/.bin/nobin/_fzf_preview_file.sh {}" \
+fzf --ansi --multi --preview "$HOME/.bin/nobin/_fzf_preview_file.sh {}" \
   --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
   --height=100% \
   --preview-window '<30(bottom,30%):wrap' 
