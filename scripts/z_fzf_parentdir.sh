d06d4acebe17c34bfcfac85d72480c12c1816a84
Roben Kleene <contact@robenkleene.com>
2023-07-15 17:30:49 +0000

    Rename preview script

--- scripts/z_fzf_parentdir.sh
+++ scripts/z_fzf_parentdir.sh
@@ -6,4 +6,4 @@ export CLICOLOR=1
 export SHELL=sh
 # We're not using `z_list_parentdir`, z version probably not worth the
 # additional time to run?
-exec ~/.bin/list_parentdir | fzf --exact --no-sort --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' --cycle --info=inline --layout=reverse --tabstop=1 --exit-0 --preview="~/.bin/nobin/_fzf_preview.sh {}" --preview-window '<30(bottom,30%):wrap'
+exec ~/.bin/list_parentdir | fzf --exact --no-sort --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' --cycle --info=inline --layout=reverse --tabstop=1 --exit-0 --preview="~/.bin/nobin/_fzf_preview_file.sh {}" --preview-window '<30(bottom,30%):wrap'
--- scripts/z_fzf_subdir.sh
+++ scripts/z_fzf_subdir.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 export CLICOLOR=1
 export SHELL=sh
-exec ~/.bin/z_list_subdir | fzf --exact --no-sort --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' --cycle --info=inline --layout=reverse --tabstop=1 --exit-0 --preview="~/.bin/nobin/_fzf_preview.sh {}" --preview-window '<30(bottom,30%):wrap'
+exec ~/.bin/z_list_subdir | fzf --exact --no-sort --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' --cycle --info=inline --layout=reverse --tabstop=1 --exit-0 --preview="~/.bin/nobin/_fzf_preview_file.sh {}" --preview-window '<30(bottom,30%):wrap'
