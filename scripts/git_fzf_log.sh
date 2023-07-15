5c44ba4eb950c95e5821645be7db2e37c6e4f696
Roben Kleene <contact@robenkleene.com>
2023-07-15 17:42:44 +0000

    Rename previews

--- scripts/git_fzf_log.sh
+++ scripts/git_fzf_log.sh
@@ -10,7 +10,7 @@ fi
 
 git log --graph --color=always --format="%C(auto)%h %s%d"${separator}"${1}" | \
   fzf --no-sort --ansi --tiebreak=index \
-  --preview "echo {} | ~/.bin/fzf_git_commit \"$1\"" \
+  --preview "echo {} | ~/.bin/_fzf_preview_git_commit \"$1\"" \
   --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
   --height=100% \
   --preview-window '<30(bottom,30%):wrap' | grep --only-matching "[a-f0-9]\{7\}"
--- scripts/hg_fzf_log.sh
+++ scripts/hg_fzf_log.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 
 hg log --template '{node|short} {author|user}: {desc|strip|firstline}\n' --limit 100 "$1" | \
   fzf --no-sort --ansi --tiebreak=index \
-  --preview "echo {} | ~/.bin/fzf_hg_rev \"$1\"" \
+  --preview "echo {} | ~/.bin/nobin/_fzf_preview_hg_rev \"$1\"" \
   --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
   --height=100% \
   --preview-window '<30(bottom,30%):wrap' | grep --only-matching "[a-f0-9]\{7\}"
