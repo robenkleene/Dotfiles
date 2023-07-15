d2b1abeb6a36d71bb619311caea1585faceb0415
Roben Kleene <contact@robenkleene.com>
2023-07-15 18:09:34 +0000

    Support multiple matches

--- scripts/fzf_grep.sh
+++ scripts/fzf_grep.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-fzf -d ':' -n 2.. --ansi --no-sort \
+fzf -d ':' -n 2.. --ansi --no-sort --multi \
   --preview 'bat --style=numbers --color=always --highlight-line {2} {1}' \
   --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
   --height=100% \
