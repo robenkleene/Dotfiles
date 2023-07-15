2dcca9843333b0c2ebc22dbe3ac1da466d0f1275
Roben Kleene <contact@robenkleene.com>
2023-07-15 18:01:13 +0000

    Tweak file preview

--- scripts/fzf_file.sh
+++ scripts/fzf_file.sh
@@ -2,10 +2,7 @@
 
 set -eo pipefail
 
-preview_opts="--preview '~/.bin/nobin/_fzf_preview_file.sh {}' --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' --preview-window '<30(bottom,30%)'"
-__fzfcmd() {
-  [ -n "$TMUX_PANE" ] && { [ "${FZF_TMUX:-0}" != 0 ] || [ -n "$FZF_TMUX_OPTS" ]; } &&
-    echo "fzf-tmux ${FZF_TMUX_OPTS:--d${FZF_TMUX_HEIGHT:-40%}} ${preview_opts} -- " || echo "fzf ${preview_opts}"
-}
-eval "$(__fzfcmd)"
-
+fzf --preview "$HOME/.bin/nobin/_fzf_preview_file.sh {}" \
+  --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
+  --height=100% \
+  --preview-window '<30(bottom,30%):wrap' 
