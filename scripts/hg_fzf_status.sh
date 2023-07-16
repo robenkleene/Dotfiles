e48798389f403ee8a11eb1ee9a1137912b4a0976
Roben Kleene <contact@robenkleene.com>
2023-07-16 18:32:26 +0000

    Add hg status

---
+++ scripts/hg_fzf_status.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+hg status | \
+  fzf --no-sort --ansi --multi \
+  --preview "echo {} | ~/.bin/nobin/_fzf_preview_hg_diff.sh" \
+  --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
+  --height=40% \
+  --preview-window '<30(bottom,30%):wrap' | sed 's/^[ ?][A-Z?] //'
---
+++ scripts/nobin/_fzf_preview_hg_diff.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+input=$(cat)
+file=${input#*[A-Z?] }
+if [[ ${input:0:1} != "?" ]]; then
+  hg diff --color=always "$file"
+else
+  ~/.bin/nobin/_fzf_preview_file.sh "$file"
+fi
