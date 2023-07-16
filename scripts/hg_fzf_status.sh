1e46c436ff90228a34241fc2bd554355dc4d2b50
Roben Kleene <contact@robenkleene.com>
2023-07-16 18:49:23 +0000

    Fix file output

--- scripts/hg_fzf_status.sh
+++ scripts/hg_fzf_status.sh
@@ -7,4 +7,4 @@ hg status | \
   --preview "echo {} | ~/.bin/nobin/_fzf_preview_hg_diff.sh" \
   --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
   --height=40% \
-  --preview-window '<30(bottom,30%):wrap' | sed 's/^[ ?][A-Z?] //'
+  --preview-window '<30(bottom,30%):wrap' | sed 's/^[A-Z?] //'
