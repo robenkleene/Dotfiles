2f5b596fbf36979f65a88b0800aa517df1f10bac
Roben Kleene <contact@robenkleene.com>
2023-07-16 19:35:09 +0000

    Suppress STDERR

--- scripts/hg_fzf_status.sh
+++ scripts/hg_fzf_status.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-hg status | \
+hg status 2>/dev/null | \
   fzf --no-sort --ansi --multi \
   --preview "echo {} | ~/.bin/nobin/_fzf_preview_hg_diff.sh" \
   --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
