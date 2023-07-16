84b59460500430a6ad8537f8a1dc2fff6761fbf8
Roben Kleene <contact@robenkleene.com>
2023-07-16 18:47:50 +0000

    Improve git diff preview

--- scripts/nobin/_fzf_preview_git_diff.sh
+++ scripts/nobin/_fzf_preview_git_diff.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-file=$(cat | sed 's/^[ ?][A-Z?] //' | head -1)
-
-if git ls-files --error-unmatch "$file" >/dev/null 2>&1; then
+input=$(cat)
+file=${input#*[ ?][A-Z?] }
+if [[ ${input:0:1} != "?" ]]; then
   git diff --color=always "$file"
 else
   ~/.bin/nobin/_fzf_preview_file.sh "$file"
