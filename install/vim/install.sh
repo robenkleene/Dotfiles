53a8a18c805736e3cbea8527a09505c947f0e881
Roben Kleene <contact@robenkleene.com>
2023-07-17 14:15:39 +0000

    Work on install script

--- install/vim/install.sh
+++ install/vim/install.sh
@@ -2,4 +2,8 @@
 
 set -euo pipefail
 
-
+~/.bin/sync_symlinks "$HOME/Developer/Dotfiles/vim/after/ftplugin" "$HOME/Developer/Dotfiles/config/nvim/after/ftplugin"
+~/.bin/sync_symlinks "$HOME/Developer/Dotfiles/vim/after/plugin" "$HOME/Developer/Dotfiles/config/nvim/after/plugin"
+~/.bin/sync_symlinks "$HOME/Developer/Dotfiles/vim/autoload" "$HOME/Developer/Dotfiles/config/nvim/autoload"
+~/.bin/sync_symlinks "$HOME/Developer/Dotfiles/vim/ftplugin" "$HOME/Developer/Dotfiles/config/nvim/ftplugin"
+~/.bin/sync_symlinks "$HOME/Developer/Dotfiles/vim/plugin" "$HOME/Developer/Dotfiles/config/nvim/plugin"
