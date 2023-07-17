1ff19aa88b672fb9931b7aa389703a3b31dd1c0a
Roben Kleene <contact@robenkleene.com>
2023-07-17 11:00:57 +0000

    Make archive last support piping

--- scripts/archive_last.sh
+++ scripts/archive_last.sh
@@ -3,4 +3,9 @@
 set -euo pipefail
 
 backup_root_directory=~/Archive/Text/
-find "$backup_root_directory" | sort | tail -1 | tr -d '\n'
+archive_file=$(find "$backup_root_directory" | sort | tail -1 | tr -d '\n')
+if [[ "$1" = "-f" ]]; then
+  echo $archive_file
+  exit 0
+fi
+cat >> "$archive_file"
