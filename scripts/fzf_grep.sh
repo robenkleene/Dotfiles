b237c783e9549af1f2053b6eafa814c2ec9d838a
Roben Kleene <contact@robenkleene.com>
2023-07-21 05:22:03 -0400

    Tweak rg

--- scripts/fzf_grep.sh
+++ scripts/fzf_grep.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 fzf -d ':' -n 2.. --ansi --multi \
-  --preview 'bat --style=plain --style=numbers --color=always --line-range {2}: --highlight-line {2} {1}' \
+  --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
   --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
   --height=40% \
   --preview-window '<30(bottom,30%):wrap' 
--- scripts/fzf_rg.sh
+++ scripts/fzf_rg.sh
@@ -11,10 +11,9 @@
 #             --preview-window 'up,60%,border-bottom,+{2}+3/3,~3'
 RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
 INITIAL_QUERY="${*:-}"
-: | fzf --ansi --disabled --query "$INITIAL_QUERY" \
+: | fzf --ansi --disabled --multi --query "$INITIAL_QUERY" \
     --bind "start:reload:$RG_PREFIX {q}" \
     --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
     --delimiter : \
-    --preview 'bat --style=plain --color=always {1} --highlight-line {2}' \
-    --preview-window '<30(bottom,30%):wrap' \
-    --bind 'enter:become(vim {1} +{2})'
+    --preview 'bat --style=plain --color=always {1} --line-range {2}: --highlight-line {2}' \
+    --preview-window '<30(bottom,30%):wrap'
