dd78a03059dbd96885fa65aa8a4da23a03352b0f
Roben Kleene <contact@robenkleene.com>
2023-07-21 05:25:22 -0400

    Clean up

--- scripts/fzf_rg.sh
+++ scripts/fzf_rg.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-# RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
-# INITIAL_QUERY="${*:-}"
-# : | fzf --ansi --disabled --query "$INITIAL_QUERY" \
-#         -n 2.. \
-#         --bind "start:reload:$RG_PREFIX {q}" \
-#         --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
-#         --bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
-#         --preview="bat --style=numbers --color=always --line-range {2}: --highlight-line {2} {1}" \
-#             --preview-window 'up,60%,border-bottom,+{2}+3/3,~3'
 RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
 INITIAL_QUERY="${*:-}"
 : | fzf --ansi --disabled --multi --query "$INITIAL_QUERY" \
