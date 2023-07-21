b5f029abb7cfffa9787e89e184f2de3e0a53f2de
Roben Kleene <contact@robenkleene.com>
2023-07-21 19:13:12 +0000

    Archive tmux switch

--- scripts/tmux_switch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -uo pipefail
-
-result=$(tmux list-sessions -F "#S" | fzf-tmux)
-if [[ -z "$result" ]]; then
-  exit 0
-fi
-echo "$result" | tr "\n" "\0" | xargs -0 tmux switch-client -t
--- scripts/tmux_switch_window.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-tmux list-windows -F "#W" | fzf-tmux | tr "\n" "\0" | xargs -0 tmux select-window -t
