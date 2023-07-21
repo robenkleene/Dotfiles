4b7cac74240bfd21ff42446906e304c8a97400a7
Roben Kleene <contact@robenkleene.com>
2023-07-21 05:31:19 -0400

    Fix fd file preview

--- scripts/fzf_fd.sh
+++ scripts/fzf_fd.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+RG_PREFIX="fd "
 INITIAL_QUERY="${*:-}"
 : | fzf --ansi --disabled --multi --query "$INITIAL_QUERY" \
     --bind "start:reload:$RG_PREFIX {q}" \
     --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
     --delimiter : \
-    --preview 'bat --style=plain --color=always {1} --line-range {2}: --highlight-line {2}' \
+    --preview '~/.bin/nobin/_fzf_preview_file.sh {}' \
     --preview-window '<30(bottom,30%):wrap'
