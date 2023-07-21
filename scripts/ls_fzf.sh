879f339fd1f622a93fb54a4b11d0d3d1008e55ee
Roben Kleene <contact@robenkleene.com>
2023-07-21 17:46:26 +0000

    Tweak preview

--- scripts/ls_fzf.sh
+++ scripts/ls_fzf.sh
@@ -6,4 +6,5 @@ export CLICOLOR=1
 export SHELL=sh
 # We're not using `z_list_parentdir`, z version probably not worth the
 # additional time to run?
-exec ~/.bin/find_ls "$@" | fzf --ansi --multi --preview="~/.bin/nobin/_fzf_preview_file.sh {}" ${FZF_DEFAULT_PREVIEW_OPTS:-}
+exec ~/.bin/find_ls "$@" | fzf --ansi --multi --preview="~/.bin/nobin/_fzf_preview_file.sh {}" \
+  ${FZF_DEFAULT_PREVIEW_OPTS:-}
