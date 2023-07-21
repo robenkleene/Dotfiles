a6676bcafeca68130c5d770990d8cfa8fa7625f4
Roben Kleene <contact@robenkleene.com>
2023-07-21 20:32:29 +0000

    Clean up

--- scripts/ls_fzf.sh
+++ scripts/ls_fzf.sh
@@ -4,9 +4,7 @@ set -euo pipefail
 
 export CLICOLOR=1
 export SHELL=sh
-# We're not using `z_list_parentdir`, z version probably not worth the
-# additional time to run?
-set -x
+
 ~/.bin/find_ls "$@" | fzf --ansi --multi --preview="~/.bin/nobin/_fzf_preview_file.sh {}" \
   --bind='ctrl-v:become(vim {+}),ctrl-x:execute-silent(echo {+} | pbcopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
   --height=20 --preview-window=right,50%:wrap       
