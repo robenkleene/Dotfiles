0eccf95b5dbf33e651fbdea72383db8d7b0a76c7
Roben Kleene <contact@robenkleene.com>
2023-07-22 02:13:48 +0000

    Put back copy

--- scripts/ls_fzf.sh
+++ scripts/ls_fzf.sh
@@ -6,5 +6,5 @@ export CLICOLOR=1
 export SHELL=sh
 
 ~/.bin/find_ls "$@" | fzf --ansi --multi --reverse --preview="~/.bin/nobin/_fzf_preview_file.sh {}" \
-  --bind='ctrl-e:become(vim {+}),ctrl-p:execute-silent(echo {+} | pbcopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
+  --bind='ctrl-e:become(vim {+}),ctrl-x:execute-silent(echo {+} | pbcopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
   --height=20 --preview-window=right,50%:wrap       
