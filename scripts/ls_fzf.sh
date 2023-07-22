052864a085745dd2d1be287e621b2a0423956f87
Roben Kleene <contact@robenkleene.com>
2023-07-22 13:05:09 -0400

    Tweak fzf

--- scripts/ls_fzf.sh
+++ scripts/ls_fzf.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 export CLICOLOR=1
 export SHELL=sh
 
-# Only allow files because `_fzf_preview_file` can only support `--multi` with files
-~/.bin/find_ls -f "$@" | fzf --ansi --reverse --multi --preview="~/.bin/nobin/_fzf_preview_file.sh {}" \
+~/.bin/find_ls "$@" | fzf --ansi --reverse --multi --keep-right --preview="~/.bin/nobin/_fzf_preview_file.sh {}" \
   --bind="ctrl-e:become($EDITOR {+}),alt-p:become(~/.bin/nobin/_fzf_preview_file.sh {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
   --height=20 --preview-window=right,50%:wrap
