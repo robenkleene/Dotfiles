99c5b372939b3cd06134c10439ab2ded42ac30d9
Roben Kleene <contact@robenkleene.com>
2023-07-22 02:28:32 +0000

    Fix copy

--- scripts/z_fzf.sh
+++ scripts/z_fzf.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 # exec ~/.brew/bin/zoxide query --interactive "$@"
 exec ~/.bin/z_list | fzf --ansi --multi --reverse --preview="~/.bin/nobin/_fzf_preview_file.sh {}" \
-  --bind='ctrl-e:become(vim {+}),ctrl-x:execute-silent(echo {+} | pbcopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
+  --bind='ctrl-e:become(vim {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
   --height=20 --preview-window=right,50%:wrap       
