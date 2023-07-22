abb82112fbf4d1e991489f39c9d7f316cc9207e5
Roben Kleene <contact@robenkleene.com>
2023-07-22 19:19:30 +0000

    Fix copying hg

--- scripts/hg_status_fzf.sh
+++ scripts/hg_status_fzf.sh
@@ -2,9 +2,8 @@
 
 set -euo pipefail
 
-hg status 2>/dev/null | \
+hg status 2>/dev/null | awk '{$1=$1};1' | \
   fzf --no-sort --ansi --reverse --multi \
   --preview="echo {} | ~/.bin/nobin/_fzf_preview_hg_diff.sh" \
-  --bind="ctrl-e:become($EDITOR {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
-  --height=20 --preview-window=right,50%:wrap \
-  | sed 's/^[A-Z?] //'
+  --bind="ctrl-e:become($EDITOR {+2..}),ctrl-x:execute-silent(echo {+2..} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
+  --height=20 --preview-window=right,50%:wrap
