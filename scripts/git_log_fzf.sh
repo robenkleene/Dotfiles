3f1d373476a8af9bf69c60af5afd04bea87e4be7
Roben Kleene <contact@robenkleene.com>
2023-07-22 13:23:33 -0400

    Allow preview for git

--- scripts/git_log_fzf.sh
+++ scripts/git_log_fzf.sh
@@ -8,10 +8,8 @@ if [[ -n "${1-}" ]]; then
   separator=" -- "
 fi
 
-# alt-p:become(echo {} | ~/.bin/nobin/_fzf_preview_git_commit.sh \"$1\")
 git log --graph --color=always --format="%C(auto)%h %s%d"${separator}"${1}" | \
   fzf --no-sort --ansi --reverse \
   --preview "echo {} | ~/.bin/nobin/_fzf_preview_git_commit.sh \"$1\"" \
-  --bind="ctrl-x:execute-silent(echo {} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
-  --height=20 --preview-window=right,50%:wrap \
-  | grep --only-matching "[a-f0-9]\{7\}" | sed 's/\s.*$//'
+  --bind="ctrl-x:execute-silent(echo {} | ~/.bin/safecopy)+accept,alt-p:become(echo {} | ~/.bin/nobin/_fzf_preview_git_commit.sh \"$1\"),ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
+  --height=20 --preview-window=right,50%:wrap
