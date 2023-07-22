b1e983110f7431f43e6a9ae20e0271056907567d
Roben Kleene <contact@robenkleene.com>
2023-07-22 06:00:32 -0400

    Support multiple previews

--- scripts/ls_fzf.sh
+++ scripts/ls_fzf.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 export CLICOLOR=1
 export SHELL=sh
 
-~/.bin/find_ls "$@" | fzf --ansi --multi --reverse --preview="~/.bin/nobin/_fzf_preview_file.sh {}" \
-  --bind="ctrl-e:become($EDITOR {+}),alt-p:become(~/.bin/nobin/_fzf_preview_file.sh {} {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
-  --height=20 --preview-window=right,50%:wrap       
+# Only allow files because `_fzf_preview_file` can only support `--multi` with files
+~/.bin/find_ls -f "$@" | fzf --ansi --multi --reverse --preview="~/.bin/nobin/_fzf_preview_file.sh {}" \
+  --bind="ctrl-e:become($EDITOR {+}),alt-p:become(~/.bin/nobin/_fzf_preview_file.sh {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
+  --height=20 --preview-window=right,50%:wrap
--- scripts/nobin/_fzf_preview_file.sh
+++ scripts/nobin/_fzf_preview_file.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 if [[ -f "$1" ]]; then
-  bat --style=plain --color=always "$1"
+  bat --style=plain --color=always "$@"
 elif [[ -d "$1" ]]; then
   ls -Cp "$1"
 fi
