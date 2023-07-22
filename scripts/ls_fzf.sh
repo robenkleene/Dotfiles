4a9c4942e80dc49f11679c6684310c3ead782b7b
Roben Kleene <contact@robenkleene.com>
2023-07-22 02:08:40 +0000

    Reverse

--- scripts/ls_fzf.sh
+++ scripts/ls_fzf.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 export CLICOLOR=1
 export SHELL=sh
 
-~/.bin/find_ls "$@" | fzf --ansi --multi --preview="~/.bin/nobin/_fzf_preview_file.sh {}" \
-  --bind='ctrl-v:become(vim {+}),ctrl-x:execute-silent(echo {+} | pbcopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
+~/.bin/find_ls "$@" | fzf --ansi --multi --reverse --preview="~/.bin/nobin/_fzf_preview_file.sh {}" \
+  --bind='ctrl-e:become(vim {+}),ctrl-p:execute-silent(echo {+} | pbcopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
   --height=20 --preview-window=right,50%:wrap       
