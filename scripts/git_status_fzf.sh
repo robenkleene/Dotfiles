9ab74c0e17f95a824a87020fc0fbb0a90e4696d5
Roben Kleene <contact@robenkleene.com>
2023-07-22 19:08:46 +0000

    Handle editing

--- scripts/git_status_fzf.sh
+++ scripts/git_status_fzf.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-git status --short | \
-  fzf --no-sort --ansi --reverse --multi \
+git status --short | awk '{$1=$1};1' | \
+  fzf --nth 2.. --no-sort --ansi --reverse --multi \
   --preview "echo {} | ~/.bin/nobin/_fzf_preview_git_diff.sh" \
-  --bind="ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,alt-p:become(echo {+} | ~/.bin/nobin/_fzf_preview_git_diff.sh),ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
+  --bind="ctrl-e:become($EDITOR {+2..}),ctrl-x:execute-silent(echo {+2..} | ~/.bin/safecopy)+accept,alt-p:become(echo {+} | ~/.bin/nobin/_fzf_preview_git_diff.sh),ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
   --height=20 --preview-window=right,50%:wrap
