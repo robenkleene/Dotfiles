6781f3705d57ddf7ea82fc7b737a35305958fa0f
Roben Kleene <contact@robenkleene.com>
2023-07-22 05:15:20 -0400

    Don't sort zoxide

--- scripts/z_fzf.sh
+++ scripts/z_fzf.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 # exec ~/.brew/bin/zoxide query --interactive "$@"
-exec ~/.bin/z_list | fzf --ansi --multi --reverse --preview="~/.bin/nobin/_fzf_preview_file.sh {}" \
+exec ~/.bin/z_list | fzf --ansi --multi --reverse --no-sort --preview="~/.bin/nobin/_fzf_preview_file.sh {}" \
   --bind='ctrl-e:become(vim {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down' \
-  --height=20 --preview-window=right,50%:wrap       
+  --height=20 --preview-window=right,50%:wrap
