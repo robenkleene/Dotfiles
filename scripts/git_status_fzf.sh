cefd4ebb5f481c62788dc19b4b8ba721e6ee6249
Roben Kleene <contact@robenkleene.com>
2023-07-22 05:54:46 -0400

    Work on previews

--- scripts/git_status_fzf.sh
+++ scripts/git_status_fzf.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 git status --short | \
   fzf --no-sort --ansi --multi \
   --preview "echo {} | ~/.bin/nobin/_fzf_preview_git_diff.sh" \
-  --bind="ctrl-e:become($EDITOR {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,alt-p:become(echo {} | ~/.bin/nobin/_fzf_preview_git_diff.sh),ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
+  --bind="ctrl-e:become($EDITOR {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,alt-p:become(echo {+} | ~/.bin/nobin/_fzf_preview_git_diff.sh),ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
   --height=20 --preview-window=right,50%:wrap \
   | sed 's/^[ ?][A-Z?] //'
--- scripts/hg_log_fzf.sh
+++ scripts/hg_log_fzf.sh
@@ -5,6 +5,6 @@ set -eo pipefail
 
 hg log --template '{node|short} {author|user}: {desc|strip|firstline}\n' --limit 100 "$1" | \
   fzf --no-sort --ansi \
-  --bind="ctrl-e:become($EDITOR {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
+  --bind="ctrl-x:execute-silent(echo {} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
   --height=20 --preview-window=right,50%:wrap \
   | grep --only-matching "[a-f0-9]\{7\}" | sed 's/\s.*$//'
