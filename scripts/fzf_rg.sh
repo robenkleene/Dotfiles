7ecf8f668e0827088887ad30a4be4083b921c83d
Roben Kleene <contact@robenkleene.com>
2023-07-23 16:27:12 +0000

    Allow dir

--- scripts/fzf_rg.sh
+++ scripts/fzf_rg.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
-INITIAL_QUERY="${*:-}"
-: | fzf --ansi --reverse --disabled --multi --query "$INITIAL_QUERY" \
-    --bind "start:reload:$RG_PREFIX {q}" \
-    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+# Allows search term from args
+# INITIAL_QUERY="${*:-}"
+INITIAL_QUERY=""
+: | fzf --ansi --reverse --disabled --keep-right --multi --query "$INITIAL_QUERY" \
+    --bind "start:reload:$RG_PREFIX {q} $@" \
+    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} $@ || true" \
     --delimiter : \
     --preview 'bat --style=plain --color=always {1} --line-range {2}: --highlight-line {2}' \
     --bind="ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
