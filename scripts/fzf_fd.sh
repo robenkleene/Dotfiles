14d51f5de88bb34a014e741c7db997da84a2858d
Roben Kleene <contact@robenkleene.com>
2023-07-23 16:23:55 +0000

    Allow dir

--- scripts/fzf_fd.sh
+++ scripts/fzf_fd.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-RG_PREFIX="fd "
-INITIAL_QUERY="${*:-}"
-: | fzf --ansi --reverse --disabled --multi --query "$INITIAL_QUERY" \
-    --bind "start:reload:$RG_PREFIX {q}" \
-    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+RG_PREFIX="fd --color=always "
+# Allows seatch term from args
+# INITIAL_QUERY="${*:-}"
+INITIAL_QUERY=""
+: | fzf --ansi --reverse --disabled --keep-right --multi --query "$INITIAL_QUERY" \
+    --bind "start:reload:$RG_PREFIX {q} $@" \
+    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} $@ || true" \
     --delimiter : \
     --preview "$HOME/.bin/nobin/_fzf_preview_file.sh {}" \
     --bind="ctrl-e:become($EDITOR {+}),alt-p:become(~/.bin/nobin/_fzf_preview_file.sh {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
