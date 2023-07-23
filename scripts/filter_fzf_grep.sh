b4cbc2ae8b38b3535f4892456a26ec1d2cea4fbd
Roben Kleene <contact@robenkleene.com>
2023-07-23 21:30:31 +0000

    Fix printing args

--- scripts/filter_fzf_grep.sh
+++ scripts/filter_fzf_grep.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 fzf -d ':' -n 2.. --ansi --reverse --multi \
   --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
-  --bind="ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
+  --bind="ctrl-x:execute-silent(~/.bin/print_args {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
   --height=20 --preview-window=right,50%:wrap
