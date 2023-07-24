c0d97188b113f7154ae61f172e588251c6fe01b6
Roben Kleene <contact@robenkleene.com>
2023-07-24 13:39:53 +0000

    Use grep binding

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -13,7 +13,7 @@ return {
     { '<leader>l' },
     { '<leader>f' },
     -- { '<leader>F' },
-    { '<leader>s' },
+    { '<leader>g' },
     -- { '<leader>S' },
     { '<leader>i' },
     { '<leader>I' },
@@ -50,7 +50,7 @@ return {
     -- vim.keymap.set('n', '<leader>F', require('telescope.builtin').find_files)
     -- vim.keymap.set('n', '<leader>s', '<cmd>lua require("telescope.builtin").live_grep({cwd = "%:h"})<CR>')
     -- vim.keymap.set('n', '<leader>S', require('telescope.builtin').live_grep)
-    vim.keymap.set('n', '<leader>s', require('telescope.builtin').live_grep)
+    vim.keymap.set('n', '<leader>g', require('telescope.builtin').live_grep)
     vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files)
     vim.keymap.set('n', '<leader>i', require('telescope.builtin').lsp_document_symbols)
     vim.keymap.set('n', '<leader>I', require('telescope.builtin').lsp_dynamic_workspace_symbols)
--- vim/plugin/plugins/fzf.vim
+++ vim/plugin/plugins/fzf.vim
@@ -3,7 +3,7 @@ set rtp+=$HOMEBREW_DIR/opt/fzf
 nnoremap <leader>b :Buffers<CR>
 nnoremap <leader>l :BLines<CR>
 nnoremap <leader>r :History<CR>
-nnoremap <leader>s :RG<CR>
+nnoremap <leader>g :RG<CR>
 nnoremap <leader>f :Files<CR>
 nnoremap <leader>m :Marks<CR>
 nnoremap <leader>i :BTags<CR>
