9396d16b11412e58cbfa2582fb2d1863e4341c2e
Roben Kleene <contact@robenkleene.com>
2023-07-27 13:55:00 +0000

    Add back CDPATH

--- config/fish/functions/fish_sync_variables.fish
+++ config/fish/functions/fish_sync_variables.fish
@@ -3,6 +3,8 @@ function fish_sync_variables
         set -e $v
     end
 
+    set -U CDPATH . ~
+
     # Fish
     set -Ux fish_help_browser echo
     # Suppress default greeting
--- zshenv
+++ zshenv
@@ -5,6 +5,8 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
 
+export CDPATH=~
+
 # export VIM_COMMAND=nvim
 export VIM_COMMAND=vim
 
