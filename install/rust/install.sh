cd6e4f427e31222f9d966d7a2bad1ee099c6c181
Roben Kleene <contact@robenkleene.com>
2023-07-29 18:46:52 -0400

    cd to script

--- install/rust/install.sh
+++ install/rust/install.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+cd "$(dirname "$0")" || exit 1
+
 # `-y`: Disable confirmation
 # `--no-modify-path`: Don't customize path
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
