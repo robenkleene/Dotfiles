d8558c2cd5ecb2afd525c26ced06ec1ebbde3b8f
Roben Kleene <contact@robenkleene.com>
2023-07-30 17:22:07 -0400

    Tweak scripts

--- scripts/md_title.sh
+++ scripts/md_title.sh
@@ -2,13 +2,12 @@
 
 filename=$(basename "$1")
 filename_no_ext=${filename%.*}
-script_dir=$(dirname "$0")
 if [[ $filename_no_ext =~ ^[a-z0-9_-]*$ ]]; then
 # If the filename contains only lowercase letters, hyphens, and underscores then assume
 # convert it to title case with spaces.
-title=$(echo "$filename_no_ext" | tr "-" " " | tr "_" " " | "$script_dir"/title_case)
+title=$(echo "$filename_no_ext" | tr "-" " " | tr "_" " " | ~/.bin/f_to_titlecase)
 else
-title=$(echo "$filename_no_ext" | "$script_dir"/title_case)
+title=$(echo "$filename_no_ext" | ~/.bin/f_to_titlecase)
 fi
 
 echo "# $title"
