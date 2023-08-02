e12a0e044789bd83f32e4303277c7bf5210753fc
Roben Kleene <contact@robenkleene.com>
2023-08-02 20:53:56 +0000

    Fix preview

--- scripts/hg_log_fzf.sh
+++ scripts/hg_log_fzf.sh
@@ -6,5 +6,5 @@ set -eo pipefail
 hg log --template '{node|short} {author|user}: {desc|strip|firstline}\n' --limit 100 "$1" | \
   fzf --nth 2.. --no-sort --ansi --reverse \
   --preview "echo {} | ~/.bin/nobin/_fzf_preview_hg_rev.sh \"$1\"" \
-  --bind="ctrl-x:execute-silent(echo {1} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
+  --bind="ctrl-x:execute-silent(echo {1} | ~/.bin/safecopy)+accept,alt-p:become(echo {} | ~/.bin/nobin/_fzf_preview_hg_rev.sh \"$1\"),ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
   --height=20 --preview-window=right,50%:wrap
