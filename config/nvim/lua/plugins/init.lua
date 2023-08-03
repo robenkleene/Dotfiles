5ee234bdc49dfe1a793fbe8c35c8d4bb7c907fc3
Roben Kleene <contact@robenkleene.com>
2023-08-03 04:56:28 -0400

    Try oil again

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -10,15 +10,15 @@ return {
     config = true,
     event = "VeryLazy"
   },
-  { 'tpope/vim-vinegar' },
+  -- { 'tpope/vim-vinegar' },
   -- Opening files from oil can cause `ftplugin` to not fire, as a workaround just use the fuzzy finder instead
-  -- {
-  --   'stevearc/oil.nvim',
-  --   config = function()
-  --     require("oil").setup()
-  --     vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
-  --   end
-  -- },
+  {
+    'stevearc/oil.nvim',
+    config = function()
+      require("oil").setup()
+      vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+    end
+  },
   {
     'L3MON4D3/LuaSnip',
     -- This can't co-exist with GitHub Copilot
