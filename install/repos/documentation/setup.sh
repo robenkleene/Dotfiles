e2b54356faaa066313ab357817f14b8b6eb931fb
Roben Kleene <contact@robenkleene.com>
2023-08-03 10:53:48 -0400

    Cleanup scripts

--- install/repos/documentation/setup.sh
+++ install/repos/documentation/setup.sh
@@ -31,7 +31,7 @@ mkdir -p ~/Documentation/
 
 cd "$(dirname "$0")" || exit 1
 if [[ "$use_https" == "true" ]]; then
-  ~/.bin/git_sync_clone -p ~/Documentation/ -s <(~/.bin/git_swap_format -H < documentation.txt) -f
+  ~/.bin/git_sync_clone -p ~/Documentation/ -s <(~/.bin/f_git_url_to_swapped_format -H < documentation.txt) -f
 else
   ~/.bin/git_sync_clone -p ~/Documentation/ -s documentation.txt -f
 fi
--- scripts/houdini.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-cd /Applications/Houdini/Current/Frameworks/Houdini.framework/Versions/Current/Resources/
-source ./houdini_setup >/dev/null
-cd - >/dev/null
-/Applications/Houdini/Current/Frameworks/Houdini.framework/Versions/Current/Resources/bin/houdini -apprentice "$@"
