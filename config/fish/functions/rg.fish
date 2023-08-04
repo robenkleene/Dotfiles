8f6909188106fbf56fc5324bbb225f394de7d517
Roben Kleene <contact@robenkleene.com>
2023-08-04 16:09:30 -0400

    Cleanup

--- config/fish/functions/rg.fish
+++ config/fish/functions/rg.fish
@@ -1,4 +1,4 @@
-function rg --wraps='rg_custom -g "!tags"' --wraps=rg_custom --description 'alias rg=rg_custom'
+function rg --wraps=rg_custom --description 'alias rg=rg_custom'
   rg_custom $argv
         
 end
