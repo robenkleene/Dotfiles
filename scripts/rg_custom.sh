7b5f4c75e5b37746d213dca1d2646fb3a4292690
Roben Kleene <contact@robenkleene.com>
2023-08-15 17:55:57 +0000

    Follow symlinks

--- scripts/rg_custom.sh
+++ scripts/rg_custom.sh
@@ -8,12 +8,12 @@ if [[ ! -t 0 ]]; then
 elif [[ -t 1 ]]; then
   if [[ "$TERM" = "dumb" ]]; then
     # Emacs
-    exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:239' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@"
+    exec rg --follow --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:239' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@"
   else
     # Default
-    exec rg --no-heading --with-filename --smart-case --colors 'match:fg:white' --colors 'match:bg:239' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@"
+    exec rg --follow --no-heading --with-filename --smart-case --colors 'match:fg:white' --colors 'match:bg:239' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@"
   fi
 else
   # Use grep format and no colors when piping *from* rg
-  exec rg --no-heading --with-filename --smart-case --line-number "$@"
+  exec rg --follow --no-heading --with-filename --smart-case --line-number "$@"
 fi
