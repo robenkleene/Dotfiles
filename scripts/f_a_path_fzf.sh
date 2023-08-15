0fb94dd93e0a9b54bbafae20f08017724b71bb00
Roben Kleene <contact@robenkleene.com>
2023-08-15 05:01:18 -0400

    Archive fzf

--- scripts/f_a_path_fzf.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-fzf --ansi --reverse --multi --preview "$HOME/.bin/nobin/_fzf_preview_file.sh {}" \
-  --bind="ctrl-e:become($EDITOR {+}),alt-p:become(~/.bin/nobin/_fzf_preview_file.sh {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
-  --height=20 --preview-window=right,50%:wrap
--- scripts/fzf_fd.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-RG_PREFIX="fd --color=always --follow"
-: | fzf --ansi --reverse --disabled --keep-right --multi --query "$INITIAL_QUERY" \
-    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} $@ || true" \
-    --delimiter : \
-    --preview "$HOME/.bin/nobin/_fzf_preview_file.sh {}" \
-    --bind="ctrl-e:become($EDITOR {+}),alt-p:become(~/.bin/nobin/_fzf_preview_file.sh {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
-    --height=20 --preview-window=right,50%:wrap
