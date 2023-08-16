17a0144ac9a476c85aa78b652852cbe376ada788
Roben Kleene <contact@robenkleene.com>
2023-08-16 14:59:55 +0000

    Improve preview

--- scripts/nobin/_fzf_preview_hg_rev.sh
+++ scripts/nobin/_fzf_preview_hg_rev.sh
@@ -3,7 +3,8 @@
 # Allow unbound variable for $1
 set -eo pipefail
 
-hash=$(cat | grep --only-matching "[a-f0-9]\{12\}" | sed 's/\s.*$//' | head -1)
+# hg log shows `9` characters, but other presentations of hashes only have `9`
+hash=$(cat | grep --only-matching "[a-f0-9]\{9\}" | sed 's/\s.*$//' | head -1)
 
 if [[ "$#" -eq 1 ]]; then
   hg log --color=always --patch --rev "$hash" "$1"
