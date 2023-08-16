f80725312fec34b850f7745b89a9640c36a6e94c
Roben Kleene <contact@robenkleene.com>
2023-08-16 15:53:15 +0000

    Disable rg smartcase

--- scripts/rg_custom.sh
+++ scripts/rg_custom.sh
@@ -4,16 +4,16 @@ set -e
 
 if [[ ! -t 0 ]]; then
   # Don't show any decorations when piping *to* `rg`
-  exec rg --smart-case "$@"
+  exec rg "$@"
 elif [[ -t 1 ]]; then
   if [[ "$TERM" = "dumb" ]]; then
     # Emacs
-    exec rg --no-heading --with-filename --smart-case --line-number --colors 'match:fg:white' --colors 'match:bg:239' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@"
+    exec rg --no-heading --with-filename --line-number --colors 'match:fg:white' --colors 'match:bg:239' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@"
   else
     # Default
-    exec rg --no-heading --with-filename --smart-case --colors 'match:fg:white' --colors 'match:bg:239' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@"
+    exec rg --no-heading --with-filename --colors 'match:fg:white' --colors 'match:bg:239' --colors 'path:fg:cyan' --colors 'line:fg:white' "$@"
   fi
 else
   # Use grep format and no colors when piping *from* rg
-  exec rg --no-heading --with-filename --smart-case --line-number "$@"
+  exec rg --no-heading --with-filename --line-number "$@"
 fi
