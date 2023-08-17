1a4c0aa80dba2fd0992682a779b511f7127e2ba5
Roben Kleene <contact@robenkleene.com>
2023-08-17 22:19:40 +0000

    Update git hg fzf

--- scripts/hg_log_fzf.sh
+++ scripts/hg_log_fzf.sh
@@ -6,5 +6,5 @@ set -eo pipefail
 hg log --template '{node|short} {author|user}: {desc|strip|firstline}\n' --limit 100 "$1" | \
   fzf --nth 2.. --no-sort --ansi --reverse --multi \
   --preview "echo {} | ~/.bin/nobin/_fzf_preview_hg_rev.sh \"$1\"" \
-  --bind="ctrl-x:execute-silent(echo {1} | ~/.bin/safecopy)+accept,alt-p:become(echo {} | ~/.bin/nobin/_fzf_preview_hg_rev.sh \"$1\"),ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
+  --bind="alt-p:become(echo {} | ~/.bin/nobin/_fzf_preview_hg_rev.sh \"$1\"),alt-u:become(echo {} | ~/.bin/nobin/_fzf_update_hg_rev.sh),ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
   --height=100% --preview-window=right,50%:wrap
