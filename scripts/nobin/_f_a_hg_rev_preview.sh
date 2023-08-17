f0cd8c891d4652f8a9a3ea9a5b56abfefb6a060a
Roben Kleene <contact@robenkleene.com>
2023-08-17 23:20:13 +0000

    Add spaces

--- scripts/nobin/_f_a_hg_rev_preview.sh
+++ scripts/nobin/_f_a_hg_rev_preview.sh
@@ -4,7 +4,7 @@
 set -eo pipefail
 
 # hg log shows `9` characters, but other presentations of hashes only have `9`
-hash=$(cat | grep --only-matching "[a-f0-9]\{9\}" | sed 's/\s.*$//' | head -1)
+hash=$(cat | grep --only-matching " [a-f0-9]\{9\} " | sed 's/\s.*$//' | head -1)
 
 if [[ "$#" -eq 1 ]]; then
   hg log --color=always --patch --rev "$hash" "$1"
