b85581b4ea9484ad15b09c19ba8cff5f8cb2d6cc
Roben Kleene <contact@robenkleene.com>
2023-08-17 19:10:11 +0000

    Restore scripts

---
+++ scripts/fzf_rg.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case --follow"
+: | fzf --ansi --reverse --disabled --keep-right --multi --query "$INITIAL_QUERY" \
+    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} $@ || true" \
+    --delimiter : \
+    --preview 'bat --style=plain --color=always {1} --line-range {2}: --highlight-line {2}' \
+    --bind="ctrl-x:execute-silent(~/.bin/print_args {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
+    --height=20 --preview-window=right,50%:wrap
+
