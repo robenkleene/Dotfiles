409232f8b889d081948741d0c2dc610ef822b589
Roben Kleene <contact@robenkleene.com>
2023-08-17 22:13:23 +0000

    Add checkout

--- scripts/git_log_fzf.sh
+++ scripts/git_log_fzf.sh
@@ -11,5 +11,5 @@ fi
 git log --graph --color=always --format="%C(auto)%h %s%d"${separator}"${1}" | \
   fzf --no-sort --ansi --reverse --multi \
   --preview "echo {} | ~/.bin/nobin/_fzf_preview_git_commit.sh \"$1\"" \
-  --bind="ctrl-x:execute-silent(echo {2} | ~/.bin/safecopy)+accept,alt-p:become(echo {} | ~/.bin/nobin/_fzf_preview_git_commit.sh \"$1\"),ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
+  --bind="ctrl-x:execute-silent(echo {2} | ~/.bin/safecopy)+accept,alt-p:become(echo {} | ~/.bin/nobin/_fzf_preview_git_commit.sh \"$1\"),alt-u:become(echo {} | ~/.bin/nobin/_fzf_checkout_git_commit.sh \"$1\"),ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
   --height=20 --preview-window=right,50%:wrap
