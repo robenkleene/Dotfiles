8222690683d473fd2ca68c7863bd2c0cb3fd3ac2
Roben Kleene <contact@robenkleene.com>
2023-08-18 14:50:10 +0000

    Rename to hg file diff

--- scripts/hg_status_fzf.sh
+++ scripts/hg_status_fzf.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 hg status 2>/dev/null | awk '{$1=$1};1' | \
   fzf --no-sort --ansi --reverse --multi \
-  --preview="~/.bin/nobin/_f_a_hg_file_preview.sh {+}" \
-  --bind="ctrl-e:become(${EDITOR:-vim} {+2..}),alt-p:become(~/.bin/nobin/_f_a_hg_file_preview.sh {+}),ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
+  --preview="~/.bin/nobin/_f_a_hg_file_diff.sh {+}" \
+  --bind="ctrl-e:become(${EDITOR:-vim} {+2..}),alt-p:become(~/.bin/nobin/_f_a_hg_file_diff.sh {+}),ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
   --height=20 --preview-window=right,50%:wrap
