bbd3cc7a921ba243a3004e2938d2a393ad2a3322
Roben Kleene <contact@robenkleene.com>
2023-08-18 14:53:58 +0000

    Rename git preview to git diff

--- scripts/git_status_fzf.sh
+++ scripts/git_status_fzf.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 git -c color.status=always status --short | \
   fzf --nth 2.. --no-sort --ansi --reverse --multi \
-  --preview "~/.bin/nobin/_f_a_git_file_preview.sh {}" \
-  --bind="ctrl-e:become(${EDITOR:-vim} {+2..}),alt-p:become(~/.bin/nobin/_f_a_git_file_preview.sh {+}),alt-s:become(~/.bin/nobin/_f_a_git_file_stage_toggle.sh {+}),ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
+  --preview "~/.bin/nobin/_f_a_git_file_diff.sh {}" \
+  --bind="ctrl-e:become(${EDITOR:-vim} {+2..}),alt-p:become(~/.bin/nobin/_f_a_git_file_diff.sh {+}),alt-s:become(~/.bin/nobin/_f_a_git_file_stage_toggle.sh {+}),ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
   --height=100% --preview-window=right,50%:wrap
