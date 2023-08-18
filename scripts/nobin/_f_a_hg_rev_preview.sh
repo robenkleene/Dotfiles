20ee6d069df55bb2ddd4dc57e4561ba85e7c37c9
Roben Kleene <contact@robenkleene.com>
2023-08-18 15:12:28 +0000

    Fix hg preview

--- scripts/nobin/_f_a_hg_rev_preview.sh
+++ scripts/nobin/_f_a_hg_rev_preview.sh
@@ -4,7 +4,8 @@
 set -eo pipefail
 
 # hg log shows `9` characters, but other presentations of hashes only have `9`
-hash=$(cat | grep --only-matching " [a-f0-9]\{9\} " | sed 's/\s.*$//' | head -1)
+# [^A-Z] Removes things like diff and task #s
+hash=$(cat | grep --only-matching "[^A-Z][a-f0-9]\{9\}[a-f0-9]* " | sed 's/\s.*$//' | head -1)
 
 if [[ "$#" -eq 1 ]]; then
   hg log --color=always --patch --rev "$hash" "$1"
