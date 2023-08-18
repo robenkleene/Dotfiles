8ce6c11f9cb57808dafedcae504045be7b2d8fc9
Roben Kleene <contact@robenkleene.com>
2023-08-18 18:06:50 +0000

    Add difftool

--- scripts/git_status_fzf.sh
+++ scripts/git_status_fzf.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 git -c color.status=always status --short | \
   fzf --nth 2.. --no-sort --ansi --reverse --multi \
   --preview "~/.bin/nobin/_f_a_git_file_diff.sh {}" \
-  --bind="ctrl-e:become(${EDITOR:-vim} {+2..}),alt-p:become(~/.bin/nobin/_f_a_git_file_diff.sh {+}),alt-s:become(~/.bin/nobin/_f_a_git_file_stage_toggle.sh {+}),ctrl-u:preview-page-up,ctrl-d:preview-page-down,ctrl-a:toggle-all" \
+  --bind="ctrl-e:become(${EDITOR:-vim} {+2..}),ctrl-t:become(git difftool {+2..}),alt-p:become(~/.bin/nobin/_f_a_git_file_diff.sh {+}),alt-s:become(~/.bin/nobin/_f_a_git_file_stage_toggle.sh {+}),ctrl-u:preview-page-up,ctrl-d:preview-page-down,ctrl-a:toggle-all" \
   --height=100% --preview-window=right,50%:wrap
