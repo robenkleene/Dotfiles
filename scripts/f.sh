d25c10767230ef407ab3313935617caa9e708242
Roben Kleene <contact@robenkleene.com>
2023-08-19 14:11:37 -0400

    Support piping to f and g

--- scripts/f.sh
+++ scripts/f.sh
@@ -2,4 +2,10 @@
 
 set -euo pipefail
 
-exec ~/.bin/fzf_fd "$@"
+if [[ ! -t 0 ]]; then
+  fzf --ansi --reverse --multi --preview "$HOME/.bin/nobin/_preview_file.sh {}" \
+    --bind="ctrl-e:become(${EDITOR:vim} {+}),alt-p:become(~/.bin/nobin/_preview_file.sh {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
+    --height=20 --preview-window=right,50%:wrap
+else
+  exec ~/.bin/fzf_fd "$@"
+fi
