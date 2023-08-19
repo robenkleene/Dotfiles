b414a7603090d1dac6264473e112f31d81b81985
Roben Kleene <contact@robenkleene.com>
2023-08-19 18:33:38 -0400

    Fix f

--- scripts/f.sh
+++ scripts/f.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 if [[ ! -t 0 ]]; then
   fzf --ansi --reverse --multi --preview "$HOME/.bin/nobin/_preview_file.sh {}" \
-    --bind="ctrl-e:become(${EDITOR:vim} {+}),alt-p:become(~/.bin/nobin/_preview_file.sh {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
+    --bind="ctrl-e:become(${EDITOR:-vim} {+}),alt-p:become(~/.bin/nobin/_preview_file.sh {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
     --height=20 --preview-window=right,50%:wrap
 else
   exec ~/.bin/fzf_fd "$@"
