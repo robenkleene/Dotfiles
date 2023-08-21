36ce3bc11f548f6031a5332a276245fe5aae8bf5
Roben Kleene <contact@robenkleene.com>
2023-08-21 21:38:19 +0000

    Remove scripts

--- scripts/fzf_fd.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-RG_PREFIX="fd --color=always --follow"
-: | fzf --ansi --reverse --disabled --keep-right --multi --query "$INITIAL_QUERY" \
-    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} $@ || true" \
-    --delimiter : \
-    --preview "$HOME/.bin/nobin/_preview_file.sh {}" \
-    --bind="ctrl-e:become(${EDITOR:-vim} {+}),alt-p:become(~/.bin/nobin/_preview_file.sh {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-page-up,ctrl-d:preview-page-down,ctrl-a:toggle-all" \
-    --height=20 --preview-window=right,50%:wrap
