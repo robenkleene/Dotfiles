02c2b99d0ed3639e351101b7dfaf61e2c921c9e4
Roben Kleene <contact@robenkleene.com>
2023-08-22 05:11:32 -0400

    Use new f and g

--- scripts/f.sh
+++ scripts/f.sh
@@ -3,9 +3,11 @@
 set -euo pipefail
 
 if [[ ! -t 0 ]]; then
-  fzf --ansi --reverse --multi --preview "$HOME/.bin/nobin/_preview_file.sh {}" \
+  fzf --ansi --reverse --keep-right --multi --preview "$HOME/.bin/nobin/_preview_file.sh {}" \
     --bind="ctrl-e:become(${EDITOR:-vim} {+}),alt-p:become(~/.bin/nobin/_preview_file.sh {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
     --height=20 --preview-window=right,50%:wrap
 else
-  exec ~/.bin/fzf_fd "$@"
+  exec fd --color=always "$@" | fzf --ansi --reverse --keep-right --multi --preview "$HOME/.bin/nobin/_preview_file.sh {}" \
+    --bind="ctrl-e:become(${EDITOR:-vim} {+}),alt-p:become(~/.bin/nobin/_preview_file.sh {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" \
+    --height=20 --preview-window=right,50%:wrap
 fi
--- scripts/g.sh
+++ scripts/g.sh
@@ -3,11 +3,13 @@
 set -euo pipefail
 
 if [[ ! -t 0 ]]; then
-  fzf -d ':' -n 2.. --ansi --reverse --multi \
+  fzf -d ':' -n 2.. --ansi --reverse --keep-right --multi \
     --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
     --bind="ctrl-x:execute-silent(~/.bin/print_args {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
     --height=20 --preview-window=right,50%:wrap
-  # Piping to
 else
-  exec ~/.bin/fzf_rg "$@"
+  exec rg --no-heading --with-filename --line-number --color=always "$@" | fzf -d ':' -n 2.. --ansi --reverse --keep-right --multi \
+    --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
+    --bind="ctrl-x:execute-silent(~/.bin/print_args {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
+    --height=20 --preview-window=right,50%:wrap
 fi
