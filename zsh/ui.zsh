088926389477596caafebb53c30aedbe1e55bf59
Roben Kleene <contact@robenkleene.com>
2023-08-22 06:02:28 -0400

    Change job indicator order

--- zsh/ui.zsh
+++ zsh/ui.zsh
@@ -52,5 +52,5 @@ chpwd() {
 # Note the last character is a non-breaking space in order to make prompts
 # easier to find in search. This is `<C-k><space><space>` in Vim.
 # Add ${SSH_CONNECTION:+%#} after the last #% to put back double SSH prompt
-PS1='%F{$comment}%D{%I:%M %p} %F{yellow}${SSH_CONNECTION:+%m }%F{cyan}${prompt_path} %F{yellow}%(1j.%j& .)%F{red}%(?..%?? )%f%# '
+PS1='%F{$comment}%D{%I:%M %p} %F{yellow}${SSH_CONNECTION:+%m }%F{cyan}${prompt_path} %F{yellow}%(1j.&%j .)%F{red}%(?..%?? )%f%# '
 # RPROMPT='${vcs_info_msg_0_}'
