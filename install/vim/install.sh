a2beb0d798c45ebec544d7376bc58f4b6cc74a02
Roben Kleene <contact@robenkleene.com>
2023-08-22 12:12:08 +0000

    Fix sync script

--- install/vim/install.sh
+++ install/vim/install.sh
@@ -2,12 +2,10 @@
 
 set -euo pipefail
 mkdir -p "$HOME/Developer/Dotfiles/config/nvim/after/ftplugin"
-mkdir -p "$HOME/Developer/Dotfiles/config/nvim/after/plugin"
 mkdir -p "$HOME/Developer/Dotfiles/config/nvim/after/autoload"
 mkdir -p "$HOME/Developer/Dotfiles/config/nvim/ftplugin"
 mkdir -p "$HOME/Developer/Dotfiles/config/nvim/plugin"
 ~/.bin/sync_symlinks -s "$HOME/Developer/Dotfiles/vim/after/ftplugin" -d "$HOME/Developer/Dotfiles/config/nvim/after/ftplugin" -f
-~/.bin/sync_symlinks -s "$HOME/Developer/Dotfiles/vim/after/plugin" -d "$HOME/Developer/Dotfiles/config/nvim/after/plugin" -f
 ~/.bin/sync_symlinks -s "$HOME/Developer/Dotfiles/vim/after/autoload" -d "$HOME/Developer/Dotfiles/config/nvim/after/autoload" -f
 ~/.bin/sync_symlinks -s "$HOME/Developer/Dotfiles/vim/ftplugin" -d "$HOME/Developer/Dotfiles/config/nvim/ftplugin" -f
 ~/.bin/sync_symlinks -s "$HOME/Developer/Dotfiles/vim/plugin" -d "$HOME/Developer/Dotfiles/config/nvim/plugin" -f
