5796d796a783562fe27075b24bdf4ca154821285
Roben Kleene <contact@robenkleene.com>
2023-08-27 20:35:03 +0000

    Work on prompt

--- zsh/ui.zsh
+++ zsh/ui.zsh
@@ -37,23 +37,25 @@ autoload -Uz vcs_info
 # zstyle ':vcs_info:*' formats '%F{cyan}%r %F{green}%b%f%u%c'
 # zstyle ':vcs_info:*' actionformats '%F{cyan}%r %F{green}%b%f%u%c %F{red}(%a)%f'
 
-# Prompt
-update_prompt() {
-  # `man zshmisc` for more info
-  prompt_path=%2~
-}
-precmd() {
-  update_prompt
-}
-chpwd() {
-  update_prompt
-}
+# # Prompt
+# update_prompt() {
+#   # `man zshmisc` for more info
+#   # prompt_path=%2~
+#   prompt_path=%15<..<%~%<<%
+# }
+# precmd() {
+#   update_prompt
+# }
+# chpwd() {
+#   update_prompt
+# }
 
 # Note the last character is a non-breaking space in order to make prompts
 # easier to find in search. This is `<C-k><space><space>` in Vim.
 # Add ${SSH_CONNECTION:+%#} after the last #% to put back double SSH prompt
 # `man zshmisc` for explanation
-PS1='%F{$comment}%D{%I:%M %p} %F{yellow}${SSH_CONNECTION:+%m }%F{cyan}${prompt_path} %F{yellow}%(1j.&%j .)'
+PS1='%F{$comment}%D{%I:%M %p} %F{yellow}${SSH_CONNECTION:+%m }%F{cyan}%20<...<%~%<< %F{yellow}%(1j.&%j .)'
+# Exit status
 # Ignore exit status `146` because it's sent every time a job is backgrounded
 PS1+='%F{red}%(146?..%(?..%?? ))'
 PS1+='%f%# '
