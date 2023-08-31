b9bb61bcb9bba581b2aca53095c4bd038a7a7dea
Roben Kleene <contact@robenkleene.com>
2023-08-31 14:34:40 +0000

    Archive scripts

--- scripts/adb_package_kill.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-for var in "$@"; do
-  adb shell am kill "$var"
-done
--- scripts/adb_package_list.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-exec adb shell cmd package list packages "$@" | cut -d: -f 2
