d2620df663b97879212067fbf31276dc010fd89d
Roben Kleene <contact@robenkleene.com>
2023-09-06 22:12:38 +0000

    Add vim open grep

--- scripts/g.sh
+++ scripts/g.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 if [[ ! -t 0 ]]; then
   fzf -d ':' -n 2.. --ansi --reverse --keep-right --multi \
     --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
-    --bind="ctrl-x:execute-silent(~/.bin/print_args {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
+    --bind="alt-v:become(echo {} | vim -c GrepBuffer -),ctrl-x:execute-silent(~/.bin/print_args {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
     --height=20 --preview-window=right,50%:wrap
 else
   exec rg --no-heading --with-filename --line-number --color=always "$@" | fzf -d ':' -n 2.. --ansi --reverse --keep-right --multi \
     --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
-    --bind="ctrl-x:execute-silent(~/.bin/print_args {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
+    --bind="alt-v:become(echo {} | vim -c GrepBuffer -),ctrl-x:execute-silent(~/.bin/print_args {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-page-up,ctrl-d:preview-page-down" \
     --height=20 --preview-window=right,50%:wrap
 fi
