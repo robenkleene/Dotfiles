51aeb82d9bad67987b72369e6833d61a961fc7a7
Roben Kleene <contact@robenkleene.com>
2023-09-09 19:35:19 +0000

    Remove control char

--- scripts/safecopy.sh
+++ scripts/safecopy.sh
@@ -6,15 +6,15 @@ if command -v pbcopy &> /dev/null; then
   if [ -n "${TMUX:-}" ] && [ -z "${INSIDE_EMACS:-}" ]; then
     # Always also copy to the tmux clipboard so that pasting inside tmux with
     # `paste-buffer` works
-    sed s'/⏎$//' | tee >(pbcopy) | tmux loadb -
+    sed s'/⏎$//' | sed '/^\^C$/d' | tee >(pbcopy) | tmux loadb -
   else
-    sed s'/⏎$//' | pbcopy
+    sed s'/⏎$//' | sed '/^\^C$/d' | pbcopy
   fi
 elif [ -n "${TMUX:-}" ] && [ -z "${INSIDE_EMACS:-}" ]; then
   # Never use `TMUX` `INSIDE_EMACS` because the `TMUX` session may no longer be
   # running Don't do anything fancy with trimming new lines, otherwise that
   # makes it hard to append multiple selections that contain trailing new lines
-  sed s'/⏎$//' | tmux loadb -
+  sed s'/⏎$//' | sed '/^\^C$/d' | tmux loadb -
 else
-  sed s'/⏎$//' > /tmp/robenkleene.transient/clipboard
+  sed s'/⏎$//' | sed '/^\^C$/d' > /tmp/robenkleene.transient/clipboard
 fi
