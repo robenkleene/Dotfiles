c10f91bf9813ed4a8798ec995e76a74807cba86b
Roben Kleene <contact@robenkleene.com>
2023-09-26 05:59:16 -0400

    Make completions automatically update

--- scripts/zsh_update.zsh
+++ scripts/zsh_update.zsh
@@ -8,6 +8,18 @@ zcompile .zshrc
 cd ~/.zsh
 for f in *.zsh; do zcompile "$f"; done
 
+# Completion
+
+if [[ ! -e ~/.zfunc ]]; then
+  mkdir ~/.zfunc
+fi
+
+# Rust
+if type rustup > /dev/null; then
+  rustup completions zsh cargo > ~/.zfunc/_cargo
+  rustup completions zsh > ~/.zfunc/_rustup
+fi
+
 # Re-generate completion cache
 autoload -Uz compinit
 compinit
--- zfunc/README.md
+++
@@ -1,4 +0,0 @@
-# Completion Functions
-
-- `rustup completions zsh cargo > ~/.zfunc/_cargo`
-- `rustup completions zsh > ~/.zfunc/_rustup`
