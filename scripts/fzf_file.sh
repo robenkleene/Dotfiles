7b18bc822ed397145a9d10b0bf1fc39a054a6705
Roben Kleene <contact@robenkleene.com>
2023-10-13 21:25:01 +0000

    Archive fzf

--- scripts/fzf_file.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-fzf --ansi --reverse --keep-right --multi --preview "$HOME/.bin/nobin/_preview_file.sh {}" \
-  --bind="ctrl-e:become(${EDITOR:-vim} {+}),ctrl-o:become(open {+}),alt-p:become(~/.bin/nobin/_preview_file.sh {+}),ctrl-x:execute-silent(echo {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,ctrl-a:toggle-all" \
-  --height=20 --preview-window=right,50%:wrap
--- scripts/fzf_grep.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-fzf -d ':' -n 2.. --ansi --reverse --keep-right --multi \
-  --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
-  --bind="alt-v:become(echo {+} | vim -c GrepBuffer -),alt-l:execute-silent(echo {+} | ~/.bin/f_a_txt_to_url_open)+accept,ctrl-x:execute-silent(~/.bin/print_args {+} | ~/.bin/safecopy)+accept,ctrl-u:preview-page-up,ctrl-d:preview-page-down,ctrl-a:toggle-all" \
-  --height=20 --preview-window=right,50%:wrap
