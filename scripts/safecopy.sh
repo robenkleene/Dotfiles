060107226b90486679b073a989289bcf071da337
Roben Kleene <contact@robenkleene.com>
2023-10-12 21:00:46 -0400

    Use tmux clipboard by default

--- scripts/safecopy.sh
+++ scripts/safecopy.sh
@@ -4,20 +4,16 @@ set -euo pipefail
 
 # Can't depend on `TMUX` running when `INSIDE_EMACS` is set because that
 # variable is recorded when the emacs server was started
-if command -v pbcopy &> /dev/null; then
-  if [[ -n "${INSIDE_EMACS:-}" ]]; then
-    { sed s'/⏎$//' | sed '/^\^C$/d' | tee >(pbcopy) | tmux loadb - ; } || sed s'/⏎$//' | sed '/^\^C$/d' > /tmp/robenkleene.transient/clipboard
-  elif [[ -n "${TMUX:-}" ]]; then
-    # Always also copy to the tmux clipboard so that pasting inside tmux with
-    # `paste-buffer` works
-    sed s'/⏎$//' | sed '/^\^C$/d' | tee >(pbcopy) | tmux loadb -
-  else
-    sed s'/⏎$//' | sed '/^\^C$/d' | pbcopy
-  fi
+if [[ -n "${TMUX:-}" ]]; then
+  sed s'/⏎$//' | sed '/^\^C$/d' | tmux loadb -
 elif [[ -n "${INSIDE_EMACS:-}" ]]; then
   { sed s'/⏎$//' | sed '/^\^C$/d' | tmux loadb - ; } || sed s'/⏎$//' | sed '/^\^C$/d' > /tmp/robenkleene.transient/clipboard
-elif [[ -n "${TMUX:-}" ]]; then
-  sed s'/⏎$//' | sed '/^\^C$/d' | tmux loadb -
+elif command -v pbcopy &> /dev/null; then
+  if [[ -n "${INSIDE_EMACS:-}" ]]; then
+    { sed s'/⏎$//' | sed '/^\^C$/d' | tee >(pbcopy) | tmux loadb - ; } || sed s'/⏎$//' | sed '/^\^C$/d' > /tmp/robenkleene.transient/clipboard
+ else
+    sed s'/⏎$//' | sed '/^\^C$/d' | pbcopy
+  fi
 else
   sed s'/⏎$//' | sed '/^\^C$/d' > /tmp/robenkleene.transient/clipboard
 fi
--- scripts/safepaste.sh
+++ scripts/safepaste.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-if command -v pbpaste &> /dev/null; then
-  pbpaste
-elif [ -n "${TMUX:-}" ] && [ -z "${INSIDE_EMACS:-}" ]; then
+if [[ -n "${TMUX:-}" ]] && [[ -z "${INSIDE_EMACS:-}" ]]; then
   TERM=xterm-256color tmux saveb -
+elif command -v pbpaste &> /dev/null; then
+  pbpaste
 else
   cat /tmp/robenkleene.transient/clipboard 2>/dev/null || echo ''
 fi
