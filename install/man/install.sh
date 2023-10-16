d76e25cd43e8f927ac53e75f0b05f8f889302d21
Roben Kleene <contact@robenkleene.com>
2023-10-16 05:51:37 -0400

    Fix install script

--- install/man/install.sh
+++ install/man/install.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 cd "$(dirname "$0")" || exit 1
 
 # Delete all existing
+destination_dir="$HOME/.man/man9"
 find -L "$destination_dir" -name "*.9" -type f -exec rm {} +
 
 while IFS= read -r; do
