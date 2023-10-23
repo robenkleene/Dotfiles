20565f6e52f366ada5e3c153e203543038ef468c
Roben Kleene <contact@robenkleene.com>
2023-10-23 05:27:37 -0400

    Fix emacs copy

--- scripts/safecopy.sh
+++ scripts/safecopy.sh
@@ -4,16 +4,16 @@ set -euo pipefail
 
 # Can't depend on `TMUX` running when `INSIDE_EMACS` is set because that
 # variable is recorded when the emacs server was started
-if [[ -n "${TMUX:-}" ]]; then
-  sed s'/⏎$//' | sed '/^\^C$/d' | tmux loadb -
-elif [[ -n "${INSIDE_EMACS:-}" ]]; then
-  { sed s'/⏎$//' | sed '/^\^C$/d' | tmux loadb - ; } || sed s'/⏎$//' | sed '/^\^C$/d' > /tmp/robenkleene.transient/clipboard
-elif command -v pbcopy &> /dev/null; then
-  if [[ -n "${INSIDE_EMACS:-}" ]]; then
+if [[ -n "${INSIDE_EMACS:-}" ]]; then
+  if command -v pbcopy &> /dev/null; then
     { sed s'/⏎$//' | sed '/^\^C$/d' | tee >(pbcopy) | tmux loadb - ; } || sed s'/⏎$//' | sed '/^\^C$/d' > /tmp/robenkleene.transient/clipboard
- else
-    sed s'/⏎$//' | sed '/^\^C$/d' | pbcopy
+  else
+    { sed s'/⏎$//' | sed '/^\^C$/d' | tmux loadb - ; } || sed s'/⏎$//' | sed '/^\^C$/d' > /tmp/robenkleene.transient/clipboard
   fi
+elif [[ -n "${TMUX:-}" ]]; then
+  sed s'/⏎$//' | sed '/^\^C$/d' | tmux loadb -
+elif command -v pbcopy &> /dev/null; then
+  sed s'/⏎$//' | sed '/^\^c$/d' | pbcopy
 else
   sed s'/⏎$//' | sed '/^\^C$/d' > /tmp/robenkleene.transient/clipboard
 fi
