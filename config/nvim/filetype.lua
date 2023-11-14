c2b75ed51f198425174a92f34f4d781eb650e72b
Roben Kleene <contact@robenkleene.com>
2023-11-14 23:47:51 +0000

    Use native content matching

--- config/nvim/filetype.lua
+++ config/nvim/filetype.lua
@@ -1,10 +1,18 @@
 vim.filetype.add({ extension = { omnijs = "javascript" } })
 vim.filetype.add({ filename = { eslintrc = "json" } })
 vim.filetype.add({ extension = { plist = "xml" } })
-vim.cmd([[
-augroup setup_filetype
-  " Interpret `hg show` output as `diff`
-  autocmd!
-  au! StdinReadPost * if eval('@%') == '' && getline(1) =~ '^changeset:' | setfiletype diff | endif
-augroup END
-]])
+
+vim.filetype.add {
+  pattern = {
+    ['.*'] = {
+      priority = -math.huge,
+      function(path, bufnr)
+        local content = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ''
+        if vim.regex([[^changeset:]]):match_str(content) ~= nil then
+          -- Interpret `hg show` output as `diff`
+          return 'diff'
+        end
+      end,
+    },
+  },
+}
