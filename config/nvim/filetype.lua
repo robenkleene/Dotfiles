14977c26589adb48953470b4b380a8e94205788c
Roben Kleene <contact@robenkleene.com>
2023-11-14 22:58:14 +0000

    Neovim filetype for hg show

--- config/nvim/filetype.lua
+++ config/nvim/filetype.lua
@@ -1,3 +1,10 @@
 vim.filetype.add({ extension = { omnijs = "javascript" } })
 vim.filetype.add({ filename = { eslintrc = "json" } })
 vim.filetype.add({ extension = { plist = "xml" } })
+vim.cmd([[
+augroup setup_filetype
+  " Interpret `hg show` output as `diff`
+  autocmd!
+  au! StdinReadPost * if eval('@%') == '' && getline(1) =~ '^changeset:' | setfiletype diff | endif
+augroup END
+]])
