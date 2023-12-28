6614f427dcf780ca17005e2a5bafa603de64fe51
Roben Kleene <contact@robenkleene.com>
2023-12-28 22:30:08 +0000

    Cleanup

--- emacs.d/robenkleene/functions/robenkleene-alias.el
+++ emacs.d/robenkleene/functions/robenkleene-alias.el
@@ -4,45 +4,16 @@
 
 (defalias 'ei 'edit-init)
 (defalias 'yg 'yank-to-grep-buffer)
-(defalias 'scw 'rk/source-control-open-web)
-(defalias 'scwp (lambda ()
-                  (interactive)
-                  (rk/source-control-open-web "-p")))
 (defalias 'u 'rk/egit-update)
 (defalias 'ut 'rk/git-pull-all-text)
 
 (defalias 'yd 'yank-to-diff-buffer)
-(defalias 'yl 'yank-to-virtual-dired-buffer)
 (defalias 'yg 'yank-to-grep-buffer)
-
-;; vc-mode
-(defalias 'vcs (lambda ()
-                 (interactive)
-                 (vc-dir-root)
-                 (forward-paragraph)
-                 ))
-(defalias 'vcd 'vc-root-diff)
-(defalias 'vcl 'vc-print-root-log)
-(defalias 'vccd 'vc-cd-vc-root)
-
-;; Vim
-;; (defalias 'bd 'kill-this-buffer)
-;; (defalias 'new 'rk/switch-to-empty-buffer-other-window)
-;; (defalias 'vnew 'rk/switch-to-empty-buffer-other-window-right)
-;; (defalias 'enew 'rk/switch-to-empty-buffer)
 
 (defun edit-init ()
   "Edit init."
   (interactive)
   (find-file "~/.emacs.d/robenkleene/robenkleene.el")
-  )
-
-(defun rk/source-control-open-web (&optional arg)
-  "Open the source control website for the current repository with ARG."
-  (interactive)
-  (shell-command (concat "~/.bin/source_control_open_site "
-                         arg)
-                 )
   )
 
 (provide 'robenkleene-alias)
