9405082933f8dacbce72664db5826ce0afe9831e
Roben Kleene <contact@robenkleene.com>
2024-01-03 15:47:50 +0000

    Remove edit init

--- emacs.d/robenkleene/functions/robenkleene-alias.el
+++ emacs.d/robenkleene/functions/robenkleene-alias.el
@@ -2,19 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
-(defalias 'ei 'edit-init)
 (defalias 'yg 'yank-to-grep-buffer)
 (defalias 'u 'rk/egit-update)
 (defalias 'ut 'rk/git-pull-all-text)
 
 (defalias 'yd 'yank-to-diff-buffer)
 (defalias 'yg 'yank-to-grep-buffer)
-
-(defun edit-init ()
-  "Edit init."
-  (interactive)
-  (find-file-other-window "~/.emacs.d/robenkleene/robenkleene.el")
-  )
 
 (provide 'robenkleene-alias)
 ;; Local Variables:
