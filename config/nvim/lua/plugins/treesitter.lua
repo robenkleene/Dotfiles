f742035348023e8054709b1e80a4b61ae1c85051
Roben Kleene <contact@robenkleene.com>
2024-01-16 21:10:00 +0000

    Add move text objects

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -88,6 +88,44 @@ return {
           -- and should return true of false
           include_surrounding_whitespace = true,
         },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["<localleader>]m"] = "@function.outer",
+            ["<localleader>]]"] = { query = "@class.outer", desc = "Next class start" },
+            --
+            -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+            ["<localleader>]o"] = "@loop.*",
+            -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+            --
+            -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+            -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+            ["<localleader>]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+            ["<localleader>]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+          },
+          goto_next_end = {
+            ["<localleader>]M"] = "@function.outer",
+            ["<localleader>]["] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["<localleader>[m"] = "@function.outer",
+            ["<localleader>[["] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["<localleader>[M"] = "@function.outer",
+            ["<localleader>[]"] = "@class.outer",
+          },
+          -- Below will go to either the start or the end, whichever is closer.
+          -- Use if you want more granular movements
+          -- Make it even more gradual by adding multiple queries and regex.
+          goto_next = {
+            ["<localleader>]d"] = "@conditional.outer",
+          },
+          goto_previous = {
+            ["<localleader>[d"] = "@conditional.outer",
+          }
+        },
       },
     }
   end
