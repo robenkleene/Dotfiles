6c8628e54dd73b472c1453ac6f36eb123e820bff
Roben Kleene <contact@robenkleene.com>
2024-01-17 18:26:19 +0000

    Archive scripts

--- scripts/git_root.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-cd "$(git rev-parse --show-toplevel)" || return
-if [[ -n "$1" ]]; then
-  exec "$@"
-fi
--- scripts/hg_root.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-cd "$(hg root)" || return
-if [[ -n "$1" ]]; then
-  exec "$@"
-fi
