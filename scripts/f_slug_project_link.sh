ba9389c72ed3c7b09799a30f4c35311f81355442
Roben Kleene <contact@robenkleene.com>
2024-01-19 17:30:16 +0000

    Finish script rename

--- scripts/f_slug_project_link.sh
+++ scripts/f_slug_project_link.sh
@@ -82,7 +82,7 @@ make_file() {
 
 contents="# $title
 "
-slug=$(echo "$title" | ~/.bin/f_txt_to_slug)
+slug=$(echo "$title" | ~/.bin/f_slug)
 today=$(date +%Y-%m-%d)
 dated_slug="$today-$slug"
 readme_path=$(make_file "README.md" "$dated_slug" "$contents")
--- scripts/p_txt_to_url_open.sh
+++ scripts/p_txt_to_url_open.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-~/.bin/f_txt_to_url | ~/.bin/nobin/_p_url_open.sh "$@"
+~/.bin/f_url | ~/.bin/nobin/_p_url_open.sh "$@"
