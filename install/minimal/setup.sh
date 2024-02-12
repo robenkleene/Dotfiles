d2cbc09b57f5c4ea4702a6e7ef9fdd76682275c5
Roben Kleene <contact@robenkleene.com>
2024-02-12 22:56:11 +0000

    Archive more files

--- install/minimal/setup.sh
+++ install/minimal/setup.sh
@@ -6,11 +6,15 @@ set -euo pipefail
 function cleanup_file() {
   filename="$1"
   if [[ -e "$filename" && ! -L "$filename" ]]; then
-    mkdir -p "$HOME/backup"
-    mv "$filename" "$HOME/backup/"
+    mkdir -p "$HOME/dotfiles_backup"
+    mv "$filename" "$HOME/dotfiles_backup/"
   fi
 }
 cleanup_file "$HOME/.bashrc"
 cleanup_file "$HOME/.zshrc"
+cleanup_file "$HOME/.zshenv"
 cleanup_file "$HOME/.config"
 cleanup_file "$HOME/.gitconfig"
+cleanup_file "$HOME/.gitignore"
+cleanup_file "$HOME/.inputrc"
+cleanup_file "$HOME/.vimrc"
