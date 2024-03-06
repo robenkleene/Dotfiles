bc542466aea84295d17a10017151fdc3f7921892
Roben Kleene <contact@robenkleene.com>
2024-03-06 15:17:23 +0000

    Clean up Emacs

--- emacs.d/robenkleene/config/robenkleene-bindings.el
+++ emacs.d/robenkleene/config/robenkleene-bindings.el
@@ -27,35 +27,13 @@
             'query-replace-regexp)
 (define-key rk/bindings-minor-mode-map (kbd "C-M-%") 'query-replace)
 
-;; History keys in isearch
-(define-key isearch-mode-map (kbd "<up>") 'isearch-ring-retreat)
-(define-key isearch-mode-map (kbd "<down>") 'isearch-ring-advance)
-(defun rk/isearch-delete ()
-  "Delete query."
-  (interactive)
-  (isearch-del-char most-positive-fixnum))
-(define-key isearch-mode-map (kbd "M-<backspace>") 'rk/isearch-delete)
-(define-key isearch-mode-map (kbd "M-DEL") 'rk/isearch-delete)
 
 ;; Window Management
-
 (define-key rk/bindings-minor-mode-map (kbd "C-x :")
             'switch-to-minibuffer)
-;; Automatically switch focus to new splits
-(define-key rk/bindings-minor-mode-map "\C-x2" (lambda ()
-                                                 (interactive)
-                                                 (split-window-vertically)
-                                                 (other-window 1)))
-(define-key rk/bindings-minor-mode-map "\C-x3"
-  (lambda ()
-    (interactive)
-    (split-window-horizontally)
-    (other-window 1)))
 
 ;; Improve default completion
 (global-set-key (kbd "M-/") 'hippie-expand)
-;; (global-set-key (kbd "C-x C-b") 'ibuffer-other-window)
-(global-set-key (kbd "M-<tab>") 'completion-at-point)
 
 ;; General
 (defvar dired-jump-other-window-repeat-map
@@ -64,11 +42,9 @@
     map))
 (dolist (cmd '(dired-jump-other-window))
   (put cmd 'repeat-map 'dired-jump-other-window-repeat-map))
-(define-key rk/bindings-minor-mode-map (kbd "C-x j")
-            'dired-jump)
+
 (define-key rk/bindings-minor-mode-map (kbd "C-x `") 'eshell-other-window)
-(define-key rk/bindings-minor-mode-map (kbd "M-r")
-            'reveal-in-finder)
+
 (define-key rk/bindings-minor-mode-map (kbd "M-g z")
             'z)
 (define-key rk/bindings-minor-mode-map (kbd "C-x 4 z")
@@ -77,12 +53,11 @@
             'z-other-frame)
 (define-key rk/bindings-minor-mode-map (kbd "C-x t z")
             'z-other-tab)
+
 ;; Quickly create org links
 (define-key rk/bindings-minor-mode-map (kbd "C-c l")
             'org-store-link)
-(define-key rk/bindings-minor-mode-map (kbd "C-c w") 'toggle-truncate-lines)
-;; Not available in terminal
-(define-key rk/bindings-minor-mode-map (kbd "C-c .") 'flyspell-auto-correct-word)
+
 (defvar previous-multiframe-window-repeat-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "O") #'previous-multiframe-window)
@@ -106,16 +81,6 @@
 (define-key rk/bindings-minor-mode-map (kbd "C-x u")
             'undo-only)
 
-;; (defvar paragraph-repeat-map
-;;   (let ((map (make-sparse-keymap)))
-;;     (define-key map (kbd "{") #'backward-paragraph)
-;;     (define-key map (kbd "}") #'forward-paragraph)
-;;     (define-key map (kbd "[") #'backward-paragraph)
-;;     (define-key map (kbd "]") #'forward-paragraph)
-;;     map))
-;; (dolist (cmd '(backward-paragraph forward-paragraph))
-;;   (put cmd 'repeat-map 'paragraph-repeat-map))
-
 ;; Mode
 (define-minor-mode rk/bindings-minor-mode
   "My bindings."
@@ -124,8 +89,6 @@
   :keymap 'rk/bindings-minor-mode-map)
 (rk/bindings-minor-mode 1)
 
-(add-hook 'after-load-functions 'rk/ensure-binding-priority)
-
 (defun rk/ensure-binding-priority (_file)
   "Ensure `bindings-minor-mode' always has priority."
   (unless (eq (caar minor-mode-map-alist) 'rk/bindings-minor-mode)
@@ -133,6 +96,7 @@
            (assq 'rk/bindings-minor-mode minor-mode-map-alist)))
       (assq-delete-all 'rk/bindings-minor-mode minor-mode-map-alist)
       (add-to-list 'minor-mode-map-alist new-keys))))
+(add-hook 'after-load-functions 'rk/ensure-binding-priority)
 
 (provide 'robenkleene-bindings)
 ;; Local Variables:
--- emacs.d/robenkleene/packages/robenkleene-consult.el
+++ emacs.d/robenkleene/packages/robenkleene-consult.el
@@ -4,19 +4,7 @@
 
 (use-package consult
   :bind (
-         ;; Use `conult-buffer' by default which features a preview
-         ("C-x b" . consult-buffer)
-         ("C-x 4 b" . consult-buffer-other-window)
-         ("C-x 5 b" . consult-buffer-other-frame)
-         ("C-x t b" . consult-buffer-other-tab)
-         ;; Just use default `C-x b' and jump to recent files
-         ;; ("M-g r" . consult-recent-file)
          ("M-s g" . consult-ripgrep)
-         ;; Use `consult-line' by default which fuzzy matches lines, and jumps
-         ;; to matches
-         ("M-s o" . consult-line)
-         ;; Use `consult-imenu' by default which automatically jumps to matches
-         ("M-g i" . consult-imenu)
          ("M-s f" . consult-fd)
          (:map dired-mode-map
                ("M-s f" . consult-fd)
@@ -24,48 +12,12 @@
          )
   :commands
   (
-   consult-buffer
-   consult-buffer-other-window
-   consult-buffer-other-frame
-   consult-buffer-other-tab
    consult-line
    consult-fd
-   consult-imenu
-   consult-compile-error
    consult-ripgrep
    consult-theme
-   consult-completion-in-region
    )
-
-  ;; Enable automatic preview at point in the *Completions* buffer. This is
-  ;; relevant when you use the default completion UI.
-  :hook (completion-list-mode . consult-preview-at-point-mode)
-
-  ;; The :init configuration is always executed (Not lazy)
   :init
-
-  ;; Allow two character search terms
-  (setq consult-async-min-input 2)
-
-  ;; Get initial results faster
-  ;; (setq consult-async-refresh-delay 0.01)
-  (setq consult-async-input-debounce 0.05)
-  ;; (setq consult-async-input-throttle 0.01)
-
-  ;; Optionally configure the register formatting. This improves the register
-  ;; preview for `consult-register', `consult-register-load',
-  ;; `consult-register-store' and the Emacs built-ins.
-  ;; (setq register-preview-delay 0.5
-  ;;       register-preview-function #'consult-register-format)
-
-  ;; Optionally tweak the register preview window.
-  ;; This adds thin lines, sorting and hides the mode line of the window.
-  (advice-add #'register-preview :override #'consult-register-window)
-
-  ;; Use Consult to select xref locations with preview
-  (setq xref-show-xrefs-function #'consult-xref
-        xref-show-definitions-function #'consult-xref)
-
   ;; Use `consult' for Emacs `ex' (command line) completions, and eshell
   ;; This causes eshell to complete `./update.sh' to `update.sh' which of course
   ;; fails
@@ -76,46 +28,6 @@
                      #'consult-completion-in-region
                    #'completion--in-region)
                  args)))
-
-  ;; Configure other variables and modes in the :config section,
-  ;; after lazily loading the package.
-  :config
-  ;; Optionally configure preview. The default value
-  ;; is 'any, such that any key triggers the preview.
-  ;; (setq consult-preview-key 'any)
-  ;; (setq consult-preview-key (kbd "M-."))
-  ;; (setq consult-preview-key (list (kbd "<S-down>") (kbd "<S-up>")))
-  ;; For some commands and buffer sources it is useful to configure the
-  ;; :preview-key on a per-command basis using the `consult-customize' macro.
-  (consult-customize
-   consult-theme :preview-key '(:debounce 0.2 any)
-   consult-ripgrep consult-git-grep consult-grep
-   consult-bookmark consult-recent-file consult-xref
-   consult--source-bookmark consult--source-file-register
-   consult--source-recent-file consult--source-project-recent-file
-   ;; :preview-key (kbd "M-.")
-   :preview-key '(:debounce 0.4 any))
-
-  ;; Optionally configure the narrowing key.
-  ;; Both < and C-+ work reasonably well.
-  (setq consult-narrow-key "<") ;; (kbd "C-+")
-
-  ;; Optionally make narrowing help available in the minibuffer.
-  ;; You may want to use `embark-prefix-help-command' or which-key instead.
-  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
-
-  ;; By default `consult-project-function' uses `project-root' from project.el.
-  ;; Optionally configure a different project root function.
-  ;; There are multiple reasonable alternatives to chose from.
-  ;;;; 1. project.el (the default)
-  ;; (setq consult-project-function #'consult--default-project--function)
-  ;;;; 2. projectile.el (projectile-project-root)
-  ;; (autoload 'projectile-project-root "projectile")
-  ;; (setq consult-project-function (lambda (_) (projectile-project-root)))
-  ;;;; 3. vc.el (vc-root-dir)
-  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
-  ;;;; 4. locate-dominating-file
-  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
   )
 
 (provide 'robenkleene-consult)
