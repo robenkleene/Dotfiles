54b6aec4564e1b57982277eaa512aca62ebf8492
Roben Kleene <contact@robenkleene.com>
2024-03-10 15:12:38 +0000

    Use consult

--- emacs.d/robenkleene/packages/robenkleene-consult.el
+++ emacs.d/robenkleene/packages/robenkleene-consult.el
@@ -4,6 +4,11 @@
 
 (use-package consult
   :bind (
+         ;; Use `consult-line' by default which fuzzy matches lines, and jumps
+         ;; to matches
+         ("M-s o" . consult-line)
+         ;; Use `consult-imenu' by default which automatically jumps to matches
+         ("M-g i" . consult-imenu)
          ("M-s g" . consult-ripgrep)
          ("M-s f" . consult-fd)
          (:map dired-mode-map
