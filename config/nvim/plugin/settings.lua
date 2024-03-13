b748186319968375822c3e094f98817dfb3d6ec4
Roben Kleene <contact@robenkleene.com>
2024-03-13 20:58:15 +0000

    Add back yank highlight

--- config/nvim/plugin/settings.lua
+++ config/nvim/plugin/settings.lua
@@ -1,20 +1,12 @@
 -- Good to be accustomed to not having this, because it makes it easier when
 -- using implementations of Vim mode that doesn't support this (e.g., in Visual
 -- Studio Code)
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
--- local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
--- vim.api.nvim_create_autocmd('TextYankPost', {
---   callback = function()
---     vim.highlight.on_yank({ timeout = 60, on_visual = false })
---   end,
---   group = highlight_group,
---   pattern = '*',
--- })
-
--- This causes line breaks to be misshandled when copying lines (e.g.,
--- `yy`)
--- vim.o.clipboard = vim.o.clipboard .. "unnamedplus"
+-- Highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank({ higroup="Visual", timeout = 120, on_visual = false })
+  end
+})
 
 -- Always show the `signcolumn`, this prevents an issue where the text moves
 -- because indicators are flapping
