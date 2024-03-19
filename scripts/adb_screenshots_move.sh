e6b8660b46e29e4d0eb5c5655154c021a5ce041f
Roben Kleene <contact@robenkleene.com>
2024-03-19 13:21:17 +0000

    Update adb screenshots

---
+++ scripts/adb_screenshots_move.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+~/.bin/adb_screenshots_sync
+~/.bin/adb_screenshots_delete
---
+++ scripts/adb_screenshots_sync.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+adb pull /sdcard/Oculus/Screenshots/
+adb pull /sdcard/Oculus/VideoShots/
