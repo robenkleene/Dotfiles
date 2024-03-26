6274aecba86c7359105162a1b0c07b00829ecdf9
Roben Kleene <contact@robenkleene.com>
2024-03-26 08:03:18 -0400

    Support title

---
+++ scripts/f_from_filename_to_title.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+while read -r filename; do
+  filename_no_ext=${filename%.*}
+  if [[ $filename_no_ext =~ ^[a-z0-9_-]*$ ]]; then
+    # If the filename contains only lowercase letters, hyphens, and underscores then assume
+    # convert it to title case with spaces.
+    title=$(echo "$filename_no_ext" | tr "-" " " | tr "_" " " | ~/.bin/f_titlecase)
+  else
+    title=$(echo "$filename_no_ext" | ~/.bin/f_titlecase)
+  fi
+  echo "$title"
+one
+
--- scripts/md_title.sh
+++ scripts/md_title.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
 filename=$(basename "$1")
-filename_no_ext=${filename%.*}
-if [[ $filename_no_ext =~ ^[a-z0-9_-]*$ ]]; then
-  # If the filename contains only lowercase letters, hyphens, and underscores then assume
-  # convert it to title case with spaces.
-  title=$(echo "$filename_no_ext" | tr "-" " " | tr "_" " " | ~/.bin/f_titlecase)
-else
-  title=$(echo "$filename_no_ext" | ~/.bin/f_titlecase)
-fi
-
+title=$(echo "$filename" | ~/.bin/f_from_filename_to_title | tr -d '\n')
 echo "# $title"
 echo
