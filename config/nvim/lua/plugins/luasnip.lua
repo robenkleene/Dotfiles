a02853f039c6bde5ddf58120622c73e85f0b893e
Roben Kleene <contact@robenkleene.com>
2024-04-08 08:26:25 -0400

    Add snippets

---
+++ config/nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,19 @@
+return {
+  'L3MON4D3/LuaSnip',
+  -- This can't co-exist with GitHub Copilot
+  -- keys = {
+  --   { '<Tab>', mode = "i" },
+  -- },
+  event = "VeryLazy",
+  config = function()
+    require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/Code/User/snippets" } })
+    vim.api.nvim_set_keymap(
+      'i', '<Tab>',
+      'luasnip#expand_or_jumpable() ? "<Plug>luasnip-expand-or-jump" : "<Tab>"',
+      { expr = true, silent = true }
+    )
+    local luasnip = require 'luasnip'
+    luasnip.filetype_extend("zsh", { "sh" })
+    luasnip.filetype_extend("typescript", { "javascript" })
+  end
+}
