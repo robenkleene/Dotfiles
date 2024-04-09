130252e00c9f708ac18421219cfa5db57d9f7928
Roben Kleene <contact@robenkleene.com>
2024-04-08 21:24:26 -0400

    Disable overlays

--- emacs.d/robenkleene/packages/robenkleene-smartparens.el
+++ emacs.d/robenkleene/packages/robenkleene-smartparens.el
@@ -12,6 +12,10 @@
   ;; Make smartparens work better with Emacs lisp, e.g., code in comments with
   ;; `' and single-quoting variables
   (require 'smartparens-config)
+  ;; Disable the overlays that show scope while typing
+  (setq sp-highlight-pair-overlay nil)
+  (setq sp-highlight-wrap-overlay nil)
+  (setq sp-highlight-wrap-tag-overlay nil)
   )
 
 (provide 'robenkleene-smartparens)
