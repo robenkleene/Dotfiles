8cf2fb0c727c49ce0c085e9d999108f6d4b06633
Roben Kleene <contact@robenkleene.com>
2024-04-10 20:12:13 +0000

    Use strict mode

--- emacs.d/robenkleene/packages/robenkleene-smartparens.el
+++ emacs.d/robenkleene/packages/robenkleene-smartparens.el
@@ -7,7 +7,7 @@
   :commands (smartparens-strict-mode smartparens-mode)
   :init
   (add-hook 'emacs-lisp-mode-hook #'smartparens-strict-mode)
-  (add-hook 'eval-expression-minibuffer-setup-hook #'smartparens-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'smartparens-strict-mode)
   :config
   ;; Make smartparens work better with Emacs lisp, e.g., code in comments with
   ;; `' and single-quoting variables
