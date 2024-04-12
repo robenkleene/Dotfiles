98b1c9e4347a11f95b743a1bf639a8d084edd825
Roben Kleene <contact@robenkleene.com>
2024-04-12 01:44:58 +0000

    Add local snippets

--- config/nvim/lua/plugins/luasnip.lua
+++ config/nvim/lua/plugins/luasnip.lua
@@ -7,6 +7,10 @@ return {
   event = "VeryLazy",
   config = function()
     require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/Code/User/snippets" } })
+    local f=io.open(vim.env.HOME .. '/.local_snippets/package.json')
+    if f~=nil then io.close(f)
+      require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.local_snippets/" } })
+    end
     vim.api.nvim_set_keymap(
       'i', '<Tab>',
       'luasnip#expand_or_jumpable() ? "<Plug>luasnip-expand-or-jump" : "<Tab>"',
