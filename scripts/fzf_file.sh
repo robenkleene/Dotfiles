65213de6164fba706bacd2040f69ed8bdcfb2093
Roben Kleene <contact@robenkleene.com>
2024-04-16 19:45:38 -0400

    Add editor support

--- scripts/fzf_file.sh
+++ scripts/fzf_file.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 fzf --ansi --reverse --keep-right --multi --preview "bat {}" \
-  --bind="ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,ctrl-a:toggle-all" \
+  --bind="ctrl-e:execute(${EDITOR:-vim} {+}),ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,ctrl-a:toggle-all" \
   --height=20 --preview-window=right,50%:wrap
