765c9c317194b0f0df0e7e2d57de28bf4698c81b
Roben Kleene <contact@robenkleene.com>
2024-04-17 10:18:10 -0400

    Use full parameter names

--- scripts/fzf_diff.sh
+++ scripts/fzf_diff.sh
@@ -17,7 +17,7 @@ trap cleanup EXIT SIGINT SIGTERM
 cat > "$tempfile"
 
 # `/dev/null` makes grep think it's dealing with multiple files, which forces it to print the filename
-grep --line-number --extended-regexp '^\+\+\+ b\/*' "$tempfile" /dev/null | fzf -d ':' -n 2.. --ansi --reverse --keep-right --multi \
+grep --line-number --extended-regexp '^\+\+\+ b\/*' "$tempfile" /dev/null | fzf --delimiter=':' -nth=2.. --ansi --reverse --keep-right --multi \
   --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
   --bind="ctrl-e:execute(${EDITOR:-vim} +{2} {1}),ctrl-u:preview-page-up,ctrl-d:preview-page-down,ctrl-a:toggle-all" \
   --height=20 --preview-window=right,50%:wrap
