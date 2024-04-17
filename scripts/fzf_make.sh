4975bb56afff2f2d745eeb67ecf2f060ba590dae
Roben Kleene <contact@robenkleene.com>
2024-04-17 18:30:23 +0000

    Add fzf make

---
+++ scripts/fzf_make.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+set -euo pipefail
+
+tempfile=$(mktemp)
+tempfile_ext="${tempfile}.diff"
+if [[ ! -f "$tempfile_ext" ]]; then
+  tempfile="$tempfile_ext"
+fi
+
+cleanup() {
+  rm -f "$tempfile"
+}
+
+trap cleanup EXIT SIGINT SIGTERM
+
+cat > "$tempfile"
+
+# `/dev/null` makes grep think it's dealing with multiple files, which forces it to print the filename
+grep --line-number --extended-regexp "([^: ]+):([0-9]+):?([0-9]+:)?" "$tempfile" /dev/null | fzf --delimiter=':' --with-nth=3.. --ansi --reverse --keep-right --multi \
+  --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
+  --bind="ctrl-e:become(${EDITOR:-vim} +{4} {3}),ctrl-u:preview-page-up,ctrl-d:preview-page-down,ctrl-a:toggle-all" \
+  --height=20 --preview-window=right,50%:wrap
