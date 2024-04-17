87cc0fd4b67d1e921dfbb666b31cb68f373bee6e
Roben Kleene <contact@robenkleene.com>
2024-04-17 09:44:36 -0400

    Work on diff

--- scripts/fzf_diff.sh
+++ scripts/fzf_diff.sh
@@ -2,22 +2,9 @@
 
 set -euo pipefail
 
-typeset -A file_to_diff
-
-while read line; do
-    if [[ $line == +++\ b\/* ]]; then
-        file_path=${line:6}
-        file_to_diff[$file_path]=""
-    else
-        if [[ -n ${file_path:-} ]]; then
-            if [[ -n ${file_to_diff[$file_path]} ]]; then
-                file_to_diff[$file_path]+=$'\n'
-            fi
-            file_to_diff[$file_path]+=$line
-        fi
-    fi
-done < "${1:-/dev/stdin}"
-
+tmp_file=$(mktemp)
+tmp_file_ext="${tmp_file}.diff"
+diff_file="$tmp_file_ext"
 echo "${(@k)file_to_diff}" | fzf --ansi --reverse --keep-right --multi --preview "bat --style=plain --color=always {}" \
   --bind="ctrl-e:execute(${EDITOR:-vim} {+}),ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,ctrl-a:toggle-all" \
   --height=20 --preview-window=right,50%:wrap
