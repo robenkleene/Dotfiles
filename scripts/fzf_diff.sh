985ec9529ff6a4148326f95c792fa9214bcd0c25
Roben Kleene <contact@robenkleene.com>
2024-04-17 11:45:23 -0400

    Start supporting hunks

--- scripts/fzf_diff.sh
+++ scripts/fzf_diff.sh
@@ -17,7 +17,7 @@ trap cleanup EXIT SIGINT SIGTERM
 cat > "$tempfile"
 
 # `/dev/null` makes grep think it's dealing with multiple files, which forces it to print the filename
-grep --line-number --extended-regexp '^\+\+\+ b\/*' "$tempfile" /dev/null | fzf --delimiter=':' --with-nth=3.. --ansi --reverse --keep-right --multi \
+grep --line-number --extended-regexp '^(\+\+\+ b\/|@@)' "$tempfile" /dev/null | fzf --delimiter=':' --with-nth=3.. --ansi --reverse --keep-right --multi \
   --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
   --bind="ctrl-e:execute(f_from_diff_to_args {2} < {1} | xargs ${EDITOR:-vim}),ctrl-u:preview-page-up,ctrl-d:preview-page-down,ctrl-a:toggle-all" \
   --height=20 --preview-window=right,50%:wrap
