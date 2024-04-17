8a06037bfa74de71dd4bc592fac6c84733373ad5
Roben Kleene <contact@robenkleene.com>
2024-04-16 22:03:01 -0400

    Work on fzf diff

--- scripts/fzf_diff.sh
+++ scripts/fzf_diff.sh
@@ -1,32 +1,22 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 set -euo pipefail
-
-#!/bin/zsh
 
 typeset -A file_to_diff
 
 while read line; do
-    if [[ $line == +++* ]]; then
-        file_path=${line:3}
-done < "${source_file:-/dev/stdin}"
-
-# Iterate over lines to populate the associative array
-for line in $lines; do
     if [[ $line == +++\ b\/* ]]; then
-        file_path=${line:5}
+        file_path=${line:6}
         file_to_diff[$file_path]=""
     else
-        # Append the line to the current key's value in the array
-        if [[ -n $file_path ]]; then
-            # Append the line with a newline unless it's the first line
+        if [[ -n ${file_path:-} ]]; then
             if [[ -n ${file_to_diff[$file_path]} ]]; then
                 file_to_diff[$file_path]+=$'\n'
             fi
             file_to_diff[$file_path]+=$line
         fi
     fi
-done
+done < "${1:-/dev/stdin}"
 
 # Print the associative array's contents
 for key in "${(@k)file_to_diff}"; do
