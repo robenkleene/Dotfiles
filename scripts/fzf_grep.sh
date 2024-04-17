b42bdf21f79498f20831b7e0478b69f72b12d257
Roben Kleene <contact@robenkleene.com>
2024-04-16 22:05:40 -0400

    Use exec

--- scripts/fzf_grep.sh
+++ scripts/fzf_grep.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-fzf -d ':' -n 2.. --ansi --reverse --keep-right --multi \
+exec fzf -d ':' -n 2.. --ansi --reverse --keep-right --multi \
   --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
   --bind="ctrl-e:execute(${EDITOR:-vim} +{2} {1}),ctrl-u:preview-page-up,ctrl-d:preview-page-down,ctrl-a:toggle-all" \
   --height=20 --preview-window=right,50%:wrap
