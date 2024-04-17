43aa5e2ddd5f2bcdc46621db3bae096459ed59c6
Roben Kleene <contact@robenkleene.com>
2024-04-16 21:42:04 -0400

    Work on fzf diff

--- scripts/fzf_diff.sh
+++ scripts/fzf_diff.sh
@@ -4,40 +4,33 @@ set -euo pipefail
 
 #!/bin/zsh
 
-# Declare an associative array
-typeset -A contentMap
+typeset -A file_to_diff
 
-# Read from STDIN into a variable
-input=$(< /dev/stdin)
-
-# Variable to keep track of the current key
-currentKey=""
-
-# Split input into lines
-IFS=$'\n' read -r -d '' -A lines <<< "$input"
+while read line; do
+    if [[ $line == +++* ]]; then
+        file_path=${line:3}
+done < "${source_file:-/dev/stdin}"
 
 # Iterate over lines to populate the associative array
 for line in $lines; do
-    if [[ $line == +++* ]]; then
-        # Extract the key, stripping off the leading '+++'
-        currentKey=${line:3}
-        # Initialize the current key in the associative array
-        contentMap[$currentKey]=""
+    if [[ $line == +++\ b\/* ]]; then
+        file_path=${line:5}
+        file_to_diff[$file_path]=""
     else
         # Append the line to the current key's value in the array
-        if [[ -n $currentKey ]]; then
+        if [[ -n $file_path ]]; then
             # Append the line with a newline unless it's the first line
-            if [[ -n ${contentMap[$currentKey]} ]]; then
-                contentMap[$currentKey]+=$'\n'
+            if [[ -n ${file_to_diff[$file_path]} ]]; then
+                file_to_diff[$file_path]+=$'\n'
             fi
-            contentMap[$currentKey]+=$line
+            file_to_diff[$file_path]+=$line
         fi
     fi
 done
 
 # Print the associative array's contents
-for key in "${(@k)contentMap}"; do
+for key in "${(@k)file_to_diff}"; do
     echo "Key: $key"
-    echo "Value: ${contentMap[$key]}"
+    echo "Value: ${file_to_diff[$key]}"
     echo "-------------"
 done
