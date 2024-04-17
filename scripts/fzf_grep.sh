37f83795b1af52d7d374bb72a6472eceed57c8c3
Roben Kleene <contact@robenkleene.com>
2024-04-17 10:19:50 -0400

    Fix flag

--- scripts/fzf_grep.sh
+++ scripts/fzf_grep.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-exec fzf --delimiter=':' -nth=2.. --ansi --reverse --keep-right --multi \
+exec fzf --delimiter=':' --nth=2.. --ansi --reverse --keep-right --multi \
   --preview 'bat --style=header --color=always --line-range {2}: --highlight-line {2} {1}' \
   --bind="ctrl-e:execute(${EDITOR:-vim} +{2} {1}),ctrl-u:preview-page-up,ctrl-d:preview-page-down,ctrl-a:toggle-all" \
   --height=20 --preview-window=right,50%:wrap
