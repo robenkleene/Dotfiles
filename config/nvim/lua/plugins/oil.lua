537094588fa932a14bd426d2c7a87649299b50b7
Roben Kleene <contact@robenkleene.com>
2024-04-23 18:44:30 +0000

    Remove line numbers

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,11 @@
 return {
   'stevearc/oil.nvim',
   config = function()
-    require("oil").setup()
+    require("oil").setup({
+      win_options = {
+        number = false,
+      }
+    })
     vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
   end
 }
