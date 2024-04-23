371c8cfca94272a79918016c9c1601756650d0b4
Roben Kleene <contact@robenkleene.com>
2024-04-23 07:01:33 -0400

    Update gems

--- install/ruby/gems/update.sh
+++ install/ruby/gems/update.sh
@@ -2,27 +2,4 @@
 
 set -euo pipefail
 
-force=""
-while getopts ":fh" option; do
-  case "$option" in
-    f)
-      force="-f"
-      ;;
-    h)
-      echo "Usage: command [-hf]"
-      exit 0
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument" >&2
-      exit 1
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-  esac
-done
-
-cd "$(dirname "$0")" || exit 1
-./cleanup.sh "$force"
-./install.sh "$force"
+bundle update
