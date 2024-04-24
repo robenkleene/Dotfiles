4147baf8d8aa63fcfa2938de03f424a53a33db1c
Roben Kleene <contact@robenkleene.com>
2024-04-24 20:34:36 +0000

    Fix xargs

--- scripts/fzf_diff.sh
+++ scripts/fzf_diff.sh
@@ -19,10 +19,10 @@ cat > "$tempfile"
 # `/dev/null` makes grep think it's dealing with multiple files, which forces it to print the filename
 result=$(grep --line-number --extended-regexp '^\+\+\+ b\/' "$tempfile" /dev/null | fzf --delimiter=':' --with-nth=3.. --ansi --reverse --keep-right --multi \
   --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
-  --bind="alt-e:execute(printf \"%s\n\" {+} | cut -d':' -f 2 | xargs bash -c 'f_from_diff_to_file \"\$@\" < "$tempfile"' _ | uniq | tr '\n' '\0' | xargs -0 ${EDITOR:-vim}),alt-g:execute(f_from_diff_to_arg {2} < {1} | xargs ${EDITOR:-vim}),ctrl-v:preview-page-up,alt-v:preview-page-down,alt-a:toggle-all" \
+  --bind="alt-e:execute(printf \"%s\n\" {+} | cut -d':' -f 2 | xargs bash -c 'f_from_diff_to_file \"\$@\" < "$tempfile"' _ | uniq | tr '\n' '\0' | xargs -0 -o ${EDITOR:-vim}),alt-g:execute(f_from_diff_to_arg {2} < {1} | xargs -o ${EDITOR:-vim}),ctrl-v:preview-page-up,alt-v:preview-page-down,alt-a:toggle-all" \
   --height=20 --preview-window=right,50%:wrap | cut -d':' -f 2)
 
 awk -v start_line=$result '{ if (NR >= start_line) { if (/^---/) exit; if (/^@@/) printf "%s:%d:%s\n", FILENAME, NR, $0 } }' "$tempfile" | fzf --delimiter=':' --with-nth=3.. --ansi --reverse --keep-right --multi \
   --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
-  --bind="alt-e:execute(printf \"%s\n\" {+} | cut -d':' -f 2 | xargs bash -c 'f_from_diff_to_file \"\$@\" < "$tempfile"' _ | uniq | tr '\n' '\0' | xargs -0 ${EDITOR:-vim}),alt-g:execute(f_from_diff_to_arg {2} < {1} | xargs ${EDITOR:-vim}),ctrl-v:preview-page-up,alt-v:preview-page-down,alt-a:toggle-all" \
+  --bind="alt-e:execute(printf \"%s\n\" {+} | cut -d':' -f 2 | xargs bash -c 'f_from_diff_to_file \"\$@\" < "$tempfile"' _ | uniq | tr '\n' '\0' | xargs -0 -o ${EDITOR:-vim}),alt-g:execute(f_from_diff_to_arg {2} < {1} | xargs -o ${EDITOR:-vim}),ctrl-v:preview-page-up,alt-v:preview-page-down,alt-a:toggle-all" \
   --height=20 --preview-window=right,50%:wrap
--- scripts/fzf_grep.sh
+++ scripts/fzf_grep.sh
@@ -4,6 +4,5 @@ set -euo pipefail
 
 exec fzf --delimiter=':' --nth=2.. --ansi --reverse --keep-right --multi \
   --preview 'bat --style=header --color=always --line-range {2}: --highlight-line {2} {1}' \
-  --bind="alt-e:become(printf \"%s\n\" {+} | cut -d':' -f 1 | tr '\n' '\0' | xargs -0 ${EDITOR:-vim}),alt-g:execute(${EDITOR:-vim} +{2} {1}),ctrl-v:preview-page-up,alt-v:preview-page-down,alt-a:toggle-all" \
+  --bind="alt-e:become(printf \"%s\n\" {+} | cut -d':' -f 1 | tr '\n' '\0' | xargs -0 -o ${EDITOR:-vim}),alt-g:execute(${EDITOR:-vim} +{2} {1}),ctrl-v:preview-page-up,alt-v:preview-page-down,alt-a:toggle-all" \
   --height=20 --preview-window=right,50%:wrap
-  # --bind="alt-e:execute(echo {+} | cut -d':' -f 1 | tr '\n' '\0' | xargs -0 ${EDITOR:-vim}),alt-g:execute(${EDITOR:-vim} +{2} {1}),ctrl-v:preview-page-up,alt-v:preview-page-down,alt-a:toggle-all" \
