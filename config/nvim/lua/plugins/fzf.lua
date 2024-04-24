02ae35198fe51da41614fd077cd186c2f8e6d465
Roben Kleene <contact@robenkleene.com>
2024-04-24 19:50:09 -0400

    Change some commands

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -5,8 +5,6 @@ return {
   },
   keys = {
     { '<leader>b' },
-    { '<leader>f' },
-    { '<leader>g' },
     { '<leader>l' },
     { '<leader>o' },
     { '<leader>t' },
@@ -16,8 +14,6 @@ return {
     set rtp+=$HOMEBREW_DIR/opt/fzf
 
     nnoremap <silent> <leader>b :Buffers<CR>
-    nnoremap <silent> <leader>f :Files<CR>
-    nnoremap <silent> <leader>g :RG<CR>
     nnoremap <silent> <leader>l :BLines<CR>
     nnoremap <silent> <leader>o :FZFHistory<CR>
     nnoremap <silent> <leader>t :BTags<CR>
