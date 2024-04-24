4e10e7a47628b2c9dad12adf52d0e2fc097459f1
Roben Kleene <contact@robenkleene.com>
2024-04-24 19:25:15 +0000

    New snippet setup

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -6,6 +6,18 @@ return {
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-vsnip',
+    {
+      'hrsh7th/vim-vsnip',
+      event = 'VeryLazy',
+      config = function()
+        vim.cmd([[
+        let g:vsnip_snippet_dir = '~/.config/Code/User/snippets'
+        let g:vsnip_filetypes = {}
+        let g:vsnip_filetypes.zsh = ['sh']
+        let g:vsnip_filetypes.typescript = ['typescript']
+        ]])
+      end
+    }
   },
   -- Set the same for cmp, treesitter, lsp
   ft = {
@@ -31,6 +43,14 @@ return {
   },
   config = function()
     local cmp = require 'cmp'
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+    local feedkey = function(key, mode)
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+    end
     cmp.setup {
       snippet = {
         expand = function(args)
@@ -56,6 +76,25 @@ return {
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
         ['<C-u>'] = cmp.mapping.scroll_docs(-4),
         ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif vim.fn["vsnip#available"](1) == 1 then
+            feedkey("<Plug>(vsnip-expand-or-jump)", "")
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function()
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+            feedkey("<Plug>(vsnip-jump-prev)", "")
+          end
+        end, { "i", "s" }),
       },
       sources = {
         { name = 'nvim_lsp' },
@@ -74,11 +113,12 @@ return {
         }
       },
     }
-    vim.cmd([[
-    augroup NvimCmp
-      au!
-      au FileType markdown lua require('cmp').setup.buffer { enabled = false }
-    augroup END
-    ]])
+    -- Only way to enter snippets now is via selecting them from completion
+    -- vim.cmd([[
+    -- augroup NvimCmp
+    --   au!
+    --   au FileType markdown lua require('cmp').setup.buffer { enabled = false }
+    -- augroup END
+    -- ]])
   end
 }
--- vim/plugin/bindings.vim
+++ vim/plugin/bindings.vim
@@ -3,11 +3,11 @@ nnoremap Y y$
 " Add `zv` to quickfix bindings to show the cursor line, because these
 " bindings don't respect the `foldopen` option
 " (This solution is from `unimpaired`)
-nnoremap <expr> <CR> len(getqflist()) ? ":cnext<CR>zv" : len(argv()) > 1 ? ":next<CR>" : ":bnext<CR>"
+" nnoremap <expr> <CR> len(getqflist()) ? ":cnext<CR>zv" : len(argv()) > 1 ? ":next<CR>" : ":bnext<CR>"
 " `<S-CR>` can't be bound on Apple Terminal
-nnoremap <expr> <BS> len(getqflist()) ? ":cprev<CR>zv" : len(argv()) > 1 ? ":prev<CR>" : ":bprev<CR>"
-augroup fix_return_bindings
-  autocmd CmdwinEnter * nnoremap <buffer> <CR> <CR>
-  autocmd BufReadPost quickfix nnoremap <buffer> <CR> <CR>
-augroup END
+" nnoremap <expr> <BS> len(getqflist()) ? ":cprev<CR>zv" : len(argv()) > 1 ? ":prev<CR>" : ":bprev<CR>"
+" augroup fix_return_bindings
+"   autocmd CmdwinEnter * nnoremap <buffer> <CR> <CR>
+"   autocmd BufReadPost quickfix nnoremap <buffer> <CR> <CR>
+" augroup END
 
