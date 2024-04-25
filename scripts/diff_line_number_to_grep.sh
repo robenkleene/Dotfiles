eadd9c9a6b09251f17a27e4c46b1353a2500f09e
Roben Kleene <contact@robenkleene.com>
2024-04-24 22:59:18 -0400

    Tweak fzf diff

---
+++ scripts/diff_line_number_to_grep.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# `awk '{print $0}'` force a new line
+awk '{print $0}' | while IFS= read -r line; do
+  ~/.bin/f_from_diff_to_grep $line < "$1" | sed 's/$/: /'
+done
--- scripts/fzf_diff.sh
+++ scripts/fzf_diff.sh
@@ -17,12 +17,22 @@ trap cleanup EXIT SIGINT SIGTERM
 cat > "$tempfile"
 
 # `/dev/null` makes grep think it's dealing with multiple files, which forces it to print the filename
-result=$(grep --line-number --extended-regexp '^\+\+\+ b/' "$tempfile" /dev/null | fzf --delimiter=':' --with-nth=3.. --ansi --reverse --keep-right --multi \
+result=$(grep --line-number --extended-regexp '^\+\+\+ b/' "$tempfile" /dev/null | \
+  fzf --delimiter=':' --with-nth=3.. --ansi --reverse --keep-right \
   --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
   --bind="alt-e:execute(printf \"%s\n\" {+} | cut -d':' -f 2 | xargs bash -c 'f_from_diff_to_file \"\$@\" < "$tempfile"' _ | uniq | tr '\n' '\0' | xargs -0 -o ${EDITOR:-vim}),alt-g:execute(f_from_diff_to_arg {2} < {1} | xargs -o ${EDITOR:-vim}),ctrl-v:preview-page-up,alt-v:preview-page-down,alt-a:toggle-all" \
   --height=20 --preview-window=right,50%:wrap | cut -d':' -f 2)
 
-awk -v start_line=$result '{ if (NR >= start_line) { if (/^---/) exit; if (/^@@/) printf "%s:%d:%s\n", FILENAME, NR, $0 } }' "$tempfile" | fzf --delimiter=':' --with-nth=3.. --ansi --reverse --keep-right --multi \
-  --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
-  --bind="enter:become(f_from_diff_to_arg {2} < {1} | xargs -o ${EDITOR:-vim})+accept,alt-e:execute(printf \"%s\n\" {+} | cut -d':' -f 2 | xargs bash -c 'f_from_diff_to_file \"\$@\" < "$tempfile"' _ | uniq | tr '\n' '\0' | xargs -0 -o ${EDITOR:-vim}),alt-g:execute(f_from_diff_to_arg {2} < {1} | xargs -o ${EDITOR:-vim}),ctrl-v:preview-page-up,alt-v:preview-page-down,alt-a:toggle-all" \
-  --height=20 --preview-window=right,50%:wrap
+if [[ -z "$EDITOR" || "$EDITOR" = "nvim" ]]; then
+  awk -v start_line=$result '{ if (NR >= start_line) { if (/^---/) exit; if (/^@@/) printf "%s:%d:%s\n", FILENAME, NR, $0 } }' "$tempfile" | \
+    fzf --delimiter=':' --with-nth=3.. --ansi --reverse --keep-right --multi \
+    --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
+    --bind="enter:become(echo {+} | cut -d':' -f 2 | diff_line_number_to_grep {1} | ${EDITOR:-vim} -c 'cbuffer | bprevious | bdelete' -),shift-up:preview-up,shift-down:preview-down,alt-a:select-all,alt-d:deselect-all" \
+    --height=20 --preview-window=right,50%:wrap
+else
+  awk -v start_line=$result '{ if (NR >= start_line) { if (/^---/) exit; if (/^@@/) printf "%s:%d:%s\n", FILENAME, NR, $0 } }' "$tempfile" | \
+    fzf --delimiter=':' --with-nth=3.. --ansi --reverse --keep-right \
+    --preview 'bat --style=plain --color=always --line-range {2}: --highlight-line {2} {1}' \
+    --bind="enter:become(f_from_diff_to_arg {2} < {1} | xargs -o ${EDITOR:-vim}),shift-up:preview-up,shift-down:preview-down" \
+    --height=20 --preview-window=right,50%:wrap
+fi
