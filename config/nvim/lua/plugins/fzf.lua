f78ba503184923bd379ff1447aad4cf593817317
Roben Kleene <contact@robenkleene.com>
2024-04-24 21:36:10 -0400

    Rename commands

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -18,13 +18,13 @@ return {
 
     nnoremap <silent> <leader>b :Buffers<CR>
     nnoremap <silent> <leader>l :BLines<CR>
-    nnoremap <silent> <leader>o :FZFHistory<CR>
+    nnoremap <silent> <leader>o :FzfOldfiles<CR>
     nnoremap <silent> <leader>t :BTags<CR>
 
     " Don't sort old files
-    command! -bang -nargs=* FZFHistory call fzf#vim#history(fzf#vim#with_preview({'options': '--no-sort'}))
-    " Rebind `Rg` to not shell escape so flags can be used
-    command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case " .
+    command! -bang -nargs=* FzfOldfiles call fzf#vim#history(fzf#vim#with_preview({'options': '--no-sort'}))
+    " `Rg` version that doesn't shell escape so flags can be used
+    command! -bang -nargs=* FzfRg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case " .
                 \ <q-args>, fzf#vim#with_preview(), <bang>0)
     ]])
   end
--- vim/plugin/plugins/fzf.vim
+++ vim/plugin/plugins/fzf.vim
@@ -2,11 +2,11 @@ set rtp+=$HOMEBREW_DIR/opt/fzf
 
 nnoremap <silent> <leader>b :Buffers<CR>
 nnoremap <silent> <leader>l :BLines<CR>
-nnoremap <silent> <leader>o :FZFHistory<CR>
+nnoremap <silent> <leader>o :FzfOldfiles<CR>
 nnoremap <silent> <leader>t :BTags<CR>
 
 " Don't sort old files
-command! -bang -nargs=* FZFHistory call fzf#vim#history(fzf#vim#with_preview({'options': '--no-sort'}))
+command! -bang -nargs=* FzfOldfiles call fzf#vim#history(fzf#vim#with_preview({'options': '--no-sort'}))
 " Rebind `Rg` to not shell escape so flags can be used
-command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case " .
+command! -bang -nargs=* FzfRg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case " .
       \ <q-args>, fzf#vim#with_preview(), <bang>0)
