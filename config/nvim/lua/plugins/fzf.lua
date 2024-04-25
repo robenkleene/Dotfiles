d4b8a663f9846367a74d31b2003b4de08530aeb1
Roben Kleene <contact@robenkleene.com>
2024-04-24 20:01:33 -0400

    Improve rg command

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -3,6 +3,9 @@ return {
   dependencies = {
     { 'junegunn/fzf', },
   },
+  cmd = {
+    "Rg"
+  },
   keys = {
     { '<leader>b' },
     { '<leader>l' },
@@ -18,7 +21,11 @@ return {
     nnoremap <silent> <leader>o :FZFHistory<CR>
     nnoremap <silent> <leader>t :BTags<CR>
 
+    " Don't sort old files
     command! -bang -nargs=* FZFHistory call fzf#vim#history(fzf#vim#with_preview({'options': '--no-sort'}))
+    " Rebind `Rg` to not shell escape so flags can be used
+    command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case " .
+                \ <q-args>, fzf#vim#with_preview(), <bang>0)
     ]])
   end
 }
--- vim/plugin/plugins/fzf.vim
+++ vim/plugin/plugins/fzf.vim
@@ -5,4 +5,8 @@ nnoremap <silent> <leader>l :BLines<CR>
 nnoremap <silent> <leader>o :FZFHistory<CR>
 nnoremap <silent> <leader>t :BTags<CR>
 
+" Don't sort old files
 command! -bang -nargs=* FZFHistory call fzf#vim#history(fzf#vim#with_preview({'options': '--no-sort'}))
+" Rebind `Rg` to not shell escape so flags can be used
+command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case " .
+      \ <q-args>, fzf#vim#with_preview(), <bang>0)
