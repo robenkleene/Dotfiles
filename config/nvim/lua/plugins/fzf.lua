abbd8363cb7eb9594d17c2e83e9c377ffcdeab1c
Roben Kleene <contact@robenkleene.com>
2024-04-26 17:29:17 -0400

    Add back tags

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -9,6 +9,7 @@ return {
   keys = {
     { '<leader>b' },
     { '<leader>o' },
+    { '<leader>t' },
   },
   config = function()
     vim.cmd([[
@@ -16,6 +17,7 @@ return {
 
     nnoremap <silent> <leader>b :Buffers<CR>
     nnoremap <silent> <leader>o :FzfOldfiles<CR>
+    nnoremap <silent> <leader>t :BTags<CR>
 
     " Don't sort old files
     command! -bang -nargs=* FzfOldfiles call fzf#vim#history(fzf#vim#with_preview({'options': '--no-sort'}))
--- vim/plugin/plugins/fzf.vim
+++ vim/plugin/plugins/fzf.vim
@@ -2,6 +2,7 @@ set rtp+=$HOMEBREW_DIR/opt/fzf
 
 nnoremap <silent> <leader>b :Buffers<CR>
 nnoremap <silent> <leader>o :FzfOldfiles<CR>
+nnoremap <silent> <leader>t :BTags<CR>
 
 " Don't sort old files
 command! -bang -nargs=* FzfOldfiles call fzf#vim#history(fzf#vim#with_preview({'options': '--no-sort'}))
