c67d6e0fa5d80a1c2de22a423112e406314ddf0a
Roben Kleene <contact@robenkleene.com>
2024-05-01 16:16:39 +0000

    Finish script

--- scripts/f_fr_diff_to_conflicted_hunks.sh
+++ scripts/f_fr_diff_to_conflicted_hunks.sh
@@ -7,29 +7,26 @@ hunk=()
 in_hunk=false
 conflict_found=false
 
-# Read from stdin
 while IFS= read -r line; do
-    if [[ "$line" =~ ^diff\ --git|^---|^\+\+\+ ]]; then
-        # If we are starting a new file/section and the previous hunk had a conflict, print it
+    if [[ "$line" =~ ^diff\ --git ]]; then
         if $conflict_found; then
             printf "%s\n" "${header[@]}"
             printf "%s\n" "${hunk[@]}"
-            echo  # extra newline for separation
+            echo
         fi
         
-        # Reset for the new section
         header=("$line")
         hunk=()
         in_hunk=false
         conflict_found=false
 
+    elif [[ "$line" =~ ^---|^\+\+\+ ]]; then
+        header+=("$line")
     elif [[ "$line" =~ ^@@ ]]; then
-        # If we are starting a new hunk, print the previous one if it had a conflict
         if $conflict_found; then
+            printf "%s\n" "${header[@]}"
             printf "%s\n" "${hunk[@]}"
         fi
-
-        # Start a new hunk
         hunk=("$line")
         in_hunk=true
         conflict_found=false
@@ -40,13 +37,10 @@ while IFS= read -r line; do
             if [[ "$line" == *'<<<<<'* ]]; then
                 conflict_found=true
             fi
-        else
-            header+=("$line")
         fi
     fi
 done
 
-# Check the last hunk in the input
 if $conflict_found; then
     printf "%s\n" "${header[@]}"
     printf "%s\n" "${hunk[@]}"
