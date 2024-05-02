aee0da2e9c32762a135f821788655867ec0542e2
Roben Kleene <contact@robenkleene.com>
2024-05-02 15:13:27 +0000

    Tweak fish history search

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,12 @@
+# This function is called when starting a new shell session
 bind \cq push_line
 bind \cw backward-kill-bigword
+# Custom function that does prefix search, the original search is still
+# available with `<C-p>`
+bind _k up up_or_prefix_search
+bind \e\[A up_or_prefix_search
+bind _k down down_or_prefix_search
+bind \e\[B down_or_prefix_search
 # This fails occassionaly, and running the same command manually with single
 # quotes instead of double around the term seems to fix it
 # Just use `↑` `C-a` `history delete`, e.g., prefix the command to delete with `history delete`
