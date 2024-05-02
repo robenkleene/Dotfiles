d8b1682ba1d2b03bc22086dded1015d7344f382e
Roben Kleene <contact@robenkleene.com>
2024-05-02 06:42:16 -0400

    Add comments

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -18,7 +18,11 @@ return {
     " for old files, and using `^<prefix>` or `<postfix>$` searches, which are
     " particularly helpful for specifying a file type for old files
     nnoremap <silent> <leader>b :Buffers<CR>
+    " `:Files` is useful because it doesn't require `fd` to finish executing,
+    " like the `Fd:` command, which means it's usually faster
     nnoremap <silent> <leader>f :Files<CR>
+    " `:Files` and `:RG` are particularly useful when editing in directories of
+    " text files
     nnoremap <silent> <leader>g :RG<CR>
     nnoremap <silent> <leader>o :FzfOldfiles<CR>
     nnoremap <silent> <leader>t :BTags<CR>
--- vim/plugin/plugins/fzf.vim
+++ vim/plugin/plugins/fzf.vim
@@ -1,7 +1,11 @@
 set rtp+=$HOMEBREW_DIR/opt/fzf
 
 nnoremap <silent> <leader>b :Buffers<CR>
+" `:Files` is useful because it doesn't require `fd` to finish executing,
+" like the `Fd:` command, which means it's usually faster
 nnoremap <silent> <leader>f :Files<CR>
+" `:Files` and `:RG` are particularly useful when editing in directories of
+" text files
 nnoremap <silent> <leader>g :RG<CR>
 nnoremap <silent> <leader>o :FzfOldfiles<CR>
 nnoremap <silent> <leader>t :BTags<CR>
