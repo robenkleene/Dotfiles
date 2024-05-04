e33768f9d81f8516d8cba0c918a68fbc6a39d624
Roben Kleene <contact@robenkleene.com>
2024-05-04 17:16:57 +0000

    Use custom copy

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 # This function is called when starting a new shell session
 bind \cq push_line
 bind \cw backward-kill-bigword
+bind \cx copy_command_line
 # This fails occassionaly, and running the same command manually with single
 # quotes instead of double around the term seems to fix it
 # Just use `↑` `C-a` `history delete`, e.g., prefix the command to delete with `history delete`
