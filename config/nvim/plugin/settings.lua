6058b4c10fbc762103715586093b173736a595f3
Roben Kleene <contact@robenkleene.com>
2024-05-06 16:11:04 +0000

    Disable visual yank

--- config/nvim/plugin/settings.lua
+++ config/nvim/plugin/settings.lua
@@ -2,11 +2,11 @@
 -- using implementations of Vim mode that doesn't support this (e.g., in Visual
 -- Studio Code)
 -- Highlight on yank
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank({ higroup="Visual", timeout = 180, on_visual = false })
-  end
-})
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+--   callback = function()
+--     vim.highlight.on_yank({ higroup="Visual", timeout = 180, on_visual = false })
+--   end
+-- })
 
 -- Always show the `signcolumn`, this prevents an issue where the text moves
 -- because indicators are flapping
