e7d55a95cfc934ed97b070aff077a4c952866cdb
Roben Kleene <contact@robenkleene.com>
2024-05-07 06:29:22 -0400

    Clean up quotes

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -3,9 +3,9 @@ return {
   dependencies = {
     { 'junegunn/fzf', },
     {
-      "ibhagwan/fzf-lua",
+      'ibhagwan/fzf-lua',
       config = function()
-        require("fzf-lua").setup({})
+        require('fzf-lua').setup({})
       end
     },
   },
