81753156135ab700b9e6cd94f90222be704668bc
Roben Kleene <contact@robenkleene.com>
2024-05-08 13:48:38 +0000

    Use same directory

--- scripts/adb_screenshots_move.sh
+++ scripts/adb_screenshots_move.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 # The `-f` option suppresses the error if there are no files to delete
-adb pull /sdcard/Oculus/Screenshots/ && adb shell 'cd /sdcard/Oculus/Screenshots && rm -f *.jpg' || true
-adb pull /sdcard/Oculus/VideoShots/ && adb shell 'cd /sdcard/Oculus/VideoShots && rm -f *.mp4' || true
+adb shell 'ls /sdcard/Oculus/Screenshots/' | tr -s "\r\n" "\0" | xargs -0 -n1 adb pull && adb shell 'rm -f /sdcard/Oculus/Screenshots/*.{jpg,png}' || true
+adb shell 'ls /sdcard/Oculus/VideoShots/' | tr -s "\r\n" "\0" | xargs -0 -n1 adb pull && adb shell 'rm -f /sdcard/Oculus/VideoShots/*.mp4' || true
