bbad4bc6d12c94ae29477e5fd2e52f889ae346a2
Roben Kleene <contact@robenkleene.com>
2024-05-09 19:37:39 +0000

    Add fzf capabilities

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -1,23 +1,30 @@
 return {
   'ibhagwan/fzf-lua',
   keys = {
+    { '<leader>D' },
+    { '<leader>S' },
+    { '<leader>T' },
     { '<leader>b' },
+    { '<leader>d' },
+    { '<leader>f' },
     { '<leader>g' },
     { '<leader>o' },
-    { '<leader>f' },
-    { '<leader>t' },
-    { '<leader>d' },
     { '<leader>s' },
+    { '<leader>t' },
   },
   config = function()
     require('fzf-lua').setup({})
     vim.cmd([[
+    nnoremap <silent> <leader>D :lua require("fzf-lua").diagnostics_workspace()<cr>
+    nnoremap <silent> <leader>S :lua require("fzf-lua").lsp_workspace_symbols()<cr>
+    nnoremap <silent> <leader>T :lua require("fzf-lua").tags()<cr>
     nnoremap <silent> <leader>b :lua require("fzf-lua").buffers()<cr>
-    nnoremap <silent> <leader>d :lua require("fzf-lua").diagnostics_workspace()<cr>
+    nnoremap <silent> <leader>d :lua require("fzf-lua").diagnostics_document()<cr>
     nnoremap <silent> <leader>f :lua require("fzf-lua").files()<cr>
     nnoremap <silent> <leader>g :lua require("fzf-lua").live_grep()<cr>
     nnoremap <silent> <leader>o :lua require("fzf-lua").oldfiles()<cr>
     nnoremap <silent> <leader>s :lua require("fzf-lua").lsp_document_symbols()<cr>
+    nnoremap <silent> <leader>t :lua require("fzf-lua").btags()<cr>
     ]])
   end
 }
--- vim/plugin/plugins/fzf.vim
+++ vim/plugin/plugins/fzf.vim
@@ -9,6 +9,7 @@ nnoremap <silent> <leader>f :Files<CR>
 nnoremap <silent> <leader>g :RG<CR>
 nnoremap <silent> <leader>o :FzfOldfiles<CR>
 nnoremap <silent> <leader>t :BTags<CR>
+nnoremap <silent> <leader>T :Tags<CR>
 
 " Don't sort old files
 command! -bang -nargs=* FzfOldfiles call fzf#vim#history(fzf#vim#with_preview({'options': '--no-sort'}))
