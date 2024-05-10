5baafb705c66070923bbc0c9c27130a18a71760b
Roben Kleene <contact@robenkleene.com>
2024-05-10 15:44:13 +0000

    Fix script

--- scripts/fzf_hg_log.sh
+++ scripts/fzf_hg_log.sh
@@ -2,12 +2,10 @@
 
 set -euo pipefail
 
-# `~/.bin/f_hash` only print hashes which is what might be useful for another command
-# `|| true` prevent a non-zero exit status if a binding replaces the `fzf` process
 if [[ -z "$EDITOR" || "$EDITOR" = "nvim" ]]; then
-  exec fzf --ansi --preview "echo {} | ~/.bin/f_hash | xargs hg --color=always show" \
-    --bind="alt-u:become(echo {} | ~/.bin/f_hash | xargs hg update),alt-e:execute(echo {} | ~/.bin/f_hash | xargs hg show | ${EDITOR:-vim} -)" | ~/.bin/f_hash || true
+  exec fzf --ansi --preview "echo {} | ~/.bin/f_hash | xargs hg --color=always log --patch --rev" \
+    --bind="alt-u:become(echo {} | ~/.bin/f_hash | head -n1 | xargs hg update),alt-e:become(echo {} | ~/.bin/f_hash | head -n1 | xargs hg log --patch --rev | ${EDITOR:-vim} -)"
 else
-  exec fzf --ansi --preview "echo {} | ~/.bin/f_hash | xargs hg --color=always show" \
-    --bind="alt-u:become(echo {} | ~/.bin/f_hash | xargs hg update)" | ~/.bin/f_hash || true
+  exec fzf --ansi --preview "echo {} | ~/.bin/f_hash | head -n1 | hg --color=always log --patch --rev" \
+    --bind="alt-u:become(echo {} | ~/.bin/f_hash | head -n1 | xargs hg update)"
 fi
