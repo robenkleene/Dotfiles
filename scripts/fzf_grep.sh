a5452ec5c18699f9a196206877e4186e64923b02
Roben Kleene <contact@robenkleene.com>
2024-05-10 16:17:17 +0000

    Tweak fzf

--- scripts/fzf_grep.sh
+++ scripts/fzf_grep.sh
@@ -7,7 +7,7 @@ if [[ -z "$EDITOR" || "$EDITOR" = "nvim" ]]; then
     --preview 'bat --style=header --color=always --line-range {2}: --highlight-line {2} {1}' \
     --bind="alt-e:execute(printf \"%s\n\" {+} | ${EDITOR:-vim} -c 'cbuffer | bprevious | bdelete' -),alt-a:select-all,alt-d:deselect-all"
 else
-  exec fzf --delimiter=':' --nth=2.. --ansi --keep-right \
+  exec fzf --delimiter=':' --nth=2.. --ansi --keep-right --multi \
     --preview 'bat --style=header --color=always --line-range {2}: --highlight-line {2} {1}' \
     --bind="alt-e:execute(${EDITOR:-vim} +{2} {1}),alt-a:select-all,alt-d:deselect-all"
 fi
--- scripts/fzf_hg_log.sh
+++ scripts/fzf_hg_log.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 if [[ -z "$EDITOR" || "$EDITOR" = "nvim" ]]; then
-  exec fzf --ansi --preview "echo {} | ~/.bin/f_hash | xargs hg --color=always log --patch --rev" \
-    --bind="alt-u:become(echo {} | ~/.bin/f_hash | head -n1 | xargs hg update),alt-e:become(echo {} | ~/.bin/f_hash | head -n1 | xargs hg log --patch --rev | ${EDITOR:-vim} -)"
+  exec fzf --ansi --multi --preview "echo {} | ~/.bin/f_hash | xargs hg --color=always log --patch --rev" \
+    --bind="enter:become(printf \"%s\n\" {+} | ~/.bin/f_hash),alt-u:become(echo {} | ~/.bin/f_hash | head -n1 | xargs hg update),alt-e:become(echo {} | ~/.bin/f_hash | head -n1 | xargs hg log --patch --rev | ${EDITOR:-vim} -)"
 else
-  exec fzf --ansi --preview "echo {} | ~/.bin/f_hash | head -n1 | hg --color=always log --patch --rev" \
-    --bind="alt-u:become(echo {} | ~/.bin/f_hash | head -n1 | xargs hg update)"
+  exec fzf --ansi --multi --preview "echo {} | ~/.bin/f_hash | head -n1 | hg --color=always log --patch --rev" \
+    --bind="enter:become(printf \"%s\n\" {+} | ~/.bin/f_hash),alt-u:become(echo {} | ~/.bin/f_hash | head -n1 | xargs hg update)"
 fi
