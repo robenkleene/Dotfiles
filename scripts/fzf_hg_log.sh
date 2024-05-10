d18f67e7ca865c131f404421469932261698b582
Roben Kleene <contact@robenkleene.com>
2024-05-10 08:54:31 -0400

    Only dump hash

--- scripts/fzf_hg_log.sh
+++ scripts/fzf_hg_log.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 if [[ -z "$EDITOR" || "$EDITOR" = "nvim" ]]; then
   exec fzf --ansi --preview "echo {} | ~/.bin/f_hash | xargs hg --color=always show" \
-    --bind="alt-e:execute(echo {} | ~/.bin/f_hash | xargs hg update),alt-e:execute(echo {} | ~/.bin/f_hash | xargs hg show | ${EDITOR:-vim} -)"
+    --bind="alt-e:execute(echo {} | ~/.bin/f_hash | xargs hg update),alt-e:execute(echo {} | ~/.bin/f_hash | xargs hg show | ${EDITOR:-vim} -)" | ~/.bin/f_hash
 else
   exec fzf --ansi --preview "echo {} | ~/.bin/f_hash | xargs hg --color=always show" \
-    --bind="alt-e:execute(echo {} | ~/.bin/f_hash | xargs hg update)"
+    --bind="alt-e:execute(echo {} | ~/.bin/f_hash | xargs hg update)" | ~/.bin/f_hash
 fi
