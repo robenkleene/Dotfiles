c0a348a0cd65df0673f04431e658e2d554a66e0f
Roben Kleene <contact@robenkleene.com>
2024-05-13 15:24:34 +0000

    Improve compatibility

--- scripts/f_hash.sh
+++ scripts/f_hash.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 # `[^A-Z]` removes some things that look like hashes, like `T0000000`
-grep --only-matching "[^A-Z][a-f0-9]\{9\}[a-f0-9]*" | sed 's/\s.*$//'
+grep --extended-regexp --only-matching "[^A-Z][a-f0-9]{9}[a-f0-9]*" | sed 's/\s.*$//'
