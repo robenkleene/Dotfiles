88d936aa382dc23917b6b9e135c298e29f33a9e0
Roben Kleene <contact@robenkleene.com>
2024-05-18 06:25:37 -0400

    Add back cursor color

--- emacs.d/robenkleene/config/robenkleene-gui.el
+++ emacs.d/robenkleene/config/robenkleene-gui.el
@@ -74,7 +74,7 @@
 (add-to-list 'initial-frame-alist
              '(vertical-scroll-bars . nil))
 ;; Cursor color
-;; (add-to-list 'default-frame-alist '(cursor-color . "#4BC840"))
+(add-to-list 'default-frame-alist '(cursor-color . "#4BC840"))
 
 ;; Never use GUI dialog boxes
 (setq use-dialog-box nil)
