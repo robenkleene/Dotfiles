88d9be42782cdf28fb4bf9ea4850200e09501317
Roben Kleene <contact@robenkleene.com>
2024-05-23 15:30:46 +0000

    Disable completion in minibuffer

--- emacs.d/robenkleene/packages/robenkleene-corfu.el
+++ emacs.d/robenkleene/packages/robenkleene-corfu.el
@@ -44,14 +44,14 @@
                               ))
 
   ;; Enable in minibuffer
-  (defun corfu-enable-in-minibuffer ()
-    "Enable Corfu in the minibuffer."
-    (when (local-variable-p 'completion-at-point-functions)
-      ;; (setq-local corfu-auto nil) ;; Enable/disable auto completion
-      (setq-local corfu-echo-delay nil ;; Disable automatic echo and popup
-                  corfu-popupinfo-delay nil)
-      (corfu-mode 1)))
-  (add-hook 'minibuffer-setup-hook #'corfu-enable-in-minibuffer)
+  ;; (defun corfu-enable-in-minibuffer ()
+  ;;   "Enable Corfu in the minibuffer."
+  ;;   (when (local-variable-p 'completion-at-point-functions)
+  ;;     ;; (setq-local corfu-auto nil) ;; Enable/disable auto completion
+  ;;     (setq-local corfu-echo-delay nil ;; Disable automatic echo and popup
+  ;;                 corfu-popupinfo-delay nil)
+  ;;     (corfu-mode 1)))
+  ;; (add-hook 'minibuffer-setup-hook #'corfu-enable-in-minibuffer)
   :config
   (setq corfu-auto t)
   )
