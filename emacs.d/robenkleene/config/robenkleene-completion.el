bf4eee623efb6df85a0f2c593e3aa287d4482232
Roben Kleene <contact@robenkleene.com>
2024-05-29 20:19:55 +0000

    Fix completion

--- emacs.d/robenkleene/config/robenkleene-completion.el
+++ emacs.d/robenkleene/config/robenkleene-completion.el
@@ -11,10 +11,10 @@
 ;; In text and programming modes, use prefix completion which works more like
 ;; autocomplete
 (add-hook 'text-mode-hook (lambda ()
-                            (setq-local completion-styles '(prefix partial-completion))
+                            (setq-local completion-styles '(partial-completion))
                             ))
 (add-hook 'prog-mode-hook (lambda ()
-                            (setq-local completion-styles '(prefix partial-completion))
+                            (setq-local completion-styles '(partial-completion))
                             ))
 
 (provide 'robenkleene-completion)
