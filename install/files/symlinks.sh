895cbff32e8f130cd401cc805a0be94f1dc5f458
Roben Kleene <contact@robenkleene.com>
2024-06-01 15:32:31 -0400

    Sync snippets

--- install/files/symlinks.sh
+++ install/files/symlinks.sh
@@ -35,6 +35,9 @@ vscode_insiders_path="$HOME/.config/Code - Insiders"
 if [[ -e "$vscode_path" && ! -e "$vscode_insiders_path" ]]; then
   ln -s "$vscode_path" "$vscode_insiders_path"
 fi
+if [[ ! -e "$vscode_path/User/snippets/global.code-snippets" ]]; then
+  ln -s "$vscode_path/User/snippets/global.json" "$vscode_path/User/snippets/global.code-snippets"
+fi
 if [[ "$(uname)" = "Darwin" ]]; then
   if [[ ! -e "$HOME/Library/Application Support/Code" ]]; then
     ln -s "$vscode_path" "$HOME/Library/Application Support/Code"
