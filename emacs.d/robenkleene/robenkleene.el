0a76df4b68dddc321195f30837d505343eaad90f
Roben Kleene <contact@robenkleene.com>
2024-06-03 21:02:32 +0000

    Remove treesit

--- emacs.d/robenkleene/robenkleene.el
+++ emacs.d/robenkleene/robenkleene.el
@@ -75,7 +75,6 @@
 (require 'robenkleene-rust)
 (require 'robenkleene-swift)
 (require 'robenkleene-text)
-(require 'robenkleene-treesit-auto)
 (require 'robenkleene-typescript)
 (require 'robenkleene-yaml)
 
