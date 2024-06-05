83ed8939917b8277699bed56f81c300512945dc0
Roben Kleene <contact@robenkleene.com>
2024-06-05 17:10:11 +0000

    Tweak history search

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -6,3 +6,11 @@ bind \cx copy_command_line
 # quotes instead of double around the term seems to fix it
 # Just use `↑` `C-a` `history delete`, e.g., prefix the command to delete with `history delete`
 # bind \cx\cd 'history delete --exact --case-sensitive (commandline); history save; commandline -f kill-whole-line'
+
+# Custom function that does prefix search, the original search is still
+# available with `<C-p>`
+# Problem with these is that the first result is the same as autocomplete
+bind _k up up_or_prefix_search
+bind \e\[A up_or_prefix_search
+bind _k down down_or_prefix_search
+bind \e\[B down_or_prefix_search
---
+++ config/fish/functions/up_or_prefix_search.fish
@@ -0,0 +1,29 @@
+function up_or_prefix_search --description 'Search back or move cursor up 1 line'
+    # If we are already in search mode, continue
+    if commandline --search-mode
+        commandline -f history-prefix-search-backward
+        return
+    end
+
+    # If we are navigating the pager, then up always navigates
+    if commandline --paging-mode
+        commandline -f up-line
+        return
+    end
+
+    # We are not already in search mode.
+    # If we are on the top line, start search mode,
+    # otherwise move up
+    set -l lineno (commandline -L)
+
+    switch $lineno
+        case 1
+            # Run this twice to simulate the default `up-or-search` skipping
+            # the current completion match
+            commandline -f history-prefix-search-backward
+            commandline -f history-prefix-search-backward
+
+        case '*'
+            commandline -f up-line
+    end
+end
