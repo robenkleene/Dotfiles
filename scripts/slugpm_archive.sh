e476ec827c3b40fa4805e9dcce93b57b89a17ce7
Roben Kleene <contact@robenkleene.com>
2024-07-12 06:38:05 -0400

    Make slugpm take STDIN

--- scripts/slugpm_archive.sh
+++ scripts/slugpm_archive.sh
@@ -4,18 +4,17 @@ set -euo pipefail
 
 project_dir="$1"
 
+if [[ -z "$project_dir" ]]; then
+  text=$(cat)
+fi
+
+if [[ ! -d "$project_dir" && -z "${text:-}" ]]; then
+  echo "Error: Nothing to archive" >&2
+  exit 1
+fi
+
 if [[ ! -d "$project_dir" ]]; then
   project_dir=$(dirname "$1")
-fi
-
-if [[ -z "$project_dir" ]]; then
-  echo "Error: No project directory specified" >&2
-  exit 1
-fi
-
-if [[ ! -d "$project_dir" ]]; then
-  echo "Error: No valid project directory found $project_dir" >&2
-  exit 1
 fi
 
 # Remove leading and trailing slash and leading period
@@ -24,16 +23,21 @@ fi
 # project_dir=${project_dir#\.}
 project_dir=${project_dir%/}
 
-destination_dir="$project_dir/../../archive/projects"
+if [[ -z "${text:-}" ]]; then
+  destination_dir="$project_dir/../../archive/projects"
 
-if [[ ! -d "$destination_dir" ]]; then
-  echo -n "$destination_dir does not exist" >&2
-  exit 1
+  if [[ ! -d "$destination_dir" ]]; then
+    echo -n "$destination_dir does not exist" >&2
+    exit 1
+  fi
+
+  # Convert o absolute path which helps in the case where current directory is
+  # just `.`
+  src_dir=$(cd "$project_dir" ; pwd)
+  mv "$src_dir" "$destination_dir"
+  dir_name=$(basename "$src_dir")
+  echo -n "$destination_dir/$dir_name"
+else
+  destination_file="$project_dir/../../archive/README.md"
+  echo "$text" >>"$destination_file"
 fi
-
-# Convert o absolute path which helps in the case where current directory is
-# just `.`
-src_dir=$(cd "$project_dir" ; pwd)
-mv "$src_dir" "$destination_dir"
-dir_name=$(basename "$src_dir")
-echo -n "$destination_dir/$dir_name"
