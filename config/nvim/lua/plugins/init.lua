211197588982bab6ffd2e175938a9d7d797c5601
Roben Kleene <contact@robenkleene.com>
2024-07-16 21:43:21 +0000

    Tweak oil

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -4,9 +4,10 @@ return {
   -- { 'tpope/vim-vinegar' },
   {
     'stevearc/oil.nvim',
-    opts = {},
     config = function()
-      require("oil").setup()
+      require("oil").setup({
+        skip_confirm_for_simple_edits = true,
+      })
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end
   },
