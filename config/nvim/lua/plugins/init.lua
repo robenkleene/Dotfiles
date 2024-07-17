a4b9471aa7a5f90e211cf2ece93aa28464c84507
Roben Kleene <contact@robenkleene.com>
2024-07-17 17:15:46 +0000

    Allow lcd

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -6,8 +6,11 @@ return {
     'stevearc/oil.nvim',
     config = function()
       require("oil").setup({
+        keymaps = {
+          ["~"] = { "actions.cd", opts = { scope = "win" }, desc = ":lcd to the current oil directory" },
+        },
         skip_confirm_for_simple_edits = true,
-        experimental_watch_for_changes = true
+        watch_for_changes = true
       })
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end
