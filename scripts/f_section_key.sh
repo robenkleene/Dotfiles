9ffe2c9a0f9ad45afc68e7ad876141aa2a93a136
Roben Kleene <contact@robenkleene.com>
2024-07-22 16:19:43 +0000

    Add section key script

---
+++ scripts/f_section_key.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/awk -f
+
+/SECTIONSTART/ {
+    in_block = 1;
+    block = ""
+}
+/SECTIONSTART/,/SECTIONEND/ {
+    if (in_block) {
+        block = block $0 "\n";
+    }
+    if (/SECTIONKEY/) {
+        found_key = 1;
+    }
+}
+/SECTIONEND/ {
+    if (in_block) {
+        if (found_key) {
+            printf "%s", block;
+        }
+        in_block = 0;
+        found_key = 0;
+    }
+}
