9811ede552708c5e5dd9c8071b04b1e6df689191
Roben Kleene <contact@robenkleene.com>
2024-07-23 16:25:44 +0000

    Fish update abbr

--- config/fish/abbr.fish
+++ config/fish/abbr.fish
@@ -1,5 +1,6 @@
+abbr -a -- - 'cd -'
 abbr --add hd history delete -eC
-abbr --add printf_var printf "%s"
+abbr --add printf_var printf '%s\n'
 
 if test -f ~/.local_abbr.fish
     source ~/.local_abbr.fish
