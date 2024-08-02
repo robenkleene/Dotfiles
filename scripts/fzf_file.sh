de536e0512d9f17c1e735aac1c3ad3c04278476a
Roben Kleene <contact@robenkleene.com>
2024-08-02 16:18:28 +0000

    Archive scripts

--- scripts/fzf_file.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-exec fzf --ansi --keep-right --multi --preview "bat --style=plain --color=always {}" \
-  --bind="alt-e:execute(${EDITOR:-vim} {+}),alt-a:select-all,alt-d:deselect-all"
--- scripts/p_tmux_display.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-temp_file=$(mktemp)
-cat > "$temp_file"
-# Put an empty line in front to make it easier to cut and paste without copying the copy mode line indicator
-tmux run-shell "echo; cat $temp_file"
-rm "$temp_file"
