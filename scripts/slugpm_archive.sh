67d00d77585a853bba6abd08a259bc6f4a3ee95a
Roben Kleene <contact@robenkleene.com>
2024-08-06 21:41:03 +0000

    Handle multiple projects

--- scripts/slugpm_archive.sh
+++ scripts/slugpm_archive.sh
@@ -2,28 +2,29 @@
 
 set -euo pipefail
 
-project_dir="${1:-}"
-
-if [[ -z "$project_dir" ]]; then
+if [[ $# -eq 0 ]]; then
   text=$(cat)
+  if [[ -z "${text:-}" ]]; then
+    echo "Error: Nothing to archive" >&2
+    exit 1
+  fi
+  destination_file="$PWD/archive/README.md"
+  echo "$text" >>"$destination_file"
+  exit 0
 fi
 
-if [[ ! -d "$project_dir" && -z "${text:-}" ]]; then
-  echo "Error: Nothing to archive" >&2
-  exit 1
-fi
+for project_dir in "$@"; do
+  # Remove leading and trailing slash and leading period
+  # Can't remove leading dot because that will break if already in the
+  # `project_dir` so the entire path is `.`
+  # project_dir=${project_dir#\.}
+  project_dir=${project_dir%/}
 
-if [[ ! -d "$project_dir" ]]; then
-  project_dir="$PWD"
-fi
+  if [[ ! -d "$project_dir" ]]; then
+    echo -n "$project_dir isn't a directory" >&2
+    exit 1
+  fi
 
-# Remove leading and trailing slash and leading period
-# Can't remove leading dot because that will break if already in the
-# `project_dir` so the entire path is `.`
-# project_dir=${project_dir#\.}
-project_dir=${project_dir%/}
-
-if [[ -z "${text:-}" ]]; then
   destination_dir="$project_dir/../../archive/projects"
 
   if [[ ! -d "$destination_dir" ]]; then
@@ -35,9 +36,6 @@ if [[ -z "${text:-}" ]]; then
   # just `.`
   src_dir=$(cd "$project_dir" ; pwd)
   mv "$src_dir" "$destination_dir"
-  dir_name=$(basename "$src_dir")
-  echo -n "$destination_dir/$dir_name"
-else
-  destination_file="$project_dir/archive/README.md"
-  echo "$text" >>"$destination_file"
-fi
+  # dir_name=$(basename "$src_dir")
+  # echo -n "$destination_dir/$dir_name"
+done
