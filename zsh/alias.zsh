0cda5896dec9a060be26d7f6e8bd07292d946712
Roben Kleene <contact@robenkleene.com>
2024-08-11 14:48:30 -0400

    Rename commands

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -13,8 +13,8 @@ fi
 # egit
 # These are aliases instead of shell functions because they set the `ssh`
 # status of the current shell
-alias u_push='ssh_start && { egit -p || egitn } && ~/.bin/sgitt_auto'
-alias u_pull='ssh_start && ~/.bin/egit_update'
-alias u_pull_text='ssh_start && ssh_git_pull_all -t'
-alias u_pull_all='ssh_start && ssh_git_pull_all'
-alias u_pull_settings='ssh_start && ~/.bin/egit_update -s'
+alias rk_push='ssh_start && { egit -p || egitn } && ~/.bin/sgitt_auto'
+alias rk_pull='ssh_start && ~/.bin/egit_update'
+alias rk_pull_text='ssh_start && ssh_git_pull_all -t'
+alias rk_pull_all='ssh_start && ssh_git_pull_all'
+alias rk_pull_settings='ssh_start && ~/.bin/egit_update -s'
