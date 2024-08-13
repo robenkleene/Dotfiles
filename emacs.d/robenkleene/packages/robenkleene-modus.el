e70f3d8db17f25086ac5b887247513ad4cc967fc
Roben Kleene <contact@robenkleene.com>
2024-08-13 19:47:44 +0000

    Tweak colors

--- emacs.d/robenkleene/packages/robenkleene-modus.el
+++ emacs.d/robenkleene/packages/robenkleene-modus.el
@@ -11,19 +11,26 @@
           (border-mode-line-inactive unspecified)
           ;; Reduce the intensity a bit
           (modus-themes-preset-overrides-faint)
-          ;; Make the fringe transparent, this makes windows without line
-          ;; numbers look more minimal
-          (fringe unspecified)
-          ;; Remove the line number background, which looks better when the
-          ;; fringe isn't present (e.g., when editing a text file)
-          (bg-line-number-inactive unspecified)
-          (bg-line-number-active unspecified)
+          ;; ;; Make the fringe transparent, this makes windows without line
+          ;; ;; numbers look more minimal
+          ;; (fringe unspecified)
+          ;; ;; Remove the line number background, which looks better when the
+          ;; ;; fringe isn't present (e.g., when editing a text file)
+          ;; (bg-line-number-inactive unspecified)
+          ;; (bg-line-number-active unspecified)
           ;; Make the current tab the same color as background
           (bg-tab-current bg-main)
           ;; Dim the highlight line, the default was making it sometimes hard to
           ;; find the cursor
           (bg-hl-line bg-inactive)
+
+          ;; Disable colorful headings
+          (fg-heading-1 fg-main)
+          (fg-heading-2 fg-main)
+          (fg-heading-3 fg-main)
+          (fg-heading-4 fg-main)
           ))
+
   (setq modus-vivendi-tinted-palette-overrides
         '(
           (bg-main "#211930")
