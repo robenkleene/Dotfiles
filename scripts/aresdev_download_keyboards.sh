ed9de5e65e21097e824df09340d9efad3249baa1
Roben Kleene <contact@robenkleene.com>
2024-08-15 10:18:10 +0000

    Update keyboards

--- scripts/aresdev_download_keyboards.sh
+++ scripts/aresdev_download_keyboards.sh
@@ -3,7 +3,6 @@
 # Remove `e` to allow failing if a keyboard is missing
 set -uo pipefail
 
-sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/planck_ez_base_robenkleene_macos.bin .
 sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/ergodox_ez_base_robenkleene_macos2.hex .
 sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/moonlander_robenkleene_macos.bin .
 sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/zsa_voyager_robenkleene_macos.bin .
