cb8348825a46790182324d799124ee6d88ae982c
Roben Kleene <contact@robenkleene.com>
2024-08-21 17:54:35 +0000

    Improve expand region for org and markdown

--- emacs.d/robenkleene/packages/robenkleene-expand-region.el
+++ emacs.d/robenkleene/packages/robenkleene-expand-region.el
@@ -6,24 +6,68 @@
   :bind
   ("M-'" . er/expand-region)
   :init
-  ;; Add support for Markdown inline code, and code blocks
-  (defun rk/mark-markdown-inline-code ()
-    "Marks one CSS declaration, eg. font-weight: bold;"
+  (defun rk/mark-markdown-link-title ()
+    "Marks between tilde."
     (interactive)
-    (search-backward-regexp "`" (line-beginning-position))
+    (search-backward "[" (line-beginning-position))
+    (forward-char)
+    (set-mark (point))
+    (search-forward "]" (line-end-position))
+    (backward-char)
+    (exchange-point-and-mark))
+
+  (defun rk/mark-markdown-link-url ()
+    "Marks between tilde."
+    (interactive)
+    (search-backward "(" (line-beginning-position))
+    (forward-char)
+    (set-mark (point))
+    (search-forward ")" (line-end-position))
+    (backward-char)
+    (exchange-point-and-mark))
+
+  (defun rk/mark-markdown-inline-code ()
+    "Marks between tilde."
+    (interactive)
+    (search-backward "`" (line-beginning-position))
     (forward-char)
     (set-mark (point))
     (search-forward "`" (line-end-position))
     (backward-char)
     (exchange-point-and-mark))
+
   (defun rk/add-markdown-mode-expansions ()
     (make-variable-buffer-local 'er/try-expand-list)
     (setq er/try-expand-list (append
                               er/try-expand-list
                               '(
                                 rk/mark-markdown-inline-code
+                                rk/mark-markdown-link-title
+                                rk/mark-markdown-link-url
                                 ))))
+
   (add-hook 'markdown-mode-hook 'rk/add-markdown-mode-expansions)
+
+  (defun rk/mark-org-inline-code ()
+    "Marks between `~'."
+    (interactive)
+    (search-backward "~" (line-beginning-position))
+    (forward-char)
+    (set-mark (point))
+    (search-forward "~" (line-end-position))
+    (backward-char)
+    (exchange-point-and-mark))
+
+  (defun rk/add-org-mode-expansions ()
+    (make-variable-buffer-local 'er/try-expand-list)
+    (setq er/try-expand-list (append
+                              er/try-expand-list
+                              '(
+                                rk/mark-org-inline-code
+                                ))))
+
+  (add-hook 'org-mode-hook 'rk/add-org-mode-expansions)
+
   )
 
 (provide 'robenkleene-expand-region)
