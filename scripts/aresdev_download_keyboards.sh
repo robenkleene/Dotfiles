72e3d2de1d78ab844b6fccfbc315290ae3ea3ddc
Roben Kleene <contact@robenkleene.com>
2024-09-17 17:19:59 +0000

    Update keyboard

--- scripts/aresdev_download_keyboards.sh
+++ scripts/aresdev_download_keyboards.sh
@@ -3,6 +3,19 @@
 # Remove `e` to allow failing if a keyboard is missing
 set -uo pipefail
 
+if [[ "$1" = "moonlander" ]]; then
+  sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/moonlander_robenkleene_macos.bin .
+  exit 0
+fi
+if [[ "$1" = "ergodox" ]]; then
+  sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/ergodox_ez_base_robenkleene_macos2.hex .
+  exit 0
+fi
+if [[ "$1" = "voyager" ]]; then
+  sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/zsa_voyager_robenkleene_macos.bin .
+  exit 0
+fi
+
 sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/ergodox_ez_base_robenkleene_macos2.hex .
 sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/moonlander_robenkleene_macos.bin .
 sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/zsa_voyager_robenkleene_macos.bin .
