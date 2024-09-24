b432a9b8ee0989bfad9bea0deeb4ff9fd2ac6de5
Roben Kleene <contact@robenkleene.com>
2024-09-24 16:51:30 +0000

    Fallback to emacs

--- scripts/emacs.sh
+++ scripts/emacs.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 if [[ -f "/Applications/Emacs.app/Contents/MacOS/Emacs" ]]; then
   exec "/Applications/Emacs.app/Contents/MacOS/Emacs" "-nw" "$@"
-else
+else [[ -f "$HOME/.brew/bin/emacs" ]]; then
   exec "$HOME/.brew/bin/emacs" "-nw" "$@"
+else
+  exec emacs "-nw" "$@"
 fi
