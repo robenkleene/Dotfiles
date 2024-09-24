45460446af24f71cb8afcb200f6b52882f9edc6c
Roben Kleene <contact@robenkleene.com>
2024-09-24 16:58:04 +0000

    Fix Emacs location

--- scripts/emacs.sh
+++ scripts/emacs.sh
@@ -6,6 +6,6 @@ if [[ -f "/Applications/Emacs.app/Contents/MacOS/Emacs" ]]; then
   exec "/Applications/Emacs.app/Contents/MacOS/Emacs" "-nw" "$@"
 elif [[ -f "$HOME/.brew/bin/emacs" ]]; then
   exec "$HOME/.brew/bin/emacs" "-nw" "$@"
-elif [[ -f "$HOME/.bin/emacs" ]]; then
-  exec "$HOME/.bin/emacs" "-nw" "$@"
+elif [[ -f "/usr/bin/emacs" ]]; then
+  exec "/usr/bin/emacs" "-nw" "$@"
 fi
