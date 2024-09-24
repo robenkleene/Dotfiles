1dd209a29867823ef02e13036ebf0766c6978ff1
Roben Kleene <contact@robenkleene.com>
2024-09-24 16:52:40 +0000

    Fix script

--- scripts/emacs.sh
+++ scripts/emacs.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 if [[ -f "/Applications/Emacs.app/Contents/MacOS/Emacs" ]]; then
   exec "/Applications/Emacs.app/Contents/MacOS/Emacs" "-nw" "$@"
-else [[ -f "$HOME/.brew/bin/emacs" ]]; then
+elif [[ -f "$HOME/.brew/bin/emacs" ]]; then
   exec "$HOME/.brew/bin/emacs" "-nw" "$@"
 else
   exec emacs "-nw" "$@"
