e39b9c556df0cc837d12b066fd5e089385a38826
Roben Kleene <contact@robenkleene.com>
2024-09-24 16:55:50 +0000

    Support other emacs location

--- scripts/emacs.sh
+++ scripts/emacs.sh
@@ -6,6 +6,6 @@ if [[ -f "/Applications/Emacs.app/Contents/MacOS/Emacs" ]]; then
   exec "/Applications/Emacs.app/Contents/MacOS/Emacs" "-nw" "$@"
 elif [[ -f "$HOME/.brew/bin/emacs" ]]; then
   exec "$HOME/.brew/bin/emacs" "-nw" "$@"
-else
-  exec emacs "-nw" "$@"
+elif [[ -f "$HOME/.bin/emacs" ]]; then
+  exec "$HOME/.bin/emacs" "-nw" "$@"
 fi
