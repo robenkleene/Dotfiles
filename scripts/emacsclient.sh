f82233fb6f5e78cc136dd00f99fbbc168f354b51
Roben Kleene <contact@robenkleene.com>
2024-09-26 20:44:09 +0000

    Fix emacsclient

--- scripts/emacsclient.sh
+++ scripts/emacsclient.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 if [[ -f "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient" ]]; then
   exec "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient" "$@"
-else
+elif [[ -f "$HOME/.brew/bin/emacsclient" ]]; then
   exec "$HOME/.brew/bin/emacsclient" "$@"
+elif [[ -f "/usr/bin/emacsclient" ]]; then
+  exec "/usr/bin/emacsclient" "$@"
 fi
