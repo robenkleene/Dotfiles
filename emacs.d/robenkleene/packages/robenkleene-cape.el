31d09f71db0560ce26d7065ca42a077fd8ce2400
Roben Kleene <contact@robenkleene.com>
2024-10-04 20:50:10 +0000

    Add comment

--- emacs.d/robenkleene/packages/robenkleene-cape.el
+++ emacs.d/robenkleene/packages/robenkleene-cape.el
@@ -11,7 +11,16 @@
     :init
     (add-to-list 'completion-at-point-functions #'yasnippet-capf)
     )
+
   ;; Exclusive
+  ;; When `completion-at-point-functions' is with the last element as true it's
+  ;; supposed to go from the local to global value of
+  ;; `completion-at-point-fucntions'
+  ;; But if a completion function is marked as `exclusive', then if it handles a
+  ;; completion, the rest of the functions are skipped
+  ;; The `cape' completions are marked not marked as exclusive, but the default
+  ;; ones typically are
+  ;; So we need to manually mark them as not exclusive here.
   (add-hook
    'emacs-lisp-mode-hook
    (lambda ()
