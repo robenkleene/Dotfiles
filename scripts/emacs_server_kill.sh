2c3c1bb2a3b86d7b1811329c3c18c274e4a897db
Roben Kleene <contact@robenkleene.com>
2024-10-05 16:47:07 -0400

    Fix kill

--- scripts/emacs_server_kill.sh
+++ scripts/emacs_server_kill.sh
@@ -2,4 +2,7 @@
 
 set -euo pipefail
 
-emacsclient -e '(save-buffers-kill-emacs t)'
+# This is safer but sometimes fails, probably a better practice to just connect
+# to Emacs and run `save-buffers-kill-emacs` there
+# emacsclient -e '(save-buffers-kill-emacs t)'
+emacsclient -e '(kill-emacs)'
