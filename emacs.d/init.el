da16f293bd646a98e6a789eda75cf603146514b2
Roben Kleene <contact@robenkleene.com>
2024-10-15 15:46:38 +0000

    Remove superfluous ensure

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -13,7 +13,6 @@
 
 ;; Uncomment to benchmark startup
 ;; (use-package benchmark-init
-;;   :ensure t
 ;;   :config
 ;;   ;; To disable collection of benchmark data after init is done.
 ;;   (add-hook 'after-init-hook 'benchmark-init/deactivate))
