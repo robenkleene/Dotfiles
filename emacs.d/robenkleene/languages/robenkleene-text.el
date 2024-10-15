c4b3ba013e4d77f166541f8e024d366b646dbbb1
Roben Kleene <contact@robenkleene.com>
2024-10-15 15:45:51 +0000

    Eval after load text

--- emacs.d/robenkleene/languages/robenkleene-text.el
+++ emacs.d/robenkleene/languages/robenkleene-text.el
@@ -2,20 +2,40 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Goto grep line number and use `find-file-at-point' by default
-(defun rk/find-file-at-point-goto-line (ret)
-  "Ignore RET and jump to line number given in `ffap-string-at-point'."
-  (when (and
-         (stringp ffap-string-at-point)
-         (string-match ":\\([0-9]+\\)" ffap-string-at-point))
-    (goto-line (string-to-number (match-string 1 ffap-string-at-point)))
-    )
-  ret)
-(advice-add 'find-file-at-point :filter-return #'rk/find-file-at-point-goto-line)
-;; This also binds for `dired-mode', which isn't desirable
-;; (ffap-bindings)
-
 (with-eval-after-load 'text-mode
+  ;; Add characters to wrap on (in particular this helps URLs wrap more naturally)
+  (setq rk/text-category-table (copy-category-table))
+  (modify-category-entry ?- ?| rk/text-category-table)
+  (modify-category-entry ?/ ?| rk/text-category-table)
+  ;; It looks funny to have line breaks on `:`, `.` and `,` in normal prose
+  ;; (modify-category-entry ?. ?| rk/text-category-table)
+  ;; (modify-category-entry ?, ?| rk/text-category-table)
+  ;; (modify-category-entry ?: ?| rk/text-category-table)
+  (modify-category-entry ?_ ?| rk/text-category-table)
+  (add-hook 'text-mode-hook (lambda ()
+                              ;; Wrap in text modes
+                              (set 'truncate-lines nil)
+                              (set-category-table rk/text-category-table)
+                              (setq-local word-wrap-by-category t)
+                              ;; Show trailing whitespace
+                              (setq-local show-trailing-whitespace t)
+                              ))
+
+  ;; Goto grep line number and use `find-file-at-point' by default
+  (defun rk/find-file-at-point-goto-line (ret)
+    "Ignore RET and jump to line number given in `ffap-string-at-point'."
+    (when (and
+           (stringp ffap-string-at-point)
+           (string-match ":\\([0-9]+\\)" ffap-string-at-point))
+      (goto-line (string-to-number (match-string 1 ffap-string-at-point)))
+      )
+    ret)
+  (advice-add 'find-file-at-point :filter-return #'rk/find-file-at-point-goto-line)
+  ;; This also binds for `dired-mode', which isn't desirable
+  ;; (ffap-bindings)
+
+
+
   ;; Allow following grep line matches
   (define-key text-mode-map (kbd "C-x C-f") 'ffap)
   ;; (define-key text-mode-map (kbd "M-<down>") 'rk/move-line-down)
@@ -24,24 +44,6 @@
   (modify-syntax-entry ?' "\"")
   (modify-syntax-entry ?\" "\"")
   )
-
-;; Add characters to wrap on (in particular this helps URLs wrap more naturally)
-(setq rk/text-category-table (copy-category-table))
-(modify-category-entry ?- ?| rk/text-category-table)
-(modify-category-entry ?/ ?| rk/text-category-table)
-;; It looks funny to have line breaks on `:`, `.` and `,` in normal prose
-;; (modify-category-entry ?. ?| rk/text-category-table)
-;; (modify-category-entry ?, ?| rk/text-category-table)
-;; (modify-category-entry ?: ?| rk/text-category-table)
-(modify-category-entry ?_ ?| rk/text-category-table)
-(add-hook 'text-mode-hook (lambda ()
-                            ;; Wrap in text modes
-                            (set 'truncate-lines nil)
-                            (set-category-table rk/text-category-table)
-                            (setq-local word-wrap-by-category t)
-                            ;; Show trailing whitespace
-                            (setq-local show-trailing-whitespace t)
-                            ))
 
 (provide 'robenkleene-text)
 ;; Local Variables:
