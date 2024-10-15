13893ddaeb5d7f4563321042bf025c395e9f522f
Roben Kleene <contact@robenkleene.com>
2024-10-15 17:50:23 +0000

    Simplify

--- install/emacs/packages.el
+++ install/emacs/packages.el
@@ -1,6 +1,7 @@
-(require 'package)
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
+
+(require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives '(
                          ("gnu" . "https://elpa.gnu.org/packages/")
@@ -8,13 +9,15 @@
                          ("melpa" . "https://melpa.org/packages/")
                          ))
 (package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package)
-  (eval-when-compile
-    (unless (bound-and-true-p package--initialized)
-      (package-initialize))
-    (require 'use-package)))
+
+;;  This is built-in now
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package)
+;;   (eval-when-compile
+;;     (unless (bound-and-true-p package--initialized)
+;;       (package-initialize))
+;;     (require 'use-package)))
 (package-autoremove)
 ;; Do upgrade early because this also does a refresh
 (package-upgrade-all)
