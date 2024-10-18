6bd58f834f8aa07138a6ae82b560aba27f181fa5
Roben Kleene <contact@robenkleene.com>
2024-10-18 20:43:06 +0000

    Clean up script

--- scripts/tmux_init_reset.sh
+++ scripts/tmux_init_reset.sh
@@ -91,22 +91,22 @@ tmux set-option -ug remain-on-exit
 tmux set-option -ug window-active-style
 tmux set-option -ug window-style
 tmux unbind-key -a
-tmux bind C-b send-prefix
-tmux bind C-o rotate-window
-tmux bind C-z suspend-client
-tmux bind Space next-layout
-tmux bind ! break-pane
+tmux bind 'C-b' send-prefix
+tmux bind 'C-o' rotate-window
+tmux bind 'C-z' suspend-client
+tmux bind 'Space' next-layout
+tmux bind '!' break-pane
 tmux bind '"' split-window
 tmux bind '#' list-buffers
 tmux bind '$' command-prompt -I'#S' "rename-session -- '%%'"
-tmux bind % split-window -h
-tmux bind \& confirm-before -p"kill-window #W? (y/n)" kill-window
+tmux bind '%' split-window -h
+tmux bind '&' confirm-before -p"kill-window #W? (y/n)" kill-window
 tmux bind "'" command-prompt -pindex "select-window -t ':%%'"
-tmux bind \( switch-client -p
-tmux bind \) switch-client -n
-tmux bind , command-prompt -I'#W' "rename-window -- '%%'"
-tmux bind - delete-buffer
-tmux bind . command-prompt "move-window -t '%%'"
+tmux bind '(' switch-client -p
+tmux bind ')' switch-client -n
+tmux bind ',' command-prompt -I'#W' "rename-window -- '%%'"
+tmux bind '-' delete-buffer
+tmux bind '.' command-prompt "move-window -t '%%'"
 tmux bind 0 select-window -t:=0
 tmux bind 1 select-window -t:=1
 tmux bind 2 select-window -t:=2
@@ -117,8 +117,8 @@ tmux bind 6 select-window -t:=6
 tmux bind 7 select-window -t:=7
 tmux bind 8 select-window -t:=8
 tmux bind 9 select-window -t:=9
-tmux bind : command-prompt
-tmux bind "\;" last-pane
+tmux bind ':' command-prompt
+tmux bind ';' last-pane
 tmux bind = choose-buffer -Z
 tmux bind ? list-keys
 tmux bind D choose-client -Z
@@ -151,14 +151,14 @@ tmux bind -r Up select-pane -U
 tmux bind -r Down select-pane -D
 tmux bind -r Left select-pane -L
 tmux bind -r Right select-pane -R
-tmux bind M-1 select-layout even-horizontal
-tmux bind M-2 select-layout even-vertical
-tmux bind M-3 select-layout main-horizontal
-tmux bind M-4 select-layout main-vertical
-tmux bind M-5 select-layout tiled
-tmux bind M-n next-window -a
-tmux bind M-o rotate-window -D
-tmux bind M-p previous-window -a
+tmux bind 'M-1' select-layout even-horizontal
+tmux bind 'M-2' select-layout even-vertical
+tmux bind 'M-3' select-layout main-horizontal
+tmux bind 'M-4' select-layout main-vertical
+tmux bind 'M-5' select-layout tiled
+tmux bind 'M-n' next-window -a
+tmux bind 'M-o' rotate-window -D
+tmux bind 'M-p' previous-window -a
 tmux bind -r S-Up refresh-client -U 10
 tmux bind -r S-Down refresh-client -D 10
 tmux bind -r S-Left refresh-client -L 10
@@ -186,24 +186,24 @@ tmux bind '<' display-menu -xW -yS -T "#[align=centre]#{window_index}:#{window_n
 tmux bind -n MouseDown3Pane if -Ft= '#{||:#{mouse_any_flag},#{pane_in_mode}}' 'select-pane -t=; send-keys -M' {display-menu -t= -xM -yM -T "#[align=centre]#{pane_index} (#{pane_id})"  '#{?mouse_word,Search For #[underscore]#{=/9/...:mouse_word},}' 'C-r' {copy-mode -t=; send -Xt= search-backward "#{q:mouse_word}"} '#{?mouse_word,Type #[underscore]#{=/9/...:mouse_word},}' 'C-y' {send-keys -l -- "#{q:mouse_word}"} '#{?mouse_word,Copy #[underscore]#{=/9/...:mouse_word},}' 'c' {set-buffer -- "#{q:mouse_word}"} '#{?mouse_line,Copy Line,}' 'l' {set-buffer -- "#{q:mouse_line}"} '' 'Horizontal Split' 'h' {split-window -h} 'Vertical Split' 'v' {split-window -v} '' 'Swap Up' 'u' {swap-pane -U} 'Swap Down' 'd' {swap-pane -D} '#{?pane_marked_set,,-}Swap Marked' 's' {swap-pane} '' 'Kill' 'X' {kill-pane} 'Respawn' 'R' {respawn-pane -k} '#{?pane_marked,Unmark,Mark}' 'm' {select-pane -m} '#{?window_zoomed_flag,Unzoom,Zoom}' 'z' {resize-pane -Z}}
 tmux bind -n M-MouseDown3Pane display-menu -t= -xM -yM -T "#[align=centre]#{pane_index} (#{pane_id})"  '#{?mouse_word,Search For #[underscore]#{=/9/...:mouse_word},}' 'C-r' {copy-mode -t=; send -Xt= search-backward "#{q:mouse_word}"} '#{?mouse_word,Type #[underscore]#{=/9/...:mouse_word},}' 'C-y' {send-keys -l -- "#{q:mouse_word}"} '#{?mouse_word,Copy #[underscore]#{=/9/...:mouse_word},}' 'c' {set-buffer -- "#{q:mouse_word}"} '#{?mouse_line,Copy Line,}' 'l' {set-buffer -- "#{q:mouse_line}"} '' 'Horizontal Split' 'h' {split-window -h} 'Vertical Split' 'v' {split-window -v} '' 'Swap Up' 'u' {swap-pane -U} 'Swap Down' 'd' {swap-pane -D} '#{?pane_marked_set,,-}Swap Marked' 's' {swap-pane} '' 'Kill' 'X' {kill-pane} 'Respawn' 'R' {respawn-pane -k} '#{?pane_marked,Unmark,Mark}' 'm' {select-pane -m} '#{?window_zoomed_flag,Unzoom,Zoom}' 'z' {resize-pane -Z}
 tmux bind '>' display-menu -xP -yP -T "#[align=centre]#{pane_index} (#{pane_id})"  '#{?mouse_word,Search For #[underscore]#{=/9/...:mouse_word},}' 'C-r' {copy-mode -t=; send -Xt= search-backward "#{q:mouse_word}"} '#{?mouse_word,Type #[underscore]#{=/9/...:mouse_word},}' 'C-y' {send-keys -l -- "#{q:mouse_word}"} '#{?mouse_word,Copy #[underscore]#{=/9/...:mouse_word},}' 'c' {set-buffer -- "#{q:mouse_word}"} '#{?mouse_line,Copy Line,}' 'l' {set-buffer -- "#{q:mouse_line}"} '' 'Horizontal Split' 'h' {split-window -h} 'Vertical Split' 'v' {split-window -v} '' 'Swap Up' 'u' {swap-pane -U} 'Swap Down' 'd' {swap-pane -D} '#{?pane_marked_set,,-}Swap Marked' 's' {swap-pane} '' 'Kill' 'X' {kill-pane} 'Respawn' 'R' {respawn-pane -k} '#{?pane_marked,Unmark,Mark}' 'm' {select-pane -m} '#{?window_zoomed_flag,Unzoom,Zoom}' 'z' {resize-pane -Z}
-tmux bind -Tcopy-mode C-Space send -X begin-selection
-tmux bind -Tcopy-mode C-a send -X start-of-line
-tmux bind -Tcopy-mode C-c send -X cancel
-tmux bind -Tcopy-mode C-e send -X end-of-line
-tmux bind -Tcopy-mode C-f send -X cursor-right
-tmux bind -Tcopy-mode C-b send -X cursor-left
-tmux bind -Tcopy-mode C-g send -X clear-selection
-tmux bind -Tcopy-mode C-k send -X copy-end-of-line
-tmux bind -Tcopy-mode C-n send -X cursor-down
-tmux bind -Tcopy-mode C-p send -X cursor-up
-tmux bind -Tcopy-mode C-r command-prompt -ip'(search up)' -I'#{pane_search_string}' 'send -X search-backward-incremental "%%%"'
-tmux bind -Tcopy-mode C-s command-prompt -ip'(search down)' -I'#{pane_search_string}' 'send -X search-forward-incremental "%%%"'
-tmux bind -Tcopy-mode C-v send -X page-down
-tmux bind -Tcopy-mode C-w send -X copy-selection-and-cancel
+tmux bind -Tcopy-mode 'C-Space' send -X begin-selection
+tmux bind -Tcopy-mode 'C-a' send -X start-of-line
+tmux bind -Tcopy-mode 'C-c' send -X cancel
+tmux bind -Tcopy-mode 'C-e' send -X end-of-line
+tmux bind -Tcopy-mode 'C-f' send -X cursor-right
+tmux bind -Tcopy-mode 'C-b' send -X cursor-left
+tmux bind -Tcopy-mode 'C-g' send -X clear-selection
+tmux bind -Tcopy-mode 'C-k' send -X copy-end-of-line
+tmux bind -Tcopy-mode 'C-n' send -X cursor-down
+tmux bind -Tcopy-mode 'C-p' send -X cursor-up
+tmux bind -Tcopy-mode 'C-r' command-prompt -ip'(search up)' -I'#{pane_search_string}' 'send -X search-backward-incremental "%%%"'
+tmux bind -Tcopy-mode 'C-s' command-prompt -ip'(search down)' -I'#{pane_search_string}' 'send -X search-forward-incremental "%%%"'
+tmux bind -Tcopy-mode 'C-v' send -X page-down
+tmux bind -Tcopy-mode 'C-w' send -X copy-selection-and-cancel
 tmux bind -Tcopy-mode Escape send -X cancel
 tmux bind -Tcopy-mode Space send -X page-down
-tmux bind -Tcopy-mode , send -X jump-reverse
-tmux bind -Tcopy-mode "\;" send -X jump-again
+tmux bind -Tcopy-mode ',' send -X jump-reverse
+tmux bind -Tcopy-mode ';' send -X jump-again
 tmux bind -Tcopy-mode F command-prompt -1p'(jump backward)' 'send -X jump-backward "%%%"'
 tmux bind -Tcopy-mode N send -X search-reverse
 tmux bind -Tcopy-mode R send -X rectangle-toggle
@@ -228,49 +228,49 @@ tmux bind -Tcopy-mode Up send -X cursor-up
 tmux bind -Tcopy-mode Down send -X cursor-down
 tmux bind -Tcopy-mode Left send -X cursor-left
 tmux bind -Tcopy-mode Right send -X cursor-right
-tmux bind -Tcopy-mode M-1 command-prompt -Np'(repeat)' -I1 'send -N "%%%"'
-tmux bind -Tcopy-mode M-2 command-prompt -Np'(repeat)' -I2 'send -N "%%%"'
-tmux bind -Tcopy-mode M-3 command-prompt -Np'(repeat)' -I3 'send -N "%%%"'
-tmux bind -Tcopy-mode M-4 command-prompt -Np'(repeat)' -I4 'send -N "%%%"'
-tmux bind -Tcopy-mode M-5 command-prompt -Np'(repeat)' -I5 'send -N "%%%"'
-tmux bind -Tcopy-mode M-6 command-prompt -Np'(repeat)' -I6 'send -N "%%%"'
-tmux bind -Tcopy-mode M-7 command-prompt -Np'(repeat)' -I7 'send -N "%%%"'
-tmux bind -Tcopy-mode M-8 command-prompt -Np'(repeat)' -I8 'send -N "%%%"'
-tmux bind -Tcopy-mode M-9 command-prompt -Np'(repeat)' -I9 'send -N "%%%"'
+tmux bind -Tcopy-mode 'M-1' command-prompt -Np'(repeat)' -I1 'send -N "%%%"'
+tmux bind -Tcopy-mode 'M-2' command-prompt -Np'(repeat)' -I2 'send -N "%%%"'
+tmux bind -Tcopy-mode 'M-3' command-prompt -Np'(repeat)' -I3 'send -N "%%%"'
+tmux bind -Tcopy-mode 'M-4' command-prompt -Np'(repeat)' -I4 'send -N "%%%"'
+tmux bind -Tcopy-mode 'M-5' command-prompt -Np'(repeat)' -I5 'send -N "%%%"'
+tmux bind -Tcopy-mode 'M-6' command-prompt -Np'(repeat)' -I6 'send -N "%%%"'
+tmux bind -Tcopy-mode 'M-7' command-prompt -Np'(repeat)' -I7 'send -N "%%%"'
+tmux bind -Tcopy-mode 'M-8' command-prompt -Np'(repeat)' -I8 'send -N "%%%"'
+tmux bind -Tcopy-mode 'M-9' command-prompt -Np'(repeat)' -I9 'send -N "%%%"'
 tmux bind -Tcopy-mode 'M-<' send -X history-top
 tmux bind -Tcopy-mode 'M->' send -X history-bottom
-tmux bind -Tcopy-mode M-R send -X top-line
-tmux bind -Tcopy-mode M-b send -X previous-word
-tmux bind -Tcopy-mode C-M-b send -X previous-matching-bracket
-tmux bind -Tcopy-mode M-f send -X next-word-end
-tmux bind -Tcopy-mode C-M-f send -X next-matching-bracket
-tmux bind -Tcopy-mode M-m send -X back-to-indentation
-tmux bind -Tcopy-mode M-r send -X middle-line
-tmux bind -Tcopy-mode M-v send -X page-up
-tmux bind -Tcopy-mode M-w send -X copy-selection-and-cancel
+tmux bind -Tcopy-mode 'M-R' send -X top-line
+tmux bind -Tcopy-mode 'M-b' send -X previous-word
+tmux bind -Tcopy-mode 'C-M-b' send -X previous-matching-bracket
+tmux bind -Tcopy-mode 'M-f' send -X next-word-end
+tmux bind -Tcopy-mode 'C-M-f' send -X next-matching-bracket
+tmux bind -Tcopy-mode 'M-m' send -X back-to-indentation
+tmux bind -Tcopy-mode 'M-r' send -X middle-line
+tmux bind -Tcopy-mode 'M-v' send -X page-up
+tmux bind -Tcopy-mode 'M-w' send -X copy-selection-and-cancel
 tmux bind -Tcopy-mode 'M-{' send -X previous-paragraph
 tmux bind -Tcopy-mode 'M-}' send -X next-paragraph
-tmux bind -Tcopy-mode M-Up send -X halfpage-up
-tmux bind -Tcopy-mode M-Down send -X halfpage-down
-tmux bind -Tcopy-mode C-Up send -X scroll-up
-tmux bind -Tcopy-mode C-Down send -X scroll-down
-tmux bind -Tcopy-mode-vi C-c send -X cancel
-tmux bind -Tcopy-mode-vi C-d send -X halfpage-down
-tmux bind -Tcopy-mode-vi C-e send -X scroll-down
-tmux bind -Tcopy-mode-vi C-b send -X page-up
-tmux bind -Tcopy-mode-vi C-f send -X page-down
-tmux bind -Tcopy-mode-vi C-h send -X cursor-left
-tmux bind -Tcopy-mode-vi C-j send -X copy-selection-and-cancel
+tmux bind -Tcopy-mode 'M-Up' send -X halfpage-up
+tmux bind -Tcopy-mode 'M-Down' send -X halfpage-down
+tmux bind -Tcopy-mode 'C-Up' send -X scroll-up
+tmux bind -Tcopy-mode 'C-Down' send -X scroll-down
+tmux bind -Tcopy-mode-vi 'C-c' send -X cancel
+tmux bind -Tcopy-mode-vi 'C-d' send -X halfpage-down
+tmux bind -Tcopy-mode-vi 'C-e' send -X scroll-down
+tmux bind -Tcopy-mode-vi 'C-b' send -X page-up
+tmux bind -Tcopy-mode-vi 'C-f' send -X page-down
+tmux bind -Tcopy-mode-vi 'C-h' send -X cursor-left
+tmux bind -Tcopy-mode-vi 'C-j' send -X copy-selection-and-cancel
 tmux bind -Tcopy-mode-vi Enter send -X copy-selection-and-cancel
-tmux bind -Tcopy-mode-vi C-u send -X halfpage-up
-tmux bind -Tcopy-mode-vi C-v send -X rectangle-toggle
-tmux bind -Tcopy-mode-vi C-y send -X scroll-up
+tmux bind -Tcopy-mode-vi 'C-u' send -X halfpage-up
+tmux bind -Tcopy-mode-vi 'C-v' send -X rectangle-toggle
+tmux bind -Tcopy-mode-vi 'C-y' send -X scroll-up
 tmux bind -Tcopy-mode-vi Escape send -X clear-selection
 tmux bind -Tcopy-mode-vi Space send -X begin-selection
 tmux bind -Tcopy-mode-vi '$' send -X end-of-line
-tmux bind -Tcopy-mode-vi , send -X jump-reverse
-tmux bind -Tcopy-mode-vi / command-prompt -p'(search down)' 'send -X search-forward "%%%"'
-tmux bind -Tcopy-mode-vi 0 send -X start-of-line
+tmux bind -Tcopy-mode-vi ',' send -X jump-reverse
+tmux bind -Tcopy-mode-vi '/' command-prompt -p'(search down)' 'send -X search-forward "%%%"'
+tmux bind -Tcopy-mode-vi '0' send -X start-of-line
 tmux bind -Tcopy-mode-vi 1 command-prompt -Np'(repeat)' -I1 'send -N "%%%"'
 tmux bind -Tcopy-mode-vi 2 command-prompt -Np'(repeat)' -I2 'send -N "%%%"'
 tmux bind -Tcopy-mode-vi 3 command-prompt -Np'(repeat)' -I3 'send -N "%%%"'
@@ -280,9 +280,9 @@ tmux bind -Tcopy-mode-vi 6 command-prompt -Np'(repeat)' -I6 'send -N "%%%"'
 tmux bind -Tcopy-mode-vi 7 command-prompt -Np'(repeat)' -I7 'send -N "%%%"'
 tmux bind -Tcopy-mode-vi 8 command-prompt -Np'(repeat)' -I8 'send -N "%%%"'
 tmux bind -Tcopy-mode-vi 9 command-prompt -Np'(repeat)' -I9 'send -N "%%%"'
-tmux bind -Tcopy-mode-vi : command-prompt -p'(goto line)' 'send -X goto-line "%%%"'
+tmux bind -Tcopy-mode-vi ':' command-prompt -p'(goto line)' 'send -X goto-line "%%%"'
 tmux bind -Tcopy-mode-vi "\;" send -X jump-again
-tmux bind -Tcopy-mode-vi ? command-prompt -p'(search up)' 'send -X search-backward "%%%"'
+tmux bind -Tcopy-mode-vi '?' command-prompt -p'(search up)' 'send -X search-backward "%%%"'
 tmux bind -Tcopy-mode-vi A send -X append-selection-and-cancel
 tmux bind -Tcopy-mode-vi B send -X previous-space
 tmux bind -Tcopy-mode-vi D send -X copy-end-of-line
@@ -315,7 +315,7 @@ tmux bind -Tcopy-mode-vi v send -X rectangle-toggle
 tmux bind -Tcopy-mode-vi w send -X next-word
 tmux bind -Tcopy-mode-vi '{' send -X previous-paragraph
 tmux bind -Tcopy-mode-vi '}' send -X next-paragraph
-tmux bind -Tcopy-mode-vi % send -X next-matching-bracket
+tmux bind -Tcopy-mode-vi '%' send -X next-matching-bracket
 tmux bind -Tcopy-mode-vi MouseDown1Pane select-pane
 tmux bind -Tcopy-mode-vi MouseDrag1Pane select-pane\; send -X begin-selection
 tmux bind -Tcopy-mode-vi MouseDragEnd1Pane send -X copy-selection-and-cancel
