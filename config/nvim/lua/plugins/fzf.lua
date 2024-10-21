28982a2889f973dc364b1ce18128b26fe8b75a18
Roben Kleene <contact@robenkleene.com>
2024-10-21 17:23:49 +0000

    Clean up fzf

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -4,12 +4,8 @@ return {
     { '<leader>D' },
     { '<leader>S' },
     { '<leader>T' },
-    { '<leader>b' },
     { '<leader>d' },
     { '<leader>f' },
-    { '<leader>g' },
-    { '<leader>o' },
-    { '<leader>q' },
     { '<leader>s' },
     { '<leader>t' },
   },
@@ -19,14 +15,9 @@ return {
     nnoremap <silent> <leader>D :lua require("fzf-lua").diagnostics_workspace()<cr>
     nnoremap <silent> <leader>S :lua require("fzf-lua").lsp_workspace_symbols()<cr>
     nnoremap <silent> <leader>T :lua require("fzf-lua").tags()<cr>
-    nnoremap <silent> <leader>b :lua require("fzf-lua").buffers()<cr>
     nnoremap <silent> <leader>d :lua require("fzf-lua").diagnostics_document()<cr>
     nnoremap <silent> <leader>f :lua require("fzf-lua").files()<cr>
-    nnoremap <silent> <leader>g :lua require("fzf-lua").live_grep()<cr>
-    nnoremap <silent> <leader>o :lua require("fzf-lua").oldfiles()<cr>
-    nnoremap <silent> <leader>q :lua require("fzf-lua").quickfix()<cr>
     nnoremap <silent> <leader>s :lua require("fzf-lua").lsp_document_symbols()<cr>
-    nnoremap <silent> <leader>t :lua require("fzf-lua").btags()<cr>
     nnoremap <silent> <leader>t :lua require("fzf-lua").btags()<cr>
     ]])
   end
--- vim/plugin/plugins/fzf.vim
+++ vim/plugin/plugins/fzf.vim
@@ -1,13 +1,6 @@
 set rtp+=$HOMEBREW_DIR/opt/fzf
 
-nnoremap <silent> <leader>b :Buffers<CR>
-" `:Files` is useful because it doesn't require `fd` to finish executing,
-" like the `Fd:` command, which means it's usually faster
 nnoremap <silent> <leader>f :Files<CR>
-" `:Files` and `:RG` are particularly useful when editing in directories of
-" text files
-nnoremap <silent> <leader>g :RG<CR>
-nnoremap <silent> <leader>o :FzfOldfiles<CR>
 nnoremap <silent> <leader>t :BTags<CR>
 nnoremap <silent> <leader>T :Tags<CR>
 
