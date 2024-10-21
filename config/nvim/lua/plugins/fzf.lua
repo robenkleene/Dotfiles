c7ad80d851fa8122412b21869e39f833afca83c5
Roben Kleene <contact@robenkleene.com>
2024-10-21 22:09:19 +0000

    Disable files

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -5,7 +5,7 @@ return {
     { '<leader>S' },
     { '<leader>T' },
     { '<leader>d' },
-    { '<leader>f' },
+    -- { '<leader>f' },
     { '<leader>s' },
     { '<leader>t' },
   },
@@ -16,7 +16,7 @@ return {
     nnoremap <silent> <leader>S :lua require("fzf-lua").lsp_workspace_symbols()<cr>
     nnoremap <silent> <leader>T :lua require("fzf-lua").tags()<cr>
     nnoremap <silent> <leader>d :lua require("fzf-lua").diagnostics_document()<cr>
-    nnoremap <silent> <leader>f :lua require("fzf-lua").files()<cr>
+    "nnoremap <silent> <leader>f :lua require("fzf-lua").files()<cr>
     nnoremap <silent> <leader>s :lua require("fzf-lua").lsp_document_symbols()<cr>
     nnoremap <silent> <leader>t :lua require("fzf-lua").btags()<cr>
     ]])
--- vim/plugin/plugins/fzf.vim
+++ vim/plugin/plugins/fzf.vim
@@ -1,6 +1,6 @@
 set rtp+=$HOMEBREW_DIR/opt/fzf
 
-nnoremap <silent> <leader>f :Files<CR>
+"nnoremap <silent> <leader>f :Files<CR>
 nnoremap <silent> <leader>t :BTags<CR>
 nnoremap <silent> <leader>T :Tags<CR>
 
