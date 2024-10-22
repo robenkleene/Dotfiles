992a5b5ec22a52b568a44f366b2b5401a505c8c6
Roben Kleene <contact@robenkleene.com>
2024-10-22 15:44:42 +0000

    Clean up

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -5,7 +5,6 @@ return {
     { '<leader>S' },
     { '<leader>T' },
     { '<leader>d' },
-    -- { '<leader>f' },
     { '<leader>s' },
     { '<leader>t' },
   },
@@ -16,7 +15,6 @@ return {
     nnoremap <silent> <leader>S :lua require("fzf-lua").lsp_workspace_symbols()<cr>
     nnoremap <silent> <leader>T :lua require("fzf-lua").tags()<cr>
     nnoremap <silent> <leader>d :lua require("fzf-lua").diagnostics_document()<cr>
-    "nnoremap <silent> <leader>f :lua require("fzf-lua").files()<cr>
     nnoremap <silent> <leader>s :lua require("fzf-lua").lsp_document_symbols()<cr>
     nnoremap <silent> <leader>t :lua require("fzf-lua").btags()<cr>
     ]])
