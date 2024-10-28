d4d7c46e75810d82dd2c51bdaf6ed5c203fc06b8
Roben Kleene <contact@robenkleene.com>
2024-10-28 06:39:47 -0400

    Disable icomplete

---
+++ emacs.d/robenkleene/config/robenkleene-minibuffer.el
@@ -0,0 +1,24 @@
+;;; robenkleene-minibuffer.el --- robenkleene-minibuffer
+;;; Commentary:
+;;; Code:
+
+;; Persist minibuffer history between restarts (packages like `vertico' will use
+;; this to prioritized commonly used commands)
+(savehist-mode)
+
+;; Enable recursive minibuffers so `C-u M-! <command>' can be used when naming
+;; files.
+;; This fixes an issue where using `M-!' (`M-x shell-command') printed an error
+;; when using Terminal Emacs through `emacsclient'
+;; This breaks using `M-n' after `M-!' to run a shell command on the current
+;; buffer
+;; Try disabling recursive minibuffers, in practice it's really confusing
+;; (setq enable-recursive-minibuffers t)
+
+;; (icomplete-vertical-mode)
+
+(provide 'robenkleene-minibuffer)
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;;; robenkleene-minibuffer.el ends here
