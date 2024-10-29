ab0e6402c8cdb648e5ed8448b087f773140b6dd9
Roben Kleene <contact@robenkleene.com>
2024-10-29 16:28:30 +0000

    Update case

--- emacs.d/robenkleene/config/robenkleene-completion.el
+++ emacs.d/robenkleene/config/robenkleene-completion.el
@@ -8,6 +8,11 @@
 ;; Show completion matches on second tab
 (setq completion-auto-select 'second-tab)
 
+;; Ignore case
+(setq completion-ignore-case  t)
+(setq read-file-name-completion-ignore-case t)
+(setq read-buffer-completion-ignore-case t)
+
 ;; In text and programming modes, use prefix completion which works more like
 ;; autocomplete
 (add-hook 'text-mode-hook (lambda ()
