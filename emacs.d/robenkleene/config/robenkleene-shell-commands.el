efa6f2734fcbb164044cf9a0ba3db67ef01a2b87
Roben Kleene <contact@robenkleene.com>
2024-11-04 20:32:55 +0000

    Detect file type

--- emacs.d/robenkleene/config/robenkleene-shell-commands.el
+++ emacs.d/robenkleene/config/robenkleene-shell-commands.el
@@ -2,9 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Set `Shell Command Output' buffer to view only
+;; Set `Shell Command Output' buffer to view only and detect file type
 (define-advice shell-command-on-region (:after (&rest _) rk/shell-command-output)
-  "Enable `view-mode' in `*Shell Command Output*' buffer."
+  "Enable `view-mode' and detect file type in `*Shell Command Output*' buffer."
   (let ((buffer (get-buffer "*Shell Command Output*")))
     (when (buffer-live-p buffer)
       (with-current-buffer buffer
