4b53027ac1adf7d77faf8cb2a6b360ab01210bb3
Roben Kleene <contact@robenkleene.com>
2024-11-04 06:18:27 -0500

    Add Swift support

--- emacs.d/robenkleene/packages/robenkleene-eglot.el
+++ emacs.d/robenkleene/packages/robenkleene-eglot.el
@@ -4,44 +4,43 @@
 
 (use-package eglot
   :hook (((
+	   swift-mode
            c++-mode
            c-mode
+           css-mode
+           html-mode
            js-mode
+           json-mode
            lua-mode
+           python-mode
            ruby-mode
            rust-mode
            sh-mode
            typescript-mode
+
            ;; markdown-mode
-           python-mode
-           json-mode
-           html-mode
-           css-mode
            ) . eglot-ensure)
          )
   :config
   ;; Turn off automatic syntax checking
   ;; (setq eglot-send-changes-idle-time (* 60 60))
 
-  ;; Disable eglot messages
-  (defun eglot--message (format &rest args)
-    "Override to do nothing."
-    nil)
-
   ;; Languages
-  (if (not (assoc '(c++-mode c-mode) eglot-server-programs))
-      (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
-    )
+  (add-to-list 'eglot-server-programs '(css-mode "vscode-css-language-server"))
+  (add-to-list 'eglot-server-programs '(html-mode "vscode-html-language-server"))
   (add-to-list 'eglot-server-programs '(js-mode "typescript-language-server" "--stdio"))
+  (add-to-list 'eglot-server-programs '(json-mode "vscode-json-language-server"))
   (add-to-list 'eglot-server-programs '(lua-mode "lua-language-server"))
-  (add-to-list 'eglot-server-programs '(typescript-mode "typescript-language-server" "--stdio"))
+  (add-to-list 'eglot-server-programs '(python-mode "pyright"))
   (add-to-list 'eglot-server-programs '(ruby-mode "solargraph" "socket" "--port" :autoport))
   (add-to-list 'eglot-server-programs '(rust-mode "rust-analyzer"))
   (add-to-list 'eglot-server-programs '(shell-script-mode "bash-language-server"))
-  (add-to-list 'eglot-server-programs '(python-mode "pyright"))
-  (add-to-list 'eglot-server-programs '(json-mode "vscode-json-language-server"))
-  (add-to-list 'eglot-server-programs '(html-mode "vscode-html-language-server"))
-  (add-to-list 'eglot-server-programs '(css-mode "vscode-css-language-server"))
+  (add-to-list 'eglot-server-programs '(swift-mode "sourcekit-lsp"))
+  (add-to-list 'eglot-server-programs '(typescript-mode "typescript-language-server" "--stdio"))
+  ;; Special
+  (if (not (assoc '(c++-mode c-mode) eglot-server-programs))
+      (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+    )
   ;; (add-to-list 'eglot-server-programs '(markdown-mode "marksman"))
   ;; `settings.json'
   ;; (setq-default eglot-workspace-configuration
