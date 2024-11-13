e3d6229cb91ba229db5812dbb3215bc36af24b1d
Roben Kleene <contact@robenkleene.com>
2024-11-13 19:17:38 +0000

    Add a modified flag

--- install/man/install.sh
+++ install/man/install.sh
@@ -2,14 +2,41 @@
 
 set -euo pipefail
 
+modified="false"
+while getopts ":mh" option; do
+  case "$option" in
+    m)
+      modified="true"
+      ;;
+    h)
+      echo "Usage: command [-hf] [-p <file_path>]"
+      exit 0
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument" >&2
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
 cd "$(dirname "$0")" || exit 1
 
-# Delete all existing
-destination_dir="$HOME/.man/man9"
-if [[ -d "$destination_dir" ]]; then
-  find -L "$destination_dir" -name "*.9" -type f -exec rm {} +
-fi
+if [[ "$modified" == "true" ]]; then
+  while IFS= read -r; do
+    ~/.bin/md_man_update "$REPLY"
+  done < <( git ls-files --modified markdown/*.md )
+else
+  # Delete all existing
+  destination_dir="$HOME/.man/man9"
+  if [[ -d "$destination_dir" ]]; then
+    find -L "$destination_dir" -name "*.9" -type f -exec rm {} +
+  fi
 
-while IFS= read -r; do
-  ~/.bin/md_man_update "$REPLY"
-done < <( find markdown -type f -name "*.md" )
+  while IFS= read -r; do
+    ~/.bin/md_man_update "$REPLY"
+  done < <( find markdown -type f -name "*.md" )
+fi
