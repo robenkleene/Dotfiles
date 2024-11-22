05db4b4aa7918f14ab60e87404e98fdd2d85a568
Roben Kleene <contact@robenkleene.com>
2024-11-22 23:11:12 +0000

    Add iris

--- scripts/aresdev_download_keyboards.sh
+++ scripts/aresdev_download_keyboards.sh
@@ -15,7 +15,12 @@ if [[ "$1" = "voyager" ]]; then
   sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/zsa_voyager_robenkleene_macos.bin .
   exit 0
 fi
+if [[ "$1" = "lily" ]]; then
+  sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/keebio_iris_rev8_robenkleene_macos.elf .
+  exit 0
+fi
 
 sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/ergodox_ez_base_robenkleene_macos2.hex .
 sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/moonlander_robenkleene_macos.bin .
 sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/zsa_voyager_robenkleene_macos.bin .
+sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/keebio_iris_rev8_robenkleene_macos.elf .
