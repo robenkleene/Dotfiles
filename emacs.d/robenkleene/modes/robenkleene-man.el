882a95a255378ad2cbd9771f31ac2152564b866f
Roben Kleene <contact@robenkleene.com>
2024-11-22 12:15:56 -0500

    Use faster man

--- emacs.d/robenkleene/modes/robenkleene-man.el
+++ emacs.d/robenkleene/modes/robenkleene-man.el
@@ -10,11 +10,16 @@
 (defadvice man (around rk/man-interactive activate)
   (interactive (list (let* ((manpath (getenv "MANPATH"))
                             (man-dirs (delete-dups (split-string manpath ":" t)))
-                            (files (mapcan (lambda (dir)
-                                             (when (file-exists-p dir)
-                                               (mapcar #'file-name-base
-                                                       (directory-files-recursively dir ""))))
-                                           man-dirs))
+                            (find-command (concat "find -L "
+                                                  (mapconcat 'identity man-dirs " ")
+                                                  " \\( -type l -o -type f \\) | sed -E 's|^.*/||; s|\\.[^.]*$||' | sort | uniq"))
+                            (files (split-string (shell-command-to-string find-command) "\n" t))
+                            ;; Native lisp is slower
+                            ;; (files (mapcan (lambda (dir)
+                            ;;                  (when (file-exists-p dir)
+                            ;;                    (mapcar #'file-name-base
+                            ;;                            (directory-files-recursively dir ""))))
+                            ;;                man-dirs))
                             )
                        (completing-read "man: " files rk/man-history t)
                        )))
