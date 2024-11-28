786fc6698d558c363f3b146c8a81932e9f88f17a
Roben Kleene <contact@robenkleene.com>
2024-11-28 19:59:24 +0000

    Disable save in remember

--- emacs.d/robenkleene/modes/robenkleene-remember.el
+++ emacs.d/robenkleene/modes/robenkleene-remember.el
@@ -4,6 +4,12 @@
 
 ;; Don't add the current buffer file name to the file
 (setq remember-annotation-functions nil)
+(with-eval-after-load 'remember
+  (define-key remember-mode-map (kbd "C-x C-s")
+              (lambda ()
+                (interactive)
+                (message "Use `C-c C-c' to finalize or `C-c C-k' to cancel")))
+  )
 
 (provide 'robenkleene-remember)
 ;; Local Variables:
