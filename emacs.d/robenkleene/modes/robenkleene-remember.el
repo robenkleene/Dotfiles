bac4fbbcb49e23505d3c118bd2e705d5a8b29635
Roben Kleene <contact@robenkleene.com>
2024-12-02 23:07:35 +0000

    Use syntax table for remember

--- emacs.d/robenkleene/modes/robenkleene-remember.el
+++ emacs.d/robenkleene/modes/robenkleene-remember.el
@@ -11,6 +11,18 @@
                 (message "Use `C-c C-c' to finalize or `C-c C-k' to cancel")))
   )
 
+(add-hook 'remember-mode-hook
+          (lambda ()
+            ;; Set backtick as a string delimiter, this makes the `sexp'
+            ;; commands (like `mark-sexp') work with backticks
+            (modify-syntax-entry ?` "\"")
+            ;; Set `"' as a string delimiter
+            (modify-syntax-entry ?\" "\"")
+            ;; Set `*' as a string delimiter
+            (modify-syntax-entry ?* "\"")
+            )
+          )
+
 (provide 'robenkleene-remember)
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
