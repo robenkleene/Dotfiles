5b8e9a4ca2086261a9ea38df95c65ff6f7ef3c70
Roben Kleene <contact@robenkleene.com>
2024-12-10 21:30:00 +0000

    Add shell command dir

--- emacs.d/robenkleene/config/robenkleene-shell-commands.el
+++ emacs.d/robenkleene/config/robenkleene-shell-commands.el
@@ -2,6 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
+;; Show current directory when running shell command
+(setq shell-command-prompt-show-cwd t)
+
 ;; Set `Shell Command Output' buffer to view only and detect file type
 (define-advice shell-command-on-region (:after (&rest _) rk/shell-command-output)
   "Enable `view-mode' and detect file type in `*Shell Command Output*' buffer."
