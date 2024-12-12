ce015368bcc5f31cb34b6997c77fa7eee8accdae
Roben Kleene <contact@robenkleene.com>
2024-12-11 20:11:06 -0500

    Add comment

--- emacs.d/robenkleene/packages/robenkleene-orderless.el
+++ emacs.d/robenkleene/packages/robenkleene-orderless.el
@@ -5,6 +5,8 @@
 (use-package orderless
   :custom
   (completion-styles '(orderless basic))
+  ;; `TRAMP' hostname complete needs `basic'
+  ;; `partial-completion' is good for `/u/s/l' for `/usr/share/local'
   (completion-category-overrides '((file (styles basic partial-completion)))))
 
 (provide 'robenkleene-orderless)
