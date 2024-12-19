8ed928781a7e18d9d39e53c21acbd11eafe3904b
Roben Kleene <contact@robenkleene.com>
2024-12-19 23:13:29 +0000

    Update title script

--- scripts/f_fr_filename_to_title.sh
+++ scripts/f_fr_filename_to_title.sh
@@ -7,8 +7,12 @@ while read -r filename; do
   filename_no_ext=${filename%.*}
   if [[ $filename_no_ext =~ ^[a-z0-9_-]*$ ]]; then
     # If the filename contains only lowercase letters, hyphens, and underscores then assume
-    # convert it to title case with spaces.
-    title=$(echo "$filename_no_ext" | tr "-" " " | tr "_" " " | ~/.bin/f_titlecase)
+    # convert it to title case with hyphens and underscores replaced with spaces.
+    # For hyphens, only convert if both sides aren't numbers, e.g., so `2024-12-19` gets preserved
+    # 's/([0-9]+)-([^0-9]+)/\1 \2/g' replace digits on one side with spaces (e.g., replace the hyphen in `123-abc`)
+    # 's/([^0-9]+)-([0-9]+)/\1 \2/g' replace digits on the other side with spaces (e.g., replace the hyphen in `abc-123`)
+    # 's/([^0-9]+)-([^0-9]+)/\1 \2/g' replace digits on the neither side with spaces (e.g., replace the hyphen in `abc-abc`)
+    title=$(echo "$filename_no_ext" | sed -E 's/([0-9]+)-([^0-9]+)/\1 \2/g' | sed -E 's/([^0-9]+)-([0-9]+)/\1 \2/g' | sed -E 's/([^0-9]+)-([^0-9]+)/\1 \2/g' | tr "_" " " | ~/.bin/f_titlecase)
   else
     title=$(echo "$filename_no_ext" | ~/.bin/f_titlecase)
   fi
