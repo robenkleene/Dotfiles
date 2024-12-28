36108b94fd5e4b08f9b9fe7bea8b27c0edca1773
Roben Kleene <contact@robenkleene.com>
2024-12-28 07:04:52 -0500

    Clean up whitespace

--- scripts/t_diff_path.sh
+++ scripts/t_diff_path.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 if [ $# -gt 0 ]; then
   input=$(</dev/stdin)
   for line in "$@"; do
-    echo "$input" | ~/.bin/t_diff_grep "$line" | cut -d ':' -f 1 
+    echo "$input" | ~/.bin/t_diff_grep "$line" | cut -d ':' -f 1
   done
 else
-  ~/.bin/t_diff_grep | cut -d ':' -f 1 
+  ~/.bin/t_diff_grep | cut -d ':' -f 1
 fi
