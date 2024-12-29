dcce2572384681d79f356e9cc87799e2342141f7
Roben Kleene <contact@robenkleene.com>
2024-12-29 08:15:59 -0500

    Revert buffers and oldfiles

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -3,8 +3,6 @@ return {
   keys = {
     { '<leader>S' },
     { '<leader>T' },
-    { '<leader>b' },
-    { '<leader>o' },
     { '<leader>s' },
     { '<leader>t' },
   },
@@ -13,8 +11,6 @@ return {
     vim.cmd([[
     nnoremap <silent> <leader>S :lua require("fzf-lua").lsp_workspace_symbols()<cr>
     nnoremap <silent> <leader>T :lua require("fzf-lua").tags()<cr>
-    nnoremap <silent> <leader>b :lua require("fzf-lua").buffers()<cr>
-    nnoremap <silent> <leader>o :lua require("fzf-lua").oldfiles()<cr>
     nnoremap <silent> <leader>s :lua require("fzf-lua").lsp_document_symbols()<cr>
     nnoremap <silent> <leader>t :lua require("fzf-lua").btags()<cr>
     ]])
