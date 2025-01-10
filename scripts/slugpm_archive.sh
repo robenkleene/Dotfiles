c17d0186beb42d6d3546e451bb8ed61741da78d7
Roben Kleene <contact@robenkleene.com>
2025-01-10 18:23:40 +0000

    Handle single files to archive better

--- scripts/slugpm_archive.sh
+++ scripts/slugpm_archive.sh
@@ -21,8 +21,8 @@ for file_path in "$@"; do
   # file_path=${file_path#\.}
   file_path=${file_path%/}
 
-  if [[ -d "$file_path/../projects/" && ( -f "$file_path/README.md" ||  -f "$file_path/README.org" ) ]]; then
-    # With a parent `projects` and a `README` treat as a project
+  if [[ -d "$file_path" && -d "$file_path/../projects/" && ( -f "$file_path/README.md" ||  -f "$file_path/README.org" ) ]]; then
+    # If it's a directory with a parent `projects` and a `README` treat as a project
     destination_dir="$file_path/../../archive/projects/"
 
     if [[ ! -d "$destination_dir" ]]; then
@@ -35,13 +35,14 @@ for file_path in "$@"; do
     src_dir=$(cd "$file_path" ; pwd)
     mv "$src_dir" "$destination_dir"
   else
+    # If it's not a project, treat it as a single file
     if [[ -d "$file_path" ]]; then
       # Convert an absolute path which helps in the case where current
       # directory is just `.`
       file_path=$(cd "$file_path" ; pwd)
-      destination_dir="$file_path/../../archive/projects/"
+      destination_dir="$file_path/../../archive/"
     else
-      destination_dir="$(dirname $file_path)/archive/projects/"
+      destination_dir="$(dirname $file_path)/archive/"
     fi
     if [[ ! -d "$destination_dir" ]]; then
       echo "$destination_dir does not exist" >&2
