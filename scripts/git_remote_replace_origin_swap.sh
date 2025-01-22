c620a0149f12775a35ab597b40a08d30f544f360
Roben Kleene <contact@robenkleene.com>
2025-01-22 17:03:19 +0000

    Rename scripts

--- scripts/git_remote_replace_origin_swap.sh
+++ scripts/git_remote_replace_origin_swap.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-~/.bin/git_remote_replace_origin $(git ls-remote --get-url | ~/.bin/t_url_git)
+~/.bin/git_remote_replace_origin $(git ls-remote --get-url | ~/.bin/t_url_url_git)
