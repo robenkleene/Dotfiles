04146a3e25543699148cf4668aa147cd7d51a403
Roben Kleene <contact@robenkleene.com>
2025-01-28 18:02:18 +0000

    Add gx plugin

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -15,4 +15,14 @@ return {
     'vim-utils/vim-man',
     cmd = { 'Man' },
   },
+  {
+    "chrishrb/gx.nvim",
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    cmd = { "Browse" },
+    init = function ()
+      vim.g.netrw_nogx = 1 -- disable netrw gx
+    end,
+    config = true,
+    submodules = false
+  },
 }
