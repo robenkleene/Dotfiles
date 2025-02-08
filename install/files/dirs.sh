620fc12134bbb6becb1e59a7fbb90c08056d4e99
Roben Kleene <contact@robenkleene.com>
2025-02-08 13:08:59 -0500

    Remove org

--- install/files/dirs.sh
+++ install/files/dirs.sh
@@ -13,26 +13,10 @@ if [[ -e "$HOME/../Shared/Max 9/" ]]; then
 fi
 
 # Directories
-
 developer_path="${HOME}/Developer"
 if [[ ! -e "$developer_path" ]]; then
   mkdir -p "$developer_path"
 elif [[ ! -d "$developer_path" ]]; then
   echo "Error: A file exists at $developer_path and it's not a directory" >&2
-  exit 1
-fi
-
-org_src_path="${HOME}/Documents/Text/org/"
-org_dst_path="${HOME}/org"
-if [[ ! -L "$org_dst_path" ]]; then
-  if [[ ! -e "$org_src_path" ]]; then
-    mkdir -p "$org_src_path"
-  elif [[ ! -d "$org_src_path" ]]; then
-    echo "Error: A file exists at $org_src_path and it's not a directory" >&2
-    exit 1
-  fi
-  ln -s "$org_src_path" "$org_dst_path"
-elif [[ -e "$org_dst_path" ]]; then
-  echo "Error: A file exists at $org_dst_path is not a symlink" >&2
   exit 1
 fi
