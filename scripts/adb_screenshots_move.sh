5b7c8df83de543d9b89816fc7f7430c2a73e58dd
Roben Kleene <contact@robenkleene.com>
2025-02-13 23:19:56 +0000

    Add PanelShots

--- scripts/adb_screenshots_move.sh
+++ scripts/adb_screenshots_move.sh
@@ -5,3 +5,4 @@ set -euo pipefail
 # The `-f` option suppresses the error if there are no files to delete
 adb shell 'cd /sdcard/Oculus/Screenshots/ || exit 0; ls | sed s#^#$PWD/#' | tr -s "\r\n" "\0" | xargs -0 -n1 adb pull && adb shell 'rm -f /sdcard/Oculus/Screenshots/*.{jpg,png}' || true
 adb shell 'cd /sdcard/Oculus/VideoShots/ || exit 0; ls | sed s#^#$PWD/#' | tr -s "\r\n" "\0" | xargs -0 -n1 adb pull && adb shell 'rm -f /sdcard/Oculus/VideoShots/*.mp4' || true
+adb shell 'cd /sdcard/Oculus/PanelShots/ || exit 0; ls | sed s#^#$PWD/#' | tr -s "\r\n" "\0" | xargs -0 -n1 adb pull && adb shell 'rm -f /sdcard/Oculus/PanelShorts/*.{jpg,png}' || true
