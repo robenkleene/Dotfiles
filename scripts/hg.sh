ee898ca96df0105f8dc6727484ac8530f6919bde
Roben Kleene <contact@robenkleene.com>
2025-03-03 21:53:34 +0000

    Remove script

--- scripts/hg.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-exec ~/.brew/bin/hg --config pager.pager='delta --syntax-theme=none --hyperlinks --hyperlinks-file-link-format=" vscode://file/{path}:{line}"' "$@"
