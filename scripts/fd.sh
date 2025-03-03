a37c5a69f4f3cca1e0396738e535d2df577bf430
Roben Kleene <contact@robenkleene.com>
2025-03-03 21:30:21 +0000

    Fix colors

--- scripts/fd.sh
+++ scripts/fd.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 # Force fd to use default LS colors
 # The default colors are hard to read on light backgrounds
-exec LS_COLORS='' ~/.brew/bin/fd "$@"
+LS_COLORS='' exec ~/.brew/bin/fd "$@"
