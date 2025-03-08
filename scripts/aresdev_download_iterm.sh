58f2980850e725b1f0812031613f2c7f4dc18643
Roben Kleene <contact@robenkleene.com>
2025-03-08 16:48:04 -0500

    Add download iTerm

---
+++ scripts/aresdev_download_iterm.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+ssh aresdev 'cd ~/Developer/Settings/Apps && tar -czvf iTerm.tar.gz iTerm'
+sftp aresdev:~/Developer/Settings/Apps/iTerm.tar.gz .
+ssh aresdev 'rm ~/Developer/Settings/Apps/iTerm.tar.gz'
