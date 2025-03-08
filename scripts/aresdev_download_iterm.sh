4bed9c69c988720ce78863fa240ad623b8a0e5c6
Roben Kleene <contact@robenkleene.com>
2025-03-08 16:53:16 -0500

    Fix iTerm script

--- scripts/aresdev_download_iterm.sh
+++ scripts/aresdev_download_iterm.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 ssh aresdev 'cd ~/Developer/Settings/Apps && tar -czvf iTerm.tar.gz iTerm'
-sftp aresdev:~/Developer/Settings/Apps/iTerm.tar.gz .
+sftp aresdev:Developer/Settings/Apps/iTerm.tar.gz .
 ssh aresdev 'rm ~/Developer/Settings/Apps/iTerm.tar.gz'
