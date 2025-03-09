feb6ace3c4a70898d501591c45b3deaf9d008b5b
Roben Kleene <contact@robenkleene.com>
2025-03-09 16:19:51 -0400

    Update man sync

--- install/code/man_sync_snippets.sh
+++ install/code/man_sync_snippets.sh
@@ -25,6 +25,22 @@ done
 
 cd "$(dirname "$0")" || exit 1
 
+make_snippets() {
+  src="$1"
+  lang=$(basename "${src%.*}")
+  if [[ "$lang" = "javascript" ]]; then
+    lang="js"
+  elif [[ "$lang" = "shellscript" ]]; then
+    lang="bash"
+  elif [[ "$lang" = "objective-c" ]]; then
+    lang="objc"
+  fi
+  if [[ "$lang" != "package" && "$lang" != "global" ]]; then
+    dst="$dst_dir/rk_lang_${lang}_snippets.md"
+    ./snippet_to_md.sh "$src" "$dst"
+  fi
+}
+
 dst_dir="../man/markdown/snippets"
 src_dir="$HOME/.config/Code/User/snippets"
 if [[ ! -e "$dst_dir" ]]; then
@@ -36,18 +52,7 @@ if [[ "$modified" == "true" ]]; then
     # `git ls-files --modified` actually includes deleted files, so skip those
     if [[ -e "$REPLY" ]]; then
       src="$REPLY"
-      lang=$(basename "${src%.*}")
-      if [[ "$lang" = "javascript" ]]; then
-        lang="js"
-      elif [[ "$lang" = "shellscript" ]]; then
-        lang="bash"
-      elif [[ "$lang" = "objective-c" ]]; then
-        lang="objc"
-      fi
-      if [[ "$lang" != "package" && "$lang" != "global" ]]; then
-        dst="$dst_dir/rk_lang_${lang}_snippets.md"
-        ./snippet_to_md.sh "$src" "$dst"
-      fi
+      make_snippets "$src"
     fi
   done < <( git ls-files --modified --others '$src_dir/*.json' )
 else
@@ -60,10 +65,6 @@ else
 
   while IFS= read -r; do
     src="$REPLY"
-    lang=$(basename "${src%.*}")
-    if [[ "$lang" != "package" && "$lang" != "global" ]]; then
-      dst="$dst_dir/rk_lang_${lang}_snippets.md"
-      ./snippet_to_md.sh "$src" "$dst"
-    fi
+    make_snippets "$src"
   done < <( find "$src_dir" -type f -name "*.json" )
 fi
