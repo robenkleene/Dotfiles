87bae3c064fabad3cf6fba0ffe7d2c51f00cda43
Roben Kleene <contact@robenkleene.com>
2025-03-28 14:48:36 +0000

    Remove host

--- scripts/tmux_set_ssh_status.sh
+++ scripts/tmux_set_ssh_status.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 
 if [ -n "$SSH_CONNECTION" ]; then
   tmux set -g status-style bg=yellow,fg=black
-  tmux set -g status-right '#{?client_prefix,-- PREFIX --,} #H'
+  # The host (`#H`) is usually visible from the shell prompt (or front TUI
+  # program) so this usually duplicates visible information
+  # tmux set -g status-right '#{?client_prefix,-- PREFIX --,} #H'
 fi
