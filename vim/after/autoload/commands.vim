99aa5505e3256f84a80764002dac6020eac7d9a3
Roben Kleene <contact@robenkleene.com>
2025-03-28 19:46:00 +0000

    Remove dc

--- vim/after/autoload/commands.vim
+++ vim/after/autoload/commands.vim
@@ -19,13 +19,3 @@ function! commands#Zcmd(cd, terms) abort
   execute a:cd." ".l:result
   pwd
 endfunction
-
-function! commands#Dc(term) abort
-  let l:term = a:term
-  if l:term =~ '\D' && l:term < 0
-    let l:term -= 1
-  endif
-  let l:result = histget(':', l:term)
-  call histdel(':', l:term)
-  echo "Deleted '".l:result."' from history."
-endfunction
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -41,38 +41,3 @@ ssh_start() {
 ssh_stop() {
   ssh-agent -k
 }
-
-# Accepts one history line number as argument.
-# `dc` with no arguments deletes the most recent line
-# Use `dc -1` to remove the last line.
-# The naming convention `dc` is probably based on `fc` which is the underlying
-# history management program for Bash and Zsh. `fc` stands for "fix command"
-# (because with history you retrieve a command and can then fix it). So `dc`
-# probably stands for "delete command".
-dc () {
-  # Default to the most recent line
-  line="${@:--1}"
-  # If the line starts with `-` treat is a most recent history
-  # Otherwise remove the line itself
-  if [ "${line[1]}" = '-' ]; then
-    line="${(b)$(fc -ln $line $line)}"
-  fi
-  # Prevent the specified history line from being
-  # saved.
-  local HISTORY_IGNORE="${(b)line}"
-
-  # Write out the history to file, excluding lines that
-  # match `$HISTORY_IGNORE`.
-  fc -W
-
-  # Dispose of the current history and read the new
-  # history from file.
-  fc -p $HISTFILE $HISTSIZE $SAVEHIST
-
-  # TA-DA!
-  print "Deleted '$HISTORY_IGNORE' from history."
-}
-# Never add `dc` commands to history
-zshaddhistory() {
- [[ $1 != 'dc '* ]]
-}
