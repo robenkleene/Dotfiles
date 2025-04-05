e853be5c858a62832163cd6b13660135ed346f0c
Roben Kleene <contact@robenkleene.com>
2025-04-05 07:21:15 -0400

    Reformat

--- emacs.d/robenkleene/modes/robenkleene-man.el
+++ emacs.d/robenkleene/modes/robenkleene-man.el
@@ -9,27 +9,31 @@
 (defvar rk/man-history nil "History list for `man'.")
 (defvar rk/man-cache nil "Cache for `man' completion file list.")
 (defadvice man (around rk/man-interactive activate)
-  (interactive (list (let* ((manpath (getenv "MANPATH"))
-                            (man-dirs (delete-dups (split-string manpath ":" t)))
-                            (find-command (concat "find -L "
-                                                  (mapconcat 'identity man-dirs " ")
-                                                  " \\( -type l -o -type f \\) | sed -E 's|^.*/||; s|\\.[^.]*$||' | sort | uniq"))
-                            (files
-                             (or rk/man-cache
-                                 (setq rk/man-cache
-                                       (split-string (shell-command-to-string find-command) "\n" t)
-                                       )
-                                 )
-                             )
-                            ;; Native lisp is slower
-                            ;; (files (mapcan (lambda (dir)
-                            ;;                  (when (file-exists-p dir)
-                            ;;                    (mapcar #'file-name-base
-                            ;;                            (directory-files-recursively dir ""))))
-                            ;;                man-dirs))
-                            )
-                       (completing-read "man: " files rk/man-history t)
-                       )))
+  (interactive
+   (list
+    (let*
+        ((manpath (getenv "MANPATH"))
+         (man-dirs (delete-dups (split-string manpath ":" t)))
+         (find-command
+          (concat "find -L "
+                  (mapconcat 'identity man-dirs " ")
+                  " \\( -type l -o -type f \\) | sed -E 's|^.*/||; s|\\.[^.]*$||' | sort | uniq"))
+         (files
+          (or rk/man-cache
+              (setq rk/man-cache
+                    (split-string (shell-command-to-string find-command) "\n" t)
+                    )
+              )
+          )
+         ;; Native lisp is slower
+         ;; (files (mapcan (lambda (dir)
+         ;;                  (when (file-exists-p dir)
+         ;;                    (mapcar #'file-name-base
+         ;;                            (directory-files-recursively dir ""))))
+         ;;                man-dirs))
+         )
+      (completing-read "man: " files rk/man-history t)
+      )))
   ad-do-it)
 
 (provide 'robenkleene-man)
