cbb48480a3ec03a87072238ba69d6aa7ce42a3f5
Roben Kleene <contact@robenkleene.com>
2025-04-07 17:29:50 +0000

    Update theme

--- emacs.d/robenkleene/config/robenkleene-config.el
+++ emacs.d/robenkleene/config/robenkleene-config.el
@@ -173,10 +173,10 @@
 ;; Turn on built-in editorconfig
 (editorconfig-mode 1)
 
-;; (if window-system
-;;     (load-theme 'modus-vivendi :no-confirm-loading)
-;;   )
-(load-theme 'modus-vivendi :no-confirm-loading)
+(if window-system
+    (load-theme 'modus-vivendi :no-confirm-loading)
+  )
+; (load-theme 'modus-vivendi :no-confirm-loading)
 
 (provide 'robenkleene-config)
 ;; Local Variables:
