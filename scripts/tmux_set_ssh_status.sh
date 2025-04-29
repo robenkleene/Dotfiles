17fc0894cc4fc58e341866fdc398a451db99a34c
Roben Kleene <contact@robenkleene.com>
2025-04-29 13:16:59 +0000

    Set message style

--- scripts/tmux_set_ssh_status.sh
+++ scripts/tmux_set_ssh_status.sh
@@ -3,7 +3,12 @@
 set -euo pipefail
 
 if [ -n "$SSH_CONNECTION" ]; then
+  # Green is default `status-style`
+  # Yellow is default `message-style`
+  # Invert those for `SSH`
   tmux set -g status-style bg=yellow,fg=black
+  tmux set -g message-style bg=green,fg=black
+
   # The host (`#H`) is usually visible from the shell prompt (or front TUI
   # program) so this usually duplicates visible information
   # tmux set -g status-right '#{?client_prefix,-- PREFIX --,} #H'
