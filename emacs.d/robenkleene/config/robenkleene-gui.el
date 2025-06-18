935c40240fb7bc57fee9917f8eb3e833a3f1d54a
Roben Kleene <contact@robenkleene.com>
2025-06-17 20:32:11 -0400

    Update Emacs

--- emacs.d/robenkleene/config/robenkleene-gui.el
+++ emacs.d/robenkleene/config/robenkleene-gui.el
@@ -41,12 +41,15 @@
 ;; Make `rg' called from within Emacs use `ripgreprc'
 (setenv "RIPGREP_CONFIG_PATH" (expand-file-name "~/.ripgreprc"))
 
-(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font-13:Regular")
+;; (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font-13:Regular")
 ;; (set-face-attribute 'default nil :font "Menlo-13:Regular")
+(set-face-attribute 'default nil :font "SF Mono-13:Regular")
 
 ;; Not pure black background looks a bit more natural
-;; BBEdit black
-(set-face-background 'default "#141414")
+;; BBEdit Black
+;; (set-face-background 'default "#141414")
+;; Xcode Black
+(set-face-background 'default "#292A30")
 
 ;; Save window state
 ;; Don't restore window state because it makes it hard to enforce that Emacs
