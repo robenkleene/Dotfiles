7149682aa91c10b798a0a32c8c5c5ad224cd4d3f
Roben Kleene <contact@robenkleene.com>
2025-06-23 17:34:20 +0000

    Prefer consult

--- emacs.d/robenkleene/packages/robenkleene-vertico.el
+++ emacs.d/robenkleene/packages/robenkleene-vertico.el
@@ -9,10 +9,11 @@
 
 (use-package consult
   :bind (
-         ("M-g M-i" . consult-imenu)
-         ("M-s M-g" . consult-ripgrep)
+         ("M-g i" . consult-imenu)
          ("M-s M-f" . consult-fd)
-         ("M-s M-o" . consult-line)
+         ("C-x b" . consult-buffer)
+         ("M-s M-g" . consult-ripgrep)
+         ("M-s o" . consult-line)
          )
   :init
   ;; Time to wait before updating results, the default is `0.2'
