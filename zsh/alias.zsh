5ba0116c0c13afdef1398b526d2f2f11580eeec1
Roben Kleene <contact@robenkleene.com>
2025-06-24 20:06:02 +0000

    Archive egit

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -11,12 +11,3 @@ fi
 
 # Use `nvim` for clipboard provider
 alias vim='nvim'
-
-# egit
-# These are aliases instead of shell functions because they set the `ssh`
-# status of the current shell
-alias rk_push='ssh_start && { egit -p || egitn } && ~/.bin/sgitt_auto'
-alias rk_pull='ssh_start && ~/.bin/egit_update'
-alias rk_pull_text='ssh_start && ssh_git_pull_all -t'
-alias rk_pull_all='ssh_start && ssh_git_pull_all'
-alias rk_pull_settings='ssh_start && ~/.bin/egit_update -s'
