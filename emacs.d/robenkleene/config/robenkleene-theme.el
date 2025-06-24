3798a2415bb2a6d5ea1edc8627ab5f7503b510e0
Roben Kleene <contact@robenkleene.com>
2025-06-24 19:49:45 +0000

    Use black background

--- emacs.d/robenkleene/config/robenkleene-theme.el
+++ emacs.d/robenkleene/config/robenkleene-theme.el
@@ -13,15 +13,18 @@
 ;;   )
 
 (load-theme 'modus-vivendi :no-confirm-loading)
-(if window-system
-    ;; Not pure black background looks a bit more natural
-    ;; BBEdit Black
-    ;; (set-face-background 'default "#141414")
-    ;; Xcode Black
-    (set-face-background 'default "#292A30")
-  ;; Kleene Purple
-  ;; (set-face-background 'default "#211930")
-  )
+
+;; Custom Background Color
+;; `git diff' in `term-mode' from `eshell-visual' expects a pure black background
+;; (if window-system
+;;     ;; Not pure black background looks a bit more natural
+;;     ;; BBEdit Black
+;;     ;; (set-face-background 'default "#141414")
+;;     ;; Xcode Black
+;;     (set-face-background 'default "#292A30")
+;;   ;; Kleene Purple
+;;   ;; (set-face-background 'default "#211930")
+;;   )
 
 (provide 'robenkleene-theme)
 ;; Local Variables:
