c7c527b92a46346cb0502f46fb9501c9196f044a
Roben Kleene <contact@robenkleene.com>
2025-07-24 20:34:05 +0000

    Consolas for Alacritty, Berkeley Mono for Emacs

--- emacs.d/robenkleene/config/robenkleene-gui.el
+++ emacs.d/robenkleene/config/robenkleene-gui.el
@@ -45,6 +45,8 @@
 ;; (set-face-attribute 'default nil :font "Menlo-13:Regular")
 ;; (set-face-attribute 'default nil :font "SF Mono-13:Regular")
 ;; (set-face-attribute 'default nil :font "SF Mono-13:Medium")
+;; Consolas is shifted slightly-upwards in Emacs, this is a problem originating
+;; in the font file itself
 ;; (set-face-attribute 'default nil :font "Consolas-14:Regular")
 (set-face-attribute 'default nil :font "Berkeley Mono-14:Regular")
 
