b0d15fa32dff601ae7408c95a4f5de7745799334
Roben Kleene <contact@robenkleene.com>
2025-08-25 17:45:00 +0000

    Exclude repos

--- install/repos/documentation/setup.sh
+++ install/repos/documentation/setup.sh
@@ -31,7 +31,7 @@ mkdir -p ~/Documentation/
 
 cd "$(dirname "$0")" || exit 1
 if [[ "$use_https" == "true" ]]; then
-  ~/.bin/git_sync_clone -p ~/Documentation/ -s <(~/.bin/t_url_url_git -H < documentation.txt) -f
+  ~/.bin/git_sync_clone -p ~/Documentation/ -s <(~/.bin/t_url_url_git -H < documentation.txt | grep -v -E '(tutorial|game)\-references') -f
 else
   ~/.bin/git_sync_clone -p ~/Documentation/ -s documentation.txt -f
 fi
