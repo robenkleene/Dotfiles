03ed1662a386027f14d91c6ca158afa3fde8b80b
Roben Kleene <contact@robenkleene.com>
2025-08-25 16:56:37 +0000

    Use absolute path

--- install/rust/crates/install.sh
+++ install/rust/crates/install.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-cargo install rep-grep
-cargo install ren-find
+# On intial install, cargo won't be in the `$PATH` yet so use an absolute path
+
+~/.cargo/bin/cargo install rep-grep
+~/.cargo/bin/cargo install ren-find
 # Helper for generating cargo files
 # If this is failing just comment it out
 # cargo install cargo-generate
