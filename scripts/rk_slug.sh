7c2da182c134482375b4afcabafca4009a897bb2
Roben Kleene <contact@robenkleene.com>
2025-09-19 21:06:45 +0000

    Work on slug

--- scripts/rk_slug.sh
+++ scripts/rk_slug.sh
@@ -2,8 +2,18 @@
 
 set -euo pipefail
 
-if [[ $# -gt 0]]; then
-  echo "$@" | ~/.bin/f_slug
+
+
+if [[ $# -gt 1 ]]; then
+  for arg in "$@"; do
+    if [[ "$arg" == "-d" ]]; then
+      today=$(~/.bin/rk_today)
+      args+="$today "
+    else
+      args+="$arg "
+    fi
+  done
+  echo "$args" | ~/.bin/f_slug
 else
   ~/.bin/f_slug
 fi
