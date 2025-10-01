18518263b6238637dc9695479e077faacbb4783b
Roben Kleene <contact@robenkleene.com>
2025-10-01 22:52:47 +0000

    Clean up

--- emacs.d/robenkleene/modes/robenkleene-dired.el
+++ emacs.d/robenkleene/modes/robenkleene-dired.el
@@ -8,7 +8,9 @@
   (require 'dired-x)
 
   ;; This is `dired-find-file-other-window' by default
-  (define-key dired-mode-map (kbd "<mouse-2>") 'dired-find-file)
+  ;; This is inconsistent with other modes, e.g., `org-mode' follows links in
+  ;; the other window
+  ;; (define-key dired-mode-map (kbd "<mouse-2>") 'dired-find-file)
 
   ;; Don't ask `Kill buffer of' when deleting a buffer, just kill it
   (setq dired-clean-confirm-killing-deleted-buffers nil)
