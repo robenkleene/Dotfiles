355fda4c36ca1a7ca5df73768e57e3e505fa9aff
Roben Kleene <contact@robenkleene.com>
2025-12-15 15:18:47 +0000

    Update range script

--- scripts/date_range.sh
+++ scripts/date_range.sh
@@ -4,40 +4,40 @@ usage () {
   echo "Usage: daterange [-f forward modifier] [-b backward modifier] [-c length of range]"
 }
 
-DATECOUNT=0
-FORWARDMODIFIER=0
-BACKWARDMODIFIER=0
+date_count=0
+mod_forward=0
+mod_backward=0
 while getopts f:b:c:h option
 do
-	case "$option"
-	in
-	    c)  DATECOUNT=$OPTARG
-			;;
-	    f)  FORWARDMODIFIER=$OPTARG
-			;;
-	    b)  BACKWARDMODIFIER=$OPTARG
-			;;
-	    h)  usage
-	        exit 0 
-	        ;;
-	    :)  usage # Error for missing value after arguement
-	        exit 1
-	        ;;
-	    \?) usage
-	        exit 1
-	        ;;
-	esac
+  case "$option"
+  in
+      c)  date_count=$OPTARG
+      ;;
+      f)  mod_forward=$OPTARG
+      ;;
+      b)  mod_backward=$OPTARG
+      ;;
+      h)  usage
+          exit 0 
+          ;;
+      :)  usage # Error for missing value after arguement
+          exit 1
+          ;;
+      \?) usage
+          exit 1
+          ;;
+  esac
 done
 
-if [[ $DATECOUNT -lt 1 ]]; then
-	DATECOUNT=1
+if [[ $date_count -lt 1 ]]; then
+  date_count=1
 fi
 
-for i in `seq 1 $DATECOUNT`
+for i in `seq 1 $date_count`
 do
-	DATEOFFSET=$(($i-1+$FORWARDMODIFIER-$BACKWARDMODIFIER))
-	if [[ $DATEOFFSET -lt 0 ]]; then
-		DATEOFFSET=${DATEOFFSET#-} # Absolute value
-	fi
-  echo `date -v+"$DATEOFFSET"d '+%Y-%m-%d'`
+  date_offset=$(($i-1+$mod_forward-$mod_backward))
+  if [[ $date_offset -lt 0 ]]; then
+    date_offset=${date_offset#-} # Absolute value
+  fi
+  echo `date -v+"$date_offset"d '+%Y-%m-%d'`
 done
