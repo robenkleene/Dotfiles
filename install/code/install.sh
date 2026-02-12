a4a0d3b49327f712ff89d18a608e3bfa7f2f0780
Roben Kleene <contact@robenkleene.com>
2026-02-12 17:57:12 +0000

    Use safe link

--- install/code/install.sh
+++ install/code/install.sh
@@ -4,46 +4,34 @@ set -euo pipefail
 
 cd "$(dirname "$0" || exit 1)"
 
+safe_symlink() {
+  local src="$1"
+  local dest="$2"
+  if [[ ! -e "$dest" ]]; then
+    ln -s "$src" "$dest"
+  elif [[ ! -L "$dest" ]]; then
+    echo "Warning: $dest exists and is not a symlink" >&2
+  fi
+}
+
 # VS Code
 vscode_path="$HOME/.config/Code"
 vscode_insiders_path="$HOME/.config/Code - Insiders"
-if [[ -e "$vscode_path" && ! -e "$vscode_insiders_path" ]]; then
-  ln -s "$vscode_path" "$vscode_insiders_path"
-else
-  # TODO warn about not symlink
+if [[ -e "$vscode_path" ]]; then
+  safe_symlink "$vscode_path" "$vscode_insiders_path"
 fi
-if [[ ! -e "$vscode_path/User/snippets/global.code-snippets" ]]; then
-  ln -s "$vscode_path/User/snippets/global.json" "$vscode_path/User/snippets/global.code-snippets"
-else
-  # TODO warn about not symlink
-fi
+safe_symlink "$vscode_path/User/snippets/global.json" "$vscode_path/User/snippets/global.code-snippets"
 if [[ "$(uname)" = "Darwin" ]]; then
-  if [[ ! -e "$HOME/Library/Application Support/Code" ]]; then
-    ln -s "$vscode_path" "$HOME/Library/Application Support/Code"
-  else
-    # TODO warn about not symlink
-  fi
-  if [[ ! -e "$HOME/Library/Application Support/Code - Insiders" ]]; then
-    ln -s "$vscode_path" "$HOME/Library/Application Support/Code - Insiders"
-  else
-    # TODO warn about not symlink
-  fi
+  safe_symlink "$vscode_path" "$HOME/Library/Application Support/Code"
+  safe_symlink "$vscode_path" "$HOME/Library/Application Support/Code - Insiders"
 fi
 
 cd $(dirname "$0" || exit 1)
 if [[ "\$(uname)" = "Darwin" ]]; then
   if [[ "$HOME/.vscode/extensions/" ]]; then
-    if [[ ! -e "$HOME/.vscode/extensions/robenkleene.robenkleene-0.0.1/" ]]; then
-      ln -s vscode-robenkleene/ "$HOME/.vscode/extensions/robenkleene.robenkleene-0.0.1/"
-    else
-      # TODO warn about not symlink
-    fi
+    safe_symlink vscode-robenkleene/ "$HOME/.vscode/extensions/robenkleene.robenkleene-0.0.1/"
   elif [[ "\$(uname)" = "Linux" ]]; then
-    if [[ ! -e "$HOME/.vscode-server/extensions/robenkleene.robenkleene-0.0.1/" ]]; then
-      ln -s vscode-robenkleene/ "$HOME/.vscode-server/extensions/robenkleene.robenkleene-0.0.1/"
-    else
-      # TODO warn about not symlink
-    fi
+    safe_symlink vscode-robenkleene/ "$HOME/.vscode-server/extensions/robenkleene.robenkleene-0.0.1/"
   fi
 fi
 
