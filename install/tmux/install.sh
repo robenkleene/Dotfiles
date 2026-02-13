f017bea3d92a41a1de6f3b07bce5d8415cce5829
Roben Kleene <contact@robenkleene.com>
2026-02-13 23:42:34 +0000

    Make tmux install more reliable

--- install/tmux/install.sh
+++ install/tmux/install.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 if [[ ! -e "$HOME/.tmux/plugins/tpm" ]] && command -v tmux; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-  ~/.tmux/plugins/tpm/bin/install_plugins
 fi
+~/.tmux/plugins/tpm/bin/install_plugins
