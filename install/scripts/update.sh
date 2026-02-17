5e750645e2e0222100282c8c0fb77b73fe916908
Roben Kleene <contact@robenkleene.com>
2026-02-17 16:18:54 +0000

    Update scripts

--- install/scripts/update.sh
+++ install/scripts/update.sh
@@ -3,9 +3,9 @@
 set -e
 
 cd "$(dirname "$0")" || exit 1
-source_dir="$(pwd -P)/bin/";
+src_dir="$(pwd -P)/bin/";
 
-destination_dir="$HOME/.bin/"
+dest_dir="$HOME/.bin/"
 
 function make_symlink() {
   source="$1"
@@ -17,19 +17,19 @@ function make_symlink() {
   fi
 }
 
-destination_dir="$HOME/.bin/"
-if [ ! -e "$destination_dir" ]; then
-  mkdir "$destination_dir"
-elif [ ! -d "$destination_dir" ]; then
-  echo "Destination $destination_dir exist and it's not a directory" >&2
+dest_dir="$HOME/.bin/"
+if [ ! -e "$dest_dir" ]; then
+  mkdir "$dest_dir"
+elif [ ! -d "$dest_dir" ]; then
+  echo "Destination $dest_dir exist and it's not a directory" >&2
   exit 1
 fi
 
 # Cleanup dead symlinks
-cd "$destination_dir" &&
+cd "$dest_dir" &&
   find -L . -name . -o -type d -prune -o -type l -exec rm {} +
 
-for file in "$source_dir"*; do
+for file in "$src_dir"*; do
   if [[ $file == "tags" || $file == "TAGS" ]]; then
     continue
   fi
@@ -39,13 +39,13 @@ for file in "$source_dir"*; do
 
   executable_name=$(basename "${file%.*}")
   chmod a+x "$file"
-  make_symlink "$file" "$destination_dir/$executable_name"
+  make_symlink "$file" "$dest_dir/$executable_name"
 done
 
 # Symlink the no bin directory so scripts can reference them
-nobin_destintation=$destination_dir/nobin
+nobin_destintation=$dest_dir/nobin
 if [ ! -e "$nobin_destintation" ]; then
-  ln -s "$source_dir/nobin" "$nobin_destintation"
+  ln -s "$src_dir/nobin" "$nobin_destintation"
 elif [ ! -L "$nobin_destintation" ]; then
   echo "Warning: $nobin_destintation is a file and it's not a symlink" >&2
 fi
