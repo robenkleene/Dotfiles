781de19ebc206a89d35c108a03ec62c19f87b662
Roben Kleene <contact@robenkleene.com>
2026-02-22 13:15:12 +0000

    Switch to Homebrew

--- install/rs/crates/install.sh
+++ install/rs/crates/install.sh
@@ -4,8 +4,9 @@ set -e
 
 # On intial install, cargo won't be in the `$PATH` yet so use an absolute path
 
-~/.cargo/bin/cargo install rep-grep
-~/.cargo/bin/cargo install ren-find
+# Switch to Homebrew install
+# ~/.cargo/bin/cargo install rep-grep
+# ~/.cargo/bin/cargo install ren-find
 # Helper for generating cargo files
 # If this is failing just comment it out
 # cargo install cargo-generate
