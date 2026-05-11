6d8f621cd8c4f15dbcb9d045534fc305d2ba7bd3
Roben Kleene <contact@robenkleene.com>
2026-05-11 13:39:53 +0000

    Rename scripts

--- install/scripts/bin/file-mkdir.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-file="$1"
-mkdir -p "${file%/*}" && touch "$file"
--- install/scripts/bin/print-args.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-for i in "$@"; do 
-  echo "$i"
-done
