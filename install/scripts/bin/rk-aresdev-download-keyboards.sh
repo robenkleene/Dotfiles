c08274c6bc0c0e03f2af9d68b4b1a80049e2bfb7
Roben Kleene <contact@robenkleene.com>
2026-08-27 15:09:22 +0000

    Fix keyboard download

--- install/scripts/bin/rk-aresdev-download-keyboards.sh
+++ install/scripts/bin/rk-aresdev-download-keyboards.sh
@@ -3,20 +3,20 @@
 # Remove `e` to allow failing if a keyboard is missing
 set -uo pipefail
 
-if [[ "$1" = "moonlander" ]]; then
-  sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/zsa_moonlander_robenkleene_macos.bin .
+if [[ "${1:-}" = "moonlander" ]]; then
+  sftp aresdev:Developer/Vendor/qmk_firmware/zsa_moonlander_robenkleene_macos.bin .
   exit 0
 fi
-if [[ "$1" = "ergodox" ]]; then
-  sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/ergodox_ez_base_robenkleene_macos2.hex .
+if [[ "${1:-}" = "ergodox" ]]; then
+  sftp aresdev:Developer/Vendor/qmk_firmware/.build/ergodox_ez_base_robenkleene_macos2.hex .
   exit 0
 fi
-if [[ "$1" = "voyager" ]]; then
-  sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/zsa_voyager_robenkleene_macos.bin .
+if [[ "${1:-}" = "voyager" ]]; then
+  sftp aresdev:Developer/Vendor/qmk_firmware/qmk_firmware/.build/zsa_voyager_robenkleene_macos.bin .
   exit 0
 fi
-if [[ "$1" = "iris" ]]; then
-  sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/keebio_iris_rev8_robenkleene_macos.uf2 .
+if [[ "${1:-}" = "iris" ]]; then
+  sftp aresdev:Developer/Vendor/qmk_firmware/qmk_firmware/.build/keebio_iris_rev8_robenkleene_macos.uf2 .
   exit 0
 fi
 
