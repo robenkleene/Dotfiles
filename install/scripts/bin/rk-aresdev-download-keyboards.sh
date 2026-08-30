6b2bfcc775f140ecb1756f345345390a30910891
Roben Kleene <contact@robenkleene.com>
2026-08-30 06:36:07 -0400

    Fix paths

--- install/scripts/bin/rk-aresdev-download-keyboards.sh
+++ install/scripts/bin/rk-aresdev-download-keyboards.sh
@@ -20,7 +20,7 @@ if [[ "${1:-}" = "iris" ]]; then
   exit 0
 fi
 
-sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/ergodox_ez_base_robenkleene_macos2.hex .
-sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/moonlander_robenkleene_macos.bin .
-sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/zsa_voyager_robenkleene_macos.bin .
+sftp aresdev:Developer/Vendor/qmk_firmware/.build/zsa_moonlander_robenkleene_macos.bin .
+sftp aresdev:Developer/Vendor/qmk_firmware/.build/ergodox_ez_base_robenkleene_macos2.hex .
+sftp aresdev:Developer/Vendor/qmk_firmware/.build/zsa_voyager_robenkleene_macos.bin .
 # sftp aresdev:Developer/Projects/Hardware/qmk_firmware/.build/keebio_iris_rev8_robenkleene_macos.elf .
